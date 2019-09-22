cb1bab8601581b7869f712684544a93681abdfe1
unknown <andrew.f.fogarty@gmail.com>
2019-09-21 20:27:46 -0700

    Uncomment the change

--- stl/inc/xatomic.h
+++ stl/inc/xatomic.h
@@ -55,15 +55,15 @@ _STL_DISABLE_CLANG_WARNINGS
 
 _STD_BEGIN
 
-//#if _HAS_CXX20
-//enum class memory_order : unsigned int { relaxed, consume, acquire, release, acq_rel, seq_cst };
-//inline constexpr memory_order memory_order_relaxed = memory_order::relaxed;
-//inline constexpr memory_order memory_order_consume = memory_order::consume;
-//inline constexpr memory_order memory_order_acquire = memory_order::acquire;
-//inline constexpr memory_order memory_order_release = memory_order::release;
-//inline constexpr memory_order memory_order_acq_rel = memory_order::acq_rel;
-//inline constexpr memory_order memory_order_seq_cst = memory_order::seq_cst;
-//#else
+#if _HAS_CXX20
+enum class memory_order : unsigned int { relaxed, consume, acquire, release, acq_rel, seq_cst };
+inline constexpr memory_order memory_order_relaxed = memory_order::relaxed;
+inline constexpr memory_order memory_order_consume = memory_order::consume;
+inline constexpr memory_order memory_order_acquire = memory_order::acquire;
+inline constexpr memory_order memory_order_release = memory_order::release;
+inline constexpr memory_order memory_order_acq_rel = memory_order::acq_rel;
+inline constexpr memory_order memory_order_seq_cst = memory_order::seq_cst;
+#else
 enum memory_order {
     memory_order_relaxed,
     memory_order_consume,
