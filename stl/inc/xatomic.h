610d38ba6144edce9e4b13ae10b5737d11a0af0a
unknown <andrew.f.fogarty@gmail.com>
2019-09-21 19:50:05 -0700

    Move memory_order to enum class

--- stl/inc/xatomic.h
+++ stl/inc/xatomic.h
@@ -56,14 +56,13 @@ _STL_DISABLE_CLANG_WARNINGS
 _STD_BEGIN
 
 // ENUM memory_order
-enum memory_order {
-    memory_order_relaxed,
-    memory_order_consume,
-    memory_order_acquire,
-    memory_order_release,
-    memory_order_acq_rel,
-    memory_order_seq_cst
-};
+enum class memory_order { relaxed, consume, acquire, release, acq_rel, seq_cst };
+inline constexpr memory_order memory_order_relaxed = memory_order::relaxed;
+inline constexpr memory_order memory_order_consume = memory_order::consume;
+inline constexpr memory_order memory_order_acquire = memory_order::acquire;
+inline constexpr memory_order memory_order_release = memory_order::release;
+inline constexpr memory_order memory_order_acq_rel = memory_order::acq_rel;
+inline constexpr memory_order memory_order_seq_cst = memory_order::seq_cst;
 
 using _Atomic_counter_t = unsigned long;
 
