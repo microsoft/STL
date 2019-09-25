db7882cd96c262df45cad1aab2545627e8b1428d
Billy Robert O'Neal III <bion@microsoft.com>
2019-09-24 18:32:43 -0700

    Clarify failure message.

--- ado/enforce-clang-format.cmd
+++ ado/enforce-clang-format.cmd
@@ -1,4 +1,4 @@
 @echo off
 clang-format -style=file -i stl/inc/** stl/src/** 2>&1
-echo If your build fails here, the following files were changed by clang-format.
+echo If your build fails here, you need to format the following files with clang-format 8.0.1
 git status --porcelain 1>&2
