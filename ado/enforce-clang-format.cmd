5c608913ba5dc560cd986a53f154fdf96ced6769
Billy Robert O'Neal III <bion@microsoft.com>
2019-09-24 18:37:36 -0700

    Add subdirectories.

--- ado/enforce-clang-format.cmd
+++ ado/enforce-clang-format.cmd
@@ -1,4 +1,4 @@
 @echo off
-clang-format -style=file -i stl/inc/** stl/src/** 2>&1
+clang-format -style=file -i stl/inc/* stl/inc/cvt/* stl/inc/experimental/* stl/src/* 2>&1
 echo If your build fails here, you need to format the following files with clang-format 8.0.1
 git status --porcelain 1>&2
