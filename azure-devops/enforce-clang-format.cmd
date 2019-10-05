77242776a667e4c02e7ba34fff450d7d70784e26
Billy Robert O'Neal III <bion@microsoft.com>
2019-10-04 21:36:09 -0700

    Attempt to make Pipelines happy, 5 of N

--- azure-devops/enforce-clang-format.cmd
+++ azure-devops/enforce-clang-format.cmd
@@ -1,6 +1,5 @@
 :: Copyright (c) Microsoft Corporation.
 :: SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
-@echo off
-"%1" "\"clang-format.exe -style=file -i\"" stl/inc stl/inc/cvt stl/inc/experimental stl/src tools/inc tools/jobify/jobify.cpp tools/parallelize/parallelize.cpp
+"%1" "clang-format.exe -style=file -i" stl/inc stl/inc/cvt stl/inc/experimental stl/src tools/inc tools/jobify/jobify.cpp tools/parallelize/parallelize.cpp
 echo If your build fails here, you need to format the following files with clang-format 8.0.1.
 git status --porcelain stl 1>&2
