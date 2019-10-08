8ee38e004223e78609d171f9d80d026c89093793
Billy Robert O'Neal III <bion@microsoft.com>
2019-10-07 18:14:24 -0700

    Enforce clang-format in the tools directory.

--- azure-devops/enforce-clang-format.cmd
+++ azure-devops/enforce-clang-format.cmd
@@ -2,4 +2,4 @@
 :: SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 "%1" "clang-format.exe -style=file -i" stl/inc stl/inc/cvt stl/inc/experimental stl/src tools/inc tools/jobify/jobify.cpp tools/parallelize/parallelize.cpp
 echo If your build fails here, you need to format the following files with clang-format 8.0.1.
-git status --porcelain stl 1>&2
+git status --porcelain stl tools 1>&2
