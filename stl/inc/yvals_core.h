22cdbd3dd15cbd8fb4dfd697bfbcb0e07c760a2a
Stephan T. Lavavej <stl@microsoft.com>
2019-10-20 20:10:03 -0700

    yvals_core.h should also list P1651R0.

--- stl/inc/yvals_core.h
+++ stl/inc/yvals_core.h
@@ -49,6 +49,7 @@
 // P1227R2 Signed std::ssize(), Unsigned span::size()
 //     (partially implemented)
 // P1357R1 is_bounded_array, is_unbounded_array
+// P1651R0 bind_front() Should Not Unwrap reference_wrapper
 // P1754R1 Rename Concepts To standard_case
 // P????R? directory_entry::clear_cache()
 
