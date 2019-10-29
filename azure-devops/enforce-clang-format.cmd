0670cc13293d351f78ce1ae95a008008966e0f48
Stephan T. Lavavej <stl@microsoft.com>
2019-10-28 22:03:44 -0700

    Add validate to azure-devops.

--- azure-devops/enforce-clang-format.cmd
+++ azure-devops/enforce-clang-format.cmd
@@ -1,6 +1,6 @@
 :: Copyright (c) Microsoft Corporation.
 :: SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
-"%1" "clang-format.exe -style=file -i" stl/inc stl/inc/cvt stl/inc/experimental stl/src tools/inc tools/jobify/jobify.cpp tools/parallelize/parallelize.cpp
+"%1" "clang-format.exe -style=file -i" stl/inc stl/inc/cvt stl/inc/experimental stl/src tools/inc tools/jobify/jobify.cpp tools/parallelize/parallelize.cpp tools/validate/validate.cpp
 @echo If your build fails here, you need to format the following files with
 @clang-format.exe --version
 @git status --porcelain stl tools 1>&2
