28ec9a32952e0d7443936f8d5ae5d675ba6cf65c
Daniel Marshall <xandan@gmail.com>
2019-11-01 21:32:39 +0000

    P1209R0 erase_if(), erase() (#236)
    Resolves #55.
    
    * Deprecate experimental::erase
    
    * Implement P1209R0
    
    * Update yvals_core.h
    
    * Update deprecations and remove <experimental/xutility>
    
    * move and reorder erase/erase_if
    
    * moved _Erase and remove and friends to <xmemory>
    
    * Consistently place erase_if() definitions.

--- stl/inc/yvals_core.h
+++ stl/inc/yvals_core.h
@@ -47,6 +47,7 @@
 // P0898R3 Standard Library Concepts
 // P0919R3 Heterogeneous Lookup For Unordered Containers
 // P0966R1 string::reserve() Should Not Shrink
+// P1209R0 erase_if(), erase()
 // P1227R2 Signed std::ssize(), Unsigned span::size()
 //     (partially implemented)
 // P1357R1 is_bounded_array, is_unbounded_array
@@ -835,7 +836,18 @@
 #define _CXX20_DEPRECATE_IS_POD
 #endif // ^^^ warning disabled ^^^
 
-// next warning number: STL4026
+#if _HAS_CXX20 && !defined(_SILENCE_EXPERIMENTAL_ERASE_DEPRECATION_WARNING)
+#define _DEPRECATE_EXPERIMENTAL_ERASE                                                                                 \
+    [[deprecated("warning STL4026: "                                                                                  \
+                 "std::experimental::erase() and std::experimental::erase_if() are deprecated by Microsoft and will " \
+                 "be REMOVED. They are superseded by std::erase() and std::erase_if(). "                              \
+                 "You can define _SILENCE_EXPERIMENTAL_ERASE_DEPRECATION_WARNING to acknowledge that you have "       \
+                 "received this warning.")]]
+#else // ^^^ warning enabled / warning disabled vvv
+#define _DEPRECATE_EXPERIMENTAL_ERASE
+#endif // ^^^ warning disabled ^^^
+
+// next warning number: STL4027
 
 
 // LIBRARY FEATURE-TEST MACROS
@@ -953,6 +965,7 @@
 #endif // _HAS_STD_BOOLEAN
 #endif // defined(__cpp_concepts) && __cpp_concepts > 201507L
 
+#define __cpp_lib_erase_if 201811L
 #define __cpp_lib_generic_unordered_lookup 201811L
 #define __cpp_lib_list_remove_return_type 201806L
 #define __cpp_lib_to_array 201907L
