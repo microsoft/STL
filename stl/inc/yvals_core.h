d4dc329c19ff4f0906e3ad8558fc77715ad683b4
Billy Robert O'Neal III <bion@microsoft.com>
2020-01-07 19:32:49 -0800

    Define _CONSTEXPR20_ICE in the right order.

--- stl/inc/yvals_core.h
+++ stl/inc/yvals_core.h
@@ -471,13 +471,6 @@
 #define _CONSTEXPR20 inline
 #endif // ^^^ inline (not constexpr) in C++17 and earlier ^^^
 
-// Functions that became constexpr in C++20, and requires is_constant_evaluated
-#ifdef __cpp_lib_is_constant_evaluated
-#define _CONSTEXPR20_ICE constexpr
-#else // ^^^ constexpr with is_constant_evaluated / vvv not constexpr without is_constant_evaluated vvv
-#define _CONSTEXPR20_ICE
-#endif // ^^^ not constexpr without is_constant_evaluated ^^^
-
 // P0607R0 Inline Variables For The STL
 #if _HAS_CXX17
 #define _INLINE_VAR inline
@@ -1018,6 +1011,12 @@
 #define __cpp_lib_experimental_erase_if   201411L
 #define __cpp_lib_experimental_filesystem 201406L
 
+// Functions that became constexpr in C++20, and requires is_constant_evaluated
+#ifdef __cpp_lib_is_constant_evaluated
+#define _CONSTEXPR20_ICE constexpr
+#else // ^^^ constexpr with is_constant_evaluated / vvv not constexpr without is_constant_evaluated vvv
+#define _CONSTEXPR20_ICE
+#endif // ^^^ not constexpr without is_constant_evaluated ^^^
 
 #ifdef _RTC_CONVERSION_CHECKS_ENABLED
 #ifndef _ALLOW_RTCc_IN_STL
