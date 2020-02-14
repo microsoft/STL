0b1ab955545283e9fcd5cb413e03f0447a650664
Curtis.Bezault <curtbezault@gmail.com>
2020-02-14 14:42:51 -0800

    Only add -m64 when targeting AMD64

--- tests/utils/stl/test/Test.py
+++ tests/utils/stl/test/Test.py
@@ -51,7 +51,9 @@ class StlTest(Test):
         compile_flags.extend(default_cxx.compile_flags or [])
         link_flags.extend(default_cxx.link_flags or [])
 
-        if ('clang' in os.path.basename(cxx)):
+        if ('clang' in os.path.basename(cxx) and
+                self.config.target_architecture.casefold() ==
+                'AMD64'.casefold()):
             compile_flags.append('-m64')
 
         self.cxx = CXXCompiler(cxx, flags, compile_flags, link_flags)
