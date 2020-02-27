5eb0ae6f20615652fbfcdb873a13ed24972c640a
Billy Robert O'Neal III <bion@microsoft.com>
2020-02-27 11:54:04 -0800

    Deduplicate the "ranges" version, strengthen noexcept on construct_at and destroy_at, and add test.

---
+++ tests/std/tests/P0784R7_library_support_for_more_constexpr_containers/test.cpp
@@ -0,0 +1,48 @@
+// Copyright (c) Microsoft Corporation.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+#include <assert.h>
+#include <memory>
+#include <string.h>
+#include <string>
+#include <utility>
+
+static_assert(noexcept(std::construct_at(std::declval<int*>(), 42)));
+static_assert(!noexcept(std::construct_at(std::declval<std::string*>(), "hello")));
+static_assert(noexcept(std::construct_at(std::declval<const int*>(), 42)));
+static_assert(!noexcept(std::construct_at(std::declval<const std::string*>(), "hello")));
+static_assert(noexcept(std::construct_at(std::declval<volatile int*>(), 42)));
+static_assert(!noexcept(std::construct_at(std::declval<volatile std::string*>(), "hello")));
+static_assert(noexcept(std::construct_at(std::declval<const volatile int*>(), 42)));
+static_assert(!noexcept(std::construct_at(std::declval<const volatile std::string*>(), "hello")));
+
+static_assert(noexcept(std::destroy_at(std::declval<int*>())));
+static_assert(noexcept(std::destroy_at(std::declval<std::string*>())));
+static_assert(noexcept(std::destroy_at(std::declval<const int*>())));
+static_assert(noexcept(std::destroy_at(std::declval<const std::string*>())));
+static_assert(noexcept(std::destroy_at(std::declval<volatile int*>())));
+static_assert(noexcept(std::destroy_at(std::declval<volatile std::string*>())));
+static_assert(noexcept(std::destroy_at(std::declval<const volatile int*>())));
+static_assert(noexcept(std::destroy_at(std::declval<const volatile std::string*>())));
+
+template <class Ty>
+void test_runtime(const Ty& _Val) {
+    alignas(Ty) unsigned char storage[sizeof(Ty)];
+    memset(storage, 42, sizeof(Ty));
+    const auto asPtrTy = reinterpret_cast<Ty*>(&storage);
+    std::construct_at(asPtrTy, _Val);
+    assert(*asPtrTy == _Val);
+    std::destroy_at(asPtrTy);
+
+    // test voidify:
+    const auto asCv = static_cast<const volatile _Ty*>(_AsPtrTy);
+    std::construct_at(asCv, _Val);
+    assert(const_cast<const _Ty&>(*asCv) == _Val);
+    std::destroy_at(asCv);
+}
+
+int main() {
+    test_runtime(1234);
+    test_runtime(std::string("hello world"));
+    test_runtime(std::string("hello to some really long world that certainly doesn't fit in SSO"));
+}
