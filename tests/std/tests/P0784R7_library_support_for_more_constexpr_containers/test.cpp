8877a1ad86dd02fd53819325af6c9822028238cc
Billy Robert O'Neal III <bion@microsoft.com>
2020-02-27 22:35:20 -0800

    Add constraints and other CR feedback.

--- tests/std/tests/P0784R7_library_support_for_more_constexpr_containers/test.cpp
+++ tests/std/tests/P0784R7_library_support_for_more_constexpr_containers/test.cpp
@@ -5,7 +5,57 @@
 #include <memory>
 #include <string.h>
 #include <string>
+#include <type_traits>
 #include <utility>
+
+struct X {};
+
+template <class _Void, class Ty, class... Types>
+inline constexpr bool can_construct_at = false;
+
+template <class Ty, class... Types>
+inline constexpr bool can_construct_at<
+    std::void_t<decltype(std::construct_at(std::declval<Ty*>(), std::declval<Types>()...))>, Ty, Types...> = true;
+
+static_assert(can_construct_at<void, int>);
+static_assert(can_construct_at<void, const int>);
+static_assert(can_construct_at<void, volatile int>);
+static_assert(can_construct_at<void, const volatile int>);
+static_assert(can_construct_at<void, int, int>);
+static_assert(can_construct_at<void, const int, int>);
+static_assert(can_construct_at<void, volatile int, int>);
+static_assert(can_construct_at<void, const volatile int, int>);
+static_assert(can_construct_at<void, int, int&>);
+static_assert(can_construct_at<void, const int, int&>);
+static_assert(can_construct_at<void, volatile int, int&>);
+static_assert(can_construct_at<void, const volatile int, int&>);
+
+static_assert(!can_construct_at<void, int, X>);
+static_assert(!can_construct_at<void, X, int>);
+
+static_assert(!can_construct_at<void, int&>); // note that references can be constructed by not new'd
+static_assert(!can_construct_at<void, const int&>);
+static_assert(!can_construct_at<void, volatile int&>);
+static_assert(!can_construct_at<void, const volatile int&>);
+static_assert(!can_construct_at<void, int&, int>);
+static_assert(!can_construct_at<void, const int&, int>);
+static_assert(!can_construct_at<void, volatile int&, int>);
+static_assert(!can_construct_at<void, const volatile int&, int>);
+static_assert(!can_construct_at<void, int&, int&>);
+static_assert(!can_construct_at<void, const int&, const int&>);
+static_assert(!can_construct_at<void, volatile int&, volatile int&>);
+static_assert(!can_construct_at<void, const volatile int&, const volatile int&>);
+
+static_assert(can_construct_at<void, X>);
+static_assert(can_construct_at<void, X, X>);
+static_assert(can_construct_at<void, X, const X&>);
+static_assert(can_construct_at<void, X, X&>);
+static_assert(can_construct_at<void, X, X&&>);
+
+static_assert(can_construct_at<void, std::string>);
+static_assert(can_construct_at<void, std::string, std::size_t, char>);
+static_assert(!can_construct_at<void, std::string, std::size_t, char, char>);
+static_assert(!can_construct_at<void, std::string, X>);
 
 static_assert(noexcept(std::construct_at(std::declval<int*>(), 42)));
 static_assert(!noexcept(std::construct_at(std::declval<std::string*>(), "hello")));
@@ -25,19 +75,30 @@ static_assert(noexcept(std::destroy_at(std::declval<volatile std::string*>())));
 static_assert(noexcept(std::destroy_at(std::declval<const volatile int*>())));
 static_assert(noexcept(std::destroy_at(std::declval<const volatile std::string*>())));
 
+struct throwing_dtor {
+    ~throwing_dtor() noexcept(false) {}
+};
+
+static_assert(noexcept(std::destroy_at(std::declval<throwing_dtor*>())));
+
 template <class Ty>
-void test_runtime(const Ty& _Val) {
+void test_runtime(const Ty& val) {
     alignas(Ty) unsigned char storage[sizeof(Ty)];
     memset(storage, 42, sizeof(Ty));
     const auto asPtrTy = reinterpret_cast<Ty*>(&storage);
-    std::construct_at(asPtrTy, _Val);
-    assert(*asPtrTy == _Val);
+    assert(asPtrTy == std::construct_at(asPtrTy, val));
+    assert(*asPtrTy == val);
+    std::destroy_at(asPtrTy);
+
+    // test ranges:
+    assert(asPtrTy == std::ranges::construct_at(asPtrTy, val));
+    assert(*asPtrTy == val);
     std::destroy_at(asPtrTy);
 
     // test voidify:
     const auto asCv = static_cast<const volatile Ty*>(asPtrTy);
-    std::construct_at(asCv, _Val);
-    assert(const_cast<const Ty&>(*asCv) == _Val);
+    assert(asPtrTy == std::construct_at(asCv, val));
+    assert(const_cast<const Ty&>(*asCv) == val);
     std::destroy_at(asCv);
 }
 
