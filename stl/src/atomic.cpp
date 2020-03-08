d80cc81e84a48fe3d8c9b7208a1a42930679abd5
AlexGutenev <gutenev@gmail.com>
2020-03-08 09:37:17 +0200

    Changed from events to semahores, as it is hard to control count of wakes

--- stl/src/atomic.cpp
+++ stl/src/atomic.cpp
@@ -48,13 +48,20 @@ static void _Atomic_spin_count_initialize() {
 }
 
 enum _Atomic_spin_phase {
-    _ATOMIC_SPIN_PHASE_MASK            = 0xF000'0000,
-    _ATOMIC_SPIN_VALUE_MASK            = 0x0FFF'FFFF,
-    _ATOMIC_SPIN_PHASE_INIT_SPIN_COUNT = 0x0000'0000,
-    _ATOMIC_SPIN_PHASE_INIT_SPIN       = 0x1000'0000,
-    _ATOMIC_SPIN_PHASE_INIT_SWITCH_THD = 0x2000'0000,
-    _ATOMIC_SPIN_PHASE_INIT_SLEEP_ZERO = 0x3000'0000,
-    _ATOMIC_SPIN_PHASE_INIT_SLEEP      = 0x4000'0000,
+    _ATOMIC_SPIN_PHASE_MASK              = 0xF000'0000,
+    _ATOMIC_WAIT_PHASE_MASK              = 0x0F00'0000,
+    _ATOMIC_SPIN_VALUE_MASK              = 0x00FF'FFFF,
+    _ATOMIC_WAIT_PHASE_SPIN              = 0x0000'0000,
+    _ATOMIC_WAIT_PHASE_WAIT_SET          = 0x0100'0000,
+    _ATOMIC_WAIT_PHASE_WAIT_CLEAR        = 0x0200'0000,
+    _ATOMIC_WAIT_PHASE_WAIT_NO_SEMAPHORE = 0x0300'0000,
+    _ATOMIC_SPIN_PHASE_INIT_SPIN_COUNT   = 0x0000'0000,
+    _ATOMIC_SPIN_PHASE_INIT_SPIN         = 0x1000'0000,
+    _ATOMIC_SPIN_PHASE_INIT_SWITCH_THD   = 0x2000'0000,
+    _ATOMIC_SPIN_PHASE_INIT_SLEEP_ZERO   = 0x3000'0000,
+    _ATOMIC_SPIN_PHASE_INIT_SLEEP        = 0x4000'0000,
+
+    _ATOMIC_SPIN_MASK = _ATOMIC_SPIN_PHASE_MASK | _ATOMIC_SPIN_VALUE_MASK,
 };
 
 bool __cdecl _Atomic_spin_active_only(long& _Spin_context) {
@@ -87,7 +94,7 @@ void __cdecl _Atomic_spin(long& _Spin_context) {
             YieldProcessor();
             return;
         }
-        _Spin_context = _ATOMIC_SPIN_PHASE_INIT_SWITCH_THD;
+        _Spin_context = _ATOMIC_SPIN_PHASE_INIT_SWITCH_THD | (_Spin_context & _ATOMIC_WAIT_PHASE_MASK);
         [[fallthrough]];
 
     case _ATOMIC_SPIN_PHASE_INIT_SWITCH_THD:
@@ -96,7 +103,7 @@ void __cdecl _Atomic_spin(long& _Spin_context) {
             ::SwitchToThread();
             return;
         }
-        _Spin_context = _ATOMIC_SPIN_PHASE_INIT_SLEEP_ZERO;
+        _Spin_context = _ATOMIC_SPIN_PHASE_INIT_SLEEP_ZERO | (_Spin_context & _ATOMIC_WAIT_PHASE_MASK);
         [[fallthrough]];
 
     case _ATOMIC_SPIN_PHASE_INIT_SLEEP_ZERO:
@@ -105,7 +112,7 @@ void __cdecl _Atomic_spin(long& _Spin_context) {
             ::Sleep(0);
             return;
         }
-        _Spin_context = _ATOMIC_SPIN_PHASE_INIT_SLEEP;
+        _Spin_context = _ATOMIC_SPIN_PHASE_INIT_SLEEP | (_Spin_context & _ATOMIC_WAIT_PHASE_MASK);
         [[fallthrough]];
 
     case _ATOMIC_SPIN_PHASE_INIT_SLEEP:
@@ -119,8 +126,9 @@ inline bool is_win8_wait_on_address_available() {
     return true;
 #else
     // WaitOnAddress ONLY available on Windows 8+
-    DYNAMICGETCACHEDFUNCTION(PFNWAITONADDRESS, WaitOnAddress, pfWaitOnAddress);
-    return pfWaitOnAddress != nullptr;
+    // DYNAMICGETCACHEDFUNCTION(PFNWAITONADDRESS, WaitOnAddress, pfWaitOnAddress);
+    // return pfWaitOnAddress != nullptr;
+    return false;
 #endif
 }
 
@@ -137,41 +145,41 @@ struct alignas(64) _Contention_table_entry {
     // Size is largest of lock-free to make aliasing problem into hypothetical
     std::atomic<std::uint64_t> _Counter;
     // Event to wait on in case of no atomic ops
-    std::atomic<HANDLE> _Event;
+    std::atomic<HANDLE> _Semaphore;
     // Event use count, can delete event if drops to zero
     // Initialized to one to keep event used when progam runs, will drop to zero on program exit
-    std::atomic<std::size_t> _Event_use_count = 1;
-    // Flag whether event should be set
-    std::atomic<std::uint32_t> _Event_should_set = 0;
-    // Once flag for event creation
-    std::once_flag _Event_created;
-    // Once flag for event deletion
-    static std::once_flag _Events_dereference_registered;
+    std::atomic<std::size_t> _Semaphore_use_count = 1;
+    // Flag whether semaphore should be released
+    std::atomic<LONG> _Semaphore_own_count = 0;
+    // Once flag for semaphore creation
+    std::once_flag _Semaphore_created;
+    // Once flag for semaphore deletion
+    static std::once_flag _Semaphore_dereference_registered;
 
-    static void _Dereference_all_events();
+    static void _Dereference_all_semaphores();
 
-    HANDLE _Reference_event() {
-        std::call_once(_Event_created, [this] {
-            std::call_once(_Events_dereference_registered, [] { atexit(_Dereference_all_events); });
+    HANDLE _Reference_semaphore() {
+        std::call_once(_Semaphore_created, [this] {
+            std::call_once(_Semaphore_dereference_registered, [] { atexit(_Dereference_all_semaphores); });
 
             // Try create just once, if low resources, use fall back permanently
-            HANDLE event = ::CreateEvent(nullptr, TRUE, FALSE, nullptr);
-            _Event.store(event, std::memory_order_relaxed);
+            HANDLE _Semaphore_local = ::CreateSemaphore(nullptr, 0, MAXLONG, nullptr);
+            _Semaphore.store(_Semaphore_local, std::memory_order_release);
         });
-        _Event_use_count.fetch_add(1, std::memory_order_acquire);
-        return _Event.load(std::memory_order_relaxed);
+        _Semaphore_use_count.fetch_add(1, std::memory_order_relaxed);
+        return _Semaphore.load(std::memory_order_consume);
     }
 
-    void _Dereference_event() {
-        if (_Event_use_count.fetch_sub(1, std::memory_order_release) == 1) {
-            HANDLE event = _Event.exchange(nullptr);
-            if (event != nullptr)
-                ::CloseHandle(event);
+    void _Dereference_semaphore() {
+        if (_Semaphore_use_count.fetch_sub(1, std::memory_order_relaxed) == 1) {
+            HANDLE _Semaphore_local = _Semaphore.exchange(nullptr, std::memory_order_acq_rel);
+            if (_Semaphore_local != nullptr)
+                ::CloseHandle(_Semaphore_local);
         }
     }
 };
 
-std::once_flag _Contention_table_entry::_Events_dereference_registered;
+std::once_flag _Contention_table_entry::_Semaphore_dereference_registered;
 
 #pragma warning(pop)
 
@@ -184,54 +192,70 @@ _Contention_table_entry& _Atomic_contention_table(const void* _Storage) {
     return _Contention_table[index & TABLE_MASK];
 }
 
-void _Contention_table_entry::_Dereference_all_events() {
+void _Contention_table_entry::_Dereference_all_semaphores() {
     for (_Contention_table_entry& entry : _Contention_table)
-        entry._Dereference_event();
+        entry._Dereference_semaphore();
 }
 
 
 void __cdecl _Atomic_wait_fallback(const void* _Storage, long& _Spin_context) noexcept {
-    if ((_Spin_context & _ATOMIC_SPIN_PHASE_MASK) >= _ATOMIC_SPIN_PHASE_INIT_SWITCH_THD) {
-        // Wait phase
-        auto& _Table = _Atomic_contention_table(_Storage);
-        HANDLE event = _Table._Reference_event();
-        if (event != nullptr)
-            ::WaitForSingleObject(event, INFINITE);
-        else
-            _Atomic_spin(_Spin_context);
-        _Table._Dereference_event();
-    } else {
-        // Spin phase
+    switch (_Spin_context & _ATOMIC_WAIT_PHASE_MASK) {
+
+    case _ATOMIC_WAIT_PHASE_SPIN:
         if (_Atomic_spin_active_only(_Spin_context))
-            return;
-        // Spin is over, preparing to wait
+            break;
+
+        _Spin_context = _ATOMIC_WAIT_PHASE_WAIT_CLEAR | (_Spin_context & _ATOMIC_SPIN_MASK);
+        [[fallthrough]];
+
+    case _ATOMIC_WAIT_PHASE_WAIT_CLEAR: {
         auto& _Table = _Atomic_contention_table(_Storage);
-        HANDLE event = _Table._Reference_event();
-        if (event != nullptr) {
-            ::ResetEvent(event);
-            // As to set event
-            _Table._Event_should_set.fetch_add(1, std::memory_order_relaxed);
-        }
+        _Table._Semaphore_own_count.fetch_add(1);
         std::atomic_thread_fence(std::memory_order_seq_cst);
-        _Table._Dereference_event();
-        // Caller would check value once more
+        _Spin_context = _ATOMIC_WAIT_PHASE_WAIT_SET | (_Spin_context & _ATOMIC_SPIN_MASK);
+        break; // query again directly before waiting
+    }
+
+    case _ATOMIC_WAIT_PHASE_WAIT_SET: {
+        auto& _Table      = _Atomic_contention_table(_Storage);
+        HANDLE _Semaphore = _Table._Reference_semaphore();
+        if (_Semaphore != nullptr)
+            ::WaitForSingleObject(_Semaphore, INFINITE);
+        _Table._Dereference_semaphore();
+
+        if (_Semaphore != nullptr) {
+            _Spin_context = _ATOMIC_WAIT_PHASE_WAIT_CLEAR | (_Spin_context & _ATOMIC_SPIN_MASK);
+            break;
+        }
+
+        _Spin_context = _ATOMIC_WAIT_PHASE_WAIT_NO_SEMAPHORE | (_Spin_context & _ATOMIC_SPIN_MASK);
+        [[fallthrough]];
+    }
+
+    case _ATOMIC_WAIT_PHASE_WAIT_NO_SEMAPHORE:
+        _Atomic_spin(_Spin_context);
+        break;
     }
 }
 
+void __cdecl _Atomic_unwait_fallback(const void* _Storage, long& _Spin_context) {
+    if ((_Spin_context & _ATOMIC_WAIT_PHASE_MASK) == _ATOMIC_WAIT_PHASE_WAIT_SET) {
+        auto& _Table = _Atomic_contention_table(_Storage);
+        _Table._Semaphore_own_count.fetch_sub(1);
+    }
+}
 
 void __cdecl _Atomic_notify_fallback(void* _Storage) noexcept {
     auto& _Table = _Atomic_contention_table(_Storage);
     std::atomic_thread_fence(std::memory_order_seq_cst);
-
-    for (;;) {
-        auto _Set_event = _Table._Event_should_set.load(std::memory_order_relaxed);
-        if (_Set_event == 0)
-            break;
-        HANDLE event = _Table._Reference_event();
-        ::SetEvent(event);
-        _Table._Dereference_event();
-        _Table._Event_should_set.fetch_sub(_Set_event, std::memory_order_relaxed);
-    }
+    LONG _Semaphore_own_count = _Table._Semaphore_own_count.load();
+    if (_Semaphore_own_count <= 0)
+        return;
+    HANDLE _Semaphore = _Table._Reference_semaphore();
+    if (_Semaphore != nullptr)
+        ::ReleaseSemaphore(_Semaphore, _Semaphore_own_count, nullptr);
+    _Table._Dereference_semaphore();
+    _Table._Semaphore_own_count.fetch_sub(_Semaphore_own_count);
 }
 
 
@@ -281,6 +305,16 @@ void __cdecl _Atomic_notify_indirect(void* _Storage) noexcept {
     } else {
         _Atomic_notify_fallback(_Storage);
     }
+}
+
+void __cdecl _Atomic_unwait_direct(const void* _Storage, long& _Spin_context) {
+    if (!is_win8_wait_on_address_available())
+        _Atomic_unwait_fallback(_Storage, _Spin_context);
+}
+
+void __cdecl _Atomic_unwait_indirect(const void* _Storage, long& _Spin_context) {
+    if (!is_win8_wait_on_address_available())
+        _Atomic_unwait_fallback(_Storage, _Spin_context);
 }
 
 _END_EXTERN_C
