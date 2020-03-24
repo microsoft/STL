c48e2d3e105d711e13eb247ad54cb58773bbeb83
Curtis.Bezault <curtbezault@gmail.com>
2020-03-23 17:07:35 -0700

    Add the LLVM banner to tests.py

--- tests/utils/stl/test/tests.py
+++ tests/utils/stl/test/tests.py
@@ -1,5 +1,11 @@
 # Copyright (c) Microsoft Corporation.
+#===----------------------------------------------------------------------===##
+#
+# Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+# See https://llvm.org/LICENSE.txt for license information.
 # SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+#
+#===----------------------------------------------------------------------===##
 
 from itertools import chain
 from pathlib import Path
