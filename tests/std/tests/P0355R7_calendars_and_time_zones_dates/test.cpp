589eaf1386749652b27a50da275e540a3422126d
Daniel Marshall <xandan@gmail.com>
2020-04-04 09:12:23 +0100

    Finish day_test()

--- tests/std/tests/P0355R7_calendars_and_time_zones_dates/test.cpp
+++ tests/std/tests/P0355R7_calendars_and_time_zones_dates/test.cpp
@@ -8,7 +8,7 @@
 using namespace std;
 using namespace std::chrono;
 
-constexpr bool day_test() {
+constexpr void day_test() {
     static_assert(is_trivially_copyable_v<day>, "chrono::day is not trivially copyable");
     static_assert(is_standard_layout_v<day>, "chrono::day is not standard layout");
 
@@ -28,13 +28,30 @@ constexpr bool day_test() {
     assert(static_cast<unsigned>(d1) == 0);
 
     day d2{0u};
-    assert(d1 == d2);
+    assert(d1 == d2++);
+    assert(d1 < d2);
+    assert(d2 > d1);
 
+    day d3{0};
+    assert(!d3.ok());
+    ++d3;
+    for (int i = 1; i <= 31; ++i, ++d3) {
+        assert(d3.ok());
+    }
+    assert(!d3.ok());
+
+    const auto d4 = 0d;
+    assert(d1 == d4);
+    static_assert(is_same_v<day, decltype(0d)>, "0d is not chrono::day");
+    // static_assert(is_same_v<day, decltype(d4)>, "d4 is not chrono::day");
+}
+
+constexpr bool test() {
+    day_test();
     return true;
 }
 
-
 int main() {
-    day_test();
-    static_assert(day_test());
+    test();
+    static_assert(test());
 }
