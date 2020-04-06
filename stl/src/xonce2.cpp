c9c82923a429122585792fb40e777c520227f234
Alex Guteniev <gutenev@gmail.com>
2020-04-06 20:15:16 +0300

    clang format

--- stl/src/xonce2.cpp
+++ stl/src/xonce2.cpp
@@ -14,13 +14,15 @@
 // these declarations must be in sync with those in xcall_once.h
 
 _STD_BEGIN
-int __CLRCALL_PURE_OR_CDECL __std_execute_once_begin(once_flag& _Once_flag, int& _Pending) noexcept { // wrap Win32 InitOnceBeginInitialize()
+int __CLRCALL_PURE_OR_CDECL __std_execute_once_begin(
+    once_flag& _Once_flag, int& _Pending) noexcept { // wrap Win32 InitOnceBeginInitialize()
     static_assert(sizeof(_Once_flag._Opaque) == sizeof(INIT_ONCE), "invalid size");
 
     return ::InitOnceBeginInitialize(reinterpret_cast<PINIT_ONCE>(&_Once_flag._Opaque), 0, &_Pending, nullptr);
 }
 
-int __CLRCALL_PURE_OR_CDECL __std_execute_once_complete(once_flag& _Once_flag, unsigned long _Flags) noexcept { // wrap Win32 InitOnceComplete()
+int __CLRCALL_PURE_OR_CDECL __std_execute_once_complete(
+    once_flag& _Once_flag, unsigned long _Flags) noexcept { // wrap Win32 InitOnceComplete()
     static_assert(sizeof(_Once_flag._Opaque) == sizeof(INIT_ONCE), "invalid size");
 
     return ::InitOnceComplete(reinterpret_cast<PINIT_ONCE>(&_Once_flag._Opaque), _Flags, nullptr);
