792994c6305060a92e0ebd01e944dfc7aa551498
Alex Guteniev <gutenev@gmail.com>
2020-04-09 20:56:57 +0300

    #error correctly, simplify checks

--- tests/std/tests/VSO_1062649_overaligned_function/test.cpp
+++ tests/std/tests/VSO_1062649_overaligned_function/test.cpp
@@ -7,29 +7,26 @@
 #include <functional>
 
 #ifndef __cpp_aligned_new
-#pragma error("This test is only valid when C++17 over-aligned allication is supported")
+#error This test is only valid when C++17 over-aligned allocation is supported
 #endif
 
 #pragma warning(disable : 4324) // "was padded due to alignment specifier", thanks, Captain
 
-struct alignas(4 * alignof(std::max_align_t)) overaligned_t {
+struct alignas(2 * alignof(std::max_align_t)) overaligned_t {
     char non_empty;
 
-    void operator()(const void* const storage, const std::size_t storage_size) const {
+    void operator()(const void* storage, std::size_t storage_size) const {
         const unsigned char* storage_bytes = reinterpret_cast<const unsigned char*>(storage);
         const unsigned char* this_bytes    = reinterpret_cast<const unsigned char*>(this);
 
-        // loop instead of range comparison to avoid UB when not in range
-        for (std::size_t i = 0; i < storage_size; i++) {
-            assert(storage_bytes + i != this_bytes);
-        }
-        assert(reinterpret_cast<std::uintptr_t>(this) % alignof(overaligned_t) == 0);
+        // platform-specific behavior not covered by Standard C++, but fine for such test
+        assert(this_bytes < storage_bytes || this_bytes > storage_bytes + storage_size);
+        assert(static_cast<std::size_t>(reinterpret_cast<std::uintptr_t>(this) % alignof(overaligned_t)) == 0);
     }
 };
 
-static_assert(alignof(overaligned_t) == 16, "overaligned_t is not aligned as expected");
+static_assert(alignof(overaligned_t) < sizeof(std::function<void()>), "overaligned_t is not aligned as expected");
 static_assert(alignof(overaligned_t) > alignof(std::max_align_t), "overaligned_t is not overaligned");
-static_assert(alignof(std::max_align_t) == 8, "max_align_t has changed, the whole stuff should be revised");
 
 struct functions_t {
     using function_t = std::function<void(const void* storage, std::size_t storage_size)>;
