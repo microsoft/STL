68fc4d54691f7e9d5d9cae4147bd0b5867bde8e4
Alex Guteniev <gutenev@gmail.com>
2020-04-16 09:01:14 +0300

    Unblocked part of #651

--- stl/src/atomic.cpp
+++ stl/src/atomic.cpp
@@ -1,3 +1,4 @@
+
 // Copyright (c) Microsoft Corporation.
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
@@ -14,23 +15,21 @@ _EXTERN_C
 volatile long _Shared_ptr_flag;
 
 _CRTIMP2_PURE void __cdecl _Lock_shared_ptr_spin_lock() { // spin until _Shared_ptr_flag successfully set
-#ifdef _M_ARM
+#if defined(_M_ARM) || defined(_M_ARM64)
     while (_InterlockedExchange_acq(&_Shared_ptr_flag, 1)) {
         __yield();
     }
-#else // _M_ARM
+#else // defined(_M_ARM) || defined(_M_ARM64)
     while (_interlockedbittestandset(&_Shared_ptr_flag, 0)) { // set bit 0
     }
-#endif // _M_ARM
+#endif // defined(_M_ARM) || defined(_M_ARM64)
 }
 
 _CRTIMP2_PURE void __cdecl _Unlock_shared_ptr_spin_lock() { // release previously obtained lock
-#ifdef _M_ARM
+#if defined(_M_ARM) || defined(_M_ARM)
     __dmb(_ARM_BARRIER_ISH);
-    __iso_volatile_store32((volatile int*) &_Shared_ptr_flag, 0);
-#else // _M_ARM
-    _interlockedbittestandreset(&_Shared_ptr_flag, 0); // reset bit 0
-#endif // _M_ARM
+#endif // defined(_M_ARM) || defined(_M_ARM64)
+    __iso_volatile_store32(reinterpret_cast<volatile int*>(&_Shared_ptr_flag), 0);
 }
 
 _END_EXTERN_C
