d515d9886fa32bbdf973b834d2d46888eb3359c2
Alex Guteniev <gutenev@gmail.com>
2020-04-17 08:11:54 +0300

    As @BillyONeal  suggested, real atomic.
    Improvement is the same

--- stl/src/atomic.cpp
+++ stl/src/atomic.cpp
@@ -5,32 +5,20 @@
 
 #include <yvals.h>
 
-#include <intrin.h>
-#pragma warning(disable : 4793)
+#include <atomic>
 
 _EXTERN_C
 
-// SPIN LOCK FOR shared_ptr ATOMIC OPERATIONS
-volatile long _Shared_ptr_flag;
+std::atomic_flag _Shared_ptr_flag{};
 
 _CRTIMP2_PURE void __cdecl _Lock_shared_ptr_spin_lock() { // spin until _Shared_ptr_flag successfully set
-#if defined(_M_ARM) || defined(_M_ARM64)
-    while (_InterlockedExchange_acq(&_Shared_ptr_flag, 1)) {
-        __yield();
+    while (_Shared_ptr_flag.test_and_set(std::memory_order_acquire)) {
+        _YIELD_PROCESSOR();
     }
-#else // defined(_M_ARM) || defined(_M_ARM64)
-    while (_interlockedbittestandset(&_Shared_ptr_flag, 0)) { // set bit 0
-    }
-#endif // defined(_M_ARM) || defined(_M_ARM64)
 }
 
 _CRTIMP2_PURE void __cdecl _Unlock_shared_ptr_spin_lock() { // release previously obtained lock
-#if defined(_M_ARM) || defined(_M_ARM64)
-    __dmb(_ARM_BARRIER_ISH);
-#else // defined(_M_ARM) || defined(_M_ARM64)
-    _ReadWriteBarrier();
-#endif // defined(_M_ARM) || defined(_M_ARM64)
-    __iso_volatile_store32(reinterpret_cast<volatile int*>(&_Shared_ptr_flag), 0);
+    _Shared_ptr_flag.clear(std::memory_order_release);
 }
 
 _END_EXTERN_C
