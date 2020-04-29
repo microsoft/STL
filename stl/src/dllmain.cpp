f26887e2b2c51111f62c7544ce7ac255aa7047f7
Stephan T. Lavavej <stl@microsoft.com>
2020-04-28 19:55:49 -0700

    dllmain.cpp: [[maybe_unused]], preprocessor comments.

--- stl/src/dllmain.cpp
+++ stl/src/dllmain.cpp
@@ -9,17 +9,15 @@
 #if defined(_CRT_APP)
 // free static resource used by causality
 extern "C" void __cdecl __crtCleanupCausalityStaticFactories();
-#endif
+#endif // defined(_CRT_APP)
 
-extern "C" BOOL APIENTRY DllMain(HMODULE /* hModule */, DWORD ul_reason_for_call, LPVOID lpReserved) {
+extern "C" BOOL APIENTRY DllMain(HMODULE /* hModule */, DWORD ul_reason_for_call, [[maybe_unused]] LPVOID lpReserved) {
     if (ul_reason_for_call == DLL_PROCESS_DETACH) {
 #if defined(_CRT_APP)
         if (lpReserved == nullptr) { // only when the process is not terminating
             __crtCleanupCausalityStaticFactories();
         }
-#else
-        (void) (lpReserved);
-#endif
+#endif // defined(_CRT_APP)
     }
     return TRUE;
 }
