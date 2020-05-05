d8dbaffcbaa05dda8feda125a855ed10fc1ae101
Stephan T. Lavavej <stl@microsoft.com>
2020-05-04 20:02:17 -0700

    Fix complex's Sufficient Additional Overloads
    * GH-785 reported that `arg(real)` was implemented as `return 0;`
    but `arg(complex)` does actual work for complex numbers with
    zero imaginary parts.
    
    * `norm` should upgrade to `double` before squaring. The difference
    is observable because `double`'s range is vast.
    
    * `conj` and `proj` should return `complex`.
    
    * `conj(real)` should behave exactly like `conj(complex)`, returning
    an imaginary part of negative zero. The difference is technically
    observable.
    
    * `proj(real)` should behave exactly like `proj(complex)`, mapping
    negative infinity to positive infinity.

--- tests/std/tests/Dev10_555491_complex_linker_errors/test.cpp
+++ tests/std/tests/Dev10_555491_complex_linker_errors/test.cpp
@@ -2,11 +2,16 @@
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
 #include <assert.h>
+#include <cmath>
 #include <complex>
+#include <limits>
 #include <stdint.h>
 #include <string.h>
+#include <type_traits>
 
 using namespace std;
+
+#define STATIC_ASSERT(...) static_assert(__VA_ARGS__, #__VA_ARGS__)
 
 template <class T, class U>
 T pseudo_bit_cast(const U& source) {
@@ -90,4 +95,36 @@ int main() {
     assert(nearly_equal_partwise(asinh(-1e+307 + 2e+307i), -708.3914896859491 + 1.1071487177940904i));
     assert(nearly_equal_partwise(asinh(-2e+37f + 4e+37if), -87.386663f + 1.1071488if));
     assert(nearly_equal_partwise(asinh(-1e+307L + 2e+307il), -708.3914896859491L + 1.1071487177940904il));
+
+    // Also test GH-785 "<complex>: std::arg does not work for negative real values"
+
+    STATIC_ASSERT(is_same_v<decltype(arg(1729)), double>);
+    STATIC_ASSERT(is_same_v<decltype(imag(1729)), double>);
+    STATIC_ASSERT(is_same_v<decltype(real(1729)), double>);
+    STATIC_ASSERT(is_same_v<decltype(norm(1729)), double>);
+    STATIC_ASSERT(is_same_v<decltype(conj(1729)), complex<double>>);
+    STATIC_ASSERT(is_same_v<decltype(proj(1729)), complex<double>>);
+
+    assert((arg(-1.0) == arg(complex<double>{-1.0, 0.0})));
+    assert((arg(-1) == arg(complex<double>{-1.0, 0.0})));
+
+    assert(imag(1729.0) == 0.0);
+    assert(imag(1729) == 0.0);
+
+    assert(real(1729.0) == 1729.0);
+    assert(real(1729) == 1729.0);
+
+    assert(norm(0x1p63) == 0x1p126);
+    assert(norm(0x8000'0000'0000'0000ULL) == 0x1p126);
+
+    assert((conj(1729.0) == complex<double>{1729.0, -0.0}));
+    assert((conj(1729) == complex<double>{1729.0, -0.0}));
+    assert(signbit(conj(1729.0).imag()));
+    assert(signbit(conj(1729).imag()));
+
+    assert((proj(1729.0) == complex<double>{1729.0, 0.0}));
+    assert((proj(1729) == complex<double>{1729.0, 0.0}));
+    constexpr double inf = numeric_limits<double>::infinity();
+    assert((proj(inf) == complex<double>{inf, 0.0}));
+    assert((proj(-inf) == complex<double>{inf, 0.0}));
 }
