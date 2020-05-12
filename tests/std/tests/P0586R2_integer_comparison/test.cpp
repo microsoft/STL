310f95bab89786d974be5bbb6f594e8b4ce3fd7f
Stephan T. Lavavej <stl@microsoft.com>
2020-05-11 18:36:54 -0700

    Unify compile-time and run-time testing.

--- tests/std/tests/P0586R2_integer_comparison/test.cpp
+++ tests/std/tests/P0586R2_integer_comparison/test.cpp
@@ -11,51 +11,49 @@
 
 using namespace std;
 
-#define STATIC_ASSERT(...) static_assert(__VA_ARGS__, #__VA_ARGS__)
+constexpr bool test() {
+    assert(cmp_equal(1L, 1));
+    assert(!cmp_equal(1, -1));
+    assert(!cmp_equal(1, 42));
+    assert(!cmp_equal(42, 1));
 
-STATIC_ASSERT(cmp_equal(1L, 1));
-STATIC_ASSERT(!cmp_equal(1, -1));
-STATIC_ASSERT(!cmp_equal(1, 42));
-STATIC_ASSERT(!cmp_equal(42, 1));
+    assert(!cmp_not_equal(1L, 1));
+    assert(cmp_not_equal(1, -1));
+    assert(cmp_not_equal(1, 42));
+    assert(cmp_not_equal(42, 1));
 
-STATIC_ASSERT(!cmp_not_equal(1L, 1));
-STATIC_ASSERT(cmp_not_equal(1, -1));
-STATIC_ASSERT(cmp_not_equal(1, 42));
-STATIC_ASSERT(cmp_not_equal(42, 1));
+    assert(!cmp_less(1L, 1));
+    assert(!cmp_less(1, -1));
+    assert(cmp_less(1, 42));
+    assert(!cmp_less(42, 1));
 
-STATIC_ASSERT(!cmp_less(1L, 1));
-STATIC_ASSERT(!cmp_less(1, -1));
-STATIC_ASSERT(cmp_less(1, 42));
-STATIC_ASSERT(!cmp_less(42, 1));
+    assert(cmp_less_equal(1L, 1));
+    assert(!cmp_less_equal(1, -1));
+    assert(cmp_less_equal(1, 42));
+    assert(!cmp_less_equal(42, 1));
 
-STATIC_ASSERT(cmp_less_equal(1L, 1));
-STATIC_ASSERT(!cmp_less_equal(1, -1));
-STATIC_ASSERT(cmp_less_equal(1, 42));
-STATIC_ASSERT(!cmp_less_equal(42, 1));
+    assert(!cmp_greater(1L, 1));
+    assert(cmp_greater(1, -1));
+    assert(!cmp_greater(1, 42));
+    assert(cmp_greater(42, 1));
 
-STATIC_ASSERT(!cmp_greater(1L, 1));
-STATIC_ASSERT(cmp_greater(1, -1));
-STATIC_ASSERT(!cmp_greater(1, 42));
-STATIC_ASSERT(cmp_greater(42, 1));
+    assert(cmp_greater_equal(1L, 1));
+    assert(cmp_greater_equal(1, -1));
+    assert(!cmp_greater_equal(1, 42));
+    assert(cmp_greater_equal(42, 1));
 
-STATIC_ASSERT(cmp_greater_equal(1L, 1));
-STATIC_ASSERT(cmp_greater_equal(1, -1));
-STATIC_ASSERT(!cmp_greater_equal(1, 42));
-STATIC_ASSERT(cmp_greater_equal(42, 1));
+    assert(in_range<int>(1));
+    assert(in_range<int>(1L));
+    assert(!in_range<unsigned int>(-1));
+    assert(in_range<unsigned int>(0));
+    assert(in_range<unsigned int>(42));
+    assert(!in_range<int>(numeric_limits<long>::max()));
+    assert(!in_range<int>(numeric_limits<long>::min()));
+    assert(in_range<int>(numeric_limits<int>::max()));
+    assert(in_range<int>(numeric_limits<int>::min()));
+    assert(in_range<long>(numeric_limits<int>::max()));
+    assert(in_range<long>(numeric_limits<int>::min()));
 
-STATIC_ASSERT(in_range<int>(1));
-STATIC_ASSERT(in_range<int>(1L));
-STATIC_ASSERT(!in_range<unsigned int>(-1));
-STATIC_ASSERT(in_range<unsigned int>(0));
-STATIC_ASSERT(in_range<unsigned int>(42));
-STATIC_ASSERT(!in_range<int>(numeric_limits<long>::max()));
-STATIC_ASSERT(!in_range<int>(numeric_limits<long>::min()));
-STATIC_ASSERT(in_range<int>(numeric_limits<int>::max()));
-STATIC_ASSERT(in_range<int>(numeric_limits<int>::min()));
-STATIC_ASSERT(in_range<long>(numeric_limits<int>::max()));
-STATIC_ASSERT(in_range<long>(numeric_limits<int>::min()));
-
-int main() {
     { // Different signs
         const int i          = -1;
         const unsigned int u = 1;
@@ -98,4 +96,11 @@ int main() {
         assert(!cmp_greater_equal(u, i));
         assert(cmp_greater_equal(i, u));
     }
+
+    return true;
+}
+
+int main() {
+    assert(test());
+    static_assert(test());
 }
