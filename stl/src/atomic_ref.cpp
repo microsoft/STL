24f1f01f5b2d1487197a88b288e40719ac035cee
Alex Guteniev <gutenev@gmail.com>
2020-05-17 20:12:10 +0300

    found a way to use spinlock pointer for atomic_ref

--- stl/src/atomic_ref.cpp
+++ stl/src/atomic_ref.cpp
@@ -5,17 +5,17 @@
 
 #include <cstdint>
 #include <new>
-#include <synchapi.h>
 
-SRWLOCK* _Atomic_atomic_ref_get_mutex(const void* const _Key) noexcept {
+
+_EXTERN_C
+long* __stdcall __std_atomic_get_mutex(const void* const _Key) noexcept {
     constexpr size_t _Table_size_power = 8;
     constexpr size_t _Table_size       = 1 << _Table_size_power;
     constexpr size_t _Table_index_mask = _Table_size - 1;
-
 #pragma warning(push)
 #pragma warning(disable : 4324) // '%s': structure was padded due to alignment specifier
     struct alignas(std::hardware_destructive_interference_size) _Table_entry {
-        SRWLOCK _Mutex = SRWLOCK_INIT;
+        long _Mutex = 0;
     };
     static _Table_entry _Table[_Table_size];
 #pragma warning(pop)
@@ -23,14 +23,5 @@ SRWLOCK* _Atomic_atomic_ref_get_mutex(const void* const _Key) noexcept {
     _Index ^= _Index >> (_Table_size_power * 2);
     _Index ^= _Index >> _Table_size_power;
     return &_Table[_Index & _Table_index_mask]._Mutex;
-}
-
-_EXTERN_C
-void __stdcall __std_atomic_ref_lock(const void* const _Key) noexcept {
-    ::AcquireSRWLockExclusive(_Atomic_atomic_ref_get_mutex(_Key));
-}
-
-void __stdcall __std_atomic_ref_unlock(const void* const _Key) noexcept {
-    ::ReleaseSRWLockExclusive(_Atomic_atomic_ref_get_mutex(_Key));
 }
 _END_EXTERN_C
