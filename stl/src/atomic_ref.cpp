49b7f95cd303dc7e19f95ae2ba14cf1dc812a89b
Alex Guteniev <gutenev@gmail.com>
2020-05-23 17:25:25 +0300

    Zero pad for potential future redefinition

--- stl/src/atomic_ref.cpp
+++ stl/src/atomic_ref.cpp
@@ -11,13 +11,13 @@ long* __stdcall __std_atomic_get_mutex(const void* const _Key) noexcept {
     constexpr size_t _Table_size_power = 8;
     constexpr size_t _Table_size       = 1 << _Table_size_power;
     constexpr size_t _Table_index_mask = _Table_size - 1;
-#pragma warning(push)
-#pragma warning(disable : 4324) // '%s': structure was padded due to alignment specifier
+
     struct alignas(std::hardware_destructive_interference_size) _Table_entry {
-        long _Mutex = 0;
+        long _Mutex                                                           = 0;
+        char _Pad[std::hardware_destructive_interference_size - sizeof(long)] = {};
     };
     static _Table_entry _Table[_Table_size];
-#pragma warning(pop)
+
     auto _Index = reinterpret_cast<const std::uintptr_t>(_Key);
     _Index ^= _Index >> (_Table_size_power * 2);
     _Index ^= _Index >> _Table_size_power;
