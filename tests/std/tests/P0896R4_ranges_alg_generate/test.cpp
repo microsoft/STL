dd51ae02414ef668d52b52ea7dada6c36dc0f711
Ahana Mukhopadhyay <t-ahmukh@microsoft.com>
2020-06-16 14:38:39 -0400

    minor changes

--- tests/std/tests/P0896R4_ranges_alg_generate/test.cpp
+++ tests/std/tests/P0896R4_ranges_alg_generate/test.cpp
@@ -15,14 +15,14 @@ constexpr void smoke_test() {
     int output1[] = {13, 42, 1367};
     int output2[] = {13, 42, 1367};
     {
-        auto result = generate(std::begin(output1), std::end(output1), [](){return 7;});
+        auto result = generate(std::begin(output1), std::end(output1), []() { return 7; });
         for (int i = 0; i < 3; ++i) {
             assert(output1[i] == 7);
         }
         assert(result == std::end(output1));
     }
     {
-        auto result = generate(output2, [](){return 13;});
+        auto result = generate(output2, []() { return 13; });
         for (int i = 0; i < 3; ++i) {
             assert(output2[i] == 13);
         }
@@ -38,8 +38,8 @@ int main() {
 struct instantiator {
     template <class Out>
     static void call(Out&& out = {}) {
-        (void) ranges::generate(out, [](){return 13;});
-        (void) ranges::generate(ranges::begin(out), ranges::end(out), [](){return 13;});
+        (void) ranges::generate(out, []() { return 13; });
+        (void) ranges::generate(ranges::begin(out), ranges::end(out), []() { return 13; });
     }
 };
 
