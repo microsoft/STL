db97692a383a950570aa96f252f554ef797aaa1d
Ahana Mukhopadhyay <t-ahmukh@microsoft.com>
2020-06-16 12:42:39 -0400

    initial fill and fill_n algo commit

---
+++ tests/std/tests/P0896R4_ranges_alg_fill_n/test.cpp
@@ -0,0 +1,48 @@
+// Copyright (c) Microsoft Corporation.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+#include <algorithm>
+#include <cassert>
+#include <concepts>
+#include <range_algorithm_support.hpp>
+#include <ranges>
+#include <utility>
+
+constexpr void smoke_test() {
+    using ranges::fill_n, ranges::iterator_t;
+    using std::same_as;
+
+    int output1[] = {13, 42, 1367};
+    int output2[] = {13, 42, 1367};
+    {
+        const int value = 7;
+        auto result     = fill_n(std::begin(output1), 2, value);
+        for (int i = 0; i < 2; ++i) {
+            assert(output1[i] == 7);
+        }
+        assert(output1[2] == 1367);
+        // assert(result == std::end(output1));
+    }
+    {
+        const int value = 13;
+        auto result     = fill_n(std::begin(output2), -1, value);
+        assert(output2[0] == 13);
+        assert(output2[1] == 42);
+        assert(output2[2] == 1367);
+        assert(result == std::begin(output2));
+    }
+}
+
+int main() {
+    STATIC_ASSERT((smoke_test(), true));
+    smoke_test();
+}
+
+struct instantiator {
+    template <class Out>
+    static void call(Out&& out = {}) {
+        (void) ranges::fill_n(out, 1, 42);
+    }
+};
+
+template void test_out<instantiator>();
