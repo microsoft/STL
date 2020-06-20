1a7d698ea2d0c5f59dfc7cfc215a2c3e8a383fd5
Stephan T. Lavavej <stl@microsoft.com>
2020-06-20 00:28:45 -0700

    tests: Avoid dialog boxes, prevent stdout from being lost (#906)
    Fixes GH-781 (dialogs) and fixes GH-789 (stdout).

--- tests/std/tests/P0024R2_parallel_algorithms_set_intersection/test.cpp
+++ tests/std/tests/P0024R2_parallel_algorithms_set_intersection/test.cpp
@@ -18,10 +18,10 @@ using namespace std::execution;
 template <class T>
 void assert_message_vector(const bool b, const char* const msg, const T seedValue) {
     if (!b) {
-        cout << msg << " failed for seed value: " << seedValue << "\n";
-        cout << "This is a randomized test.\n";
-        cout << "DO NOT IGNORE/RERUN THIS FAILURE.\n";
-        cout << "You must report it to the STL maintainers.\n";
+        cerr << msg << " failed for seed value: " << seedValue << "\n";
+        cerr << "This is a randomized test.\n";
+        cerr << "DO NOT IGNORE/RERUN THIS FAILURE.\n";
+        cerr << "You must report it to the STL maintainers.\n";
         abort();
     }
 }
