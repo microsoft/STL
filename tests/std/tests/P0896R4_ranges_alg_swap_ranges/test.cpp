93a33a7c4d969dfcc7e48a2ae860a7bf40cfde04
Ahana Mukhopadhyay <t-ahmukh@microsoft.com>
2020-06-23 16:39:50 -0400

    working on fixing tests

--- tests/std/tests/P0896R4_ranges_alg_swap_ranges/test.cpp
+++ tests/std/tests/P0896R4_ranges_alg_swap_ranges/test.cpp
@@ -2,6 +2,7 @@
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
 #include <algorithm>
+#include <array>
 #include <cassert>
 #include <concepts>
 #include <range_algorithm_support.hpp>
@@ -12,41 +13,43 @@ constexpr void smoke_test() {
     using ranges::swap_ranges, ranges::swap_ranges_result, ranges::iterator_t;
     using std::same_as;
 
-    // Validate that move_result aliases in_in_result
-    STATIC_ASSERT(same_as<swap_ranges_result<int, double>, ranges::in_in_result<int, double>>);
+    // Validate that swap_ranges_result aliases in_in_result
+    STATIC_ASSERT(same_as<ranges::swap_ranges_result<int, double>, ranges::in_in_result<int, double>>);
 
     {
-        // // Validate range overload
-        // int range1[] = {13, 53, 1876};
-        // int range2[] = {34, 243, 9366};
+        // Validate range overload
+        using R        = std::array<int, 3>;
+        R const range1 = {13, 53, 1876};
+        R const range2 = {34, 243, 9366};
         // // int const expected_output1[] = {34, 243, 9366};
         // // int const expected_output2[] = {13, 53, 1876};
         // // auto result = swap_ranges(move_only_range{range1}, move_only_range{range2});
-        // auto result = swap_ranges(range1, range2);
-        // using I1    = iterator_t<std::array<int, 3>>;
-        // using I2    = iterator_t<std::array<int, 3>>;
-        // STATIC_ASSERT(same_as<decltype(result), swap_ranges_result<I1, I2>>);
+        auto result = swap_ranges(range1, range2);
+        using I1    = iterator_t<R const>;
+        using I2    = iterator_t<R const>;
+        STATIC_ASSERT(same_as<decltype(result), swap_ranges_result<I1, I2>>);
         // STATIC_ASSERT(same_as<decltype(result),
         //     swap_ranges_result<iterator_t<move_only_range<int const>>, iterator_t<move_only_range<int>>>>);
-        // assert(result.in == move_only_range{range1}.end());
+        // assert(result.in1 == move_only_range{range1}.end());
         // assert(result.out == move_only_range{range2}.end());
         // assert(ranges::equal(range1, expected_output1));
         // assert(ranges::equal(range2, expected_output2));
-    } { // Validate iterator + sentinel overload
-        // int range1[] = {13, 53, 1876};
-        // int range2[] = {34, 243, 9366};
-        // move_only_range wrapped_range1{range1};
-        // move_only_range wrapped_range2{range2};
-        // int const expected_output1[] = {34, 243, 9366};
-        // int const expected_output2[] = {13, 53, 1876};
-        // auto result =
-        //     swap_ranges(wrapped_range1.begin(), wrapped_range1.end(), wrapped_range2.begin(), wrapped_range2.end());
-        // STATIC_ASSERT(same_as<decltype(result),
-        //     swap_ranges_result<iterator_t<move_only_range<int const>>, iterator_t<move_only_range<int>>>>);
-        // assert(result.in == wrapped_range1.end());
-        // assert(result.out == wrapped_range2.end());
-        // assert(ranges::equal(range1, expected_output1));
-        // assert(ranges::equal(range2, expected_output2));
+    }
+    { // Validate iterator + sentinel overload
+      // int range1[] = {13, 53, 1876};
+      // int range2[] = {34, 243, 9366};
+      // move_only_range wrapped_range1{range1};
+      // move_only_range wrapped_range2{range2};
+      // int const expected_output1[] = {34, 243, 9366};
+      // int const expected_output2[] = {13, 53, 1876};
+      // auto result =
+      //     swap_ranges(wrapped_range1.begin(), wrapped_range1.end(), wrapped_range2.begin(), wrapped_range2.end());
+      // STATIC_ASSERT(same_as<decltype(result),
+      //     swap_ranges_result<iterator_t<move_only_range<int const>>, iterator_t<move_only_range<int>>>>);
+      // assert(result.in == wrapped_range1.end());
+      // assert(result.out == wrapped_range2.end());
+      // assert(ranges::equal(range1, expected_output1));
+      // assert(ranges::equal(range2, expected_output2));
     }
 }
 
