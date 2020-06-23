e13beda0c80e5486a2cfeb705b6d65a744d59786
Ahana Mukhopadhyay <t-ahmukh@microsoft.com>
2020-06-23 10:16:27 -0400

    tried to write a better test for generate but it threw a bunch of errors with static variable in lambda expression

--- tests/std/tests/P0896R4_ranges_alg_generate/test.cpp
+++ tests/std/tests/P0896R4_ranges_alg_generate/test.cpp
@@ -12,24 +12,36 @@ constexpr void smoke_test() {
     using ranges::generate, ranges::iterator_t;
     using std::same_as;
 
-    int output1[] = {13, 42, 1367};
-    int output2[] = {13, 42, 1367};
     {
+        int output[]    = {13, 42, 1367};
         const int value = 7;
-        auto result     = generate(std::begin(output1), std::end(output1), []() { return value; });
-        for (auto elem : output1) {
+        auto result     = generate(std::begin(output), std::end(output), []() { return value; });
+        for (auto elem : output) {
             assert(elem == value);
         }
-        assert(result == std::end(output1));
+        assert(result == std::end(output));
     }
     {
+        int output[]    = {13, 42, 1367};
         const int value = 13;
-        auto result     = generate(output2, []() { return value; });
-        for (auto elem : output2) {
+        auto result     = generate(output, []() { return value; });
+        for (auto elem : output) {
             assert(elem == value);
         }
-        assert(result == std::end(output2));
+        assert(result == std::end(output));
     }
+    // {
+    //     int output[] = {13, 42, 1367};
+    //     auto result  = generate(output, []() {
+    //         static int calls_to_generate = 0;
+    //         calls_to_generate++;
+    //         return calls_to_generate;
+    //     });
+    //     // for (auto elem : output) {
+    //     //     assert(elem == value);
+    //     // }
+    //     assert(result == std::end(output));
+    // }
 }
 
 int main() {
