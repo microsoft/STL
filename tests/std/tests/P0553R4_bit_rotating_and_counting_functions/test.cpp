42097f6ac5c27a98f4c3216825d874f100d69035
Charles Barto <barto.charlie@gmail.com>
2020-06-26 12:47:37 -0700

    fix more review comments

--- tests/std/tests/P0553R4_bit_rotating_and_counting_functions/test.cpp
+++ tests/std/tests/P0553R4_bit_rotating_and_counting_functions/test.cpp
@@ -6,7 +6,6 @@
 
 using namespace std;
 
-#ifdef __cpp_lib_bitops // TRANSITION, VSO-1020212
 template <typename T>
 constexpr bool test_countl_zero() {
     constexpr int digits = numeric_limits<T>::digits;
@@ -172,19 +171,17 @@ void test_all() {
     test_rotl<T>();
     static_assert(test_rotr<T>());
     test_rotr<T>();
-    static_assert(test_popcount_specialcases<T>());
-    test_popcount_specialcases<T>();
     static_assert(test_64bit_split_ops());
     test_64bit_split_ops();
+    static_assert(test_popcount_specialcases<T>());
+    test_popcount_specialcases<T>();
 }
-#endif // __cpp_lib_bitops
+
 
 int main() {
-#ifdef __cpp_lib_bitops // TRANSITION, VSO-1020212
     test_all<unsigned char>();
     test_all<unsigned short>();
     test_all<unsigned int>();
     test_all<unsigned long>();
     test_all<unsigned long long>();
-#endif // __cpp_lib_bitops
 }
