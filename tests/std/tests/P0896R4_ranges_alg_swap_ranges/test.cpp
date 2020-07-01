c973d2838f77da040407299e1ece9b5951a6b35c
Casey Carter <Casey@Carter.net>
2020-07-01 12:02:58 -0700

    Casey renamed `move_only_range` to `basic_borrowed_range`
    ... a couple of days ago, after making it non-movable.

--- tests/std/tests/P0896R4_ranges_alg_swap_ranges/test.cpp
+++ tests/std/tests/P0896R4_ranges_alg_swap_ranges/test.cpp
@@ -13,7 +13,7 @@
 constexpr void smoke_test() {
     using ranges::swap_ranges, ranges::swap_ranges_result, ranges::iterator_t;
     using std::same_as;
-    using I1 = iterator_t<move_only_range<int>>;
+    using I1 = iterator_t<basic_borrowed_range<int>>;
 
     // Validate that swap_ranges_result aliases in_in_result
     STATIC_ASSERT(same_as<swap_ranges_result<int, double>, ranges::in_in_result<int, double>>);
@@ -24,18 +24,18 @@ constexpr void smoke_test() {
         R range2                     = {34, 243, 9366};
         int const expected_output1[] = {34, 243, 9366};
         int const expected_output2[] = {13, 53, 1876};
-        auto result                  = swap_ranges(move_only_range{range1}, move_only_range{range2});
+        auto result                  = swap_ranges(basic_borrowed_range{range1}, basic_borrowed_range{range2});
         STATIC_ASSERT(same_as<decltype(result), swap_ranges_result<I1, I1>>);
-        assert(result.in1 == move_only_range{range1}.end());
-        assert(result.in2 == move_only_range{range2}.end());
+        assert(result.in1 == basic_borrowed_range{range1}.end());
+        assert(result.in2 == basic_borrowed_range{range2}.end());
         assert(ranges::equal(range1, expected_output1));
         assert(ranges::equal(range2, expected_output2));
     }
     { // Validate iterator + sentinel overload
         int range1[] = {13, 53, 1876};
         int range2[] = {34, 243, 9366};
-        move_only_range wrapped_range1{range1};
-        move_only_range wrapped_range2{range2};
+        basic_borrowed_range wrapped_range1{range1};
+        basic_borrowed_range wrapped_range2{range2};
         int const expected_output1[] = {34, 243, 9366};
         int const expected_output2[] = {13, 53, 1876};
         auto result =
