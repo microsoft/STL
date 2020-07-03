d3db1305b799b2aa303d11e8170d945fd73981dd
Michael Schellenberger Costa <mschellenbergercosta@googlemail.com>
2020-07-03 07:05:41 +0200

    Review coments

--- tests/std/tests/P0896R4_ranges_alg_count/test.cpp
+++ tests/std/tests/P0896R4_ranges_alg_count/test.cpp
@@ -15,14 +15,14 @@ using P = pair<int, int>;
 struct instantiator {
     static constexpr P input[5] = {{0, 99}, {1, 47}, {2, 99}, {3, 47}, {4, 99}};
 
-    template <class Read>
+    template <ranges::input_range Read>
     static constexpr void call() {
         using ranges::count;
         { // Validate iterator + sentinel overload
             Read wrapped_input{input};
 
             auto result = count(wrapped_input.begin(), wrapped_input.end(), 47, get_second);
-            STATIC_ASSERT(same_as<decltype(result), iter_difference_t<decltype(wrapped_input.begin())>>);
+            STATIC_ASSERT(same_as<decltype(result), ranges::range_difference_t<Read>>);
             assert(result == 2);
         }
         { // Validate range overload
