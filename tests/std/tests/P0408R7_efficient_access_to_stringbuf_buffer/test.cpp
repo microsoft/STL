7702315d4ff2b8e3d1240a2f8547c3223f37d613
Berrysoft <Strawberry_Str@hotmail.com>
2020-07-07 17:08:55 +0800

    Test allocate times.

--- tests/std/tests/P0408R7_efficient_access_to_stringbuf_buffer/test.cpp
+++ tests/std/tests/P0408R7_efficient_access_to_stringbuf_buffer/test.cpp
@@ -2,6 +2,7 @@
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
 #include <cassert>
+#include <iostream>
 #include <memory_resource>
 #include <sstream>
 #include <string>
@@ -67,7 +68,59 @@ void run_test() {
     run_test_util<Test<ostringstream>>();
 }
 
+template <typename T>
+struct counting_allocator {
+    using value_type = T;
+
+    std::allocator<T> allocator{};
+    std::size_t count{0};
+
+    T* allocate(std::size_t n) {
+        count++;
+        return allocator.allocate(n);
+    }
+
+    void deallocate(T* p, std::size_t n) {
+        allocator.deallocate(p, n);
+    }
+
+    counting_allocator() {}
+
+    template <typename U>
+    explicit counting_allocator(const counting_allocator<U>& c) : count(c.count) {}
+};
+
+template <typename T, typename U>
+bool operator==(const counting_allocator<T>& c1, const counting_allocator<U>& c2) {
+    return c1.allocator == c2.allocator;
+}
+
+using counting_string        = std::basic_string<char, std::char_traits<char>, counting_allocator<char>>;
+using counting_stringstream  = std::basic_stringstream<char, std::char_traits<char>, counting_allocator<char>>;
+using counting_istringstream = std::basic_istringstream<char, std::char_traits<char>, counting_allocator<char>>;
+using counting_ostringstream = std::basic_ostringstream<char, std::char_traits<char>, counting_allocator<char>>;
+
+template <typename Stream>
+struct test_counting_allocator {
+    void operator()(counting_string s) {
+        Stream stream{move(s)};
+        s = move(stream).str();
+        stream.str(move(s));
+        if constexpr (!std::is_same_v<Stream, counting_istringstream>) {
+            assert(stream.rdbuf()->get_allocator().count == 1);
+        }
+    }
+};
+
+template <template <typename> typename Test>
+void run_counting_test() {
+    run_test_util<Test<counting_stringstream>>();
+    run_test_util<Test<counting_istringstream>>();
+    run_test_util<Test<counting_ostringstream>>();
+}
+
 int main() {
     run_test<test_rvalue>();
     run_test<test_allocator>();
+    run_counting_test<test_counting_allocator>();
 }
