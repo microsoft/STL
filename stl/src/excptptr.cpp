18866f4dce985ae281e1da3b3f17908d61cb1c3c
Billy Robert O'Neal III <bion@microsoft.com>
2020-07-07 16:46:36 -0700

    Enable [[msvc::noop_dtor]] from Visual Studio 2019 version 16.8.

--- stl/src/excptptr.cpp
+++ stl/src/excptptr.cpp
@@ -37,13 +37,36 @@ extern "C" _CRTIMP2 void* __cdecl __AdjustPointer(void*, const PMD&); // defined
 using namespace std;
 
 namespace {
-#ifdef _M_CEE_PURE
+#if defined(_M_CEE_PURE)
     template <class _Ty>
     _Ty& _Immortalize() { // return a reference to an object that will live forever
         /* MAGIC */ static _Immortalizer_impl<_Ty> _Static;
         return reinterpret_cast<_Ty&>(_Static._Storage);
     }
-#else // ^^^ _M_CEE_PURE ^^^ // vvv !_M_CEE_PURE vvv
+#elif _MSC_VER > 1927
+    template <class _Ty>
+    struct _Constexpr_excptptr_immortalize_impl {
+        union {
+            _Ty _Storage;
+        }
+
+        constexpr _Constexpr_excptptr_immortalize_impl()
+            : _Storage() {
+        }
+
+        [[msvc::noop_dtor]] ~_Constexpr_excptptr_immortalize_impl() {
+            // do nothing to allow _Ty to be used during shutdown
+        }
+    };
+
+    template <class _Ty>
+    _Constexpr_excptptr_immortalize_impl<_Ty> _Immortalize_impl;
+
+    template <class _Ty>
+    _NODISCARD _Ty& _Immortalize() noexcept {
+        return _Immortalize_impl<_Ty>._Storage;
+    }
+#else // choose immortalize strategy
     template <class _Ty>
     int __stdcall _Immortalize_impl(void*, void* _Storage_ptr, void**) noexcept {
         // adapt True Placement New to _Execute_once
