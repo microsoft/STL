aa74fa72596acb9c538a8d848e9ee51c8c33285c
Michael Schellenberger Costa <mschellenbergercosta@googlemail.com>
2020-07-19 19:59:28 +0200

    Modernize the ranges::find family

--- tests/std/tests/P0896R4_ranges_alg_find_first_of/test.cpp
+++ tests/std/tests/P0896R4_ranges_alg_find_first_of/test.cpp
@@ -2,92 +2,86 @@
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
 #include <algorithm>
-#include <array>
 #include <cassert>
 #include <concepts>
 #include <ranges>
 #include <utility>
 
 #include <range_algorithm_support.hpp>
+using namespace std;
+using P = std::pair<int, int>;
 
-constexpr void smoke_test() {
-    using ranges::find_first_of, ranges::iterator_t, std::array, std::same_as, std::to_address;
-    using P = std::pair<int, int>;
-
-    // Validate dangling story
-    STATIC_ASSERT(same_as<decltype(find_first_of(borrowed<false>{}, array<int, 42>{})), ranges::dangling>);
-    STATIC_ASSERT(same_as<decltype(find_first_of(borrowed<true>{}, array<int, 42>{})), int*>);
-
-    const array pairs = {P{0, 42}, P{1, 42}, P{2, 42}, P{3, 42}, P{4, 42}, P{5, 42}, P{6, 42}};
-
-    const auto pred = [](const int x, const int y) { return x == y + 1; };
-
-    const array good_needle = {29, 1};
-    {
-        // Validate range overload [found case]
-        auto result = find_first_of(basic_borrowed_range{pairs}, good_needle, pred, get_first);
-        STATIC_ASSERT(same_as<decltype(result), iterator_t<basic_borrowed_range<const P>>>);
-        assert(result.peek() == pairs.data() + 2);
-    }
-    {
-        // Validate iterator + sentinel overload [found case]
-        basic_borrowed_range wrapped_pairs{pairs};
-        auto result = find_first_of(
-            wrapped_pairs.begin(), wrapped_pairs.end(), good_needle.begin(), good_needle.end(), pred, get_first);
-        STATIC_ASSERT(same_as<decltype(result), iterator_t<basic_borrowed_range<const P>>>);
-        assert(result.peek() == pairs.data() + 2);
-    }
-
-    const array bad_needle = {29, 17};
-    {
-        // Validate range overload [not found case]
-        auto result = find_first_of(basic_borrowed_range{pairs}, bad_needle, pred, get_first);
-        STATIC_ASSERT(same_as<decltype(result), iterator_t<basic_borrowed_range<const P>>>);
-        assert(result.peek() == pairs.data() + pairs.size());
-    }
-    {
-        // Validate iterator + sentinel overload [not found case]
-        basic_borrowed_range wrapped_pairs{pairs};
-        auto result = find_first_of(
-            wrapped_pairs.begin(), wrapped_pairs.end(), bad_needle.begin(), bad_needle.end(), pred, get_first);
-        STATIC_ASSERT(same_as<decltype(result), iterator_t<basic_borrowed_range<const P>>>);
-        assert(result.peek() == pairs.data() + pairs.size());
-    }
-}
-
-int main() {
-    STATIC_ASSERT((smoke_test(), true));
-    smoke_test();
-}
+// Validate dangling story
+STATIC_ASSERT(same_as<decltype(ranges::find_first_of(borrowed<false>{}, borrowed<true>{})), ranges::dangling>);
+STATIC_ASSERT(same_as<decltype(ranges::find_first_of(borrowed<true>{}, borrowed<true>{})), int*>);
 
 struct instantiator {
-    template <class In1, class Fwd2>
-    static void call(In1&& in1 = {}, Fwd2&& fwd2 = {}) {
-        if constexpr (!is_permissive) { // These fail to compile in C1XX's permissive mode due to VSO-566808
-            using ranges::iterator_t;
+    static constexpr P haystack[7]      = {{0, 42}, {1, 42}, {2, 42}, {3, 42}, {4, 42}, {5, 42}, {6, 42}};
+    static constexpr int good_needle[2] = {29, 1};
+    static constexpr int bad_needle[2]  = {29, 17};
 
-            (void) ranges::find_first_of(in1, fwd2);
-            (void) ranges::find_first_of(ranges::begin(in1), ranges::end(in1), ranges::begin(fwd2), ranges::end(fwd2));
+    template <ranges::input_range Read1, ranges::forward_range Read2>
+    static constexpr void call() {
+        using ranges::find_first_of, ranges::iterator_t;
+        const auto pred = [](const int x, const int y) { return x == y + 1; };
 
-            BinaryPredicateFor<iterator_t<In1>, iterator_t<Fwd2>> pred{};
-            (void) ranges::find_first_of(in1, fwd2, pred);
-            (void) ranges::find_first_of(
-                ranges::begin(in1), ranges::end(in1), ranges::begin(fwd2), ranges::end(fwd2), pred);
+        { // Validate range overload [found case]
+            Read1 wrapped_haystack{haystack};
+            Read2 wrapped_needle{good_needle};
 
-            HalfProjectedBinaryPredicateFor<iterator_t<Fwd2>> halfpred{};
-            ProjectionFor<iterator_t<In1>> halfproj{};
-            (void) ranges::find_first_of(in1, fwd2, halfpred, halfproj);
-            (void) ranges::find_first_of(
-                ranges::begin(in1), ranges::end(in1), ranges::begin(fwd2), ranges::end(fwd2), halfpred, halfproj);
+            auto result = find_first_of(wrapped_haystack, wrapped_needle, pred, get_first);
+            STATIC_ASSERT(same_as<decltype(result), iterator_t<Read1>>);
+            assert(result.peek() == begin(haystack) + 2);
+        }
+        { // Validate iterator + sentinel overload [found case]
+            Read1 wrapped_haystack{haystack};
+            Read2 wrapped_needle{good_needle};
 
-            ProjectedBinaryPredicate<0, 1> projpred{};
-            ProjectionFor<iterator_t<In1>, 0> proj1{};
-            ProjectionFor<iterator_t<Fwd2>, 1> proj2{};
-            (void) ranges::find_first_of(in1, fwd2, projpred, proj1, proj2);
-            (void) ranges::find_first_of(
-                ranges::begin(in1), ranges::end(in1), ranges::begin(fwd2), ranges::end(fwd2), projpred, proj1, proj2);
+            auto result = find_first_of(wrapped_haystack.begin(), wrapped_haystack.end(), wrapped_needle.begin(),
+                wrapped_needle.end(), pred, get_first);
+            STATIC_ASSERT(same_as<decltype(result), iterator_t<Read1>>);
+            assert(result.peek() == begin(haystack) + 2);
+        }
+
+        { // Validate range overload [not found case]
+            Read1 wrapped_haystack{haystack};
+            Read2 wrapped_needle{bad_needle};
+
+            auto result = find_first_of(wrapped_haystack, wrapped_needle, pred, get_first);
+            STATIC_ASSERT(same_as<decltype(result), iterator_t<Read1>>);
+            assert(result.peek() == end(haystack));
+        }
+        {
+            // Validate iterator + sentinel overload [not found case]
+            Read1 wrapped_haystack{haystack};
+            Read2 wrapped_needle{bad_needle};
+
+            auto result = find_first_of(wrapped_haystack.begin(), wrapped_haystack.end(), wrapped_needle.begin(),
+                wrapped_needle.end(), pred, get_first);
+            STATIC_ASSERT(same_as<decltype(result), iterator_t<Read1>>);
+            assert(result.peek() == end(haystack));
         }
     }
 };
 
-template void test_in_fwd<instantiator, const int, const int>();
+#ifdef TEST_EVERYTHING
+int main() {
+    STATIC_ASSERT((test_in_fwd<instantiator, const P, const int>(), true));
+    test_in_fwd<instantiator, const P, const int>();
+}
+#else // ^^^ test all range combinations // test only interesting range combos vvv
+using in_test_range  = test::range<input_iterator_tag, const P, test::Sized::no, test::CanDifference::no,
+    test::Common::no, test::CanCompare::no, test::ProxyRef::yes>;
+using fwd_test_range = test::range<forward_iterator_tag, const int, test::Sized::no, test::CanDifference::no,
+    test::Common::no, test::CanCompare::yes, test::ProxyRef::yes>;
+
+constexpr bool run_tests() {
+    instantiator::call<in_test_range, fwd_test_range>();
+    return true;
+}
+
+int main() {
+    STATIC_ASSERT(run_tests());
+    run_tests();
+}
+#endif // TEST_EVERYTHING
--- tests/std/tests/P0896R4_ranges_alg_find_if_not/test.cpp
+++ tests/std/tests/P0896R4_ranges_alg_find_if_not/test.cpp
@@ -9,64 +9,53 @@
 #include <utility>
 
 #include <range_algorithm_support.hpp>
+using namespace std;
+using P = std::pair<int, int>;
 
-constexpr void smoke_test() {
-    using ranges::find_if_not, ranges::iterator_t, std::same_as;
-    using P = std::pair<int, int>;
+auto matches = [](const int x) { return x == 42; };
 
-    std::array<P, 3> pairs = {{{0, 13}, {0, 13}, {0, 13}}};
-    constexpr auto equals  = [](auto x) { return [x](auto&& y) { return y == x; }; };
-
-    // Validate dangling story
-    STATIC_ASSERT(same_as<decltype(find_if_not(borrowed<false>{}, equals(42))), ranges::dangling>);
-    STATIC_ASSERT(same_as<decltype(find_if_not(borrowed<true>{}, equals(42))), int*>);
-
-    for (auto& [value, _] : pairs) {
-        value = 42;
-        {
-            // Validate range overload [found case]
-            auto result = find_if_not(basic_borrowed_range{pairs}, equals(0), get_first);
-            STATIC_ASSERT(same_as<decltype(result), iterator_t<basic_borrowed_range<P>>>);
-            assert((*result).first == 42);
-        }
-        {
-            // Validate iterator + sentinel overload [found case]
-            basic_borrowed_range wrapped_pairs{pairs};
-            auto result = find_if_not(wrapped_pairs.begin(), wrapped_pairs.end(), equals(0), get_first);
-            STATIC_ASSERT(same_as<decltype(result), iterator_t<basic_borrowed_range<P>>>);
-            assert((*result).first == 42);
-        }
-        value = 0;
-    }
-    {
-        // Validate range overload [not found case]
-        auto result = find_if_not(basic_borrowed_range{pairs}, equals(0), get_first);
-        STATIC_ASSERT(same_as<decltype(result), iterator_t<basic_borrowed_range<P>>>);
-        assert(result == basic_borrowed_range{pairs}.end());
-    }
-    {
-        // Validate iterator + sentinel overload [not found case]
-        basic_borrowed_range wrapped_pairs{pairs};
-        auto result = find_if_not(wrapped_pairs.begin(), wrapped_pairs.end(), equals(0), get_first);
-        STATIC_ASSERT(same_as<decltype(result), iterator_t<basic_borrowed_range<P>>>);
-        assert(result == wrapped_pairs.end());
-    }
-}
-
-int main() {
-    STATIC_ASSERT((smoke_test(), true));
-    smoke_test();
-}
+// Validate dangling story
+STATIC_ASSERT(same_as<decltype(ranges::find_if_not(borrowed<false>{}, matches)), ranges::dangling>);
+STATIC_ASSERT(same_as<decltype(ranges::find_if_not(borrowed<true>{}, matches)), int*>);
 
 struct instantiator {
-    template <class In>
-    static void call(In&& in = {}) {
-        using ranges::iterator_t;
-        using I = iterator_t<In>;
+    static constexpr P haystack[3] = {{1, 13}, {2, 13}, {3, 13}};
 
-        (void) ranges::find_if_not(in, UnaryPredicateFor<I>{});
-        (void) ranges::find_if_not(in, ProjectedUnaryPredicate<>{}, ProjectionFor<I>{});
+    template <ranges::input_range Read>
+    static constexpr void call() {
+        using ranges::find_if_not, ranges::iterator_t;
+        auto equals = [](auto x) { return [x](auto&& y) { return y == x; }; };
+
+        for (auto& [value, _] : haystack) {
+            { // Validate range overload [found case]
+                Read wrapped_input{haystack};
+                auto result = find_if_not(wrapped_input, equals(4), get_first);
+                STATIC_ASSERT(same_as<decltype(result), iterator_t<Read>>);
+                assert(result.peek()->first == 1);
+            }
+            { // Validate iterator + sentinel overload [found case]
+                Read wrapped_input{haystack};
+                auto result = find_if_not(wrapped_input.begin(), wrapped_input.end(), equals(0), get_first);
+                STATIC_ASSERT(same_as<decltype(result), iterator_t<Read>>);
+                assert(result.peek()->first == 1);
+            }
+        }
+        { // Validate range overload [not found case]
+            Read wrapped_input{haystack};
+            auto result = find_if_not(wrapped_input, equals(13), get_second);
+            STATIC_ASSERT(same_as<decltype(result), iterator_t<Read>>);
+            assert(result == wrapped_input.end());
+        }
+        { // Validate iterator + sentinel overload [not found case]
+            Read wrapped_input{haystack};
+            auto result = find_if_not(wrapped_input.begin(), wrapped_input.end(), equals(13), get_second);
+            STATIC_ASSERT(same_as<decltype(result), iterator_t<Read>>);
+            assert(result == wrapped_input.end());
+        }
     }
 };
 
-template void test_in<instantiator, const int>();
+int main() {
+    STATIC_ASSERT((test_in<instantiator, const P>(), true));
+    test_in<instantiator, const P>();
+}
