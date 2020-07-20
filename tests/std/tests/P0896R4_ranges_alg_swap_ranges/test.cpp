59bf6ce95d97647790088e2caff2c32ff0b249b6
Michael Schellenberger Costa <mschellenbergercosta@googlemail.com>
2020-07-20 16:29:53 +0200

    Modernize ranges::swap_ranges and ranges::distance

--- tests/std/tests/P0896R4_ranges_alg_swap_ranges/test.cpp
+++ tests/std/tests/P0896R4_ranges_alg_swap_ranges/test.cpp
@@ -2,65 +2,69 @@
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
 #include <algorithm>
-#include <array>
 #include <cassert>
 #include <concepts>
 #include <ranges>
 #include <utility>
 
 #include <range_algorithm_support.hpp>
+using namespace std;
 
-constexpr void smoke_test() {
-    using ranges::swap_ranges, ranges::swap_ranges_result, ranges::iterator_t;
-    using std::same_as;
-    using I1 = iterator_t<basic_borrowed_range<int>>;
+// Validate that swap_ranges_result aliases in_out_result
+STATIC_ASSERT(same_as<ranges::swap_ranges_result<int, double>, ranges::in_in_result<int, double>>);
 
-    // Validate that swap_ranges_result aliases in_in_result
-    STATIC_ASSERT(same_as<swap_ranges_result<int, double>, ranges::in_in_result<int, double>>);
-
-    { // Validate ranges overload
-        using R                      = std::array<int, 3>;
-        R range1                     = {13, 53, 1876};
-        R range2                     = {34, 243, 9366};
-        int const expected_output1[] = {34, 243, 9366};
-        int const expected_output2[] = {13, 53, 1876};
-        auto result                  = swap_ranges(basic_borrowed_range{range1}, basic_borrowed_range{range2});
-        STATIC_ASSERT(same_as<decltype(result), swap_ranges_result<I1, I1>>);
-        assert(result.in1 == basic_borrowed_range{range1}.end());
-        assert(result.in2 == basic_borrowed_range{range2}.end());
-        assert(ranges::equal(range1, expected_output1));
-        assert(ranges::equal(range2, expected_output2));
-    }
-    { // Validate iterator + sentinel overload
-        int range1[] = {13, 53, 1876};
-        int range2[] = {34, 243, 9366};
-        basic_borrowed_range wrapped_range1{range1};
-        basic_borrowed_range wrapped_range2{range2};
-        int const expected_output1[] = {34, 243, 9366};
-        int const expected_output2[] = {13, 53, 1876};
-        auto result =
-            swap_ranges(wrapped_range1.begin(), wrapped_range1.end(), wrapped_range2.begin(), wrapped_range2.end());
-        STATIC_ASSERT(same_as<decltype(result), swap_ranges_result<I1, I1>>);
-        assert(result.in1 == wrapped_range1.end());
-        assert(result.in2 == wrapped_range2.end());
-        assert(ranges::equal(range1, expected_output1));
-        assert(ranges::equal(range2, expected_output2));
-    }
-}
-
-int main() {
-#if defined(__clang__) || defined(__EDG__) // TRANSITION, VSO-938163
-    STATIC_ASSERT((smoke_test(), true));
-#endif // TRANSITION, VSO-938163
-    smoke_test();
-}
+// Validate dangling story
+STATIC_ASSERT(same_as<decltype(ranges::swap_ranges(borrowed<false>{}, borrowed<false>{})),
+    ranges::swap_ranges_result<ranges::dangling, ranges::dangling>>);
+STATIC_ASSERT(same_as<decltype(ranges::swap_ranges(borrowed<true>{}, borrowed<false>{})),
+    ranges::swap_ranges_result<int*, ranges::dangling>>);
+STATIC_ASSERT(same_as<decltype(ranges::swap_ranges(borrowed<false>{}, borrowed<true>{})),
+    ranges::swap_ranges_result<ranges::dangling, int*>>);
+STATIC_ASSERT(
+    same_as<decltype(ranges::swap_ranges(borrowed<true>{}, borrowed<true>{})), ranges::swap_ranges_result<int*, int*>>);
 
 struct instantiator {
-    template <class In1, class In2>
-    static void call(In1 in1 = {}, In2 in2 = {}) {
-        (void) ranges::swap_ranges(in1, in2);
-        (void) ranges::swap_ranges(ranges::begin(in1), ranges::end(in1), ranges::begin(in2), ranges::end(in2));
+    static constexpr int expected_output1[3] = {34, 243, 9366};
+    static constexpr int expected_output2[3] = {13, 53, 1876};
+
+    template <ranges::input_range ReadWrite1, ranges::input_range ReadWrite2>
+    static constexpr void call() {
+        using ranges::swap_ranges, ranges::swap_ranges_result, ranges::iterator_t, ranges::equal;
+        { // Validate iterator + sentinel overload
+            int input1[3] = {13, 53, 1876};
+            int input2[3] = {34, 243, 9366};
+            ReadWrite1 wrapped_input1{input1};
+            ReadWrite2 wrapped_input2{input2};
+
+            auto result =
+                swap_ranges(wrapped_input1.begin(), wrapped_input1.end(), wrapped_input2.begin(), wrapped_input2.end());
+            STATIC_ASSERT(
+                same_as<decltype(result), swap_ranges_result<iterator_t<ReadWrite1>, iterator_t<ReadWrite2>>>);
+            assert(result.in1 == wrapped_input1.end());
+            assert(result.in2 == wrapped_input2.end());
+            assert(equal(input1, expected_output1));
+            assert(equal(input2, expected_output2));
+        }
+        { // Validate range overload
+            int input1[3] = {13, 53, 1876};
+            int input2[3] = {34, 243, 9366};
+            ReadWrite1 wrapped_input1{input1};
+            ReadWrite2 wrapped_input2{input2};
+
+            auto result = swap_ranges(wrapped_input1, wrapped_input2);
+            STATIC_ASSERT(
+                same_as<decltype(result), swap_ranges_result<iterator_t<ReadWrite1>, iterator_t<ReadWrite2>>>);
+            assert(result.in1 == wrapped_input1.end());
+            assert(result.in2 == wrapped_input2.end());
+            assert(equal(input1, expected_output1));
+            assert(equal(input2, expected_output2));
+        }
     }
 };
 
-template void test_in_in<instantiator, int, int>();
+int main() {
+#if defined(__clang__) || defined(__EDG__) // TRANSITION, VSO-938163
+    STATIC_ASSERT((test_in_in<instantiator, int, int>(), true));
+#endif // TRANSITION, VSO-938163
+    test_in_in<instantiator, int, int>();
+}
