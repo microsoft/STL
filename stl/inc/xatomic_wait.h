66a2e6e076016c2342ebd278c965fdfac323c651
Billy Robert O'Neal III <bion@microsoft.com>
2020-07-22 19:07:36 -0700

    Add comments explaining the ABI layer to <xatomic_wait.h> and remove no longer called _Atomic_wait_get_deadline.

--- stl/inc/xatomic_wait.h
+++ stl/inc/xatomic_wait.h
@@ -30,35 +30,36 @@ enum class __std_atomic_api_level : unsigned long {
     __has_wait_on_address,
 };
 
+// This function allows testing the atomic wait support while always using the APIs for a platform with fewer
+// capabilities; it attempts to lock the APIs used to the level `_Requested_api_level`, and returns the actual API level
+// in use. Once the API level has been set by calling this function (or detected by a call to one of the atomic wait
+// functions), it can no longer be changed.
 __std_atomic_api_level __stdcall __std_atomic_set_api_level(__std_atomic_api_level _Requested_api_level) noexcept;
+
+// Support for atomic waits.
+// The "direct" functions are used when the underlying infrastructure can use WaitOnAddress directly; that is, _Size is
+// 1, 2, 4 or 8. The contract is the same as the WaitOnAddress function from the Windows SDK. If WaitOnAddress is not
+// available on the current platform, falls back to a similar solution based on SRWLOCK and CONDITION_VARIABLE.
 bool __stdcall __std_atomic_wait_direct(
     const void* _Storage, const void* _Comparand, const size_t _Size, const unsigned long _Remaining_timeout) noexcept;
 void __stdcall __std_atomic_notify_one_direct(const void* _Storage) noexcept;
 void __stdcall __std_atomic_notify_all_direct(const void* _Storage) noexcept;
 
+// The "indirect" functions are used when the size is not 1, 2, 4, or 8; these notionally wait on another value which is
+// of one of those sizes whose value changes upon notify, hence "indirect". (At time of writing this always uses the
+// fallback SRWLOCK and CONDITION_VARIABLE implementation but that is not contractual)
 bool __stdcall __std_atomic_wait_indirect(
     const void* _Storage, const void* _Comparand, const size_t _Size, const unsigned long _Remaining_timeout) noexcept;
 void __stdcall __std_atomic_notify_one_indirect(const void* _Storage) noexcept;
 void __stdcall __std_atomic_notify_all_indirect(const void* _Storage) noexcept;
 
+// These functions convert a duration into a time point in order to tolerate spurious wakes in atomic wait, and then
+// convert back from the time point to individual wait attempts (which are limited by DWORD milliseconds to a length of
+// ~49 days)
 unsigned long long __stdcall __std_atomic_wait_get_deadline(unsigned long long _Timeout) noexcept;
 unsigned long __stdcall __std_atomic_wait_get_remaining_timeout(unsigned long long _Deadline) noexcept;
 
 _END_EXTERN_C
-
-inline unsigned long long _Atomic_wait_get_deadline(const unsigned long long _Timeout) noexcept {
-    if (_Timeout == _Atomic_wait_no_deadline) {
-        return _Atomic_wait_no_deadline;
-    }
-    return __std_atomic_wait_get_deadline(_Timeout);
-}
-
-inline unsigned long _Atomic_wait_get_remaining_timeout(const unsigned long long _Deadline) noexcept {
-    if (_Deadline == _Atomic_wait_no_deadline) {
-        return _Atomic_wait_no_timeout;
-    }
-    return __std_atomic_wait_get_remaining_timeout(_Deadline);
-}
 
 #pragma pop_macro("new")
 _STL_RESTORE_CLANG_WARNINGS
