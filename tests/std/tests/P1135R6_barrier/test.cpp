fa7ae3fe162f00b8e0c5efe9962916587d28e91a
Alex Guteniev <gutenev@gmail.com>
2020-07-26 12:37:25 +0300

    fix, make test pass

--- tests/std/tests/P1135R6_barrier/test.cpp
+++ tests/std/tests/P1135R6_barrier/test.cpp
@@ -3,6 +3,37 @@
 
 #include <barrier>
 #include <cassert>
+#include <chrono>
 #include <thread>
 
-int main() {}
+void test() {
+    std::barrier barrier(2);
+
+    std::atomic<int> c{0};
+
+    std::thread t1([&] {
+        for (int i = 0; i < 5; i++) {
+            auto token = barrier.arrive();
+            barrier.wait(std::move(token));
+            c.fetch_add(1, std::memory_order_relaxed);
+        }
+    });
+
+    std::thread t2([&] {
+        for (int i = 0; i < 3; i++) {
+            barrier.arrive_and_wait();
+            c.fetch_add(1, std::memory_order_relaxed);
+        }
+        barrier.arrive_and_drop();
+    });
+
+    t1.join();
+    t2.join();
+
+    assert(c.load(std::memory_order_relaxed) == 8);
+}
+
+
+int main() {
+    test();
+}
