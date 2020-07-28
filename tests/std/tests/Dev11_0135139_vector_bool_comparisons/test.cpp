d6acf3e2f5f7a151cad2a130adff0a06e452242e
Stephan T. Lavavej <stl@microsoft.com>
2020-07-27 18:54:51 -0700

    Test vector<bool> comparisons.

--- tests/std/tests/Dev11_0135139_vector_bool_comparisons/test.cpp
+++ tests/std/tests/Dev11_0135139_vector_bool_comparisons/test.cpp
@@ -8,6 +8,58 @@
 
 using namespace std;
 
+vector<bool> vb_from_str(const char* str) {
+    vector<bool> vb;
+
+    for (; *str != '\0'; ++str) {
+        assert(*str == '0' || *str == '1');
+
+        vb.push_back(*str == '1');
+    }
+
+    return vb;
+}
+
+enum class Ordering : int { Less = -1, Equal = 0, Greater = 1 };
+
+constexpr Ordering Lt = Ordering::Less;
+constexpr Ordering Eq = Ordering::Equal;
+constexpr Ordering Gt = Ordering::Greater;
+
+void test_comparison(const char* const left_str, const char* const right_str, const Ordering order) {
+    const auto left  = vb_from_str(left_str);
+    const auto right = vb_from_str(right_str);
+
+#ifdef __cpp_lib_concepts
+    assert((left <=> right) == (static_cast<int>(order) <=> 0));
+    assert((right <=> left) == (0 <=> static_cast<int>(order)));
+#endif // __cpp_lib_concepts
+
+    switch (order) {
+    case Lt:
+        assert(!(left == right));
+        assert(!(right == left));
+        assert(left < right);
+        assert(!(right < left));
+        break;
+    case Eq:
+        assert(left == right);
+        assert(right == left);
+        assert(!(left < right));
+        assert(!(right < left));
+        break;
+    case Gt:
+        assert(!(left == right));
+        assert(!(right == left));
+        assert(!(left < right));
+        assert(right < left);
+        break;
+    default:
+        assert(false);
+        break;
+    }
+}
+
 int main() {
 
     {
@@ -79,4 +131,32 @@ int main() {
 
         assert(v == correct);
     }
+
+    // Also test GH-1046 optimizing vector<bool> spaceship and less-than comparisons.
+    test_comparison("", "", Eq); // both empty
+    test_comparison("", "00", Lt); // empty vs. partial word
+    test_comparison("", "01", Lt);
+    test_comparison("", "00000000000000000000000000000000", Lt); // empty vs. full word
+    test_comparison("", "01000000000000000000000000000000", Lt);
+    test_comparison("", "0000000000000000000000000000000000", Lt); // empty vs. full and partial words
+    test_comparison("", "0100000000000000000000000000000001", Lt);
+
+    test_comparison("010111010", "010111010", Eq);
+    test_comparison("010111010", "011110010", Lt); // test that bits are compared in the correct direction
+
+    // same test, after an initial matching word
+    test_comparison("00001111000011110000111100001111010111010", "00001111000011110000111100001111010111010", Eq);
+    test_comparison("00001111000011110000111100001111010111010", "00001111000011110000111100001111011110010", Lt);
+
+    test_comparison("00001111", "00001111", Eq);
+    test_comparison("00001111", "000011110", Lt); // matching prefixes, test size comparison
+    test_comparison("00001111", "000011111", Lt);
+    test_comparison("00001111", "00001111000000000000000000000000", Lt); // full word
+    test_comparison("00001111", "00001111000000000000000000000001", Lt);
+    test_comparison("00001111", "0000111100000000000000000000000000", Lt); // full and partial words
+    test_comparison("00001111", "0000111100000000000000000000000001", Lt);
+
+    test_comparison("10", "01111", Gt); // shorter but greater
+    test_comparison("10", "01111111111111111111111111111111", Gt); // full word
+    test_comparison("10", "0111111111111111111111111111111111", Gt); // full and partial words
 }
