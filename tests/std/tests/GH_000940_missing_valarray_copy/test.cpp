caf1fc019ce6ba6b41dfc00b8526853ec374f16b
Alex Guteniev <gutenev@gmail.com>
2020-07-28 06:35:21 +0300

    review comments

--- tests/std/tests/GH_000940_missing_valarray_copy/test.cpp
+++ tests/std/tests/GH_000940_missing_valarray_copy/test.cpp
@@ -5,102 +5,75 @@
 #include <iostream>
 #include <valarray>
 
+template <class T>
+bool eq(const std::valarray<T>& v, std::initializer_list<T> il) {
+    return std::equal(begin(v), end(v), il.begin(), il.end());
+}
+
 void test_slice() {
-    std::valarray<int> v({0, 1, 2, 3, 4});
+    std::valarray<int> v{0, 1, 2, 3, 4};
 
     std::slice_array<int> slice_array      = v[std::slice(2, 2, 2)];
     std::slice_array<int> slice_array_copy = slice_array;
     (void) slice_array_copy;
 
-    assert(v[0] == 0);
-    assert(v[1] == 1);
-    assert(v[2] == 2);
-    assert(v[3] == 3);
-    assert(v[4] == 4);
+    assert(eq(v, {0, 1, 2, 3, 4}));
 
-    std::slice_array<int> other_array_slice = v[std::slice(1, 2, 1)];
-    other_array_slice                       = slice_array;
+    std::slice_array<int> other_slice_array = v[std::slice(1, 2, 1)];
+    other_slice_array                       = slice_array;
 
-    assert(v[0] == 0);
-    assert(v[1] == 2);
-    assert(v[2] == 4);
-    assert(v[3] == 3);
-    assert(v[4] == 4);
+    assert(eq(v, {0, 2, 4, 3, 4}));
 }
 
 void test_gslice() {
-    std::valarray<int> v({0, 1, 2, 3, 4});
+    std::valarray<int> v{0, 1, 2, 3, 4};
 
-    std::gslice gslice(2, std::valarray<std::size_t>({2}), std::valarray<std::size_t>({2}));
+    std::gslice gslice(2, std::valarray<std::size_t>{2}, std::valarray<std::size_t>{2});
     std::gslice_array<int> gslice_array      = v[gslice];
     std::gslice_array<int> gslice_array_copy = gslice_array;
     (void) gslice_array_copy;
 
-    assert(v[0] == 0);
-    assert(v[1] == 1);
-    assert(v[2] == 2);
-    assert(v[3] == 3);
-    assert(v[4] == 4);
+    assert(eq(v, {0, 1, 2, 3, 4}));
 
-    std::gslice other_gslice(1, std::valarray<std::size_t>({2}), std::valarray<std::size_t>({1}));
+    std::gslice other_gslice(1, std::valarray<std::size_t>{2}, std::valarray<std::size_t>{1});
     std::gslice_array<int> other_gslice_array = v[other_gslice];
     other_gslice_array                        = gslice_array;
 
-    assert(v[0] == 0);
-    assert(v[1] == 2);
-    assert(v[2] == 4);
-    assert(v[3] == 3);
-    assert(v[4] == 4);
+    assert(eq(v, {0, 2, 4, 3, 4}));
 }
 
 void test_mask() {
-    std::valarray<int> v({0, 1, 2, 3, 4});
+    std::valarray<int> v{0, 1, 2, 3, 4};
 
-    std::valarray<bool> mask({true, false, false, false, true});
+    std::valarray<bool> mask{true, false, false, false, true};
     std::mask_array<int> mask_array      = v[mask];
     std::mask_array<int> mask_array_copy = mask_array;
     (void) mask_array_copy;
 
-    assert(v[0] == 0);
-    assert(v[1] == 1);
-    assert(v[2] == 2);
-    assert(v[3] == 3);
-    assert(v[4] == 4);
+    assert(eq(v, {0, 1, 2, 3, 4}));
 
-    std::valarray<bool> other_mask({false, true, true, false, false});
+    std::valarray<bool> other_mask{false, true, true, false, false};
     std::mask_array<int> other_mask_array = v[other_mask];
     other_mask_array                      = mask_array;
 
-    assert(v[0] == 0);
-    assert(v[1] == 0);
-    assert(v[2] == 4);
-    assert(v[3] == 3);
-    assert(v[4] == 4);
+    assert(eq(v, {0, 0, 4, 3, 4}));
 }
 
 void test_indirect() {
-    std::valarray<int> v({0, 1, 2, 3, 4});
+    std::valarray<int> v{0, 1, 2, 3, 4};
 
-    std::valarray<std::size_t> indices({2, 3});
+    std::valarray<std::size_t> indices{2, 3};
     std::indirect_array<int> indirect_array      = v[indices];
     std::indirect_array<int> indirect_array_copy = indirect_array;
     (void) indirect_array_copy;
 
-    assert(v[0] == 0);
-    assert(v[1] == 1);
-    assert(v[2] == 2);
-    assert(v[3] == 3);
-    assert(v[4] == 4);
+    assert(eq(v, {0, 1, 2, 3, 4}));
 
-    std::valarray<std::size_t> other_indices({4, 0});
+    std::valarray<std::size_t> other_indices{4, 0};
     std::indirect_array<int> other_indirect_array = v[other_indices];
     other_indirect_array                          = indirect_array;
 
-    assert(v[0] == 3);
-    assert(v[1] == 1);
-    assert(v[2] == 2);
-    assert(v[3] == 3);
-    assert(v[4] == 2);
+    assert(eq(v, {3, 1, 2, 3, 2}));
 }
 
 
