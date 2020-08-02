f5ae641d92af8918285d8a5d473ce12dc7ee39b0
Alex Guteniev <gutenev@gmail.com>
2020-08-02 07:30:54 +0300

    latch test: assumption to finish in a finite time

--- tests/std/tests/P1135R6_latch/test.cpp
+++ tests/std/tests/P1135R6_latch/test.cpp
@@ -11,33 +11,17 @@ using namespace std::chrono_literals;
 void test(const bool release_wait) {
     std::latch latch(5);
 
-    std::atomic<int> c{0};
+    std::thread t1([&] { latch.wait(); });
 
-    std::thread t1([&] {
-        latch.wait();
-        c.fetch_add(1, std::memory_order_relaxed);
-    });
-
-    std::thread t2([&] {
-        latch.arrive_and_wait(2);
-        c.fetch_add(1, std::memory_order_relaxed);
-    });
+    std::thread t2([&] { latch.arrive_and_wait(2); });
 
     latch.count_down();
-
-    std::this_thread::sleep_for(200ms);
-
-    assert(c.load(std::memory_order_relaxed) == 0);
 
     if (release_wait) {
         latch.arrive_and_wait(2);
     } else {
         latch.count_down(2);
     }
-
-    std::this_thread::sleep_for(200ms);
-
-    assert(c.load(std::memory_order_relaxed) == 2);
 
     t1.join();
     t2.join();
