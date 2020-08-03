a6a59345b8cd261e3e96fc6d4eb37293624ca0cf
statementreply <statementreply@gmail.com>
2020-08-03 22:13:29 +0800

    Improve accuracy of `log` when |z| is close to 1
    When |z| is close to 1, compute log(|z|) as log1p(norm_minus_1(z)) / 2,
    where norm_minus_1(z) = real(z) ^ 2 + imag(z) ^ 2 - 1 computed with
    double width arithmetic to avoid catastrophic cancellation.

---
+++ stl/src/float_multi_prec.hpp
@@ -0,0 +1,112 @@
+// Copyright (c) Microsoft Corporation.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+// implements multi-precision floating point arithmetic for numerical algorithms
+
+// This must be as small as possible, because its contents are
+// injected into the msvcprt.lib and msvcprtd.lib import libraries.
+// Do not include or define anything else here.
+// In particular, basic_string must not be included here.
+
+#pragma once
+
+#if defined(_M_FP_FAST) || defined(__FAST_MATH__)
+#error float_multi_prec.hpp should not be compiled with /fp:fast or -ffast-math
+#endif // defined(_M_FP_FAST) || defined(__FAST_MATH__)
+
+#include <intrin.h>
+#include <isa_availability.h>
+#include <type_traits>
+#include <xutility>
+
+extern "C" int __isa_available;
+
+_STD_BEGIN
+namespace _Float_multi_prec {
+    // multi-precision floating point types
+    template <class _Ty, int _Prec>
+    struct _Fmp_t;
+
+    template <class _Ty>
+    struct _Fmp_t<_Ty, 2> {
+        static_assert(_STD is_floating_point_v<_Ty>);
+        _Ty _Val0;
+        _Ty _Val1;
+    };
+
+    // addition
+    template <class _Ty>
+    _NODISCARD constexpr _Fmp_t<_Ty, 2> _Add_x2(const _Ty _Xval, const _Ty _Yval) noexcept {
+        const _Ty _Sum0 = _Xval + _Yval;
+        const _Ty _Ymod = _Sum0 - _Xval;
+        const _Ty _Xmod = _Sum0 - _Ymod;
+        const _Ty _Yerr = _Yval - _Ymod;
+        const _Ty _Xerr = _Xval - _Xmod;
+        return {_Sum0, _Xerr + _Yerr};
+    }
+
+    // requires: exponent(_Xval) + countr_zero(significand(_Xval)) >= exponent(_Yval) || _Xval == 0
+    template <class _Ty>
+    _NODISCARD constexpr _Fmp_t<_Ty, 2> _Add_small_x2(const _Ty _Xval, const _Ty _Yval) noexcept {
+        const _Ty _Sum0 = _Xval + _Yval;
+        const _Ty _Ymod = _Sum0 - _Xval;
+        const _Ty _Yerr = _Yval - _Ymod;
+        return {_Sum0, _Yerr};
+    }
+
+    // requires: exponent(_Xval) + countr_zero(significand(_Xval)) >= exponent(_Yval._Val0) || _Xval == 0
+    template <class _Ty>
+    _NODISCARD constexpr _Fmp_t<_Ty, 2> _Add_small_x2(const _Ty _Xval, const _Fmp_t<_Ty, 2>& _Yval) noexcept {
+        const _Fmp_t<_Ty, 2> _Sum0 = _Add_small_x2(_Xval, _Yval._Val0);
+        return _Add_small_x2(_Sum0._Val0, _Sum0._Val1 + _Yval._Val1);
+    }
+
+    template <class _Ty>
+    _NODISCARD constexpr _Ty _Add_x1(const _Fmp_t<_Ty, 2>& _Xval, const _Fmp_t<_Ty, 2>& _Yval) noexcept {
+        const _Fmp_t<_Ty, 2> _Sum00 = _Add_x2(_Xval._Val0, _Yval._Val0);
+        return _Sum00._Val0 + (_Sum00._Val1 + (_Xval._Val1 + _Yval._Val1));
+    }
+
+    // multiplication
+    _NODISCARD inline constexpr double _High_half(const double _Val) {
+        const auto _Bits           = _STD _Bit_cast<unsigned long long>(_Val);
+        const auto _High_half_bits = (_Bits + 0x3ff'ffffULL) & 0xffff'ffff'f800'0000ULL;
+        return _STD _Bit_cast<double>(_High_half_bits);
+    }
+
+    _NODISCARD inline constexpr double _Sqr_error_fallback(const double _Xval, const double _Prod0) noexcept {
+        const double _Xhigh = _High_half(_Xval);
+        const double _Xlow  = _Xval - _Xhigh;
+        return ((_Xhigh * _Xhigh - _Prod0) + 2.0 * _Xhigh * _Xlow) + _Xlow * _Xlow;
+    }
+
+#if defined(_M_IX86) || defined(_M_X64)
+    _NODISCARD inline double _Sqr_error_x86_x64_fma(const double _Xval, const double _Prod0) noexcept {
+        const __m128d _Mx      = _mm_set_sd(_Xval);
+        const __m128d _Mprod0  = _mm_set_sd(_Prod0);
+        const __m128d _Mresult = _mm_fmsub_sd(_Mx, _Mx, _Mprod0);
+        double _Result;
+        _mm_store_sd(&_Result, _Mresult);
+        return _Result;
+    }
+#endif // defined(_M_IX86) || defined(_M_X64)
+
+    _NODISCARD inline constexpr _Fmp_t<double, 2> _Sqr_x2(const double _Xval) noexcept {
+        const double _Prod0 = _Xval * _Xval;
+
+        if (_STD is_constant_evaluated()) {
+            return {_Prod0, _Sqr_error_fallback(_Xval, _Prod0)};
+        } else {
+#if defined(_M_IX86) || defined(_M_X64)
+            if (__isa_available >= __ISA_AVAILABLE_AVX2) {
+                return {_Prod0, _Sqr_error_x86_x64_fma(_Xval, _Prod0)};
+            } else {
+                return {_Prod0, _Sqr_error_fallback(_Xval, _Prod0)};
+            }
+#else // ^^^ x86, x64 / arm, arm64 vvv
+            return {_Prod0, _Sqr_error_fallback(_Xval, _Prod0)};
+#endif // ^^^ arm, arm64 ^^^
+        }
+    }
+} // namespace _Float_multi_prec
+_STD_END
---
+++ stl/src/math_algorithms.cpp
@@ -0,0 +1,148 @@
+// Copyright (c) Microsoft Corporation.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+// implements numerical algorithms for <complex>
+
+// This must be as small as possible, because its contents are
+// injected into the msvcprt.lib and msvcprtd.lib import libraries.
+// Do not include or define anything else here.
+// In particular, basic_string must not be included here.
+
+#include <cmath>
+#include <limits>
+#include <type_traits>
+#include <xutility>
+
+#include "float_multi_prec.hpp"
+
+#define _FMP ::std::_Float_multi_prec::
+
+namespace {
+    // TRANSITION: sqrt() isn't constexpr
+    // _Hypot_leg_huge = _Ty{0.5} * _STD sqrt((_STD numeric_limits<_Ty>::max)());
+    // _Hypot_leg_tiny = _STD sqrt(_Ty{2.0} * (_STD numeric_limits<_Ty>::min)() / _STD numeric_limits<_Ty>::epsilon());
+    template <class _Ty>
+    inline constexpr _Ty _Hypot_leg_huge = _Ty{6.703903964971298e+153};
+    template <>
+    inline constexpr float _Hypot_leg_huge<float> = 9.2233715e+18f;
+
+    template <class _Ty>
+    inline constexpr _Ty _Hypot_leg_tiny = _Ty{1.4156865331029228e-146};
+    template <>
+    inline constexpr float _Hypot_leg_tiny<float> = 4.440892e-16f;
+
+    template <class _Ty>
+    _NODISCARD _Ty _Norm_minus_one(const _Ty _Xval, const _Ty _Yval) noexcept {
+        // requires |_Xval| >= |_Yval| and 0.5 <= |_Xval| < 2^12
+        // returns _Xval * _Xval + _Yval * _Yval - 1
+        const _FMP _Fmp_t<_Ty, 2> _Xsqr    = _FMP _Sqr_x2(_Xval);
+        const _FMP _Fmp_t<_Ty, 2> _Ysqr    = _FMP _Sqr_x2(_Yval);
+        const _FMP _Fmp_t<_Ty, 2> _Xsqr_m1 = _FMP _Add_small_x2(_Ty{-1.0}, _Xsqr);
+        return _Add_x1(_Xsqr_m1, _Ysqr);
+    }
+
+    _NODISCARD inline float _Norm_minus_one(const float _Xval, const float _Yval) noexcept {
+        const auto _Dx = static_cast<double>(_Xval);
+        const auto _Dy = static_cast<double>(_Yval);
+        return static_cast<float>((_Dx * _Dx - 1.0) + _Dy * _Dy);
+    }
+
+    // TRANSITION: CRT log1p can be inaccurate for tiny inputs under directed rounding modes
+    template <class _Ty>
+    _NODISCARD _Ty _Logp1(const _Ty _Xval) { // returns log(1 + _Xval)
+        static_assert(_STD is_floating_point_v<_Ty>);
+
+        if (_STD _Is_nan(_Xval)) {
+            return _Xval + _Xval; // raise FE_INVALID if necessary
+        }
+
+        if (_Xval <= _Ty{-0.5} || _Ty{2.0} <= _Xval) { // naive formula is accurate
+            if (_Xval == (_STD numeric_limits<_Ty>::max)()) { // avoid overflow
+                return _STD log(_Xval);
+            }
+
+            return _STD log(_Ty{1.0} + _Xval);
+        }
+
+        const _Ty _Xabs = _STD _Float_abs(_Xval);
+        if (_Xabs < _STD numeric_limits<_Ty>::epsilon()) { // zero or tiny
+            if (_Xabs == _Ty{0.0}) {
+                return _Xval;
+            }
+
+            // honor rounding mode, raise FE_INEXACT
+            return _Xabs * (_STD _Float_copysign(_Ty{1.0}, _Xval) - _Ty{0.5} * _Xabs);
+        }
+
+        // compute log(1 + _Xval) with fixup for small _Xval
+        const _FMP _Fmp_t<_Ty, 2> _Xp1 = _FMP _Add_small_x2(_Ty{1.0}, _Xval);
+        return _STD log(_Xp1._Val0) + _Xp1._Val1 / _Xp1._Val0;
+    }
+
+    template <class _Ty>
+    _NODISCARD _Ty _Log_hypot(const _Ty _Xval, const _Ty _Yval) noexcept { // returns log(hypot(_Xval, _Yval))
+        static_assert(_STD is_floating_point_v<_Ty>);
+
+        if (!_STD _Is_finite(_Xval) || !_STD _Is_finite(_Yval)) { // Inf or NaN
+            if (_STD _Is_signaling_nan(_Xval) || _STD _Is_signaling_nan(_Yval)) {
+                return _Xval + _Yval;
+            }
+
+            if (_STD isinf(_Xval)) {
+                return _STD _Float_abs(_Xval);
+            }
+
+            if (_STD isinf(_Yval)) {
+                return _STD _Float_abs(_Yval);
+            }
+
+            return _Xval + _Yval;
+        }
+
+        _Ty _Av = _STD _Float_abs(_Xval);
+        _Ty _Bv = _STD _Float_abs(_Yval);
+
+        if (_Av < _Bv) { // ensure that _Bv <= _Av
+            _STD swap(_Av, _Bv);
+        }
+
+        if (_Bv == 0) {
+            return _STD log(_Av);
+        }
+
+        if (_Hypot_leg_tiny<_Ty> < _Av && _Av < _Hypot_leg_huge<_Ty>) { // no overflow or harmful underflow
+            constexpr _Ty _Norm_small = _Ty{0.5};
+            constexpr _Ty _Norm_big   = _Ty{3.0};
+
+            const _Ty _Bv_sqr = _Bv * _Bv;
+            const _Ty _Norm   = _Av * _Av + _Bv_sqr;
+
+            if (_Norm_small < _Norm && _Norm < _Norm_big) { // avoid catastrophic cancellation
+                return _Logp1(_Norm_minus_one(_Av, _Bv)) * _Ty{0.5};
+            } else {
+                return _STD log(_Norm) * _Ty{0.5};
+            }
+        } else { // use 1 1/2 precision to preserve bits
+            constexpr _Ty _Cm = static_cast<_Ty>(22713.0L / 32768.0L);
+            constexpr _Ty _Cl = static_cast<_Ty>(1.4286068203094172321214581765680755e-6L);
+
+            const int _Exponent      = _STD ilogb(_Av);
+            const _Ty _Av_scaled     = _STD scalbn(_Av, -_Exponent);
+            const _Ty _Bv_scaled     = _STD scalbn(_Bv, -_Exponent);
+            const _Ty _Bv_scaled_sqr = _Bv_scaled * _Bv_scaled;
+            const _Ty _Norm_scaled   = _Av_scaled * _Av_scaled + _Bv_scaled_sqr;
+            const _Ty _Real_shifted  = _STD log(_Norm_scaled) * _Ty{0.5};
+            return (_Real_shifted + _Exponent * _Cl) + _Exponent * _Cm;
+        }
+    }
+} // namespace
+
+_EXTERN_C
+_NODISCARD double __stdcall __std_math_log_hypot(const double _Xval, const double _Yval) noexcept {
+    return _Log_hypot(_Xval, _Yval);
+}
+
+_NODISCARD float __stdcall __std_math_log_hypotf(const float _Xval, const float _Yval) noexcept {
+    return _Log_hypot(_Xval, _Yval);
+}
+_END_EXTERN_C
