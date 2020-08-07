2a9273c73152780c3dd32e0ed5a65f3406ae823e
Stephan T. Lavavej <stl@microsoft.com>
2020-08-07 03:02:15 -0700

    Regen.

---
+++ monthly_table.js
@@ -0,0 +1,17 @@
+// Copyright (c) Microsoft Corporation.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+// Generated file - DO NOT EDIT manually!
+const monthly_table = [
+    { date: '2019-10-16', merge_bar: 29, },
+    { date: '2019-11-16', merge_bar: 20, },
+    { date: '2019-12-16', merge_bar: 26, },
+    { date: '2020-01-16', merge_bar: 32, },
+    { date: '2020-02-16', merge_bar: 27, },
+    { date: '2020-03-16', merge_bar: 49, },
+    { date: '2020-04-16', merge_bar: 47, },
+    { date: '2020-05-16', merge_bar: 43, },
+    { date: '2020-06-16', merge_bar: 29, },
+    { date: '2020-07-16', merge_bar: 65, },
+];
+// Generated file - DO NOT EDIT manually!
