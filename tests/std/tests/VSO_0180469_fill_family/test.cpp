5211954ef306fe6f7073c77a4a6acebe82d6214f
Adam Bucior <35536269+AdamBucior@users.noreply.github.com>
2020-08-12 13:58:04 +0200

    Simple test case for #1183

--- tests/std/tests/VSO_0180469_fill_family/test.cpp
+++ tests/std/tests/VSO_0180469_fill_family/test.cpp
@@ -20,7 +20,7 @@ using namespace std;
 // This thing is a workaround for C4309 "truncation of constant value"
 template <typename T, typename U>
 T cast(U i) {
-    return static_cast<T>(i);
+    return static_cast<remove_volatile_t<T>>(i);
 }
 
 // Tests that `fillCall`(buffer, value, startIndex, endIndex) fills [startIndex, endIndex) with `value`
@@ -130,6 +130,8 @@ int main() {
     test_fill<int, int>();
     test_fill<int, char>();
     test_fill<char, int>();
+
+    test_fill<volatile char, volatile char>();
 
     test_uninitialized_fill(
         [](count_copies* buff, size_t n, const count_copies& src) { uninitialized_fill(buff, buff + n, src); });
