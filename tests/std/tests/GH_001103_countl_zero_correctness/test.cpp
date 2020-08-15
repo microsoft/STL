71173c1621e964e5e547b2bc8d993d6545084959
Alex Guteniev <gutenev@gmail.com>
2020-08-15 14:47:10 +0300

    Apply suggestions from code review
    STL suggestions
    
    Co-authored-by: Stephan T. Lavavej <stl@nuwen.net>

--- tests/std/tests/GH_001103_countl_zero_correctness/test.cpp
+++ tests/std/tests/GH_001103_countl_zero_correctness/test.cpp
@@ -4,13 +4,13 @@
 #include <bit>
 #include <cassert>
 
-// Indirectly test countl_zero on old x86/x64 processors by testing private hepler,
+// Indirectly test countl_zero on old x86/x64 processors by testing a private helper,
 // which is different from the usual branch.
 
 // Since the fallback intrinsic is available on ARM too, don't need to exclude ARM
 
-// Currently need this test only in C++20 mode,
-// May update to older C++ if the helper is used internally too, for example in <bitset>
+// Currently need this test only in C++20 mode;
+// may update to older C++ if the helper is used internally too, for example in <bitset>.
 
 using namespace std;
 
@@ -32,10 +32,10 @@ int main() {
     assert(_Countl_zero_bsr(static_cast<unsigned short>(0x8003)) == 0);
     assert(_Countl_zero_bsr(static_cast<unsigned short>(0xF008)) == 0);
 
-    assert(_Countl_zero_bsr(static_cast<unsigned>(0x0000'0000)) == 32);
-    assert(_Countl_zero_bsr(static_cast<unsigned>(0x0000'0013)) == 27);
-    assert(_Countl_zero_bsr(static_cast<unsigned>(0x8000'0003)) == 0);
-    assert(_Countl_zero_bsr(static_cast<unsigned>(0xF000'0008)) == 0);
+    assert(_Countl_zero_bsr(static_cast<unsigned int>(0x0000'0000)) == 32);
+    assert(_Countl_zero_bsr(static_cast<unsigned int>(0x0000'0013)) == 27);
+    assert(_Countl_zero_bsr(static_cast<unsigned int>(0x8000'0003)) == 0);
+    assert(_Countl_zero_bsr(static_cast<unsigned int>(0xF000'0008)) == 0);
 
     assert(_Countl_zero_bsr(static_cast<unsigned long>(0x0000'0000)) == 32);
     assert(_Countl_zero_bsr(static_cast<unsigned long>(0x0000'0013)) == 27);
