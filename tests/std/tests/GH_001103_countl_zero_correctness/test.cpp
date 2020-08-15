c540f8e41a67f169bb660a21ac073cb21da9c374
Alex Guteniev <gutenev@gmail.com>
2020-08-15 14:52:53 +0300

    char is not unsigned integer, ARM is not x86

--- tests/std/tests/GH_001103_countl_zero_correctness/test.cpp
+++ tests/std/tests/GH_001103_countl_zero_correctness/test.cpp
@@ -7,21 +7,14 @@
 // Indirectly test countl_zero on old x86/x64 processors by testing a private helper,
 // which is different from the usual branch.
 
-// Since the fallback intrinsic is available on ARM too, don't need to exclude ARM
-
 // Currently need this test only in C++20 mode;
 // may update to older C++ if the helper is used internally too, for example in <bitset>.
 
 using namespace std;
 
 int main() {
-#ifdef _CHAR_UNSIGNED
-    assert(_Countl_zero_bsr(static_cast<char>(0x00)) == 8);
-    assert(_Countl_zero_bsr(static_cast<char>(0x13)) == 3);
-    assert(_Countl_zero_bsr(static_cast<char>(0x83)) == 0);
-    assert(_Countl_zero_bsr(static_cast<char>(0xF8)) == 0);
-#endif
-
+    // This test is applicatble only to x86 and x64 platforms
+#if defined(_MIX86) || defined(_M_X64)
     assert(_Countl_zero_bsr(static_cast<unsigned char>(0x00)) == 8);
     assert(_Countl_zero_bsr(static_cast<unsigned char>(0x13)) == 3);
     assert(_Countl_zero_bsr(static_cast<unsigned char>(0x83)) == 0);
@@ -46,6 +39,5 @@ int main() {
     assert(_Countl_zero_bsr(static_cast<unsigned long long>(0x0000'0000'0000'0013)) == 59);
     assert(_Countl_zero_bsr(static_cast<unsigned long long>(0x8000'0000'0000'0003)) == 0);
     assert(_Countl_zero_bsr(static_cast<unsigned long long>(0xF000'0000'0000'0008)) == 0);
-
-    return 0;
+#endif // ^^^ defined(_MIX86) || defined(_M_X64) ^^^
 }
