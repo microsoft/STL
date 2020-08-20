13e3d5dac51cda35ed0347ad0975522ba0d3e774
Stephan T. Lavavej <stl@microsoft.com>
2020-08-20 13:05:37 -0700

    Add braces and newlines.

--- tests/std/tests/P0912R5_coroutine/test.cpp
+++ tests/std/tests/P0912R5_coroutine/test.cpp
@@ -19,20 +19,27 @@ struct task {
                 bool await_ready() noexcept {
                     return false;
                 }
+
                 void await_resume() noexcept {}
+
                 std::coroutine_handle<> await_suspend(std::coroutine_handle<promise_type> h) {
                     // If there is no previous coroutine to resume we've reached the outermost coroutine.
                     // Return a noop coroutine to allow control to return back to the caller.
-                    if (!h.promise().prev)
+                    if (!h.promise().prev) {
                         return std::noop_coroutine();
+                    }
+
                     return h.promise().prev; // resume awaiting coroutine
                 }
             };
+
             return Awaiter{};
         }
+
         void return_value(const int v) {
             result = v;
         }
+
         void unhandled_exception() noexcept {
             std::terminate();
         }
@@ -41,9 +48,11 @@ struct task {
     bool await_ready() const noexcept {
         return false;
     }
+
     int await_resume() {
         return coro.promise().result;
     }
+
     auto await_suspend(std::coroutine_handle<> enclosing) {
         coro.promise().prev = enclosing;
         return coro; // resume ourselves.
@@ -52,20 +61,24 @@ struct task {
     task(task&& rhs) noexcept : coro(rhs.coro) {
         rhs.coro = nullptr;
     }
+
     task(task const&) = delete;
+
     task(promise_type& p) : coro(std::coroutine_handle<promise_type>::from_promise(p)) {}
 
     ~task() {
-        if (coro)
+        if (coro) {
             coro.destroy();
+        }
     }
 
     std::coroutine_handle<promise_type> coro;
 };
 
 task f(int n) {
-    if (n == 0)
+    if (n == 0) {
         co_return 0;
+    }
 
     co_return 1 + co_await f(n - 1);
 }
@@ -73,9 +86,11 @@ task f(int n) {
 int main() {
     task t                    = f(10);
     std::coroutine_handle<> h = t.coro;
+
     if (h != t.coro) {
         return 1;
     }
+
     if (!h || h.done()) {
         return 1;
     }
