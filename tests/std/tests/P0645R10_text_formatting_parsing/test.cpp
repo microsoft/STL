7c61ccc87e4d260bf9896ee07d4d5b42da880c3a
Charles Barto <barto.charlie@gmail.com>
2020-08-25 17:28:31 -0700

    tests for arg_id, quite basic right now

--- tests/std/tests/P0645R10_text_formatting_parsing/test.cpp
+++ tests/std/tests/P0645R10_text_formatting_parsing/test.cpp
@@ -10,7 +10,7 @@ using namespace std::string_view_literals;
 
 template <typename _CharT>
 struct testing_callbacks {
-    _Align expected_alignment;
+    _Align expected_alignment = _Align::_None;
     basic_string_view<_CharT> expected_fill;
     constexpr void _On_align(_Align _Aln) {
         assert(_Aln == expected_alignment);
@@ -18,9 +18,23 @@ struct testing_callbacks {
     constexpr void _On_fill(basic_string_view<_CharT> _Sv) {
         assert(_Sv == expected_fill);
     }
+    constexpr void _On_width(int) {
+        assert(false);
+    }
+    constexpr void _On_dynamic_width(int) {
+        assert(false);
+    }
+    constexpr void _On_dynamic_width(_Auto_id_tag) {
+        assert(false);
+    }
 };
 template <typename _CharT>
 testing_callbacks(_Align, basic_string_view<_CharT>) -> testing_callbacks<_CharT>;
+
+struct testing_arg_id_callbacks {
+    constexpr void _On_auto_id() {}
+    constexpr void _On_manual_id(int) {}
+};
 
 constexpr bool test_parse_align() {
     auto s0 = ""sv;
@@ -35,8 +49,50 @@ constexpr bool test_parse_align() {
     return true;
 }
 
+bool test_parse_arg_id() {
+    // note that parse arg id starts with the arg id itself, not the { beginning of the
+    // format spec
+    auto s0 = "}"sv;
+    auto s1 = ":"sv;
+    auto s2 = ":}"sv;
+    auto s3 = "0:}"sv;
+    auto s4 = "0:"sv;
+    auto s5 = "1}"sv;
+    auto i0 = "01}"sv;
+    auto i1 = "0"sv;
+
+    const char* end = nullptr;
+
+    end = _Parse_arg_id(s0.data(), s0.data() + s0.size(), testing_arg_id_callbacks{});
+    assert(end == &s0[0]);
+    end = _Parse_arg_id(s1.data(), s1.data() + s1.size(), testing_arg_id_callbacks{});
+    assert(end == &s1[0]);
+    end = _Parse_arg_id(s2.data(), s2.data() + s2.size(), testing_arg_id_callbacks{});
+    assert(end == &s2[0]);
+    end = _Parse_arg_id(s3.data(), s3.data() + s3.size(), testing_arg_id_callbacks{});
+    assert(end == &s3[1]);
+    end = _Parse_arg_id(s4.data(), s4.data() + s4.size(), testing_arg_id_callbacks{});
+    assert(end == &s4[1]);
+    end = _Parse_arg_id(s5.data(), s5.data() + s5.size(), testing_arg_id_callbacks{});
+    assert(end == &s5[1]);
+    try {
+        _Parse_arg_id(i0.data(), i0.data() + i0.size(), testing_arg_id_callbacks{});
+        assert(false);
+    } catch (const format_error&) {
+        assert(true);
+    }
+    try {
+        _Parse_arg_id(i1.data(), i1.data() + i1.size(), testing_arg_id_callbacks{});
+        assert(false);
+    } catch (const format_error&) {
+        assert(true);
+    }
+    return true;
+}
+
 int main() {
     test_parse_align();
     static_assert(test_parse_align());
+    test_parse_arg_id();
     return 0;
 }
