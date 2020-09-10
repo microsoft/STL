14f0b50e877a86c9a5e40d2f6864552f482e65b1
Charles Barto <barto.charlie@gmail.com>
2020-09-10 13:40:40 -0700

    tests for non-parsing is todo

--- tests/std/tests/P0645R10_text_formatting_parse_contexts/test.cpp
+++ tests/std/tests/P0645R10_text_formatting_parse_contexts/test.cpp
@@ -5,16 +5,8 @@
 #include <format>
 #include <string_view>
 
-// smoke tests
-// void fmt_args() {
-//     using namespace std;
-//     using test_no_named_args_type = _Format_arg_store<void, int, char, int, char*>;
-//     assert(test_no_named_args_type::_Num_args == 4);
-//     assert(test_no_named_args_type::_Num_named_args == 0);
-//     using test_named_args_type = _Format_arg_store<void, int, _Named_arg<char, int>, char*>;
-//     assert(test_named_args_type::_Num_args == 3);
-//     assert(test_named_args_type::_Num_named_args == 1);
-// }
+
+// TODO: fill in tests
 
 int main() {
     return 0;
