154ee95a058fb4a7a2ecd4e3fec60427acc70db3
Michael Schellenberger Costa <mschellenbergercosta@googlemail.com>
2020-09-16 15:41:27 +0200

    Expand uninitialized_copy test for possibly throwing copy construction

--- tests/std/tests/P0896R4_ranges_alg_uninitialized_copy/test.cpp
+++ tests/std/tests/P0896R4_ranges_alg_uninitialized_copy/test.cpp
@@ -26,6 +26,10 @@ STATIC_ASSERT(same_as<decltype(ranges::uninitialized_copy(borrowed<true>{}, borr
 STATIC_ASSERT(same_as<decltype(ranges::uninitialized_copy(borrowed<true>{}, borrowed<true>{})),
     ranges::uninitialized_copy_result<int*, int*>>);
 
+
+enum class CanThrowAtCopyConstruction : bool { no, yes };
+
+template <CanThrowAtCopyConstruction CanThrow>
 struct int_wrapper {
     inline static int constructions = 0;
     inline static int destructions  = 0;
@@ -45,9 +49,11 @@ struct int_wrapper {
         ++constructions;
     }
 
-    int_wrapper(const int_wrapper& that) {
-        if (that.val == magic_throwing_val) {
-            throw magic_throwing_val;
+    int_wrapper(const int_wrapper& that) noexcept(!static_cast<bool>(CanThrow)) {
+        if constexpr (CanThrow == CanThrowAtCopyConstruction::yes) {
+            if (that.val == magic_throwing_val) {
+                throw magic_throwing_val;
+            }
         }
 
         val = that.val;
@@ -59,11 +65,8 @@ struct int_wrapper {
     }
 
     int_wrapper& operator=(const int_wrapper& that) {
-        if (that.val == magic_throwing_val) {
-            throw magic_throwing_val;
-        }
-        val = that.val;
-        return *this;
+        // Shall never be used as we construct in place
+        throw magic_throwing_val;
     }
 
     auto operator<=>(const int_wrapper&) const = default;
@@ -86,6 +89,7 @@ void not_ranges_destroy(R&& r) { // TRANSITION, ranges::destroy
     }
 }
 
+template <CanThrowAtCopyConstruction CanThrow>
 struct instantiator {
     static constexpr int expected_output[] = {13, 55, 12345};
     static constexpr int expected_input[]  = {13, 55, 12345};
@@ -94,87 +98,101 @@ struct instantiator {
     static void call() {
         using ranges::uninitialized_copy, ranges::uninitialized_copy_result, ranges::equal, ranges::equal_to,
             ranges::iterator_t;
+        using wrapper = int_wrapper<CanThrow>;
 
         { // Validate range overload
-            int_wrapper input[3] = {13, 55, 12345};
+            wrapper input[3] = {13, 55, 12345};
             R wrapped_input{input};
-            holder<int_wrapper, 3> mem;
+            holder<wrapper, 3> mem;
             W wrapped_output{mem.as_span()};
 
-            int_wrapper::clear_counts();
+            wrapper::clear_counts();
             const same_as<uninitialized_copy_result<iterator_t<R>, iterator_t<W>>> auto result =
                 uninitialized_copy(wrapped_input, wrapped_output);
-            assert(int_wrapper::constructions == 3);
-            assert(int_wrapper::destructions == 0);
+            assert(wrapper::constructions == 3);
+            assert(wrapper::destructions == 0);
             assert(result.in == wrapped_input.end());
             assert(result.out == wrapped_output.end());
-            assert(equal(wrapped_output, expected_output, equal_to{}, &int_wrapper::val));
-            assert(equal(input, expected_input, equal_to{}, &int_wrapper::val));
+            assert(equal(wrapped_output, expected_output, equal_to{}, &wrapper::val));
+            assert(equal(input, expected_input, equal_to{}, &wrapper::val));
             not_ranges_destroy(wrapped_output);
-            assert(int_wrapper::constructions == 3);
-            assert(int_wrapper::destructions == 3);
+            assert(wrapper::constructions == 3);
+            assert(wrapper::destructions == 3);
         }
 
         { // Validate iterator overload
-            int_wrapper input[3] = {13, 55, 12345};
+            wrapper input[3] = {13, 55, 12345};
             R wrapped_input{input};
-            holder<int_wrapper, 3> mem;
+            holder<wrapper, 3> mem;
             W wrapped_output{mem.as_span()};
 
-            int_wrapper::clear_counts();
+            wrapper::clear_counts();
             const same_as<uninitialized_copy_result<iterator_t<R>, iterator_t<W>>> auto result = uninitialized_copy(
                 wrapped_input.begin(), wrapped_input.end(), wrapped_output.begin(), wrapped_output.end());
-            assert(int_wrapper::constructions == 3);
-            assert(int_wrapper::destructions == 0);
+            assert(wrapper::constructions == 3);
+            assert(wrapper::destructions == 0);
             assert(result.in == wrapped_input.end());
             assert(result.out == wrapped_output.end());
-            assert(equal(wrapped_output, expected_output, equal_to{}, &int_wrapper::val));
-            assert(equal(input, expected_input, equal_to{}, &int_wrapper::val));
+            assert(equal(wrapped_output, expected_output, equal_to{}, &wrapper::val));
+            assert(equal(input, expected_input, equal_to{}, &wrapper::val));
             not_ranges_destroy(wrapped_output);
-            assert(int_wrapper::constructions == 3);
-            assert(int_wrapper::destructions == 3);
+            assert(wrapper::constructions == 3);
+            assert(wrapper::destructions == 3);
         }
     }
 };
 
 struct throwing_test {
-    static constexpr int expected_input[] = {13, 55, int_wrapper::magic_throwing_val, 12345};
+    using wrapper                         = int_wrapper<CanThrowAtCopyConstruction::yes>;
+    static constexpr int expected_input[] = {13, 55, wrapper::magic_throwing_val, 12345};
 
     template <ranges::input_range R, ranges::forward_range W>
     static void call() {
+
         // Validate only range overload (one is plenty since they both use the same backend)
-        int_wrapper input[] = {13, 55, int_wrapper::magic_throwing_val, 12345};
+        wrapper input[] = {13, 55, wrapper::magic_throwing_val, 12345};
         R wrapped_input{input};
-        holder<int_wrapper, 4> mem;
+        holder<wrapper, 4> mem;
         W wrapped_output{mem.as_span()};
 
-        int_wrapper::clear_counts();
+        wrapper::clear_counts();
         try {
             (void) ranges::uninitialized_copy(wrapped_input, wrapped_output);
             assert(false);
         } catch (int i) {
-            assert(i == int_wrapper::magic_throwing_val);
+            assert(i == wrapper::magic_throwing_val);
         } catch (...) {
             assert(false);
         }
-        assert(int_wrapper::constructions == 2);
-        assert(int_wrapper::destructions == 2);
-        assert(ranges::equal(input, expected_input, ranges::equal_to{}, &int_wrapper::val));
+        assert(wrapper::constructions == 2);
+        assert(wrapper::destructions == 2);
+        assert(ranges::equal(input, expected_input, ranges::equal_to{}, &wrapper::val));
     }
 };
 
-template <test::ProxyRef IsProxy>
-using test_input  = test::range<test::input, int_wrapper, test::Sized::no, test::CanDifference::no, test::Common::no,
-    test::CanCompare::yes, IsProxy>;
-using test_output = test::range<test::fwd, int_wrapper, test::Sized::no, test::CanDifference::no, test::Common::no,
-    test::CanCompare::yes, test::ProxyRef::no>;
+template <test::ProxyRef IsProxy, CanThrowAtCopyConstruction CanThrow>
+using test_input = test::range<test::input, int_wrapper<CanThrow>, test::Sized::no, test::CanDifference::no,
+    test::Common::no, test::CanCompare::yes, IsProxy>;
+template <CanThrowAtCopyConstruction CanThrow>
+using test_output = test::range<test::fwd, int_wrapper<CanThrow>, test::Sized::no, test::CanDifference::no,
+    test::Common::no, test::CanCompare::yes, test::ProxyRef::no>;
 
 int main() {
     // The algorithm is oblivious to non-required category, size, difference, and "proxyness" of the input range. It
     // _is_ sensitive to proxyness in that it requires non-proxy references for the output range.
 
-    instantiator::call<test_input<test::ProxyRef::no>, test_output>();
-    instantiator::call<test_input<test::ProxyRef::yes>, test_output>();
-    throwing_test::call<test_input<test::ProxyRef::no>, test_output>();
-    throwing_test::call<test_input<test::ProxyRef::yes>, test_output>();
+    instantiator<CanThrowAtCopyConstruction::yes>::call<test_input<test::ProxyRef::no, CanThrowAtCopyConstruction::yes>,
+        test_output<CanThrowAtCopyConstruction::yes>>();
+    instantiator<CanThrowAtCopyConstruction::yes>::call<
+        test_input<test::ProxyRef::yes, CanThrowAtCopyConstruction::yes>,
+        test_output<CanThrowAtCopyConstruction::yes>>();
+    instantiator<CanThrowAtCopyConstruction::no>::call<test_input<test::ProxyRef::no, CanThrowAtCopyConstruction::no>,
+        test_output<CanThrowAtCopyConstruction::no>>();
+    instantiator<CanThrowAtCopyConstruction::no>::call<test_input<test::ProxyRef::yes, CanThrowAtCopyConstruction::no>,
+        test_output<CanThrowAtCopyConstruction::no>>();
+
+    throwing_test::call<test_input<test::ProxyRef::no, CanThrowAtCopyConstruction::yes>,
+        test_output<CanThrowAtCopyConstruction::yes>>();
+    throwing_test::call<test_input<test::ProxyRef::yes, CanThrowAtCopyConstruction::yes>,
+        test_output<CanThrowAtCopyConstruction::yes>>();
 }
