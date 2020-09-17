cef68fc4e17d940301960616803f4da9275055ca
Casey Carter <Casey@Carter.net>
2020-09-16 21:51:20 -0700

    Review comments

--- tests/std/tests/P0896R4_stream_iterators/test.cpp
+++ tests/std/tests/P0896R4_stream_iterators/test.cpp
@@ -4,6 +4,7 @@
 // Covers Ranges changes to istream_iterator, ostream_iterator, istreambuf_iterator, and ostreambuf_iterator
 
 #include <cassert>
+#include <cstddef>
 #include <iostream>
 #include <iterator>
 #include <string>
@@ -15,7 +16,7 @@ using namespace std;
 #define STATIC_ASSERT(...) static_assert(__VA_ARGS__, #__VA_ARGS__)
 
 template <class T, class CharT = char, class Traits = char_traits<CharT>, class Diff = ptrdiff_t>
-void test_istream() {
+void test_istream_iterator() {
     using I = istream_iterator<T, CharT, Traits, Diff>;
 
     // Also test strengthened noexcept on some pre-existing operations
@@ -49,7 +50,7 @@ void test_istream() {
 }
 
 template <class T, class CharT = char, class Traits = char_traits<CharT>>
-void test_ostream(basic_ostream<CharT, Traits>& os) {
+void test_ostream_iterator(basic_ostream<CharT, Traits>& os) {
     using I = ostream_iterator<T, CharT, Traits>;
 
     // Also tests strengthened noexcept on some pre-existing operations
@@ -70,7 +71,7 @@ void test_ostream(basic_ostream<CharT, Traits>& os) {
 }
 
 template <class CharT = char, class Traits = char_traits<CharT>>
-void test_istreambuf() {
+void test_istreambuf_iterator() {
 #ifdef __cpp_lib_concepts
     using I = istreambuf_iterator<CharT, Traits>;
     STATIC_ASSERT(input_iterator<I>);
@@ -88,7 +89,7 @@ void test_istreambuf() {
 }
 
 template <class CharT = char, class Traits = char_traits<CharT>>
-void test_ostreambuf(basic_ostream<CharT, Traits>& os) {
+void test_ostreambuf_iterator(basic_ostream<CharT, Traits>& os) {
     using I = ostreambuf_iterator<CharT, Traits>;
 
     // Also tests strengthened noexcept on some pre-existing operations
@@ -107,19 +108,19 @@ void test_ostreambuf(basic_ostream<CharT, Traits>& os) {
 }
 
 int main() {
-    test_istream<int>();
-    test_istream<string>();
-    test_istream<int, wchar_t>();
-    test_istream<string, wchar_t>();
+    test_istream_iterator<int>();
+    test_istream_iterator<string>();
+    test_istream_iterator<int, wchar_t>();
+    test_istream_iterator<string, wchar_t>();
 
-    test_ostream<int>(cout);
-    test_ostream<string>(cout);
-    test_ostream<int>(wcout);
-    test_ostream<string>(wcout);
+    test_ostream_iterator<int>(cout);
+    test_ostream_iterator<string>(cout);
+    test_ostream_iterator<int>(wcout);
+    test_ostream_iterator<string>(wcout);
 
-    test_istreambuf<char>();
-    test_istreambuf<wchar_t>();
+    test_istreambuf_iterator<char>();
+    test_istreambuf_iterator<wchar_t>();
 
-    test_ostreambuf(cout);
-    test_ostreambuf(wcout);
+    test_ostreambuf_iterator(cout);
+    test_ostreambuf_iterator(wcout);
 }
