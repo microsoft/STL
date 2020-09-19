fe96892d6d7dd1169f9973d6b5a50a166bc92f9c
Stephan T. Lavavej <stl@microsoft.com>
2020-09-19 00:58:09 -0700

    Centralize constexpr testing, and test constexpr modification.

--- tests/std/tests/P1007R3_assume_aligned/test.cpp
+++ tests/std/tests/P1007R3_assume_aligned/test.cpp
@@ -5,13 +5,13 @@
 #include <memory>
 #include <stddef.h>
 
-void can_change_value_via_assume_aligned(int* p) {
+constexpr void can_change_value_via_assume_aligned(int* p) {
     int* p1 = std::assume_aligned<256>(p);
     p1[0]   = 10;
     p1[1]   = -15;
 }
 
-constexpr int can_read_constexpr_value_via_assume_aligned_and_sum_it(int const* p, size_t size) {
+constexpr int can_read_value_via_assume_aligned_and_sum_it(int const* p, size_t size) {
     int const* p1 = std::assume_aligned<256>(p);
     int result    = 0;
     for (size_t i = 0; i < size; ++i) {
@@ -20,25 +20,24 @@ constexpr int can_read_constexpr_value_via_assume_aligned_and_sum_it(int const* 
     return result;
 }
 
-void assume_aligned_can_change_value() {
+constexpr bool assume_aligned_can_change_value() {
     alignas(256) int arr[2] = {0, 0};
     can_change_value_via_assume_aligned(&arr[0]);
     assert(arr[0] == 10);
     assert(arr[1] == -15);
+    return true;
 }
 
-constexpr void constexpr_assume_aligned_can_read_value() {
+constexpr bool assume_aligned_can_read_value() {
     alignas(256) constexpr int arr[3] = {10, 11, 9};
-    static_assert(can_read_constexpr_value_via_assume_aligned_and_sum_it(arr, 3) == 30);
-}
-
-void assume_aligned_can_read_value() {
-    alignas(256) constexpr int arr[3] = {10, 11, 9};
-    assert(can_read_constexpr_value_via_assume_aligned_and_sum_it(arr, 3) == 30);
+    assert(can_read_value_via_assume_aligned_and_sum_it(arr, 3) == 30);
+    return true;
 }
 
 int main() {
     assume_aligned_can_change_value();
-    constexpr_assume_aligned_can_read_value();
+    static_assert(assume_aligned_can_change_value());
+
     assume_aligned_can_read_value();
+    static_assert(assume_aligned_can_read_value());
 }
