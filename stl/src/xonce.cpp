1834d17e5f90bf71d7bfb1eda773b6b69528264c
Jordan Rabet <jorab@microsoft.com>
2020-09-24 17:09:24 -0700

    XFG: made _Execute_once XFG-compatible.

--- stl/src/xonce.cpp
+++ stl/src/xonce.cpp
@@ -13,8 +13,17 @@ _CRTIMP2_PURE int __CLRCALL_PURE_OR_CDECL _Execute_once(
     once_flag& _Flag, _Execute_once_fp_t _Callback, void* _Pv) noexcept { // wrap Win32 InitOnceExecuteOnce()
     static_assert(sizeof(_Flag._Opaque) == sizeof(INIT_ONCE), "invalid size");
 
+    // _Execute_once_fp_t and PINIT_ONCE_FN differ in type signature, therefore
+    // we introduce _Xfg_trampoline which has PINIT_ONCE_FN's type signature and
+    // calls into _Callback as an _Execute_once_fp_t for XFG compatibility.
+
+    PINIT_ONCE_FN _Xfg_trampoline = [](PINIT_ONCE _InitOnce, PVOID _Parameter, PVOID* _Context) {
+        const auto _Callback = reinterpret_cast<_Execute_once_fp_t>(_Context);
+        return static_cast<BOOL>(_Callback(_InitOnce, _Parameter, nullptr));
+    };
+
     return __crtInitOnceExecuteOnce(
-        reinterpret_cast<PINIT_ONCE>(&_Flag._Opaque), reinterpret_cast<PINIT_ONCE_FN>(_Callback), _Pv, nullptr);
+        reinterpret_cast<PINIT_ONCE>(&_Flag._Opaque), _Xfg_trampoline, _Pv, reinterpret_cast<PVOID*>(_Callback));
 }
 
 [[noreturn]] _CRTIMP2_PURE void __CLRCALL_PURE_OR_CDECL
