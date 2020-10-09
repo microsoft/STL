9bd8085c98fc9943def745c0ca4c87ac6a35d9de
Casey Carter <Casey@Carter.net>
2020-10-08 21:45:52 -0700

    Add _CONTAINER_DEBUG_LEVEL checks for optional
    When `_CONTAINER_DEBUG_LEVEL` is `1` (which is implied by `_ITERATOR_DEBUG_LEVEL != 0`) `optional::operator*` and `optional::operator->` verify the precondition that the `optional` is not empty.
    
    Drive-by: Add a default constructor to `death_test_executive` so we need not construct with a do-nothing function when there are no "normal" tests.

--- tests/std/tests/P0896R4_common_iterator_death/test.cpp
+++ tests/std/tests/P0896R4_common_iterator_death/test.cpp
@@ -156,7 +156,7 @@ void test_case_iter_swap_sentinel_right_valueless() {
 }
 
 int main(int argc, char* argv[]) {
-    std_testing::death_test_executive exec([] {});
+    std_testing::death_test_executive exec;
 
 #if _ITERATOR_DEBUG_LEVEL != 0
     exec.add_death_tests({
