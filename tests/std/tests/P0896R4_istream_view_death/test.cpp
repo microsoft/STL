c617e60f9a49e815b225eae0987614ba4738e93f
Casey Carter <cacarter@microsoft.com>
2020-10-12 22:25:57 -0700

    default construct death_test_executive

--- tests/std/tests/P0896R4_istream_view_death/test.cpp
+++ tests/std/tests/P0896R4_istream_view_death/test.cpp
@@ -75,7 +75,7 @@ void test_dereference_end_of_stream() {
 }
 
 int main(int argc, char* argv[]) {
-    std_testing::death_test_executive exec([] {});
+    std_testing::death_test_executive exec;
 
 #if _ITERATOR_DEBUG_LEVEL != 0
     exec.add_death_tests({
