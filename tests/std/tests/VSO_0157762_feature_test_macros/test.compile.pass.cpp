8b158005e830a737682860a2a87d497a6ef88eb4
Michael S. Rizkalla <Michael.Anis1995@gmail.com>
2020-10-15 16:52:24 +0100

    Update tests/std/tests/VSO_0157762_feature_test_macros/test.cpp
    Apply review suggestion
    
    Co-authored-by: Adam Bucior <35536269+AdamBucior@users.noreply.github.com>

--- tests/std/tests/VSO_0157762_feature_test_macros/test.compile.pass.cpp
+++ tests/std/tests/VSO_0157762_feature_test_macros/test.compile.pass.cpp
@@ -411,7 +411,7 @@ STATIC_ASSERT(__cpp_lib_constexpr_complex == 201711L);
 #endif
 #endif
 
-#if _HAS_CXX20
+#if _HAS_CXX20 && defined(__cpp_constexpr_dynamic_alloc)
 #ifndef __cpp_lib_constexpr_dynamic_alloc
 #error __cpp_lib_constexpr_dynamic_alloc is not defined
 #elif __cpp_lib_constexpr_dynamic_alloc != 201907L
