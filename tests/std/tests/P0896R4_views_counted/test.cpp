69b325dbb63e2e4a9d3f183015162358c3c8f0a9
Michael Schellenberger Costa <mschellenbergercosta@googlemail.com>
2020-10-15 13:40:19 +0200

    Test using output iterators

--- tests/std/tests/P0896R4_views_counted/test.cpp
+++ tests/std/tests/P0896R4_views_counted/test.cpp
@@ -12,31 +12,31 @@
 using namespace std;
 
 struct instantiator {
-    static constexpr int input[]    = {13, 42, 1729, -1, -1};
     static constexpr int expected[] = {13, 42, 1729};
 
-    template <ranges::input_range Read>
+    template <input_or_output_iterator Iter>
     static constexpr void call() {
         using ranges::contiguous_range, ranges::equal, ranges::iterator_t, ranges::random_access_range, ranges::size,
             ranges::subrange;
-        using ReadIt    = iterator_t<Read>;
-        using ReadValue = remove_reference_t<iter_reference_t<ReadIt>>;
-        Read wrapped_input{input};
+        using IterValue = remove_reference_t<iter_reference_t<Iter>>;
+        int input[]     = {13, 42, 1729, -1, -1};
 
-        auto result = ranges::views::counted(wrapped_input.begin(), 3);
-        if constexpr (contiguous_range<Read>) {
-            STATIC_ASSERT(same_as<decltype(result), span<ReadValue, dynamic_extent>>);
-        } else if constexpr (random_access_range<Read>) {
-            STATIC_ASSERT(same_as<decltype(result), subrange<ReadIt, ReadIt>>);
+        auto result = ranges::views::counted(Iter{input}, 3);
+        if constexpr (contiguous_iterator<Iter>) {
+            STATIC_ASSERT(same_as<decltype(result), span<IterValue, dynamic_extent>>);
+        } else if constexpr (random_access_iterator<Iter>) {
+            STATIC_ASSERT(same_as<decltype(result), subrange<Iter, Iter>>);
         } else {
-            STATIC_ASSERT(same_as<decltype(result), subrange<counted_iterator<ReadIt>, default_sentinel_t>>);
+            STATIC_ASSERT(same_as<decltype(result), subrange<counted_iterator<Iter>, default_sentinel_t>>);
         }
         assert(size(result) == 3);
-        assert(equal(result, expected));
+        if constexpr (input_iterator<Iter>) {
+            assert(equal(result, expected));
+        }
     }
 };
 
 int main() {
-    STATIC_ASSERT((test_in<instantiator, const int>(), true));
-    test_in<instantiator, const int>();
+    STATIC_ASSERT((with_writable_iterators<instantiator, int>::call(), true));
+    with_writable_iterators<instantiator, int>::call();
 }
