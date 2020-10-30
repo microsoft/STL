3750f0fadd5c7c80b373f5ffa6916d0d6768cc74
Casey Carter <cacarter@microsoft.com>
2020-10-30 13:11:22 -0700

    Cleanup
    product code:
    * Define `iterator_concept` so the view can adapt ranges whose iterators have no `iterator_category` (This is how we've dealt with LWG-3289 elsewhere.)
    
    * Don't try to modify members of constant iterators
    
    * `_Sentinel`'s friends are not friends of `_Iterator`
    
    test:
    * Silence shadowing warnings by using the global `expected_keys` and `expected_values` instead of passing them to `test_one` via parameters with the same names.
    
    * Replace references to `EV` with `R` since they name the same type
    
    * Don't `forward` the same rvalue input range repeatedly

--- tests/std/tests/P0896R4_views_elements/test.cpp
+++ tests/std/tests/P0896R4_views_elements/test.cpp
@@ -24,20 +24,24 @@ concept CanViewElements = requires(Rng&& r) {
     views::elements<0>(static_cast<Rng&&>(r));
 };
 
-template <ranges::input_range Rng, ranges::random_access_range Expected>
-constexpr bool test_one(Rng&& rng, Expected&& expected_keys, Expected&& expected_values) {
+constexpr P some_pairs[]        = {{0, -1}, {1, -2}, {2, -3}, {3, -4}, {4, -5}, {5, -6}, {6, -7}, {7, -8}};
+constexpr int expected_keys[]   = {0, 1, 2, 3, 4, 5, 6, 7};
+constexpr int expected_values[] = {-1, -2, -3, -4, -5, -6, -7, -8};
+
+template <ranges::input_range Rng>
+constexpr bool test_one(Rng&& rng) {
     using ranges::elements_view, ranges::bidirectional_range, ranges::common_range, ranges::contiguous_range,
         ranges::enable_borrowed_range, ranges::forward_range, ranges::input_range, ranges::iterator_t, ranges::prev,
         ranges::random_access_range, ranges::range, ranges::range_reference_t, ranges::sentinel_t;
 
-    using V  = views::all_t<Rng>;
-    using EV = elements_view<V, 0>;
-    STATIC_ASSERT(ranges::view<EV>);
-    STATIC_ASSERT(input_range<EV>);
-    STATIC_ASSERT(forward_range<EV> == forward_range<Rng>);
-    STATIC_ASSERT(bidirectional_range<EV> == bidirectional_range<Rng>);
-    STATIC_ASSERT(random_access_range<EV> == random_access_range<Rng>);
-    STATIC_ASSERT(!contiguous_range<EV>);
+    using V = views::all_t<Rng>;
+    using R = elements_view<V, 0>;
+    STATIC_ASSERT(ranges::view<R>);
+    STATIC_ASSERT(input_range<R>);
+    STATIC_ASSERT(forward_range<R> == forward_range<Rng>);
+    STATIC_ASSERT(bidirectional_range<R> == bidirectional_range<Rng>);
+    STATIC_ASSERT(random_access_range<R> == random_access_range<Rng>);
+    STATIC_ASSERT(!contiguous_range<R>);
 
     // ... with lvalue argument
     STATIC_ASSERT(CanViewElements<Rng&>);
@@ -45,10 +49,10 @@ constexpr bool test_one(Rng&& rng, Expected&& expected_keys, Expected&& expected
     if constexpr (CanViewElements<Rng&>) { // Validate lvalue
         constexpr bool is_noexcept = !is_view || is_nothrow_copy_constructible_v<V>;
 
-        STATIC_ASSERT(same_as<decltype(views::elements<0>(rng)), EV>);
+        STATIC_ASSERT(same_as<decltype(views::elements<0>(rng)), R>);
         STATIC_ASSERT(noexcept(views::elements<0>(rng)) == is_noexcept);
 
-        STATIC_ASSERT(same_as<decltype(rng | views::elements<0>), EV>);
+        STATIC_ASSERT(same_as<decltype(rng | views::elements<0>), R>);
         STATIC_ASSERT(noexcept(rng | views::elements<0>) == is_noexcept);
 
         STATIC_ASSERT(same_as<decltype(rng | pipeline), pipeline_t<Rng&>>);
@@ -60,10 +64,10 @@ constexpr bool test_one(Rng&& rng, Expected&& expected_keys, Expected&& expected
     if constexpr (is_view && copyable<V>) {
         constexpr bool is_noexcept = is_nothrow_copy_constructible_v<V>;
 
-        STATIC_ASSERT(same_as<decltype(views::elements<0>(as_const(rng))), EV>);
+        STATIC_ASSERT(same_as<decltype(views::elements<0>(as_const(rng))), R>);
         STATIC_ASSERT(noexcept(views::elements<0>(as_const(rng))) == is_noexcept);
 
-        STATIC_ASSERT(same_as<decltype(as_const(rng) | views::elements<0>), EV>);
+        STATIC_ASSERT(same_as<decltype(as_const(rng) | views::elements<0>), R>);
         STATIC_ASSERT(noexcept(as_const(rng) | views::elements<0>) == is_noexcept);
 
         STATIC_ASSERT(same_as<decltype(as_const(rng) | pipeline), pipeline_t<const remove_reference_t<Rng>&>>);
@@ -86,10 +90,10 @@ constexpr bool test_one(Rng&& rng, Expected&& expected_keys, Expected&& expected
     STATIC_ASSERT(CanViewElements<remove_reference_t<Rng>> == is_view || enable_borrowed_range<remove_cvref_t<Rng>>);
     if constexpr (is_view) {
         constexpr bool is_noexcept = is_nothrow_move_constructible_v<V>;
-        STATIC_ASSERT(same_as<decltype(views::elements<0>(move(rng))), EV>);
+        STATIC_ASSERT(same_as<decltype(views::elements<0>(move(rng))), R>);
         STATIC_ASSERT(noexcept(views::elements<0>(move(rng))) == is_noexcept);
 
-        STATIC_ASSERT(same_as<decltype(move(rng) | views::elements<0>), EV>);
+        STATIC_ASSERT(same_as<decltype(move(rng) | views::elements<0>), R>);
         STATIC_ASSERT(noexcept(move(rng) | views::elements<0>) == is_noexcept);
 
         STATIC_ASSERT(same_as<decltype(move(rng) | pipeline), pipeline_t<remove_reference_t<Rng>>>);
@@ -115,10 +119,10 @@ constexpr bool test_one(Rng&& rng, Expected&& expected_keys, Expected&& expected
     if constexpr (is_view && copyable<V>) {
         constexpr bool is_noexcept = is_nothrow_copy_constructible_v<V>;
 
-        STATIC_ASSERT(same_as<decltype(views::elements<0>(move(as_const(rng)))), EV>);
+        STATIC_ASSERT(same_as<decltype(views::elements<0>(move(as_const(rng)))), R>);
         STATIC_ASSERT(noexcept(views::elements<0>(move(as_const(rng)))) == is_noexcept);
 
-        STATIC_ASSERT(same_as<decltype(move(as_const(rng)) | views::elements<0>), EV>);
+        STATIC_ASSERT(same_as<decltype(move(as_const(rng)) | views::elements<0>), R>);
         STATIC_ASSERT(noexcept(move(as_const(rng)) | views::elements<0>) == is_noexcept);
 
         STATIC_ASSERT(same_as<decltype(move(as_const(rng)) | pipeline), pipeline_t<const remove_reference_t<Rng>>>);
@@ -138,20 +142,14 @@ constexpr bool test_one(Rng&& rng, Expected&& expected_keys, Expected&& expected
         STATIC_ASSERT(noexcept(move(as_const(rng)) | pipeline) == is_noexcept);
     }
 
-    // Validate deduction guide
-    same_as<EV> auto r = elements_view<views::all_t<Rng>, 0>{forward<Rng>(rng)};
-    using R            = decltype(r);
+    // Validate concepts are properly modeled
+    R r{forward<Rng>(rng)};
     STATIC_ASSERT(ranges::view<R>);
     STATIC_ASSERT(input_range<R>);
     STATIC_ASSERT(forward_range<R> == forward_range<Rng>);
     STATIC_ASSERT(bidirectional_range<R> == bidirectional_range<Rng>);
     STATIC_ASSERT(random_access_range<R> == random_access_range<Rng>);
     STATIC_ASSERT(!contiguous_range<R>);
-
-    // Validate views::keys and views::values
-    using VR            = elements_view<views::all_t<Rng>, 1>;
-    same_as<EV> auto kr = ranges::keys_view<Rng>{forward<Rng>(rng)};
-    same_as<VR> auto vr = ranges::values_view<Rng>{forward<Rng>(rng)};
 
     // Validate elements_view::size
     STATIC_ASSERT(CanMemberSize<R> == CanSize<V>);
@@ -196,7 +194,13 @@ constexpr bool test_one(Rng&& rng, Expected&& expected_keys, Expected&& expected
 
     // Validate content
     assert(ranges::equal(r, expected_keys));
-    assert(ranges::equal(vr, expected_values));
+
+    // Validate views::keys and views::values
+    STATIC_ASSERT(same_as<ranges::keys_view<Rng>, R>);
+    STATIC_ASSERT(same_as<ranges::values_view<Rng>, elements_view<V, 1>>);
+    if constexpr (forward_range<Rng> && is_lvalue_reference_v<Rng>) {
+        assert(ranges::equal(ranges::values_view<Rng>{rng}, expected_values));
+    }
 
     // Validate elements_view::begin
     STATIC_ASSERT(CanMemberBegin<R>);
@@ -243,28 +247,28 @@ constexpr bool test_one(Rng&& rng, Expected&& expected_keys, Expected&& expected
     }
 
     // Validate view_interface::data
-    STATIC_ASSERT(!CanData<EV>);
-    STATIC_ASSERT(!CanData<const EV>);
+    STATIC_ASSERT(!CanData<R>);
+    STATIC_ASSERT(!CanData<const R>);
 
     // Validate view_interface::front and back
     if (!is_empty) {
-        STATIC_ASSERT(CanMemberFront<EV> == forward_range<V>);
-        if constexpr (CanMemberFront<EV>) {
+        STATIC_ASSERT(CanMemberFront<R> == forward_range<V>);
+        if constexpr (CanMemberFront<R>) {
             assert(r.front() == *begin(expected_keys));
         }
 
-        STATIC_ASSERT(CanMemberBack<EV> == (bidirectional_range<V> && common_range<V>) );
-        if constexpr (CanMemberBack<EV>) {
+        STATIC_ASSERT(CanMemberBack<R> == (bidirectional_range<V> && common_range<V>) );
+        if constexpr (CanMemberBack<R>) {
             assert(r.back() == *prev(end(expected_keys)));
         }
 
-        STATIC_ASSERT(CanMemberFront<const EV> == (forward_range<const V>) );
-        if constexpr (CanMemberFront<const EV>) {
+        STATIC_ASSERT(CanMemberFront<const R> == (forward_range<const V>) );
+        if constexpr (CanMemberFront<const R>) {
             assert(as_const(r).front() == *begin(expected_keys));
         }
 
-        STATIC_ASSERT(CanMemberBack<const EV> == (bidirectional_range<const V> && common_range<const V>) );
-        if constexpr (CanMemberBack<const EV>) {
+        STATIC_ASSERT(CanMemberBack<const R> == (bidirectional_range<const V> && common_range<const V>) );
+        if constexpr (CanMemberBack<const R>) {
             assert(as_const(r).back() == *prev(end(expected_keys)));
         }
     }
@@ -283,7 +287,7 @@ constexpr bool test_one(Rng&& rng, Expected&& expected_keys, Expected&& expected
     }
 
     // Validate elements_view::base() const&
-    STATIC_ASSERT(CanMemberBase<const EV&> == copy_constructible<V>);
+    STATIC_ASSERT(CanMemberBase<const R&> == copy_constructible<V>);
     if constexpr (copy_constructible<V> && forward_range<V>) {
         same_as<V> auto b1 = as_const(r).base();
         STATIC_ASSERT(noexcept(as_const(r).base()) == is_nothrow_copy_constructible_v<V>);
@@ -309,15 +313,11 @@ constexpr bool test_one(Rng&& rng, Expected&& expected_keys, Expected&& expected
     return true;
 }
 
-static constexpr P some_pairs[]        = {{0, -1}, {1, -2}, {2, -3}, {3, -4}, {4, -5}, {5, -6}, {6, -7}, {7, -8}};
-static constexpr int expected_keys[]   = {0, 1, 2, 3, 4, 5, 6, 7};
-static constexpr int expected_values[] = {-1, -2, -3, -4, -5, -6, -7, -8};
-
 struct instantiator {
     template <ranges::input_range R>
     static constexpr void call() {
         R r{some_pairs};
-        test_one(r, expected_keys, expected_values);
+        test_one(r);
     }
 };
 
@@ -360,21 +360,21 @@ constexpr void instantiation_test() {
 int main() {
     { // Validate copyable views
         constexpr span<const P> s{some_pairs};
-        STATIC_ASSERT(test_one(s, expected_keys, expected_values));
-        test_one(s, expected_keys, expected_values);
+        STATIC_ASSERT(test_one(s));
+        test_one(s);
     }
 
     { // Validate non-views
-        STATIC_ASSERT(test_one(some_pairs, expected_keys, expected_values));
-        test_one(some_pairs, expected_keys, expected_values);
+        STATIC_ASSERT(test_one(some_pairs));
+        test_one(some_pairs);
 
         {
             vector vec(ranges::begin(some_pairs), ranges::end(some_pairs));
-            test_one(vec, expected_keys, expected_values);
+            test_one(vec);
         }
         {
             forward_list lst(ranges::begin(some_pairs), ranges::end(some_pairs));
-            test_one(lst, expected_keys, expected_values);
+            test_one(lst);
         }
 
         STATIC_ASSERT((instantiation_test(), true));
@@ -383,7 +383,7 @@ int main() {
 
     { // Validate a non-view borrowed range
         constexpr span s{some_pairs};
-        STATIC_ASSERT(test_one(s, expected_keys, expected_values));
-        test_one(s, expected_keys, expected_values);
+        STATIC_ASSERT(test_one(s));
+        test_one(s);
     }
 }
