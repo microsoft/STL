4f3b0fa5e1caf273c80c46677d1ecc7c3be7e63a
Ben Craig <ben.craig@ni.com>
2020-12-18 14:55:12 -0600

    Handle calling convention differences

--- tests/utils/kernel/src/stl_kernel/test_decls.h
+++ tests/utils/kernel/src/stl_kernel/test_decls.h
@@ -8,4 +8,4 @@ extern const wchar_t* const STL_KERNEL_NT_DEVICE_NAME;
 extern const wchar_t* const STL_KERNEL_DOS_DEVICE_NAME;
 extern KGUARDED_MUTEX g_assert_mutex;
 
-extern "C" int main();
+extern "C" int __cdecl main();
