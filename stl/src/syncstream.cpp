6afbbc737a45bf114759ccd6e7f9e895436b4498
MichaelRizkalla <Michael.Anis1995@gmail.com>
2021-01-11 21:45:01 +0000

    Move global variables to syncstream.cpp
    - this commit requires a maintainer's input

---
+++ stl/src/syncstream.cpp
@@ -0,0 +1,26 @@
+// Copyright (c) Microsoft Corporation.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+// initialize syncstream mutex map
+
+#include <syncstream>
+
+#ifdef __cpp_lib_syncbuf
+#pragma warning(disable : 4074)
+#pragma init_seg(compiler)
+
+_STD_BEGIN
+// OBJECT DECLARATIONS
+map<void*, _Mutex_count_pair> _Mutex_map{};
+shared_mutex _Mutex{};
+
+shared_mutex& _Get_map_mutex() {
+    return _Mutex;
+}
+
+map<void*, _Mutex_count_pair>& _Get_mutex_map() {
+    return _Mutex_map;
+}
+
+_STD_END
+#endif
