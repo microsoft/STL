d26d4b362bd6e6c1db3bed21bfa2fff625fbfaa9
Charles <chbarto@microsoft.com>
2021-01-29 17:57:54 -0800

    flesh out format_args_store.

---
+++ tests/std/tests/P0645R10_text_formatting_formatting/test.cpp
@@ -0,0 +1,10 @@
+// Copyright (c) Microsoft Corporation.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+#include <assert.h>
+#include <format>
+#include <string_view>
+
+// TODO: fill in tests
+
+int main() {}
