ac5e58ae29c9211eb60c36638fa54afaf127665a
Charles <chbarto@microsoft.com>
2021-02-02 17:49:38 -0800

    compile vformat (does not link)

--- tests/std/tests/P0645R10_text_formatting_formatting/test.cpp
+++ tests/std/tests/P0645R10_text_formatting_formatting/test.cpp
@@ -3,8 +3,12 @@
 
 #include <assert.h>
 #include <format>
+#include <string>
 #include <string_view>
 
 // TODO: fill in tests
+template std::back_insert_iterator<std::string> std::vformat_to(std::back_insert_iterator<std::string>, const locale&,
+    std::string_view, std::format_args_t<std::back_insert_iterator<std::string>, char>);
+
 
 int main() {}
--- tests/std/tests/P0645R10_text_formatting_parsing/test.cpp
+++ tests/std/tests/P0645R10_text_formatting_parsing/test.cpp
@@ -31,6 +31,22 @@ struct choose_literal<wchar_t> {
 #define TYPED_LITERAL(CharT, Literal) (choose_literal<CharT>::choose(Literal, L##Literal))
 
 template <typename CharT>
+struct noop_testing_callbacks {
+    constexpr void _On_align(_Align) {}
+    constexpr void _On_fill(basic_string_view<CharT>) {}
+    constexpr void _On_width(int) {}
+    constexpr void _On_dynamic_width(int) {}
+    constexpr void _On_dynamic_width(_Auto_id_tag) {}
+    constexpr void _On_precision(int) {}
+    constexpr void _On_dynamic_precision(int) {}
+    constexpr void _On_dynamic_precision(_Auto_id_tag) {}
+    constexpr void _On_sign(_Sign) {}
+    constexpr void _On_hash() {}
+    constexpr void _On_zero() {}
+    constexpr void _On_type(CharT) {}
+};
+
+template <typename CharT>
 struct testing_callbacks {
     _Align expected_alignment = _Align::_None;
     _Sign expected_sign       = _Sign::_None;
@@ -262,6 +278,23 @@ constexpr bool test_parse_format_specs() {
     return true;
 }
 
+template <class CharT>
+constexpr bool test_specs_setter() {
+    // just instantiate for now.
+    _Basic_format_specs<CharT> specs = {};
+    _Specs_setter<CharT> setter(specs);
+
+    (void) setter;
+    return true;
+}
+
+template <class CharT>
+constexpr bool test_specs_checker() {
+    _Specs_checker<noop_testing_callbacks<CharT>> checker(noop_testing_callbacks<CharT>{}, _Type::_Float32);
+    (void) checker;
+    return true;
+}
+
 int main() {
     test_parse_align<char>();
     test_parse_align<wchar_t>();
@@ -287,5 +320,16 @@ int main() {
     test_parse_format_specs<wchar_t>();
     static_assert(test_parse_format_specs<char>());
     static_assert(test_parse_format_specs<wchar_t>());
+
+    test_specs_setter<char>();
+    test_specs_setter<wchar_t>();
+    static_assert(test_specs_setter<char>());
+    static_assert(test_specs_setter<wchar_t>());
+
+    test_specs_checker<char>();
+    test_specs_checker<wchar_t>();
+    static_assert(test_specs_checker<char>());
+    static_assert(test_specs_checker<wchar_t>());
+
     return 0;
 }
