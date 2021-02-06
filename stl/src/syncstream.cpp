9af659807fabe2784b590a6c443160f10e02e7d7
Stephan T. Lavavej <stl@nuwen.net>
2021-02-05 21:47:55 -0800

    Move code into the atomic_wait satellite DLL.

--- stl/src/syncstream.cpp
+++ stl/src/syncstream.cpp
@@ -3,62 +3,72 @@
 
 // initialize syncstream mutex map
 
+#include <functional>
 #include <internal_shared.h>
 #include <map>
+#include <mutex>
+#include <new>
 #include <shared_mutex>
+#include <type_traits>
+#include <utility>
 
 #pragma warning(disable : 4074)
 #pragma init_seg(compiler)
 
-_STD_BEGIN
-// OBJECT DECLARATIONS
-struct _Mutex_count_pair {
-    shared_mutex _Mutex;
-    size_t _Ref_count = 0;
-};
+namespace {
+    // OBJECT DECLARATIONS
+    struct _Mutex_count_pair {
+        _STD shared_mutex _Mutex;
+        size_t _Ref_count = 0;
+    };
 
-template <class _Ty>
-class _Crt_allocator {
-public:
-    using value_type                             = _Ty;
-    using propagate_on_container_move_assignment = true_type;
-    using is_always_equal                        = true_type;
+    template <class _Ty>
+    class _Crt_allocator {
+    public:
+        using value_type                             = _Ty;
+        using propagate_on_container_move_assignment = _STD true_type;
+        using is_always_equal                        = _STD true_type;
 
-    constexpr _Crt_allocator() noexcept = default;
+        constexpr _Crt_allocator() noexcept = default;
 
-    constexpr _Crt_allocator(const _Crt_allocator&) noexcept = default;
-    template <class _Other>
-    constexpr _Crt_allocator(const _Crt_allocator<_Other>&) noexcept {}
+        constexpr _Crt_allocator(const _Crt_allocator&) noexcept = default;
+        template <class _Other>
+        constexpr _Crt_allocator(const _Crt_allocator<_Other>&) noexcept {}
 
-    _NODISCARD __declspec(allocator) _Ty* allocate(_CRT_GUARDOVERFLOW const size_t _Count) {
-        const auto _Ptr = _calloc_crt(_Count, sizeof(_Ty));
-        if (!_Ptr) {
-            throw bad_alloc{};
+        _NODISCARD __declspec(allocator) _Ty* allocate(_CRT_GUARDOVERFLOW const size_t _Count) {
+            const auto _Ptr = _calloc_crt(_Count, sizeof(_Ty));
+            if (!_Ptr) {
+                throw _STD bad_alloc{};
+            }
+            return static_cast<_Ty*>(_Ptr);
         }
-        return static_cast<_Ty*>(_Ptr);
-    }
 
-    void deallocate(_Ty* const _Ptr, size_t) noexcept {
-        _free_crt(_Ptr);
-    }
-};
+        void deallocate(_Ty* const _Ptr, size_t) noexcept {
+            _free_crt(_Ptr);
+        }
+    };
 
-using _Map_alloc = _Crt_allocator<pair<void* const, _Mutex_count_pair>>;
-using _Map_type  = map<void*, _Mutex_count_pair, less<void*>, _Map_alloc>;
+    using _Map_alloc = _Crt_allocator<_STD pair<void* const, _Mutex_count_pair>>;
+    using _Map_type  = _STD map<void*, _Mutex_count_pair, _STD less<void*>, _Map_alloc>;
 
-static _Map_type _Lookup_map;
-static shared_mutex _Lookup_mutex;
+    _Map_type _Lookup_map;
+    _STD shared_mutex _Lookup_mutex;
+} // unnamed namespace
 
-extern "C" _NODISCARD shared_mutex* __stdcall _Get_mutex_for_instance(void* _Ptr) noexcept {
-    shared_lock _Guard(_Lookup_mutex);
+_EXTERN_C
+
+_NODISCARD void* __stdcall __std_get_shared_mutex_for_instance(void* _Ptr) noexcept {
+    _STD shared_lock _Guard(_Lookup_mutex);
     auto _Instance_mutex_iter = _Lookup_map.find(_Ptr);
     _ASSERT_EXPR(_Instance_mutex_iter != _Lookup_map.end(), "No mutex exists for given instance!");
-    return &_Instance_mutex_iter->second._Mutex;
+    auto _Ret = &_Instance_mutex_iter->second._Mutex;
+    static_assert(_STD is_same_v<decltype(_Ret), _STD shared_mutex*>);
+    return _Ret; // caller will restore type
 }
 
-extern "C" _NODISCARD bool __stdcall _Acquire_mutex_for_instance(void* _Ptr) noexcept {
+_NODISCARD bool __stdcall __std_acquire_shared_mutex_for_instance(void* _Ptr) noexcept {
     try {
-        scoped_lock _Guard(_Lookup_mutex);
+        _STD scoped_lock _Guard(_Lookup_mutex);
         auto& _Refs = _Lookup_map.try_emplace(_Ptr).first->second._Ref_count;
         ++_Refs;
         return true;
@@ -67,8 +77,8 @@ extern "C" _NODISCARD bool __stdcall _Acquire_mutex_for_instance(void* _Ptr) noe
     }
 }
 
-extern "C" void __stdcall _Release_mutex_for_instance(void* _Ptr) noexcept {
-    scoped_lock _Guard(_Lookup_mutex);
+void __stdcall __std_release_shared_mutex_for_instance(void* _Ptr) noexcept {
+    _STD scoped_lock _Guard(_Lookup_mutex);
     const auto _Instance_mutex_iter = _Lookup_map.find(_Ptr);
     _ASSERT_EXPR(_Instance_mutex_iter != _Lookup_map.end(), "No mutex exists for given instance!");
     auto& _Refs = _Instance_mutex_iter->second._Ref_count;
@@ -77,4 +87,4 @@ extern "C" void __stdcall _Release_mutex_for_instance(void* _Ptr) noexcept {
     }
 }
 
-_STD_END
+_END_EXTERN_C
