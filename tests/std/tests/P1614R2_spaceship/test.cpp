7b205c5b9b4f4000d2f114be9bea3b28b389d8a3
Stephan T. Lavavej <stl@nuwen.net>
2021-02-17 19:33:57 -0800

    Remove outdated test comment (unrelated to other changes)

--- tests/std/tests/P1614R2_spaceship/test.cpp
+++ tests/std/tests/P1614R2_spaceship/test.cpp
@@ -1,9 +1,6 @@
 // Copyright (c) Microsoft Corporation.
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
-// Covers:
-// * spaceship for containers
-
 #include <array>
 #include <cassert>
 #include <compare>
