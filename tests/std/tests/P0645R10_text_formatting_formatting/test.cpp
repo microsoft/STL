fca0679b60fc0340c38dd6501691ff4bc10d6c2d
Charles <chbarto@microsoft.com>
2021-02-22 13:15:19 -0800

    initial replacement field tests (and numeric arg indexing)

--- tests/std/tests/P0645R10_text_formatting_formatting/test.cpp
+++ tests/std/tests/P0645R10_text_formatting_formatting/test.cpp
@@ -9,7 +9,7 @@
 #include <string_view>
 
 using namespace std;
-
+using namespace std::string_view_literals;
 // TODO: fill in tests
 template back_insert_iterator<string> std::vformat_to(
     back_insert_iterator<string>, const locale&, string_view, format_args_t<back_insert_iterator<string>, char>);
@@ -74,6 +74,27 @@ int main() {
     output_string.clear();
     vformat_to(back_insert_iterator(output_string), locale::classic(), "}}x", make_format_args());
     assert(output_string == "}x");
+
+    output_string.clear();
+    vformat_to(back_insert_iterator(output_string), locale::classic(), "{}", make_format_args((const char*) "f"));
+    assert(output_string == "f");
+
+    output_string.clear();
+    vformat_to(back_insert_iterator(output_string), locale::classic(), "{0} {0}", make_format_args((const char*) "f"));
+    assert(output_string == "f f");
+
+    // TODO: enable these in _Write function PR for sv and bool
+    /*
+    output_string.clear();
+    vformat_to(back_insert_iterator(output_string), locale::classic(), "{}", make_format_args("f"sv));
+    assert(output_string == "f");
+    */
+
+    /*
+    output_string.clear();
+    vformat_to(back_insert_iterator(output_string), locale::classic(), "{}", make_format_args(true));
+    assert(output_string == "true");
+    */
 
     return 0;
 }
