2a8165208d2bd3e6592be5053dfc20435da69414
Stephan T. Lavavej <stl@microsoft.com>
2021-02-28 21:11:59 -0800

    /analyze expects different column numbers.

--- tests/std/tests/P1208R6_source_location/test.cpp
+++ tests/std/tests/P1208R6_source_location/test.cpp
@@ -59,7 +59,11 @@ constexpr void argument_test(
 constexpr void sloc_constructor_test() {
     const s x;
     assert(x.loc.line() == __LINE__ - 1);
+#ifdef _PREFAST_
+    assert(x.loc.column() == 14);
+#else // _PREFAST_
     assert(x.loc.column() == 13);
+#endif // _PREFAST_
     if (is_constant_evaluated()) {
         assert(x.loc.function_name() == "main"sv); // TRANSITION, VSO-1285783
     } else {
@@ -79,7 +83,11 @@ constexpr void different_constructor_test() {
 constexpr void sub_member_test() {
     const s2 s;
     assert(s.x.loc.line() == __LINE__ - 1);
+#ifdef _PREFAST_
+    assert(s.x.loc.column() == 15);
+#else // _PREFAST_
     assert(s.x.loc.column() == 14);
+#endif // _PREFAST_
     if (is_constant_evaluated()) {
         assert(s.x.loc.function_name() == "main"sv); // TRANSITION, VSO-1285783
     } else {
