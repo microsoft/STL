696191603707d39c26ca7b5641b0577e925228d8
Matt Stephanson <stephanson.matt@gmail.com>
2021-03-03 22:21:27 -0800

    Unicode Windows API functions

--- stl/src/tzdb.cpp
+++ stl/src/tzdb.cpp
@@ -18,19 +18,19 @@ _RegistryLeapSecondInfo* __stdcall __std_tzdb_get_reg_leap_seconds(
     //    *current_reg_ls_size == 0,                *reg_ls_data != nullptr --> new data, failed reading
     //    *current_reg_ls_size >  prev_reg_ls_size, *reg_ls_data == nullptr --> new data, failed allocation
 
-    constexpr auto reg_key_name    = TEXT("SYSTEM\\CurrentControlSet\\Control\\LeapSecondInformation");
-    constexpr auto reg_subkey_name = TEXT("LeapSeconds");
+    constexpr auto reg_key_name    = LR"(SYSTEM\CurrentControlSet\Control\LeapSecondInformation)";
+    constexpr auto reg_subkey_name = L"LeapSeconds";
     *current_reg_ls_size           = 0;
     HKEY leap_sec_key              = 0;
 
-    LSTATUS status = RegOpenKeyEx(HKEY_LOCAL_MACHINE, reg_key_name, 0, KEY_READ, &leap_sec_key);
+    LSTATUS status = RegOpenKeyExW(HKEY_LOCAL_MACHINE, reg_key_name, 0, KEY_READ, &leap_sec_key);
     if (status != ERROR_SUCCESS) {
         // May not exist on older systems. Treat this as equivalent to the key existing but with no data.
         return nullptr;
     }
 
     DWORD byte_size = 0;
-    status          = RegQueryValueEx(leap_sec_key, reg_subkey_name, nullptr, nullptr, nullptr, &byte_size);
+    status          = RegQueryValueExW(leap_sec_key, reg_subkey_name, nullptr, nullptr, nullptr, &byte_size);
     static_assert(sizeof(_RegistryLeapSecondInfo) == 12);
     const auto ls_size   = byte_size / 12;
     *current_reg_ls_size = ls_size;
@@ -39,7 +39,7 @@ _RegistryLeapSecondInfo* __stdcall __std_tzdb_get_reg_leap_seconds(
     if ((status == ERROR_SUCCESS || status == ERROR_MORE_DATA) && ls_size > prev_reg_ls_size) {
         try {
             reg_ls_data = new _RegistryLeapSecondInfo[ls_size];
-            status      = RegQueryValueEx(
+            status      = RegQueryValueExW(
                 leap_sec_key, reg_subkey_name, nullptr, nullptr, reinterpret_cast<LPBYTE>(reg_ls_data), &byte_size);
             if (status != ERROR_SUCCESS) {
                 *current_reg_ls_size = 0;
