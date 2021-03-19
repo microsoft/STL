cb2b9853417c6d8df22f9789aa25b7eb57c39eb1
d-winsor <danwin@microsoft.com>
2021-03-18 20:09:11 -0700

    Support for zoned_time (#1752)
    Co-authored-by: Stephan T. Lavavej <stl@nuwen.net>

--- tests/std/tests/P0355R7_calendars_and_time_zones_time_zones/test.cpp
+++ tests/std/tests/P0355R7_calendars_and_time_zones_time_zones/test.cpp
@@ -1,7 +1,6 @@
 // Copyright (c) Microsoft Corporation.
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
-#include "timezone_data.h"
 #include <algorithm>
 #include <cassert>
 #include <chrono>
@@ -10,6 +9,8 @@
 #include <stdexcept>
 #include <string_view>
 #include <utility>
+
+#include <timezone_data.hpp>
 
 using namespace std;
 using namespace std::chrono;
