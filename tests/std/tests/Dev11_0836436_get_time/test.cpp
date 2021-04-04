644f2dfb9038c751b6d188939db57d9d46beff42
futuarmo <armen114@yandex.ru>
2021-04-04 17:19:03 +0300

    fixed formatting

--- tests/std/tests/Dev11_0836436_get_time/test.cpp
+++ tests/std/tests/Dev11_0836436_get_time/test.cpp
@@ -2,6 +2,8 @@
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
 #include <assert.h>
+#include <cstdlib>
+#include <cstdio>
 #include <iomanip>
 #include <ios>
 #include <locale>
@@ -10,8 +12,6 @@
 #include <string>
 #include <time.h>
 #include <tuple>
-#include <cstdlib>
-#include <cstdio>
 
 using namespace std;
 
@@ -774,8 +774,8 @@ void test_invalid_parameter_handler(const wchar_t* const expression, const wchar
 
     static int num_called = 0;
     if (num_called++ > 10) {
-        wprintf(L"Test Failed: Invalid parameter handler was called over 10 times by %s in %s:%u\n", function, file,
-            line);
+        wprintf(
+        L"Test Failed: Invalid parameter handler was called over 10 times by %s in %s:%u\n", function, file, line);
         exit(1);
     }
 }
