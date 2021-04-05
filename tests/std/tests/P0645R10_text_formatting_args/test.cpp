d8a4c4b687248e0d856d4e51088c1c14eefffefe
Elnar D <eldakesh@microsoft.com>
2021-04-05 15:43:18 -0700

    <format>: Fix arg overload resolution
    There seems to be a compiler bug picking the wrong function in an
    overload set. I've filed it here: . This sidesteps the bug, and also
    fixes a latent bug with `_Get_format_arg_storage_typee`.
    
    There is also a fix for the packed index, that wasn't very packed
    because the types of the bitset were different. This should save space
    as intended.

--- tests/std/tests/P0645R10_text_formatting_args/test.cpp
+++ tests/std/tests/P0645R10_text_formatting_args/test.cpp
@@ -213,6 +213,8 @@ void test_format_arg_store() {
     test_single_format_arg<Context, basic_string_view<char_type>, Arg_type::string_type>(get_input_sv<char_type>());
 }
 
+static_assert(sizeof(_Format_arg_store_packed_index) == sizeof(_Format_arg_store_packed_index::_Index_type));
+
 int main() {
     test_basic_format_arg<format_context>();
     test_basic_format_arg<wformat_context>();
