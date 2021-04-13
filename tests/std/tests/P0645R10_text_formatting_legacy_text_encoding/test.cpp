b81d9eb71db3ba805403e3f1e0f2ce556f6cf392
statementreply <statementreply@gmail.com>
2021-04-13 10:58:40 +0800

    <format>: Assume UTF-8 format strings when execution charset is UTF-8 (#1824)
    * Assume format strings are always UTF-8 when encoding charset is UTF-8
    
    * Add `/utf-8` tests
    
    * Run Shift-JIS tests with `/execution-charset:.932`
    
    * Apply code review feedback
    
    * constexpr function implies inline
    
    * Code review feedback.
    
    * Mitigate merge conflicts: Use _THROW.
    
    * Mitigate merge conflicts: Check setlocale(), reset to "C".
    
    Co-authored-by: Stephan T. Lavavej <stl@nuwen.net>

---
+++ tests/std/tests/P0645R10_text_formatting_legacy_text_encoding/test.cpp
@@ -0,0 +1,65 @@
+// Copyright (c) Microsoft Corporation.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+#include <cassert>
+#include <clocale>
+#include <format>
+#include <string_view>
+
+#include "test_format_support.hpp"
+
+using namespace std;
+
+void test_multibyte_format_strings() {
+    {
+        assert(setlocale(LC_ALL, ".932") != nullptr);
+        const auto s =
+            "\x93\xfa\x96{\x92\x6e\x90}"sv; // Note the use of `{` and `}` as continuation bytes (from GH-1576)
+        assert(format(s) == s);
+
+        assert(format("{:.2}", s) == "\x93\xfa"sv);
+        assert(format("{:4.2}", s) == "\x93\xfa  "sv);
+
+        assert(format("{:<4.2}", s) == "\x93\xfa  "sv);
+        assert(format("{:^4.2}", s) == " \x93\xfa "sv);
+        assert(format("{:>4.2}", s) == "  \x93\xfa"sv);
+
+        assert(format("{:\x90}<4.2}", s) == "\x93\xfa\x90}\x90}"sv);
+        assert(format("{:\x90}^4.2}", s) == "\x90}\x93\xfa\x90}"sv);
+        assert(format("{:\x90}>4.2}", s) == "\x90}\x90}\x93\xfa"sv);
+
+        assert(format("{:.3}", s) == "\x93\xfa"sv);
+        assert(format("{:4.3}", s) == "\x93\xfa  "sv);
+
+        assert(format("{:<4.3}", s) == "\x93\xfa  "sv);
+        assert(format("{:^4.3}", s) == " \x93\xfa "sv);
+        assert(format("{:>4.3}", s) == "  \x93\xfa"sv);
+
+        assert(format("{:\x90}<4.3}", s) == "\x93\xfa\x90}\x90}"sv);
+        assert(format("{:\x90}^4.3}", s) == "\x90}\x93\xfa\x90}"sv);
+        assert(format("{:\x90}>4.3}", s) == "\x90}\x90}\x93\xfa"sv);
+    }
+
+    assert(setlocale(LC_ALL, "C") != nullptr);
+}
+
+void test_parse_align() {
+    auto parse_align_fn = _Parse_align<char, testing_callbacks<char>>;
+
+    {
+        assert(setlocale(LC_ALL, ".932") != nullptr);
+        test_parse_helper(parse_align_fn, "\x93\xfa<X"sv, false, 3,
+            {.expected_alignment = _Align::_Left, .expected_fill = "\x93\xfa"sv});
+        test_parse_helper(parse_align_fn, "\x96\x7b>X"sv, false, 3,
+            {.expected_alignment = _Align::_Right, .expected_fill = "\x96\x7b"sv});
+        test_parse_helper(parse_align_fn, "\x92\x6e^X"sv, false, 3,
+            {.expected_alignment = _Align::_Center, .expected_fill = "\x92\x6e"sv});
+    }
+
+    assert(setlocale(LC_ALL, "C") != nullptr);
+}
+
+int main() {
+    test_multibyte_format_strings();
+    test_parse_align();
+}
---
+++ tests/std/tests/P0645R10_text_formatting_utf8/test.cpp
@@ -0,0 +1,64 @@
+// Copyright (c) Microsoft Corporation.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+#include <cassert>
+#include <clocale>
+#include <format>
+#include <string_view>
+
+#include "test_format_support.hpp"
+
+using namespace std;
+
+void test_multibyte_format_strings() {
+    {
+        // Filling with footballs ("\xf0\x9f\x8f\x88" is U+1F3C8 AMERICAN FOOTBALL)
+        assert(format("{:\xf0\x9f\x8f\x88>4}"sv, 42) == "\xf0\x9f\x8f\x88\xf0\x9f\x8f\x88\x34\x32");
+
+        assert(format("{:\xf0\x9f\x8f\x88<4.2}", "1") == "\x31\xf0\x9f\x8f\x88\xf0\x9f\x8f\x88\xf0\x9f\x8f\x88"sv);
+        assert(format("{:\xf0\x9f\x8f\x88^4.2}", "1") == "\xf0\x9f\x8f\x88\x31\xf0\x9f\x8f\x88\xf0\x9f\x8f\x88"sv);
+        assert(format("{:\xf0\x9f\x8f\x88>4.2}", "1") == "\xf0\x9f\x8f\x88\xf0\x9f\x8f\x88\xf0\x9f\x8f\x88\x31"sv);
+    }
+
+    {
+        try {
+            (void) format("{:\x9f\x8f\x88<10}"sv, 42); // Bad fill character encoding: missing lead byte before \x9f
+            assert(false);
+        } catch (const format_error&) {
+        }
+    }
+}
+
+void test_parse_align() {
+    auto parse_align_fn = _Parse_align<char, testing_callbacks<char>>;
+
+    {
+        // "\xf0\x9f\x8f\x88" is U+1F3C8 AMERICAN FOOTBALL
+        test_parse_helper(parse_align_fn, "\xf0\x9f\x8f\x88<X"sv, false, 5,
+            {.expected_alignment = _Align::_Left, .expected_fill = "\xf0\x9f\x8f\x88"sv});
+        test_parse_helper(parse_align_fn, "\xf0\x9f\x8f\x88>X"sv, false, 5,
+            {.expected_alignment = _Align::_Right, .expected_fill = "\xf0\x9f\x8f\x88"sv});
+        test_parse_helper(parse_align_fn, "\xf0\x9f\x8f\x88^X"sv, false, 5,
+            {.expected_alignment = _Align::_Center, .expected_fill = "\xf0\x9f\x8f\x88"sv});
+    }
+}
+
+void run_tests() {
+    test_multibyte_format_strings();
+    test_parse_align();
+}
+
+int main() {
+    run_tests();
+
+    assert(setlocale(LC_ALL, ".1252") != nullptr);
+    run_tests();
+
+    assert(setlocale(LC_ALL, ".932") != nullptr);
+    run_tests();
+
+#ifndef MSVC_INTERNAL_TESTING // TRANSITION, Windows on Contest VMs understand ".UTF-8" codepage
+    assert(setlocale(LC_ALL, ".UTF-8") != nullptr);
+    run_tests();
+#endif
+}
