224b2a18b4ef14b8131afe09c0045c421a97349c
Daniel Marshall <xandan@gmail.com>
2021-05-28 09:30:44 +0100

    Get rid of struct and is_valid, and add another test case.

--- tests/std/tests/P1682R3_to_underlying/test.cpp
+++ tests/std/tests/P1682R3_to_underlying/test.cpp
@@ -9,31 +9,31 @@ using namespace std;
 
 // TRANSITION, GH-602
 // template <class T>
-// constexpr bool is_valid = requires(T e) { to_underlying(e); };
+// constexpr bool can_underlying = requires(T e) { to_underlying(e); };
 
-template <class T, class = void>
-struct can_underlying : false_type {};
+template <class, class = void>
+constexpr bool can_underlying = false;
 template <class T>
-struct can_underlying<T, void_t<decltype(to_underlying(declval<T>()))>> : true_type {};
-
-template <class T>
-constexpr bool is_valid = can_underlying<T>::value;
+constexpr bool can_underlying<T, void_t<decltype(to_underlying(declval<T>()))>> = true;
 
 enum enum1 : char { a = '1' };
 enum class enum2 : int { b = 2 };
 enum class enum3 : long { c = 3 };
+enum class enum4 { d = 4 };
 
 struct not_an_enum {};
 
-static_assert(is_valid<enum1>);
-static_assert(is_valid<enum2>);
-static_assert(is_valid<enum3>);
-static_assert(!is_valid<not_an_enum>);
-static_assert(!is_valid<int>);
+static_assert(can_underlying<enum1>);
+static_assert(can_underlying<enum2>);
+static_assert(can_underlying<enum3>);
+static_assert(can_underlying<enum4>);
+static_assert(!can_underlying<not_an_enum>);
+static_assert(!can_underlying<int>);
 
 static_assert(is_same_v<decltype(to_underlying(enum1::a)), char>);
 static_assert(is_same_v<decltype(to_underlying(enum2::b)), int>);
 static_assert(is_same_v<decltype(to_underlying(enum3::c)), long>);
+static_assert(is_same_v<decltype(to_underlying(enum4::d)), int>);
 
 static_assert(noexcept(to_underlying(enum1::a)));
 
@@ -41,6 +41,7 @@ constexpr bool test() {
     assert(to_underlying(enum1::a) == '1');
     assert(to_underlying(enum2::b) == 2);
     assert(to_underlying(enum3::c) == 3);
+    assert(to_underlying(enum4::d) == 4);
     return true;
 }
 
