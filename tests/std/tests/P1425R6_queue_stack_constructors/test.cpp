0c910e3d27aea1f899aff1b4af5614b6e91e3f9d
Michael Schellenberger Costa <mschellenbergercosta@googlemail.com>
2021-06-09 17:09:37 +0200

    Adopt LWG-3506
    Addresses #1965

--- tests/std/tests/P1425R6_queue_stack_constructors/test.cpp
+++ tests/std/tests/P1425R6_queue_stack_constructors/test.cpp
@@ -15,7 +15,8 @@
 #include <vector>
 using namespace std;
 
-constexpr int some_data[] = {0, 1, 2, 3, 4, 5};
+constexpr int some_data[]       = {0, 1, 2, 3, 4, 5};
+constexpr int additional_data[] = {6, 7, 8, 9, 10, 11};
 
 template <typename T>
 struct custom_allocator {
@@ -43,10 +44,12 @@ template <class Range>
 void test_container() {
     Range range{begin(some_data), end(some_data)};
 
+    int result;
+#if _HAS_CXX23
     stack s1(range.begin(), range.end());
     static_assert(is_same_v<decltype(s1), stack<int, deque<int, allocator<int>>>>);
     assert(s1.size() == size(some_data));
-    int result = 5;
+    result = 5;
     while (!s1.empty()) {
         assert(s1.top() == result--);
         s1.pop();
@@ -77,6 +80,74 @@ void test_container() {
     while (!q2.empty()) {
         assert(q2.front() == result++);
         q2.pop();
+    }
+#endif // _HAS_CXX23
+
+    priority_queue pq1(range.begin(), range.end());
+    static_assert(is_same_v<decltype(pq1), priority_queue<int, vector<int, allocator<int>>, less<int>>>);
+    assert(pq1.size() == size(some_data));
+    result = 5;
+    while (!pq1.empty()) {
+        assert(pq1.top() == result--);
+        pq1.pop();
+    }
+
+    priority_queue pq2(range.begin(), range.end(), custom_allocator<int>{});
+    static_assert(is_same_v<decltype(pq2), priority_queue<int, vector<int, custom_allocator<int>>, less<int>>>);
+    assert(pq2.size() == size(some_data));
+    result = 5;
+    while (!pq2.empty()) {
+        assert(pq2.top() == result--);
+        pq2.pop();
+    }
+
+    priority_queue pq3(range.begin(), range.end(), greater<int>{}, custom_allocator<int>{});
+    static_assert(is_same_v<decltype(pq3), priority_queue<int, vector<int, custom_allocator<int>>, greater<int>>>);
+    assert(pq3.size() == size(some_data));
+    result = 0;
+    while (!pq3.empty()) {
+        assert(pq3.top() == result++);
+        pq3.pop();
+    }
+
+    vector cont(begin(additional_data), end(additional_data));
+    priority_queue pq4(range.begin(), range.end(), greater<int>{}, cont);
+    static_assert(is_same_v<decltype(pq4), priority_queue<int, vector<int, allocator<int>>, greater<int>>>);
+    assert(pq4.size() == size(some_data) + size(additional_data));
+    result = 0;
+    while (!pq4.empty()) {
+        assert(pq4.top() == result++);
+        pq4.pop();
+    }
+
+    priority_queue pq5(range.begin(), range.end(), greater<int>{},
+        vector<int, allocator<int>>{begin(additional_data), end(additional_data)});
+    static_assert(is_same_v<decltype(pq5), priority_queue<int, vector<int, allocator<int>>, greater<int>>>);
+    assert(pq5.size() == size(some_data) + size(additional_data));
+    result = 0;
+    while (!pq5.empty()) {
+        assert(pq5.top() == result++);
+        pq5.pop();
+    }
+
+    vector cont2(begin(additional_data), end(additional_data), custom_allocator<int>{});
+    priority_queue pq6(range.begin(), range.end(), greater<int>{}, cont2, custom_allocator<int>{});
+    static_assert(is_same_v<decltype(pq6), priority_queue<int, vector<int, custom_allocator<int>>, greater<int>>>);
+    assert(pq6.size() == size(some_data) + size(additional_data));
+    result = 0;
+    while (!pq6.empty()) {
+        assert(pq6.top() == result++);
+        pq6.pop();
+    }
+
+    priority_queue pq7(range.begin(), range.end(), greater<int>{},
+        vector<int, custom_allocator<int>>{begin(additional_data), end(additional_data)}, custom_allocator<int>{});
+    static_assert(is_same_v<decltype(pq7), priority_queue<int, vector<int, custom_allocator<int>>, greater<int>>>);
+    assert(pq7.size() == size(some_data) + size(additional_data));
+    result = 0;
+    while (!pq7.empty()) {
+        assert(pq7.top() == result++);
+        pq7.pop();
     }
 }
 
