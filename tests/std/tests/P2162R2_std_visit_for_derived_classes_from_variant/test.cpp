4d9c248c40afaa80350c235faca7259f76227cb7
Igor Zhukov <fsb4000@yandex.ru>
2021-06-22 13:22:57 +0700

    Apply Casey Carter suggestions
    Co-authored-by: Casey Carter <Casey@Carter.net>

--- tests/std/tests/P2162R2_std_visit_for_derived_classes_from_variant/test.cpp
+++ tests/std/tests/P2162R2_std_visit_for_derived_classes_from_variant/test.cpp
@@ -24,7 +24,6 @@ struct State : variant<Disconnected, Connecting, Connected> {
     using variant::variant;
 };
 
-
 void example1_from_p2162r2() {
     State v1       = Disconnected{45};
     const State v2 = Connecting{'d'};
@@ -34,7 +33,6 @@ void example1_from_p2162r2() {
     visit([](auto x) { assert(x.val == 45); }, move(v1));
     visit([](auto x) { assert(x.val == 'd'); }, move(v2));
 }
-
 
 struct Expr;
 
@@ -54,14 +52,6 @@ struct Expr : variant<int, Neg, Add, Mul> {
     using variant::variant;
 };
 
-namespace std {
-    template <>
-    struct variant_size<Expr> : variant_size<Expr::variant> {};
-
-    template <size_t I>
-    struct variant_alternative<I, Expr> : variant_alternative<I, Expr::variant> {};
-} // namespace std
-
 int eval(const Expr& expr) {
     struct visitor {
         int operator()(int i) const {
@@ -74,26 +64,17 @@ int eval(const Expr& expr) {
             return eval(*a.lhs) + eval(*a.rhs);
         }
         int operator()(const Mul& m) const {
-            // Optimize multiplication by 0.
-            if (int* i = get_if<int>(m.lhs.get()); i && *i == 0) {
-                return 0;
-            }
-            if (int* i = get_if<int>(m.rhs.get()); i && *i == 0) {
-                return 0;
-            }
             return eval(*m.lhs) * eval(*m.rhs);
         }
     };
     return visit(visitor{}, expr);
 }
 
-
 void example2_from_p2162r2() {
     // (1) + (3*2)
     const Expr e = Add{make_shared<Expr>(1), make_shared<Expr>(Mul{make_shared<Expr>(2), make_shared<Expr>(3)})};
     assert(eval(e) == (1 + 3 * 2));
 }
-
 
 int main() {
     example1_from_p2162r2();
