2bed2876e525d8d71975b77ad7bd9723b5294408
statementreply <statementreply@gmail.com>
2021-06-23 21:30:17 +0800

    Add [[nodiscard]]

--- stl/src/xmath.hpp
+++ stl/src/xmath.hpp
@@ -172,14 +172,14 @@ _END_EXTERN_C_UNLESS_PURE
 #endif
 
 template <typename T>
-T _Xfe_overflow(const T sign) noexcept {
+_NODISCARD T _Xfe_overflow(const T sign) noexcept {
     static_assert(_STD is_floating_point_v<T>);
     constexpr T huge = _STD numeric_limits<T>::max();
     return _STD copysign(huge, sign) * huge;
 }
 
 template <typename T>
-T _Xfe_underflow(const T sign) noexcept {
+_NODISCARD T _Xfe_underflow(const T sign) noexcept {
     static_assert(_STD is_floating_point_v<T>);
     constexpr T tiny = _STD numeric_limits<T>::min();
     return _STD copysign(tiny, sign) * tiny;
