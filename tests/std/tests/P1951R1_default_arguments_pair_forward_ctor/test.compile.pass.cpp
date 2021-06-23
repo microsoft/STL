9f72459bc6cc51900cb2d2e36df8abf158136ff3
Igor Zhukov <fsb4000@yandex.ru>
2021-06-23 16:24:56 +0700

    add more tests

--- tests/std/tests/P1951R1_default_arguments_pair_forward_ctor/test.compile.pass.cpp
+++ tests/std/tests/P1951R1_default_arguments_pair_forward_ctor/test.compile.pass.cpp
@@ -7,8 +7,12 @@
 using namespace std;
 
 void test() {
-    pair<int, unique_ptr<int>> p{42, {}};
-    (void) p;
+    pair<int, unique_ptr<int>> p1{42, {}};
+    pair<unique_ptr<int>, int> p2{{}, 42};
+    pair<unique_ptr<int>, unique_ptr<int>> p3{{}, {}};
+    (void) p1;
+    (void) p2;
+    (void) p3;
 }
 
 int main() {} // COMPILE-ONLY
