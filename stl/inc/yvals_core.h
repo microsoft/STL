99e56f7ae5a7ffffb5df45b56725b308f4420d43
Adam Bucior <35536269+AdamBucior@users.noreply.github.com>
2021-07-02 15:36:09 +0200

    Implement P0401R6

--- stl/inc/yvals_core.h
+++ stl/inc/yvals_core.h
@@ -260,6 +260,7 @@
 // Other C++20 deprecation warnings
 
 // _HAS_CXX23 directly controls:
+// P0401R6 Providing Size Feedback In The Allocator Interface
 // P1048R1 is_scoped_enum
 // P1679R3 contains() For basic_string/basic_string_view
 // P1682R3 to_underlying() For Enumerations
