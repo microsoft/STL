df30d2a236bff2f2fce4e65330ea935e09015f74
Michael Schellenberger Costa <mschellenbergercosta@googlemail.com>
2021-07-14 15:57:44 +0200

    Fix race condition in iterator debug machinery
    Fixes #2058

---
+++ tests/std/tests/GH_002058_debug_iterator_race/test.cpp
@@ -0,0 +1,51 @@
+#include <chrono>
+#include <future>
+#include <list>
+#include <set>
+#include <thread>
+#include <vector>
+
+using namespace std;
+
+// Concurrently destroy iterators and invalidate iterators
+template <class container>
+void test_concurrent_destruction() {
+    container c;
+    c.insert(c.begin(), 0);
+
+    vector<typename container::iterator> iters(1000, c.begin());
+    {
+        auto destroyIters = async(launch::async, [&]() { iters.clear(); });
+
+        auto invalidateIters = async(launch::async, [&]() { c.clear(); });
+    }
+}
+
+// Concurrently create iterators and invalidate container
+template <class container>
+void test_concurrent_creation() {
+    container c;
+    c.insert(c.begin(), 0);
+
+    vector<typename container::iterator> iters;
+    iters.reserve(1000);
+    {
+        auto createIters = async(launch::async, [&]() {
+            for (int i = 0; i < 1000; ++i) {
+                iters.push_back(c.begin());
+            };
+        });
+
+        auto invalidateIters = async(launch::async, [&]() { c = container{}; });
+    }
+}
+
+int main() {
+    test_concurrent_destruction<list<int>>();
+    test_concurrent_destruction<set<int>>();
+    test_concurrent_destruction<vector<int>>();
+
+    test_concurrent_creation<list<int>>();
+    test_concurrent_creation<set<int>>();
+    test_concurrent_creation<vector<int>>();
+}
