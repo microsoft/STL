56164f4e1bd44caa424ee29bf00f53c5f61eac73
Igor Zhukov <fsb4000@yandex.ru>
2021-07-27 08:03:58 +0700

    use predefined macros instead of custom test format

--- tests/std/tests/VSO_0000000_c_math_functions/test.compile.pass.cpp
+++ tests/std/tests/VSO_0000000_c_math_functions/test.compile.pass.cpp
@@ -1,9 +1,15 @@
 // Copyright (c) Microsoft Corporation.
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
+#if BOTH
 #include <cmath>
 #include <cstdlib>
 
 #include "type_asserts.hpp"
+#elif CMATH
+#include "cmath.cpp"
+#elif CSTDLIB
+#include "cstdlib.cpp"
+#endif
 
 int main() {} // COMPILE-ONLY
