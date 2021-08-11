5745ccf438c453225d9ae2f6503b1d715f826c4c
Michael Herwig <github.dev@michael-herwig.de>
2021-08-11 15:32:44 +0200

    apply test coding style conventions

--- tests/std/tests/GH_000814_avx512_vector_algorithms/test.cpp
+++ tests/std/tests/GH_000814_avx512_vector_algorithms/test.cpp
@@ -13,25 +13,24 @@ int main() {
 #include <cassert>
 #include <vector>
 
+using namespace std;
+
 extern "C" long __isa_enabled;
 
 namespace {
 
-    void _Test_swap() {
-        ::std::vector<unsigned char> count_up{/* */};
-        ::std::vector<unsigned char> count_invert{/* */};
+    void test_swap() {
+        constexpr size_t num_of_samples = 256;
+        vector<unsigned char> count_up(num_of_samples);
+        vector<unsigned char> count_invert(num_of_samples);
 
-        constexpr ::std::size_t num_of_samples = 256;
-        count_up.resize(num_of_samples);
-        count_invert.resize(num_of_samples);
-
-        for (::std::size_t i = 0; i < num_of_samples; ++i) {
+        for (size_t i = 0; i < num_of_samples; ++i) {
             count_up[i]     = static_cast<unsigned char>(i);
             count_invert[i] = static_cast<unsigned char>(~count_up[i]);
         }
 
-        const ::std::vector<unsigned char> count_up_original{count_up};
-        const ::std::vector<unsigned char> count_invert_original{count_invert};
+        const vector<unsigned char> count_up_original{count_up};
+        const vector<unsigned char> count_invert_original{count_invert};
 
         assert(count_up != count_invert);
 
@@ -43,9 +42,9 @@ namespace {
         assert(count_invert == count_up_original);
     }
 
-    ::std::size_t __Sum_squares(::std::size_t min_size) {
-        constexpr ::std::size_t limit = 128;
-        ::std::size_t squared_sum     = limit;
+    size_t sum_squares(size_t min_size) {
+        constexpr size_t limit = 128;
+        size_t squared_sum     = limit;
         while (min_size < 128) {
             squared_sum += min_size;
             min_size *= 2;
@@ -53,29 +52,26 @@ namespace {
         return squared_sum;
     }
 
-    template <class _BidIt>
-    void _Test_reverse_oracle(_BidIt _First, _BidIt _Last) noexcept {
-        for (; _First != _Last && _First != --_Last; ++_First) {
-            const auto _Temp = *_First;
-            *_First          = *_Last;
-            *_Last           = _Temp;
+    template <class _iterator_t>
+    void test_reverse_oracle(_iterator_t first, _iterator_t last) noexcept {
+        for (; first != last && first != --last; ++first) {
+            const auto temp = *first;
+            *first          = *last;
+            *last           = temp;
         }
     }
 
-    template <typename _Integer>
-    void _Test_reverse(void (*swap_fn)(void*, void*)) {
-        ::std::vector<unsigned char> test_vector{/* */};
+    template <typename _integer_t>
+    void test_reverse(void (*swap_fn)(void*, void*)) {
+        vector<unsigned char> test_vector(sum_squares(sizeof(_integer_t)));
 
-        ::std::size_t num_of_samples = __Sum_squares(sizeof(_Integer));
-        test_vector.resize(num_of_samples);
-
-        for (::std::size_t i = 0; i < num_of_samples; ++i) {
+        for (size_t i = 0; i < test_vector.size(); ++i) {
             test_vector[i] = static_cast<unsigned char>(i);
         }
 
-        ::std::vector<unsigned char> test_oracle{test_vector};
-        _Test_reverse_oracle(reinterpret_cast<_Integer*>(test_oracle.data()),
-            reinterpret_cast<_Integer*>(test_oracle.data() + test_oracle.size()));
+        vector<unsigned char> test_oracle{test_vector};
+        test_reverse_oracle(reinterpret_cast<_integer_t*>(test_oracle.data()),
+            reinterpret_cast<_integer_t*>(test_oracle.data() + test_oracle.size()));
 
         assert(test_oracle != test_vector);
 
@@ -84,23 +80,19 @@ namespace {
         assert(test_oracle == test_vector);
     }
 
-    template <typename _Integer>
-    void _Test_reverse_copy(void (*swap_fn)(const void*, const void*, void*)) {
-        ::std::vector<unsigned char> test_source{/* */};
+    template <typename _integer_t>
+    void test_reverse_copy(void (*swap_fn)(const void*, const void*, void*)) {
+        vector<unsigned char> test_source(sum_squares(sizeof(_integer_t)));
 
-        ::std::size_t num_of_samples = __Sum_squares(sizeof(_Integer));
-        test_source.resize(num_of_samples);
-
-        for (::std::size_t i = 0; i < num_of_samples; ++i) {
+        for (size_t i = 0; i < test_source.size(); ++i) {
             test_source[i] = static_cast<unsigned char>(i);
         }
 
-        ::std::vector<unsigned char> test_oracle{test_source};
-        _Test_reverse_oracle(reinterpret_cast<_Integer*>(test_oracle.data()),
-            reinterpret_cast<_Integer*>(test_oracle.data() + test_oracle.size()));
+        vector<unsigned char> test_oracle{test_source};
+        test_reverse_oracle(reinterpret_cast<_integer_t*>(test_oracle.data()),
+            reinterpret_cast<_integer_t*>(test_oracle.data() + test_oracle.size()));
 
-        ::std::vector<unsigned char> test_vector{/* */};
-        test_vector.resize(num_of_samples);
+        vector<unsigned char> test_vector(test_source.size());
 
         assert(test_oracle != test_vector);
 
@@ -116,17 +108,17 @@ int main() {
         return EXIT_SUCCESS;
     }
 
-    _Test_swap();
+    test_swap();
 
-    _Test_reverse<unsigned char>(__std_reverse_trivially_swappable_1);
-    _Test_reverse<unsigned short>(__std_reverse_trivially_swappable_2);
-    _Test_reverse<unsigned int>(__std_reverse_trivially_swappable_4);
-    _Test_reverse<unsigned long long>(__std_reverse_trivially_swappable_8);
+    test_reverse<unsigned char>(::__std_reverse_trivially_swappable_1);
+    test_reverse<unsigned short>(::__std_reverse_trivially_swappable_2);
+    test_reverse<unsigned int>(::__std_reverse_trivially_swappable_4);
+    test_reverse<unsigned long long>(__std_reverse_trivially_swappable_8);
 
-    _Test_reverse_copy<unsigned char>(__std_reverse_copy_trivially_copyable_1);
-    _Test_reverse_copy<unsigned short>(__std_reverse_copy_trivially_copyable_2);
-    _Test_reverse_copy<unsigned int>(__std_reverse_copy_trivially_copyable_4);
-    _Test_reverse_copy<unsigned long long>(__std_reverse_copy_trivially_copyable_8);
+    test_reverse_copy<unsigned char>(::__std_reverse_copy_trivially_copyable_1);
+    test_reverse_copy<unsigned short>(::__std_reverse_copy_trivially_copyable_2);
+    test_reverse_copy<unsigned int>(::__std_reverse_copy_trivially_copyable_4);
+    test_reverse_copy<unsigned long long>(::__std_reverse_copy_trivially_copyable_8);
 
     return EXIT_SUCCESS;
 }
