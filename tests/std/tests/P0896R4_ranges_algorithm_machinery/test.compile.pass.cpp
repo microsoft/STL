429606a12c497cceeca0dfcdbb963493d07a5953
Alex Guteniev <gutenev@gmail.com>
2021-08-21 21:44:03 +0300

    resuppress

--- tests/std/tests/P0896R4_ranges_algorithm_machinery/test.compile.pass.cpp
+++ tests/std/tests/P0896R4_ranges_algorithm_machinery/test.compile.pass.cpp
@@ -973,9 +973,11 @@ namespace gh_1089 {
 
             using PMD_Cat = int Cat::*;
             // Quantum effects: we must observe the size before defining Cat or it will become smaller.
-#pragma warning(suppress : 5243) // using incomplete class 'gh_1089::test::Cat' can cause potential one definition rule
+#pragma warning(push)
+#pragma warning(disable : 5243) // using incomplete class 'gh_1089::test::Cat' can cause potential one definition rule
                                  // violation due to ABI limitation
             STATIC_ASSERT(sizeof(PMD_Cat) > sizeof(void*));
+#pragma warning(pop)
 
             struct Cat {
                 int x = 42;
