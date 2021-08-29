8e13c671b53ec214a3606e4600d20a796db22032
Igor Zhukov <fsb4000@yandex.ru>
2021-08-29 21:06:32 +0700

    check for overflow

---
+++ tests/std/tests/GH_002168_regex_overflow/test.cpp
@@ -0,0 +1,21 @@
+// Copyright (c) Microsoft Corporation.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+#include <cassert>
+#include <regex>
+
+using namespace std;
+
+bool shouldThrow(const char* const regexString) {
+    try {
+        regex regex{regexString, regex_constants::ECMAScript};
+        return false;
+    } catch (regex_error&) {
+        return true;
+    }
+}
+
+int main() {
+    assert(shouldThrow("\\3333333334"));
+    assert(shouldThrow("\\2147483648"));
+}
