ce057ef073c27b32d47b18c4549bfe7a30d2397c
Igor Zhukov <fsb4000@yandex.ru>
2021-09-11 18:26:27 +0700

    address code review
    Co-authored-by: Alex Guteniev <gutenev@gmail.com>

--- tests/std/tests/GH_000342_filebuf_close/test.cpp
+++ tests/std/tests/GH_000342_filebuf_close/test.cpp
@@ -1,18 +1,20 @@
 // Copyright (c) Microsoft Corporation.
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
+#include <cassert>
 #include <fstream>
 
 using namespace std;
 
 int main() {
     {
-        ofstream f("test.txt");
-        f << "ab";
+        ofstream prepareTestFile("test.txt");
+        prepareTestFile << "ab";
     }
     fstream f("test.txt");
     f.seekg(1);
-    f.get(); // b
+    const int res = f.get();
+    assert(res == 'b');
     f.putback('b');
     f.putback('a');
     f.close();
