109398cf62315094fa57f1e3e6797aa02dd5fc07
Igor Zhukov <fsb4000@yandex.ru>
2021-09-12 13:24:01 +0700

    fix tests

--- tests/std/tests/P0896R4_views_transform/test.cpp
+++ tests/std/tests/P0896R4_views_transform/test.cpp
@@ -508,17 +508,6 @@ struct iterator_instantiator {
 
             assert(ranges::iter_move(i0) == add8(mutable_ints[0])); // NB: moving from int leaves it unchanged
             STATIC_ASSERT(NOEXCEPT_IDL0(ranges::iter_move(i0)));
-
-            if constexpr (forward_iterator<Iter>) {
-                auto i1 = ranges::next(i0);
-                ranges::iter_swap(i0, i1);
-                assert(mutable_ints[0] == 1);
-                assert(mutable_ints[1] == 0);
-                ranges::iter_swap(i1, i0);
-                assert(mutable_ints[0] == 0);
-                assert(mutable_ints[1] == 1);
-                STATIC_ASSERT(NOEXCEPT_IDL0(ranges::iter_swap(i0, i1)));
-            }
         }
 
         { // Validate increments
