aa0782eac36e216c3bc6e87aef9c1ab0abe06f1b
Stephan T. Lavavej <stl@nuwen.net>
2021-09-20 15:03:06 -0700

    Use uniform_int_distribution.

--- tests/tr1/include/temp_file_name.h
+++ tests/tr1/include/temp_file_name.h
@@ -8,11 +8,11 @@
 
 [[nodiscard]] inline std::string temp_file_name() {
     std::string ret{"temp_file_"};
-
+    std::uniform_int_distribution<int> dist{0, 15};
     std::random_device rd;
 
     for (int i = 0; i < 64; ++i) { // 64 hexits = 256 bits of entropy
-        ret.push_back("0123456789ABCDEF"[rd() % 16]);
+        ret.push_back("0123456789ABCDEF"[dist(rd)]);
     }
 
     ret += ".tmp";
