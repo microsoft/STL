534d275938cc729cd29edd747e9cfee667ae1964
Alex Guteniev <gutenev@gmail.com>
2021-10-06 20:22:36 +0300

    improve test coverage

--- tests/std/tests/P1272R4_byteswap/test.cpp
+++ tests/std/tests/P1272R4_byteswap/test.cpp
@@ -6,26 +6,42 @@
 
 using namespace std;
 
-static_assert(byteswap('\x13') == '\x13');
-static_assert(byteswap(static_cast<signed char>('\x13')) == static_cast<signed char>('\x13'));
-static_assert(byteswap(static_cast<unsigned char>('\x13')) == static_cast<unsigned char>('\x13'));
-static_assert(byteswap(static_cast<short>(0xAC34)) == static_cast<short>(0x34AC));
-static_assert(byteswap(static_cast<unsigned short>(0xAC34)) == static_cast<unsigned short>(0x34AC));
-static_assert(byteswap(static_cast<long>(0x1234ABCD)) == static_cast<long>(0xCDAB3412));
-static_assert(byteswap(static_cast<unsigned long>(0x1234ABCD)) == static_cast<unsigned long>(0xCDAB3412));
-static_assert(byteswap(static_cast<long long>(0x1234567890ABCDEF)) == static_cast<long long>(0xEFCDAB9078563412));
-static_assert(byteswap(static_cast<unsigned long long>(0x1234567890ABCDEF))
-              == static_cast<unsigned long long>(0xEFCDAB9078563412));
+template <typename T>
+constexpr bool test_byteswap(const auto src, const auto result) {
+    static_assert(noexcept(byteswap(T{})));
+    return byteswap(static_cast<T>(src)) == static_cast<T>(result);
+}
+
+constexpr bool test_byteswap_all_types() {
+    assert(test_byteswap<bool>(true, true));
+
+    assert(test_byteswap<char>(0x13, 0x13));
+    assert(test_byteswap<signed char>(0x13, 0x13));
+    assert(test_byteswap<unsigned char>(0x13, 0x13));
+#ifdef __cpp_char8_t
+    assert(test_byteswap<char8_t>(0x13, 0x13));
+#endif
+
+    assert(test_byteswap<short>(0xAC34, 0x34AC));
+    assert(test_byteswap<unsigned short>(0xAC34, 0x34AC));
+    assert(test_byteswap<char16_t>(0xAC34, 0x34AC));
+    assert(test_byteswap<wchar_t>(0xAC34, 0x34AC));
+    assert(test_byteswap<__wchar_t>(0xAC34, 0x34AC));
+
+    assert(test_byteswap<int>(0x1234ABCD, 0xCDAB3412));
+    assert(test_byteswap<unsigned int>(0x1234ABCD, 0xCDAB3412));
+    assert(test_byteswap<long>(0x1234ABCD, 0xCDAB3412));
+    assert(test_byteswap<unsigned long>(0x1234ABCD, 0xCDAB3412));
+    assert(test_byteswap<char32_t>(0x1234ABCD, 0xCDAB3412));
+
+    assert(test_byteswap<long long>(0x1234567890ABCDEF, 0xEFCDAB9078563412));
+    assert(test_byteswap<unsigned long long>(0x1234567890ABCDEF, 0xEFCDAB9078563412));
+
+    return true;
+}
+
+static_assert(test_byteswap_all_types());
 
 int main() {
-    assert(byteswap('\x13') == '\x13');
-    assert(byteswap(static_cast<signed char>('\x13')) == static_cast<signed char>('\x13'));
-    assert(byteswap(static_cast<unsigned char>('\x13')) == static_cast<unsigned char>('\x13'));
-    assert(byteswap(static_cast<short>(0xAC34)) == static_cast<short>(0x34AC));
-    assert(byteswap(static_cast<unsigned short>(0xAC34)) == static_cast<unsigned short>(0x34AC));
-    assert(byteswap(static_cast<long>(0x1234ABCD)) == static_cast<long>(0xCDAB3412));
-    assert(byteswap(static_cast<unsigned long>(0x1234ABCD)) == static_cast<unsigned long>(0xCDAB3412));
-    assert(byteswap(static_cast<long long>(0x1234567890ABCDEF)) == static_cast<long long>(0xEFCDAB9078563412));
-    assert(byteswap(static_cast<unsigned long long>(0x1234567890ABCDEF))
-           == static_cast<unsigned long long>(0xEFCDAB9078563412));
+    test_byteswap_all_types();
 }
