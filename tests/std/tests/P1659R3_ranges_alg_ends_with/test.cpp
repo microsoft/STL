91648681da2f03e0457cc9854823df18c832812b
Michael Schellenberger Costa <mschellenbergercosta@googlemail.com>
2021-10-14 15:30:11 +0200

    Begone infinite ranges

--- tests/std/tests/P1659R3_ranges_alg_ends_with/test.cpp
+++ tests/std/tests/P1659R3_ranges_alg_ends_with/test.cpp
@@ -2,111 +2,62 @@
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
 #include <algorithm>
-#include <array>
 #include <cassert>
 #include <concepts>
 #include <ranges>
-#include <span>
 #include <utility>
 using namespace std;
 
 #include <range_algorithm_support.hpp>
 
-constexpr void smoke_test() {
-    using ranges::dangling, ranges::equal_to, ranges::iterator_t, ranges::ends_with;
-
-    constexpr array<pair<int, int>, 3> haystack       = {{{0, 42}, {2, 42}, {4, 42}}};
-    constexpr array<pair<int, int>, 1> short_haystack = {{{4, 42}}};
-    constexpr array<pair<long, long>, 2> needle       = {{{13, 2}, {13, 4}}};
-    constexpr array<pair<long, long>, 2> wrong_needle = {{{13, 2}, {13, 3}}};
-
-    { // Validate sized ranges
-        const same_as<bool> auto match = ends_with(haystack, needle, equal_to{}, get_first, get_second);
-        assert(match);
-
-        const same_as<bool> auto no_match = ends_with(haystack, wrong_needle, equal_to{}, get_first, get_second);
-        assert(!no_match);
-
-        const same_as<bool> auto no_match2 = ends_with(short_haystack, needle, equal_to{}, get_first, get_second);
-        assert(!no_match2);
-    }
-    { // Validate infinite ranges
-        const same_as<bool> auto infinite_needle = ends_with(views::iota(0, 5), views::iota(0));
-        assert(!infinite_needle);
-    }
-    { // Validate sized iterator + sentinel pairs
-        const same_as<bool> auto match = ends_with(
-            haystack.begin(), haystack.end(), needle.begin(), needle.end(), equal_to{}, get_first, get_second);
-        assert(match);
-
-        const same_as<bool> auto no_match = ends_with(haystack.begin(), haystack.end(), wrong_needle.begin(),
-            wrong_needle.end(), equal_to{}, get_first, get_second);
-        assert(!no_match);
-
-        const same_as<bool> auto no_match2 = ends_with(short_haystack.begin(), short_haystack.end(), needle.begin(),
-            needle.end(), equal_to{}, get_first, get_second);
-        assert(!no_match2);
-    }
-    { // Validate unreachable sentinel
-        const same_as<bool> auto unreachable_needle = ends_with(
-            haystack.begin(), haystack.end(), needle.begin(), unreachable_sentinel, equal_to{}, get_first, get_second);
-        assert(!unreachable_needle);
-    }
-}
-
-int main() {
-    STATIC_ASSERT((smoke_test(), true));
-    smoke_test();
-}
-
-#ifndef _PREFAST_ // TRANSITION, GH-1030
 struct instantiator {
-    template <class In1, class In2>
-    static void call() {
-        using ranges::begin, ranges::end, ranges::ends_with, ranges::iterator_t, ranges::forward_range,
-            ranges::sized_range, ranges::sentinel_t;
+    static constexpr pair<int, int> haystack[]       = {{0, 42}, {2, 42}, {4, 42}};
+    static constexpr pair<int, int> short_haystack[] = {{4, 42}};
+    static constexpr pair<long, long> needle[]       = {{13, 2}, {13, 4}};
+    static constexpr pair<long, long> wrong_needle[] = {{13, 2}, {13, 3}};
 
-        In1 in1{span<const int, 0>{}};
-        In2 in2{span<const int, 0>{}};
+    template <ranges::input_range In1, ranges::input_range In2>
+    static constexpr void test() {
+        using ranges::begin, ranges::end, ranges::ends_with, ranges::forward_range, ranges::sized_range;
 
         if constexpr ((forward_range<In1> || sized_range<In1>) &&(forward_range<In2> || sized_range<In2>) ) {
-            if constexpr (!is_permissive) {
-                (void) ends_with(in1, in2);
+            { // Validate ranges
+                const same_as<bool> auto match = ends_with(haystack, needle, equal_to{}, get_first, get_second);
+                assert(match);
+
+                const same_as<bool> auto no_match =
+                    ends_with(haystack, wrong_needle, equal_to{}, get_first, get_second);
+                assert(!no_match);
+
+                const same_as<bool> auto no_match2 =
+                    ends_with(short_haystack, needle, equal_to{}, get_first, get_second);
+                assert(!no_match2);
             }
+            { // Validate iterator + sentinel pairs
+                const same_as<bool> auto match = ends_with(
+                    begin(haystack), end(haystack), begin(needle), end(needle), equal_to{}, get_first, get_second);
+                assert(match);
 
-            BinaryPredicateFor<iterator_t<In1>, iterator_t<In2>> pred{};
-            (void) ends_with(in1, in2, pred);
+                const same_as<bool> auto no_match = ends_with(begin(haystack), end(haystack), begin(wrong_needle),
+                    end(wrong_needle), equal_to{}, get_first, get_second);
+                assert(!no_match);
 
-            HalfProjectedBinaryPredicateFor<iterator_t<In2>> halfpred{};
-            ProjectionFor<iterator_t<In1>> halfproj{};
-            (void) ends_with(in1, in2, halfpred, halfproj);
-
-            ProjectedBinaryPredicate<0, 1> projpred{};
-            ProjectionFor<iterator_t<In1>, 0> proj1{};
-            ProjectionFor<iterator_t<In2>, 1> proj2{};
-            (void) ends_with(in1, in2, projpred, proj1, proj2);
-        }
-
-        if constexpr ((forward_iterator<iterator_t<In1>> || sized_sentinel_for<sentinel_t<In1>, iterator_t<In1>>) &&(
-                          forward_iterator<iterator_t<In2>> || sized_sentinel_for<sentinel_t<In1>, iterator_t<In2>>) ) {
-            if constexpr (!is_permissive) {
-                (void) ends_with(begin(in1), end(in1), begin(in2), end(in2));
+                const same_as<bool> auto no_match2 = ends_with(begin(short_haystack), end(short_haystack),
+                    begin(needle), end(needle), equal_to{}, get_first, get_second);
+                assert(!no_match2);
             }
-
-            BinaryPredicateFor<iterator_t<In1>, iterator_t<In2>> pred{};
-            (void) ends_with(begin(in1), end(in1), begin(in2), end(in2), pred);
-
-            HalfProjectedBinaryPredicateFor<iterator_t<In2>> halfpred{};
-            ProjectionFor<iterator_t<In1>> halfproj{};
-            (void) ends_with(begin(in1), end(in1), begin(in2), end(in2), halfpred, halfproj);
-
-            ProjectedBinaryPredicate<0, 1> projpred{};
-            ProjectionFor<iterator_t<In1>, 0> proj1{};
-            ProjectionFor<iterator_t<In2>, 1> proj2{};
-            (void) ends_with(begin(in1), end(in1), begin(in2), end(in2), projpred, proj1, proj2);
         }
+    }
+
+    template <ranges::input_range In1, ranges::input_range In2>
+    static void call() {
+        test<In1, In2>();
+        STATIC_ASSERT((test<In1, In2>(), true));
     }
 };
 
-template void test_in_in<instantiator, const int, const int>();
+int main() {
+#ifndef _PREFAST_ // TRANSITION, GH-1030
+    test_in_in<instantiator, const pair<int, int>, const pair<int, int>>();
 #endif // TRANSITION, GH-1030
+}
--- tests/std/tests/P1659R3_ranges_alg_starts_with/test.cpp
+++ tests/std/tests/P1659R3_ranges_alg_starts_with/test.cpp
@@ -2,137 +2,111 @@
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
 #include <algorithm>
-#include <array>
 #include <cassert>
 #include <concepts>
 #include <ranges>
-#include <span>
 #include <utility>
 using namespace std;
 
 #include <range_algorithm_support.hpp>
 
-constexpr void smoke_test() {
-    using ranges::dangling, ranges::equal_to, ranges::iterator_t, ranges::starts_with;
-
-    constexpr array<pair<int, int>, 3> haystack       = {{{0, 42}, {2, 42}, {4, 42}}};
-    constexpr array<pair<int, int>, 1> short_haystack = {{{0, 42}}};
-    constexpr array<pair<long, long>, 2> needle       = {{{13, 0}, {13, 2}}};
-    constexpr array<pair<long, long>, 2> wrong_needle = {{{13, 0}, {13, 3}}};
-
-    { // Validate sized ranges
-        const same_as<bool> auto match = starts_with(haystack, needle, equal_to{}, get_first, get_second);
-        assert(match);
-
-        const same_as<bool> auto no_match = starts_with(haystack, wrong_needle, equal_to{}, get_first, get_second);
-        assert(!no_match);
-
-        const same_as<bool> auto no_match2 = starts_with(short_haystack, needle, equal_to{}, get_first, get_second);
-        assert(!no_match2);
-    }
-    { // Validate non-sized ranges
-        const same_as<bool> auto match = starts_with(
-            basic_borrowed_range{haystack}, basic_borrowed_range{needle}, equal_to{}, get_first, get_second);
-        assert(match);
-
-        const same_as<bool> auto no_match = starts_with(
-            basic_borrowed_range{haystack}, basic_borrowed_range{wrong_needle}, equal_to{}, get_first, get_second);
-        assert(!no_match);
-
-        const same_as<bool> auto no_match2 = starts_with(
-            basic_borrowed_range{short_haystack}, basic_borrowed_range{needle}, equal_to{}, get_first, get_second);
-        assert(!no_match2);
-    }
-    { // Validate infinite ranges
-        const same_as<bool> auto infinite_haystack = starts_with(views::iota(0), views::iota(0, 5));
-        assert(infinite_haystack);
-
-        const same_as<bool> auto infinite_needle = starts_with(views::iota(0, 5), views::iota(0));
-        assert(!infinite_needle);
-    }
-    { // Validate sized iterator + sentinel pairs
-        const same_as<bool> auto match = starts_with(
-            haystack.begin(), haystack.end(), needle.begin(), needle.end(), equal_to{}, get_first, get_second);
-        assert(match);
-
-        const same_as<bool> auto no_match = starts_with(haystack.begin(), haystack.end(), wrong_needle.begin(),
-            wrong_needle.end(), equal_to{}, get_first, get_second);
-        assert(!no_match);
-
-        const same_as<bool> auto no_match2 = starts_with(short_haystack.begin(), short_haystack.end(), needle.begin(),
-            needle.end(), equal_to{}, get_first, get_second);
-        assert(!no_match2);
-    }
-    { // Validate non-sized iterator + sentinel pairs
-        basic_borrowed_range wrapped_haystack{haystack};
-        basic_borrowed_range wrapped_needle{needle};
-        const same_as<bool> auto match = starts_with(wrapped_haystack.begin(), wrapped_haystack.end(),
-            wrapped_needle.begin(), wrapped_needle.end(), equal_to{}, get_first, get_second);
-        assert(match);
-
-        basic_borrowed_range wrapped_haystack2{haystack};
-        basic_borrowed_range wrapped_wrong_needle{wrong_needle};
-        const same_as<bool> auto no_match = starts_with(wrapped_haystack2.begin(), wrapped_haystack2.end(),
-            wrapped_wrong_needle.begin(), wrapped_wrong_needle.end(), equal_to{}, get_first, get_second);
-        assert(!no_match);
-
-        basic_borrowed_range wrapped_short_haystack{short_haystack};
-        basic_borrowed_range wrapped_needle2{needle};
-        const same_as<bool> auto no_match2 = starts_with(wrapped_short_haystack.begin(), wrapped_short_haystack.end(),
-            wrapped_needle2.begin(), wrapped_needle2.end(), equal_to{}, get_first, get_second);
-        assert(!no_match2);
-    }
-    { // Validate unreachable sentinels
-        basic_borrowed_range wrapped_haystack{haystack};
-        basic_borrowed_range wrapped_needle{needle};
-        const same_as<bool> auto unreachable_haystack = starts_with(wrapped_haystack.begin(), unreachable_sentinel,
-            wrapped_needle.begin(), wrapped_needle.end(), equal_to{}, get_first, get_second);
-        assert(unreachable_haystack);
-
-        basic_borrowed_range wrapped_short_haystack{short_haystack};
-        basic_borrowed_range wrapped_needle2{needle};
-        const same_as<bool> auto unreachable_needle =
-            starts_with(wrapped_short_haystack.begin(), wrapped_short_haystack.end(), wrapped_needle2.begin(),
-                unreachable_sentinel, equal_to{}, get_first, get_second);
-        assert(!unreachable_needle);
-    }
-}
-
-int main() {
-    STATIC_ASSERT((smoke_test(), true));
-    smoke_test();
-}
-
-#ifndef _PREFAST_ // TRANSITION, GH-1030
 struct instantiator {
-    template <class In1, class In2>
-    static void call() {
-        using ranges::begin, ranges::end, ranges::starts_with, ranges::iterator_t;
+    static constexpr pair<int, int> haystack[]       = {{0, 42}, {2, 42}, {4, 42}};
+    static constexpr pair<int, int> short_haystack[] = {{0, 42}};
+    static constexpr pair<long, long> needle[]       = {{13, 0}, {13, 2}};
+    static constexpr pair<long, long> wrong_needle[] = {{13, 0}, {13, 3}};
 
-        In1 in1{span<const int, 0>{}};
-        In2 in2{span<const int, 0>{}};
+    template <ranges::input_range In1, ranges::input_range In2>
+    static constexpr void test() {
+        using ranges::begin, ranges::end, ranges::starts_with;
 
-        if constexpr (!is_permissive) {
-            (void) starts_with(in1, in2);
-            (void) starts_with(begin(in1), end(in1), begin(in2), end(in2));
+        { // Validate sized ranges
+            const same_as<bool> auto match = starts_with(haystack, needle, equal_to{}, get_first, get_second);
+            assert(match);
+
+            const same_as<bool> auto no_match = starts_with(haystack, wrong_needle, equal_to{}, get_first, get_second);
+            assert(!no_match);
+
+            const same_as<bool> auto no_match2 = starts_with(short_haystack, needle, equal_to{}, get_first, get_second);
+            assert(!no_match2);
         }
+        { // Validate non-sized ranges
+            const same_as<bool> auto match = starts_with(
+                basic_borrowed_range{haystack}, basic_borrowed_range{needle}, equal_to{}, get_first, get_second);
+            assert(match);
 
-        BinaryPredicateFor<iterator_t<In1>, iterator_t<In2>> pred{};
-        (void) starts_with(in1, in2, pred);
-        (void) starts_with(begin(in1), end(in1), begin(in2), end(in2), pred);
+            const same_as<bool> auto no_match = starts_with(
+                basic_borrowed_range{haystack}, basic_borrowed_range{wrong_needle}, equal_to{}, get_first, get_second);
+            assert(!no_match);
 
-        HalfProjectedBinaryPredicateFor<iterator_t<In2>> halfpred{};
-        ProjectionFor<iterator_t<In1>> halfproj{};
-        (void) starts_with(in1, in2, halfpred, halfproj);
-        (void) starts_with(begin(in1), end(in1), begin(in2), end(in2), halfpred, halfproj);
+            const same_as<bool> auto no_match2 = starts_with(
+                basic_borrowed_range{short_haystack}, basic_borrowed_range{needle}, equal_to{}, get_first, get_second);
+            assert(!no_match2);
+        }
+        { // Validate infinite ranges
+            const same_as<bool> auto infinite_haystack = starts_with(views::iota(0), views::iota(0, 5));
+            assert(infinite_haystack);
 
-        ProjectedBinaryPredicate<0, 1> projpred{};
-        ProjectionFor<iterator_t<In1>, 0> proj1{};
-        ProjectionFor<iterator_t<In2>, 1> proj2{};
-        (void) starts_with(in1, in2, projpred, proj1, proj2);
-        (void) starts_with(begin(in1), end(in1), begin(in2), end(in2), projpred, proj1, proj2);
+            const same_as<bool> auto infinite_needle = starts_with(views::iota(0, 5), views::iota(0));
+            assert(!infinite_needle);
+        }
+        { // Validate sized iterator + sentinel pairs
+            const same_as<bool> auto match = starts_with(
+                begin(haystack), end(haystack), begin(needle), end(needle), equal_to{}, get_first, get_second);
+            assert(match);
+
+            const same_as<bool> auto no_match = starts_with(begin(haystack), end(haystack), begin(wrong_needle),
+                end(wrong_needle), equal_to{}, get_first, get_second);
+            assert(!no_match);
+
+            const same_as<bool> auto no_match2 = starts_with(begin(short_haystack), end(short_haystack), begin(needle),
+                end(needle), equal_to{}, get_first, get_second);
+            assert(!no_match2);
+        }
+        { // Validate non-sized iterator + sentinel pairs
+            basic_borrowed_range wrapped_haystack{haystack};
+            basic_borrowed_range wrapped_needle{needle};
+            const same_as<bool> auto match = starts_with(begin(wrapped_haystack), end(wrapped_haystack),
+                begin(wrapped_needle), end(wrapped_needle), equal_to{}, get_first, get_second);
+            assert(match);
+
+            basic_borrowed_range wrapped_haystack2{haystack};
+            basic_borrowed_range wrapped_wrong_needle{wrong_needle};
+            const same_as<bool> auto no_match = starts_with(begin(wrapped_haystack), end(wrapped_haystack),
+                begin(wrapped_wrong_needle), end(wrapped_wrong_needle), equal_to{}, get_first, get_second);
+            assert(!no_match);
+
+            basic_borrowed_range wrapped_short_haystack{short_haystack};
+            basic_borrowed_range wrapped_needle2{needle};
+            const same_as<bool> auto no_match2 = starts_with(begin(wrapped_short_haystack), end(wrapped_short_haystack),
+                begin(wrapped_needle2), end(wrapped_needle2), equal_to{}, get_first, get_second);
+            assert(!no_match2);
+        }
+        { // Validate unreachable sentinels
+            basic_borrowed_range wrapped_haystack{haystack};
+            basic_borrowed_range wrapped_needle{needle};
+            const same_as<bool> auto unreachable_haystack = starts_with(begin(wrapped_haystack), unreachable_sentinel,
+                begin(wrapped_needle), end(wrapped_needle), equal_to{}, get_first, get_second);
+            assert(unreachable_haystack);
+
+            basic_borrowed_range wrapped_short_haystack{short_haystack};
+            basic_borrowed_range wrapped_needle2{needle};
+            const same_as<bool> auto unreachable_needle =
+                starts_with(begin(wrapped_short_haystack), end(wrapped_short_haystack), begin(wrapped_needle),
+                    unreachable_sentinel, equal_to{}, get_first, get_second);
+            assert(!unreachable_needle);
+        }
+    }
+
+    template <ranges::input_range In1, ranges::input_range In2>
+    static void call() {
+        test<In1, In2>();
+        STATIC_ASSERT((test<In1, In2>(), true));
     }
 };
 
-template void test_in_in<instantiator, const int, const int>();
+int main() {
+#ifndef _PREFAST_ // TRANSITION, GH-1030
+    test_in_in<instantiator, const pair<int, int>, const pair<int, int>>();
 #endif // TRANSITION, GH-1030
+}
