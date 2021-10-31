639c3192637612669543291eff5a3e2190e357e9
Alex Guteniev <gutenev@gmail.com>
2021-10-31 13:28:45 +0200

    inner

--- stl/inc/xthreads.h
+++ stl/inc/xthreads.h
@@ -143,54 +143,53 @@ _INLINE_VAR constexpr size_t _Stl_critical_section_size      = 36;
 _INLINE_VAR constexpr size_t _Stl_critical_section_alignment = 4;
 #endif // ^^^32  - bit OS ^^^
 
-struct alignas(_Stl_critical_section_alignment) _Data_win7_t {
-    static void __fastcall _Destroy_impl(_Data_win7_t*) {}
-    static void __fastcall _Lock_impl(_Data_win7_t* _Data) {
-        _Smtx_lock_exclusive(&_Data->_Mtx);
-    }
-
-    static bool __fastcall _Try_lock_impl(_Data_win7_t* _Data) {
-        return _Smtx_try_lock_exclusive(&_Data->_Mtx) != 0;
-    }
-
-#ifdef _M_IX86
-    static bool __fastcall _Try_lock_for_impl(_Data_win7_t* _Data, unsigned int, unsigned int) {
-        // STL will call try_lock_for once again if this call will not succeed
-        return _Data_win7_t::_Try_lock_impl(_Data);
-    }
-#else // ^^^ x86 ^^^ / vvv not x86 vvv
-    static bool __fastcall _Try_lock_for_impl(_Data_win7_t* _Data, unsigned int) {
-        // STL will call try_lock_for once again if this call will not succeed
-        return _Data_win7_t::_Try_lock_impl(_Data);
-    }
-#endif // ^^^ not x86
-
-    static void __fastcall _Unlock_impl(_Data_win7_t* _Data) {
-        _Smtx_unlock_exclusive(&_Data->_Mtx);
-    }
-
-    struct _VTable {
-        void(__fastcall* _Destroy)(_Data_win7_t*);
-        void(__fastcall* _Lock)(_Data_win7_t*);
-        bool(__fastcall* _Try_lock)(_Data_win7_t*);
-#ifdef _M_IX86
-        bool(__fastcall* _Try_lock_for)(_Data_win7_t*, unsigned int, unsigned int);
-#else // ^^^ x86 ^^^ / vvv not x86 vvv
-        bool(__fastcall* _Try_lock_for)(_Data_win7_t*, unsigned int);
-#endif // ^^^ not x86
-        void(__fastcall* _Unlock)(_Data_win7_t*);
-    };
-
-    static constexpr _VTable _Vtbl{_Destroy_impl, _Lock_impl, _Try_lock_impl, _Try_lock_for_impl, _Unlock_impl};
-
-    const _VTable* _VPtr = &_Vtbl;
-    _Smtx_t _Mtx         = {};
-};
-
 // Corresponds to internal _Mtx_internal_imp_t with changes to expose it in headers and make its construction constexpr
 struct _Mtx_internal_imp_2_t {
-    int _Type;
+    struct alignas(_Stl_critical_section_alignment) _Data_win7_t {
+        static void __fastcall _Destroy_impl(_Data_win7_t*) {}
+        static void __fastcall _Lock_impl(_Data_win7_t* _Data) {
+            _Smtx_lock_exclusive(&_Data->_Mtx);
+        }
 
+        static bool __fastcall _Try_lock_impl(_Data_win7_t* _Data) {
+            return _Smtx_try_lock_exclusive(&_Data->_Mtx) != 0;
+        }
+
+#ifdef _M_IX86
+        static bool __fastcall _Try_lock_for_impl(_Data_win7_t* _Data, unsigned int, unsigned int) {
+            // STL will call try_lock_for once again if this call will not succeed
+            return _Data_win7_t::_Try_lock_impl(_Data);
+        }
+#else // ^^^ x86 ^^^ / vvv not x86 vvv
+        static bool __fastcall _Try_lock_for_impl(_Data_win7_t* _Data, unsigned int) {
+            // STL will call try_lock_for once again if this call will not succeed
+            return _Data_win7_t::_Try_lock_impl(_Data);
+        }
+#endif // ^^^ not x86
+
+        static void __fastcall _Unlock_impl(_Data_win7_t* _Data) {
+            _Smtx_unlock_exclusive(&_Data->_Mtx);
+        }
+
+        struct _VTable {
+            void(__fastcall* _Destroy)(_Data_win7_t*);
+            void(__fastcall* _Lock)(_Data_win7_t*);
+            bool(__fastcall* _Try_lock)(_Data_win7_t*);
+#ifdef _M_IX86
+            bool(__fastcall* _Try_lock_for)(_Data_win7_t*, unsigned int, unsigned int);
+#else // ^^^ x86 ^^^ / vvv not x86 vvv
+            bool(__fastcall* _Try_lock_for)(_Data_win7_t*, unsigned int);
+#endif // ^^^ not x86
+            void(__fastcall* _Unlock)(_Data_win7_t*);
+        };
+
+        static constexpr _VTable _Vtbl{_Destroy_impl, _Lock_impl, _Try_lock_impl, _Try_lock_for_impl, _Unlock_impl};
+
+        const _VTable* _VPtr = &_Vtbl;
+        _Smtx_t _Mtx         = {};
+    };
+
+    int _Type;
     union alignas(_Stl_critical_section_alignment) _Data_t {
         char _Data[_Stl_critical_section_size];
         _Data_win7_t _Data_win7;
