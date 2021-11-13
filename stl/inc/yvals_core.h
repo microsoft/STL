2de0fa57120fab8d8e575f9e585ad624fc5238cd
Adam Bucior <35536269+AdamBucior@users.noreply.github.com>
2021-11-13 04:33:38 +0100

    Implement P1072R10 `basic_string::resize_and_overwrite` (#2258)

--- stl/inc/yvals_core.h
+++ stl/inc/yvals_core.h
@@ -276,6 +276,7 @@
 // P0448R4 <spanstream>
 // P0943R6 Supporting C Atomics In C++
 // P1048R1 is_scoped_enum
+// P1072R10 basic_string::resize_and_overwrite
 // P1132R7 out_ptr(), inout_ptr()
 // P1147R1 Printing volatile Pointers
 // P1272R4 byteswap()
@@ -1376,10 +1377,11 @@
 #define __cpp_lib_ranges_starts_ends_with 202106L
 #endif // __cpp_lib_concepts
 
-#define __cpp_lib_spanstream      202106L
-#define __cpp_lib_stdatomic_h     202011L
-#define __cpp_lib_string_contains 202011L
-#define __cpp_lib_to_underlying   202102L
+#define __cpp_lib_spanstream                  202106L
+#define __cpp_lib_stdatomic_h                 202011L
+#define __cpp_lib_string_contains             202011L
+#define __cpp_lib_string_resize_and_overwrite 202110L
+#define __cpp_lib_to_underlying               202102L
 #endif // _HAS_CXX23
 
 #define __cpp_lib_experimental_erase_if   201411L
