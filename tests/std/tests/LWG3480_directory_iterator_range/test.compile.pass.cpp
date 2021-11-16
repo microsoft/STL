54dc94f36ba8d88a65e2168cfc0a900495ed9690
Igor Zhukov <fsb4000@yandex.ru>
2021-11-16 21:08:41 +0700

    Implement LWG-3480, directory_iterator is a range

---
+++ tests/std/tests/LWG3480_directory_iterator_range/test.compile.pass.cpp
@@ -0,0 +1,25 @@
+// Copyright (c) Microsoft Corporation.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+#include <filesystem>
+#include <ranges>
+
+using namespace std;
+using namespace std::filesystem;
+using namespace std::ranges;
+
+bool is_tiff(const std::filesystem::directory_entry& entry) {
+    return entry.is_regular_file() && entry.path().extension().native() == L"tif";
+}
+
+void test1(directory_iterator dir) {
+    [[maybe_unused]] auto tif_files     = dir | views::filter(is_tiff);
+    [[maybe_unused]] auto first_5_files = dir | views::take(5);
+}
+
+void test_recursive(recursive_directory_iterator dir) {
+    [[maybe_unused]] auto tif_files     = dir | views::filter(is_tiff);
+    [[maybe_unused]] auto first_5_files = dir | views::take(5);
+}
+
+int main() {} // COMPILE-ONLY
