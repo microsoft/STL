e36788f427f8975165b0c29b94ad8233a655c74a
Stephan T. Lavavej <stl@nuwen.net>
2021-11-22 16:51:51 -0800

    Code review feedback (typo, empty line).

--- tests/std/tests/P0896R4_views_join/test.cpp
+++ tests/std/tests/P0896R4_views_join/test.cpp
@@ -564,9 +564,9 @@ int main() {
     { // Joining a non-const view without const qualified begin and end methods
         vector<vector<int>> nested_vectors = {{0}, {1, 2, 3}, {99}, {4, 5, 6, 7}, {}, {8, 9, 10}};
         auto RemoveSmallVectors            = [](const vector<int>& inner_vector) { return inner_vector.size() > 2; };
-        auto filted_and_joined             = nested_vectors | views::filter(RemoveSmallVectors) | views::join;
+        auto filtered_and_joined           = nested_vectors | views::filter(RemoveSmallVectors) | views::join;
         static constexpr int result[]      = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-        assert(ranges::equal(filted_and_joined, result));
+        assert(ranges::equal(filtered_and_joined, result));
     }
 
     STATIC_ASSERT(instantiation_test());
