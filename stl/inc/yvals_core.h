8deb17921a0d71e1d29dcf555c09a838cdf01407
Stephan T. Lavavej <stl@nuwen.net>
2021-12-16 02:12:53 -0800

    `and` is super ultra forbidden in this codebase.

--- stl/inc/yvals_core.h
+++ stl/inc/yvals_core.h
@@ -421,7 +421,7 @@
 #define _NODISCARD_CTOR
 #endif
 
-#if defined(__CUDACC__) and !defined(__clang__) // TRANSITION, VSO-568006
+#if defined(__CUDACC__) && !defined(__clang__) // TRANSITION, VSO-568006
 #define _NODISCARD_FRIEND friend
 #else // ^^^ workaround ^^^ / vvv no workaround vvv
 #define _NODISCARD_FRIEND _NODISCARD friend
