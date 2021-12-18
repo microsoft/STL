376bf286599248ccd19540e3f68ffd77b39bd06c
Alex Guteniev <gutenev@gmail.com>
2021-12-18 19:39:32 +0200

    fix test pass

--- tests/std/tests/GH_002431_byte_range_find_with_unreachable_sentinel/test.cpp
+++ tests/std/tests/GH_002431_byte_range_find_with_unreachable_sentinel/test.cpp
@@ -18,22 +18,26 @@ int main() {
     const size_t page  = si.dwPageSize;
     const size_t alloc = max<size_t>(page * 2, si.dwAllocationGranularity);
 
-    void* p  = VirtualAlloc(nullptr, alloc, MEM_RESERVE, PAGE_NOACCESS);
+    void* p = VirtualAlloc(nullptr, alloc, MEM_RESERVE, PAGE_NOACCESS);
+    assert(p != nullptr);
     void* p2 = VirtualAlloc(p, page, MEM_COMMIT, PAGE_READWRITE);
+    assert(p2 != nullptr);
 
     const auto p3    = reinterpret_cast<char*>(p2) + page - 3;
     p3[0]            = '1';
     p3[1]            = '2';
     p3[2]            = '3';
-    const auto bingo = ranges::find(p3, std::unreachable_sentinel, '3');
+    const auto bingo = ranges::find(p3, unreachable_sentinel, '3');
     assert(bingo == p3 + 2);
 
-    const auto bingo2 = ranges::find(p3 - 16, std::unreachable_sentinel, '2');
+    const auto bingo2 = ranges::find(p3 - 16, unreachable_sentinel, '2');
     assert(bingo2 == p3 + 1);
 
-    const auto bingo3 = ranges::find(p3 - 32, std::unreachable_sentinel, '1');
+    const auto bingo3 = ranges::find(p3 - 32, unreachable_sentinel, '1');
     assert(bingo3 == p3);
 
-    const auto bingo4 = ranges::find(p3 - 64, std::unreachable_sentinel, '3');
+    const auto bingo4 = ranges::find(p3 - 64, unreachable_sentinel, '3');
     assert(bingo4 == p3 + 2);
+
+    VirtualFree(p, 0, MEM_RELEASE);
 }
