bc48cced6d554d85f2dd8fc760ae4c23a4d4f57e
Matt Stephanson <stephanson.matt@gmail.com>
2022-01-21 09:51:38 -0800

    fix test

--- tests/std/tests/GH_001964_constexpr_generate_canonical/test.cpp
+++ tests/std/tests/GH_001964_constexpr_generate_canonical/test.cpp
@@ -6,7 +6,7 @@
 
 using namespace std;
 
-int naive_iterations(const int bits, const size_t gmin, const size_t gmax) {
+int naive_iterations(const int bits, const uint64_t gmin, const uint64_t gmax) {
     // Naive implementation of [rand.util.canonical]. Note that for large values of range, it's possible that
     // log2(range) == bits when range < 2^bits. This can lead to incorrect results, so we can't use this function as
     // a reference for all values.
@@ -15,9 +15,9 @@ int naive_iterations(const int bits, const size_t gmin, const size_t gmax) {
     return static_cast<int>(ceil(static_cast<double>(bits) / log2(static_cast<double>(range))));
 }
 
-bool test(const int target_bits) {
+void test(const int target_bits) {
     // Increase the range until the number of iterations repeats.
-    size_t range = 2;
+    uint64_t range = 2;
     int k        = 0;
     int prev_k   = -1;
     while (k != prev_k) {
@@ -33,9 +33,9 @@ bool test(const int target_bits) {
     for (; k > 0; --k) {
         // The largest range such that k iterations generating [1,range] produces less than target_bits bits.
         if (k == 1) {
-            range = ~size_t{0} >> (64 - target_bits);
+            range = ~uint64_t{0} >> (64 - target_bits);
         } else {
-            range = static_cast<size_t>(ceil(pow(2.0, static_cast<double>(target_bits) / k))) - 1;
+            range = static_cast<uint64_t>(ceil(pow(2.0, static_cast<double>(target_bits) / k))) - 1;
         }
 
         int k0 = (k == 1) ? 2 : naive_iterations(target_bits, 1, range);
