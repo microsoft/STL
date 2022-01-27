5fbb2748fe666b03826fed60ed66646703011bc2
A. Jiang <de34@live.cn>
2022-01-27 13:29:16 +0800

    Add files via upload

---
+++ tests/std/tests/test.cpp
@@ -0,0 +1,45 @@
+// Copyright (c) Microsoft Corporation.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+#include <assert.h>
+#include <deque>
+
+using namespace std;
+
+void test_gh_00108();
+
+int main() {
+    struct recur {
+        deque<recur> d;
+    };
+
+    recur x;
+
+    for (int n = 0; n < 1000; ++n) {
+        x.d.push_back(recur{});
+
+        if (x.d.size() < 26) {
+            continue;
+        }
+
+        x.d.pop_front();
+    }
+
+    test_gh_00108();
+}
+
+void test_gh_00108() {
+    struct recur_payload {
+        int n;
+        deque<recur_payload> d;
+    };
+
+    recur_payload x{};
+
+    x.d.push_back({ 10 });
+    x.d.push_back({ 20 });
+    x.d.push_front({ 30 });
+    x.d.push_front({ 40 });
+
+    assert(x.d.size() == 4 && x.d[0].n == 40 && x.d[1].n == 30 && x.d[2].n == 10 && x.d[3].n == 20);
+}
