9fe59b849aa15aec27972f90815a8f6668e17403
Igor Zhukov <fsb4000@yandex.ru>
2022-02-08 21:05:17 +0700

    add coverage for default constructed case
    Co-authored-by: Michael Schellenberger Costa <mschellenbergercosta@gmail.com>

--- tests/std/tests/P0718R2_atomic_smart_ptrs/test.cpp
+++ tests/std/tests/P0718R2_atomic_smart_ptrs/test.cpp
@@ -224,7 +224,8 @@ void ensure_nonmember_calls_compile() {
 
 #ifndef __EDG__ // TRANSITION, DevCom-1656924
 // LWG-3661: constinit atomic<shared_ptr<T>> a(nullptr); should work
-constinit atomic<shared_ptr<bool>> a(nullptr);
+constinit atomic<shared_ptr<bool>> a{};
+constinit atomic<shared_ptr<bool>> b{nullptr};
 #endif // __EDG__
 
 int main() {
