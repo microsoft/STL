13a53adb4b4d331da433a83b65125ae7f90e0975
A. Jiang <de34@live.cn>
2022-02-09 10:08:34 +0800

    Move static_asserts out of main in the compile-only test
    Co-authored-by: Casey Carter <cartec69@gmail.com>

--- tests/std/tests/LWG3610_iota_view_size_and_integer_class/test.compile.pass.cpp
+++ tests/std/tests/LWG3610_iota_view_size_and_integer_class/test.compile.pass.cpp
@@ -10,42 +10,42 @@ concept CanSize = requires(R& r) {
     ranges::size(r);
 };
 
-int main() {
-    // Validate standard signed integer types
-    static_assert(CanSize<ranges::iota_view<signed char, _Signed128>>);
-    static_assert(CanSize<ranges::iota_view<signed char, _Unsigned128>>);
-    static_assert(CanSize<ranges::iota_view<short, _Signed128>>);
-    static_assert(CanSize<ranges::iota_view<short, _Unsigned128>>);
-    static_assert(CanSize<ranges::iota_view<int, _Signed128>>);
-    static_assert(CanSize<ranges::iota_view<int, _Unsigned128>>);
-    static_assert(CanSize<ranges::iota_view<long, _Signed128>>);
-    static_assert(CanSize<ranges::iota_view<long, _Unsigned128>>);
-    static_assert(CanSize<ranges::iota_view<long long, _Signed128>>);
-    static_assert(CanSize<ranges::iota_view<long long, _Unsigned128>>);
+// Validate standard signed integer types
+static_assert(CanSize<ranges::iota_view<signed char, _Signed128>>);
+static_assert(CanSize<ranges::iota_view<signed char, _Unsigned128>>);
+static_assert(CanSize<ranges::iota_view<short, _Signed128>>);
+static_assert(CanSize<ranges::iota_view<short, _Unsigned128>>);
+static_assert(CanSize<ranges::iota_view<int, _Signed128>>);
+static_assert(CanSize<ranges::iota_view<int, _Unsigned128>>);
+static_assert(CanSize<ranges::iota_view<long, _Signed128>>);
+static_assert(CanSize<ranges::iota_view<long, _Unsigned128>>);
+static_assert(CanSize<ranges::iota_view<long long, _Signed128>>);
+static_assert(CanSize<ranges::iota_view<long long, _Unsigned128>>);
 
-    // Validate standard unsigned integer types
-    static_assert(CanSize<ranges::iota_view<unsigned char, _Signed128>>);
-    static_assert(CanSize<ranges::iota_view<unsigned char, _Unsigned128>>);
-    static_assert(CanSize<ranges::iota_view<unsigned short, _Signed128>>);
-    static_assert(CanSize<ranges::iota_view<unsigned short, _Unsigned128>>);
-    static_assert(CanSize<ranges::iota_view<unsigned int, _Signed128>>);
-    static_assert(CanSize<ranges::iota_view<unsigned int, _Unsigned128>>);
-    static_assert(CanSize<ranges::iota_view<unsigned long, _Signed128>>);
-    static_assert(CanSize<ranges::iota_view<unsigned long, _Unsigned128>>);
-    static_assert(CanSize<ranges::iota_view<unsigned long long, _Signed128>>);
-    static_assert(CanSize<ranges::iota_view<unsigned long long, _Unsigned128>>);
+// Validate standard unsigned integer types
+static_assert(CanSize<ranges::iota_view<unsigned char, _Signed128>>);
+static_assert(CanSize<ranges::iota_view<unsigned char, _Unsigned128>>);
+static_assert(CanSize<ranges::iota_view<unsigned short, _Signed128>>);
+static_assert(CanSize<ranges::iota_view<unsigned short, _Unsigned128>>);
+static_assert(CanSize<ranges::iota_view<unsigned int, _Signed128>>);
+static_assert(CanSize<ranges::iota_view<unsigned int, _Unsigned128>>);
+static_assert(CanSize<ranges::iota_view<unsigned long, _Signed128>>);
+static_assert(CanSize<ranges::iota_view<unsigned long, _Unsigned128>>);
+static_assert(CanSize<ranges::iota_view<unsigned long long, _Signed128>>);
+static_assert(CanSize<ranges::iota_view<unsigned long long, _Unsigned128>>);
 
-    // Validate other integer types (other than bool)
-    static_assert(CanSize<ranges::iota_view<char, _Signed128>>);
-    static_assert(CanSize<ranges::iota_view<char, _Unsigned128>>);
-    static_assert(CanSize<ranges::iota_view<wchar_t, _Signed128>>);
-    static_assert(CanSize<ranges::iota_view<wchar_t, _Signed128>>);
+// Validate other integer types (other than bool)
+static_assert(CanSize<ranges::iota_view<char, _Signed128>>);
+static_assert(CanSize<ranges::iota_view<char, _Unsigned128>>);
+static_assert(CanSize<ranges::iota_view<wchar_t, _Signed128>>);
+static_assert(CanSize<ranges::iota_view<wchar_t, _Signed128>>);
 #ifdef __cpp_char8_t
-    static_assert(CanSize<ranges::iota_view<char8_t, _Signed128>>);
-    static_assert(CanSize<ranges::iota_view<char8_t, _Unsigned128>>);
+static_assert(CanSize<ranges::iota_view<char8_t, _Signed128>>);
+static_assert(CanSize<ranges::iota_view<char8_t, _Unsigned128>>);
 #endif // __cpp_char8_t
-    static_assert(CanSize<ranges::iota_view<char16_t, _Signed128>>);
-    static_assert(CanSize<ranges::iota_view<char16_t, _Unsigned128>>);
-    static_assert(CanSize<ranges::iota_view<char32_t, _Signed128>>);
-    static_assert(CanSize<ranges::iota_view<char32_t, _Unsigned128>>);
-}
+static_assert(CanSize<ranges::iota_view<char16_t, _Signed128>>);
+static_assert(CanSize<ranges::iota_view<char16_t, _Unsigned128>>);
+static_assert(CanSize<ranges::iota_view<char32_t, _Signed128>>);
+static_assert(CanSize<ranges::iota_view<char32_t, _Unsigned128>>);
+
+int main() {} // COMPILE-ONLY
