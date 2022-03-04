11c0d1a5aac37c193d6cb71950520e84cb773464
Stephan T. Lavavej <stl@nuwen.net>
2022-03-04 10:43:18 -0800

    Weekly update.

--- src/weekly_table.ts
+++ src/weekly_table.ts
@@ -256,4 +256,5 @@ export const weekly_table: WeeklyRow[] = [
     { date: '2022-02-11', vso: 187, libcxx: 586 },
     { date: '2022-02-18', vso: 189, libcxx: 622 },
     { date: '2022-02-25', vso: 191, libcxx: 622 },
+    { date: '2022-03-04', vso: 192, libcxx: 626 },
 ];
