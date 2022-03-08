87170f75a541d44240252e4db4597976fd7263a9
Stephan T. Lavavej <stl@nuwen.net>
2022-03-07 16:59:04 -0800

    Define _EXPORT_STD and __cpp_lib_modules.
    _EXPORT_STD is activated by defined(_BUILD_STD_MODULE) and further
    guarded by _HAS_CXX23 because we don't want to allow Time-Traveling
    Standard Library Modules. However, this isn't further guarded for
    compilers. If someone (including us) wants to build Standard Library
    Modules with an untested compiler, they can try. Only the feature-test
    macro is guarded to declare support for the compilers we've tested.

--- tests/std/tests/VSO_0157762_feature_test_macros/test.compile.pass.cpp
+++ tests/std/tests/VSO_0157762_feature_test_macros/test.compile.pass.cpp
@@ -1316,6 +1316,20 @@ STATIC_ASSERT(__cpp_lib_memory_resource == 201603L);
 #endif
 #endif
 
+#if _HAS_CXX23 && !defined(__clang__) && !defined(__EDG__) // TRANSITION, Clang and EDG support for modules
+#ifndef __cpp_lib_modules
+#error __cpp_lib_modules is not defined
+#elif __cpp_lib_modules != 202207L
+#error __cpp_lib_modules is not 202207L
+#else
+STATIC_ASSERT(__cpp_lib_modules == 202207L);
+#endif
+#else
+#ifdef __cpp_lib_modules
+#error __cpp_lib_modules is defined
+#endif
+#endif
+
 #ifdef __cpp_lib_concepts
 #ifndef __cpp_lib_move_iterator_concept
 #error __cpp_lib_move_iterator_concept is not defined
