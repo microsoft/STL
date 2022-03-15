e6c28789e438ef7fe2580f272be91045a982997c
Stephan T. Lavavej <stl@nuwen.net>
2022-03-14 17:55:30 -0700

    Code review feedback.

--- tests/std/tests/GH_002299_implicit_sfinae_constraints/test.compile.pass.cpp
+++ tests/std/tests/GH_002299_implicit_sfinae_constraints/test.compile.pass.cpp
@@ -3,15 +3,13 @@
 
 #include <memory>
 #include <type_traits>
-#include <version>
+#include <utility>
 
-#ifdef __cpp_lib_optional
-#include <optional>
-#endif
-
-#ifdef __cpp_lib_any
+#if _HAS_CXX17
 #include <any>
-#endif
+#include <initializer_list>
+#include <optional>
+#endif // _HAS_CXX17
 
 #define STATIC_ASSERT(...) static_assert(__VA_ARGS__, #__VA_ARGS__)
 
@@ -24,6 +22,7 @@ constexpr bool can_shared_ptr_assign = false;
 template <class T, class U>
 constexpr bool can_shared_ptr_assign<T, U, void_t<decltype(declval<shared_ptr<T>&>() = declval<U>())>> = true;
 
+STATIC_ASSERT(can_shared_ptr_assign<int, shared_ptr<int>>);
 STATIC_ASSERT(!can_shared_ptr_assign<int, shared_ptr<long>>);
 STATIC_ASSERT(!can_shared_ptr_assign<int, const shared_ptr<long>&>);
 STATIC_ASSERT(!can_shared_ptr_assign<int, unique_ptr<long>>);
@@ -42,6 +41,7 @@ constexpr bool
 template <class T, class... Us>
 constexpr bool can_shared_ptr_reset = can_shared_ptr_reset_impl<void, T, Us...>;
 
+STATIC_ASSERT(can_shared_ptr_reset<int, int*>);
 STATIC_ASSERT(!can_shared_ptr_reset<int, long*>);
 STATIC_ASSERT(!can_shared_ptr_reset<int, long*, default_delete<long>>);
 STATIC_ASSERT(!can_shared_ptr_reset<int, long*, default_delete<long>, allocator<long>>);
@@ -53,11 +53,12 @@ constexpr bool can_weak_ptr_assign = false;
 template <class T, class U>
 constexpr bool can_weak_ptr_assign<T, U, void_t<decltype(declval<weak_ptr<T>&>() = declval<U>())>> = true;
 
+STATIC_ASSERT(can_weak_ptr_assign<int, weak_ptr<int>>);
 STATIC_ASSERT(!can_weak_ptr_assign<int, weak_ptr<long>>);
 STATIC_ASSERT(!can_weak_ptr_assign<int, const weak_ptr<long>&>);
 STATIC_ASSERT(!can_weak_ptr_assign<int, const shared_ptr<long>&>);
 
-#ifdef __cpp_lib_optional
+#if _HAS_CXX17
 // tests for make_optional
 template <class T, class = void>
 constexpr bool can_make_optional_decay = false;
@@ -74,12 +75,12 @@ constexpr bool can_make_optional_impl<void_t<decltype(make_optional<T>(declval<U
 template <class T, class... Us>
 constexpr bool can_make_optional_usual = can_make_optional_impl<void, T, Us...>;
 
+STATIC_ASSERT(can_make_optional_decay<unique_ptr<int>>);
 STATIC_ASSERT(!can_make_optional_decay<const unique_ptr<int>&>); // LWG-3627
+STATIC_ASSERT(can_make_optional_usual<int, int>);
 STATIC_ASSERT(!can_make_optional_usual<int, int, int>);
 STATIC_ASSERT(!can_make_optional_usual<int, initializer_list<int>&>);
-#endif
 
-#ifdef __cpp_lib_any
 // tests for make_any
 template <class Void, class T, class... Us>
 constexpr bool can_make_any_impl = false;
@@ -90,9 +91,10 @@ constexpr bool can_make_any_impl<void_t<decltype(make_any<T>(declval<Us>()...))>
 template <class T, class... Us>
 constexpr bool can_make_any = can_make_any_impl<void, T, Us...>;
 
+STATIC_ASSERT(can_make_any<int, int>);
 STATIC_ASSERT(!can_make_any<unique_ptr<int>, const unique_ptr<int>&>);
 STATIC_ASSERT(!can_make_any<int, int, int>);
 STATIC_ASSERT(!can_make_any<int, initializer_list<int>&>);
-#endif
+#endif // _HAS_CXX17
 
 int main() {} // COMPILE-ONLY
