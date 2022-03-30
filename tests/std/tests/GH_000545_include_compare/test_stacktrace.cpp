f4534c6271fee6707985df48a09a96e1ca255ca6
Alex Guteniev <gutenev@gmail.com>
2022-03-30 23:24:42 +0300

    comditional stacktrace

--- tests/std/tests/GH_000545_include_compare/test_stacktrace.cpp
+++ tests/std/tests/GH_000545_include_compare/test_stacktrace.cpp
@@ -1,10 +1,14 @@
 // Copyright (c) Microsoft Corporation.
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
+#ifdef __cpp_lib_stacktrace
+
 #include <stacktrace>
 
 // Testing LWG-3330 "Include <compare> from most library headers" by intentionally NOT including <compare>
 
 static_assert(std::is_eq(std::partial_ordering::equivalent));
+
+#endif
 
 void test_stacktrace() {}
