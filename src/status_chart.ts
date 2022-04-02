a84204155c2a2786eee204b095c5d2e421e4d6b1
Stephan T. Lavavej <stl@nuwen.net>
2022-04-02 14:05:23 -0700

    Increase time units: timeframe_all to year, timeframe_2021 to quarter.

--- src/status_chart.ts
+++ src/status_chart.ts
@@ -260,7 +260,7 @@ type Timeframe = {
 
 const timeframe_all: Timeframe = {
     min: '2017-06-09',
-    time: { unit: 'quarter' },
+    time: { unit: 'year' },
 };
 const timeframe_github: Timeframe = {
     min: '2019-09-20', // first Friday after 2019-09-16
@@ -268,7 +268,7 @@ const timeframe_github: Timeframe = {
 };
 const timeframe_2021: Timeframe = {
     min: '2021-01-01',
-    time: { unit: 'month' },
+    time: { unit: 'quarter' },
 };
 const timeframes = [timeframe_all, timeframe_github, timeframe_2021];
 const timeframe_github_idx = 1;
