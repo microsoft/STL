bb1033df46c0b046f0decac6bb9203bbd0f60153
Kilian <kilis-mail@web.de>
2022-04-23 18:18:53 +0200

    Remove std:: because of using namespace std;

--- tests/std/tests/P2387R3_pipe_support_for_user_defined_range_adaptors/test.cpp
+++ tests/std/tests/P2387R3_pipe_support_for_user_defined_range_adaptors/test.cpp
@@ -11,7 +11,7 @@ using namespace std;
 
 template <class T>
 concept CanInstantiateRangeAdaptorClosure = requires {
-    typename std::ranges::range_adaptor_closure<T>;
+    typename ranges::range_adaptor_closure<T>;
 };
 
 class EmptyTestType {};
@@ -27,15 +27,15 @@ static_assert(CanInstantiateRangeAdaptorClosure<IncompleteTestType>);
 
 template <class LHS, class RHS>
 concept CanPipe = requires(LHS lhs, RHS rhs) {
-    std::forward<LHS>(lhs) | std::forward<RHS>(rhs);
+    forward<LHS>(lhs) | forward<RHS>(rhs);
 };
 
 template <class LHS, class RHS, class Ret>
 concept CanPipe_R = requires(LHS lhs, RHS rhs) {
-    { std::forward<LHS>(lhs) | std::forward<RHS>(rhs) } -> std::same_as<Ret>;
+    { forward<LHS>(lhs) | forward<RHS>(rhs) } -> same_as<Ret>;
 };
 
-using TestRange = std::array<int, 1>;
+using TestRange = array<int, 1>;
 
 template <class T>
 constexpr bool does_not_satisfy_range_adaptor_closure() {
@@ -43,19 +43,19 @@ constexpr bool does_not_satisfy_range_adaptor_closure() {
         && !CanPipe<TestRange, T&&> && !CanPipe<TestRange, const T&&>;
 }
 
-struct IdentityRangeAdaptorClosure : std::ranges::range_adaptor_closure<IdentityRangeAdaptorClosure> {
+struct IdentityRangeAdaptorClosure : ranges::range_adaptor_closure<IdentityRangeAdaptorClosure> {
     template <class T>
     constexpr decltype(auto) operator()(T&& range) const {
-        return std::forward<T>(range);
+        return forward<T>(range);
     }
 };
 
 // Is not a range adaptor closure, because it is not a function object.
-struct NotCallable : std::ranges::range_adaptor_closure<NotCallable> {};
+struct NotCallable : ranges::range_adaptor_closure<NotCallable> {};
 static_assert(does_not_satisfy_range_adaptor_closure<NotCallable>());
 
 // Is not a range adaptor closure, because it does not accept a range as argument.
-struct NotCallableWithRange : std::ranges::range_adaptor_closure<NotCallableWithRange> {
+struct NotCallableWithRange : ranges::range_adaptor_closure<NotCallableWithRange> {
     void operator()() {}
 };
 static_assert(does_not_satisfy_range_adaptor_closure<NotCallableWithRange>());
@@ -67,27 +67,27 @@ struct NotDerivedFrom {
 static_assert(does_not_satisfy_range_adaptor_closure<NotDerivedFrom>());
 
 // Is not a range adaptor closure, because it inherits privately from range_adaptor_closure.
-struct DerivedPrivately : private std::ranges::range_adaptor_closure<DerivedPrivately> {
+struct DerivedPrivately : private ranges::range_adaptor_closure<DerivedPrivately> {
     void operator()(const TestRange&) {}
 };
 static_assert(does_not_satisfy_range_adaptor_closure<DerivedPrivately>());
 
 // Is not a range adaptor closure, because it inherits from the wrong specialization of range_adaptor_closure.
-struct DerivedFromWrongSpecialization : std::ranges::range_adaptor_closure<IdentityRangeAdaptorClosure> {
+struct DerivedFromWrongSpecialization : ranges::range_adaptor_closure<IdentityRangeAdaptorClosure> {
     void operator()(const TestRange&) {}
 };
 static_assert(does_not_satisfy_range_adaptor_closure<DerivedFromWrongSpecialization>());
 
 // Is not a range adaptor closure, because it has two base classes which are specializations of
 // range_adaptor_closure.
-struct DerivedFromTwoSpecializations : std::ranges::range_adaptor_closure<DerivedFromTwoSpecializations>,
-                                       std::ranges::range_adaptor_closure<IdentityRangeAdaptorClosure> {
+struct DerivedFromTwoSpecializations : ranges::range_adaptor_closure<DerivedFromTwoSpecializations>,
+                                       ranges::range_adaptor_closure<IdentityRangeAdaptorClosure> {
     void operator()(const TestRange&) {}
 };
 static_assert(does_not_satisfy_range_adaptor_closure<DerivedFromTwoSpecializations>());
 
-// Is not a range adaptor closure, because it models std::ranges::range.
-struct ModelsRange : std::ranges::range_adaptor_closure<ModelsRange> {
+// Is not a range adaptor closure, because it models ranges::range.
+struct ModelsRange : ranges::range_adaptor_closure<ModelsRange> {
     void operator()(const TestRange&) {}
 
     int* begin() {
@@ -105,50 +105,50 @@ struct ModelsRange : std::ranges::range_adaptor_closure<ModelsRange> {
 };
 static_assert(does_not_satisfy_range_adaptor_closure<ModelsRange>());
 
-struct RangeAdaptorClosureMemberRefQualTest : std::ranges::range_adaptor_closure<RangeAdaptorClosureMemberRefQualTest> {
-    constexpr std::ranges::empty_view<char> operator()(const auto&) & {
-        return std::views::empty<char>;
+struct RangeAdaptorClosureMemberRefQualTest : ranges::range_adaptor_closure<RangeAdaptorClosureMemberRefQualTest> {
+    constexpr ranges::empty_view<char> operator()(const auto&) & {
+        return views::empty<char>;
     }
 
-    constexpr std::ranges::empty_view<short> operator()(const auto&) && {
-        return std::views::empty<short>;
+    constexpr ranges::empty_view<short> operator()(const auto&) && {
+        return views::empty<short>;
     }
 
-    constexpr std::ranges::empty_view<float> operator()(const auto&) const& {
-        return std::views::empty<float>;
+    constexpr ranges::empty_view<float> operator()(const auto&) const& {
+        return views::empty<float>;
     }
 
-    constexpr std::ranges::empty_view<double> operator()(const auto&) const&& {
-        return std::views::empty<double>;
+    constexpr ranges::empty_view<double> operator()(const auto&) const&& {
+        return views::empty<double>;
     }
 };
-static_assert(CanPipe_R<TestRange, RangeAdaptorClosureMemberRefQualTest&, std::ranges::empty_view<char>>);
-static_assert(CanPipe_R<TestRange, RangeAdaptorClosureMemberRefQualTest&&, std::ranges::empty_view<short>>);
-static_assert(CanPipe_R<TestRange, RangeAdaptorClosureMemberRefQualTest const&, std::ranges::empty_view<float>>);
-static_assert(CanPipe_R<TestRange, RangeAdaptorClosureMemberRefQualTest const&&, std::ranges::empty_view<double>>);
+static_assert(CanPipe_R<TestRange, RangeAdaptorClosureMemberRefQualTest&, ranges::empty_view<char>>);
+static_assert(CanPipe_R<TestRange, RangeAdaptorClosureMemberRefQualTest&&, ranges::empty_view<short>>);
+static_assert(CanPipe_R<TestRange, RangeAdaptorClosureMemberRefQualTest const&, ranges::empty_view<float>>);
+static_assert(CanPipe_R<TestRange, RangeAdaptorClosureMemberRefQualTest const&&, ranges::empty_view<double>>);
 
 using FirstIdentityThenMemberRefQualTest = decltype(
     IdentityRangeAdaptorClosure{} | RangeAdaptorClosureMemberRefQualTest{});
-static_assert(CanPipe_R<TestRange, FirstIdentityThenMemberRefQualTest&, std::ranges::empty_view<char>>);
-static_assert(CanPipe_R<TestRange, FirstIdentityThenMemberRefQualTest&&, std::ranges::empty_view<short>>);
-static_assert(CanPipe_R<TestRange, FirstIdentityThenMemberRefQualTest const&, std::ranges::empty_view<float>>);
-static_assert(CanPipe_R<TestRange, FirstIdentityThenMemberRefQualTest const&&, std::ranges::empty_view<double>>);
+static_assert(CanPipe_R<TestRange, FirstIdentityThenMemberRefQualTest&, ranges::empty_view<char>>);
+static_assert(CanPipe_R<TestRange, FirstIdentityThenMemberRefQualTest&&, ranges::empty_view<short>>);
+static_assert(CanPipe_R<TestRange, FirstIdentityThenMemberRefQualTest const&, ranges::empty_view<float>>);
+static_assert(CanPipe_R<TestRange, FirstIdentityThenMemberRefQualTest const&&, ranges::empty_view<double>>);
 
 using FirstTransformThenMemberRefQualTest = decltype(
-    std::views::transform([](auto x) { return x; }) | RangeAdaptorClosureMemberRefQualTest{});
-static_assert(CanPipe_R<TestRange, FirstTransformThenMemberRefQualTest&, std::ranges::empty_view<char>>);
-static_assert(CanPipe_R<TestRange, FirstTransformThenMemberRefQualTest&&, std::ranges::empty_view<short>>);
-static_assert(CanPipe_R<TestRange, FirstTransformThenMemberRefQualTest const&, std::ranges::empty_view<float>>);
-static_assert(CanPipe_R<TestRange, FirstTransformThenMemberRefQualTest const&&, std::ranges::empty_view<double>>);
+    views::transform([](auto x) { return x; }) | RangeAdaptorClosureMemberRefQualTest{});
+static_assert(CanPipe_R<TestRange, FirstTransformThenMemberRefQualTest&, ranges::empty_view<char>>);
+static_assert(CanPipe_R<TestRange, FirstTransformThenMemberRefQualTest&&, ranges::empty_view<short>>);
+static_assert(CanPipe_R<TestRange, FirstTransformThenMemberRefQualTest const&, ranges::empty_view<float>>);
+static_assert(CanPipe_R<TestRange, FirstTransformThenMemberRefQualTest const&&, ranges::empty_view<double>>);
 
-using FirstMemberRefQualTestThenAll = decltype(RangeAdaptorClosureMemberRefQualTest{} | std::views::all);
-static_assert(CanPipe_R<TestRange, FirstMemberRefQualTestThenAll&, std::ranges::empty_view<char>>);
-static_assert(CanPipe_R<TestRange, FirstMemberRefQualTestThenAll&&, std::ranges::empty_view<short>>);
-static_assert(CanPipe_R<TestRange, FirstMemberRefQualTestThenAll const&, std::ranges::empty_view<float>>);
-static_assert(CanPipe_R<TestRange, FirstMemberRefQualTestThenAll const&&, std::ranges::empty_view<double>>);
+using FirstMemberRefQualTestThenAll = decltype(RangeAdaptorClosureMemberRefQualTest{} | views::all);
+static_assert(CanPipe_R<TestRange, FirstMemberRefQualTestThenAll&, ranges::empty_view<char>>);
+static_assert(CanPipe_R<TestRange, FirstMemberRefQualTestThenAll&&, ranges::empty_view<short>>);
+static_assert(CanPipe_R<TestRange, FirstMemberRefQualTestThenAll const&, ranges::empty_view<float>>);
+static_assert(CanPipe_R<TestRange, FirstMemberRefQualTestThenAll const&&, ranges::empty_view<double>>);
 
 struct RangeAdaptorClosureParameterRefQualTest
-    : std::ranges::range_adaptor_closure<RangeAdaptorClosureParameterRefQualTest> {
+    : ranges::range_adaptor_closure<RangeAdaptorClosureParameterRefQualTest> {
     constexpr char operator()(TestRange&) {
         return {};
     }
@@ -170,7 +170,7 @@ static_assert(CanPipe_R<TestRange&&, RangeAdaptorClosureParameterRefQualTest, sh
 static_assert(CanPipe_R<const TestRange&, RangeAdaptorClosureParameterRefQualTest, float>);
 static_assert(CanPipe_R<const TestRange&&, RangeAdaptorClosureParameterRefQualTest, double>);
 
-struct MoveOnlyRangeAdaptorClosure : std::ranges::range_adaptor_closure<MoveOnlyRangeAdaptorClosure> {
+struct MoveOnlyRangeAdaptorClosure : ranges::range_adaptor_closure<MoveOnlyRangeAdaptorClosure> {
     MoveOnlyRangeAdaptorClosure()                                   = default;
     MoveOnlyRangeAdaptorClosure(const MoveOnlyRangeAdaptorClosure&) = delete;
     MoveOnlyRangeAdaptorClosure(MoveOnlyRangeAdaptorClosure&&)      = default;
@@ -184,29 +184,29 @@ static_assert(CanPipe<IdentityRangeAdaptorClosure, MoveOnlyRangeAdaptorClosure>)
 static_assert(!CanPipe<MoveOnlyRangeAdaptorClosure&, IdentityRangeAdaptorClosure>);
 static_assert(!CanPipe<IdentityRangeAdaptorClosure, MoveOnlyRangeAdaptorClosure&>);
 
-class TimesTwoAdaptor : public std::ranges::range_adaptor_closure<TimesTwoAdaptor> {
+class TimesTwoAdaptor : public ranges::range_adaptor_closure<TimesTwoAdaptor> {
 public:
-    template <std::ranges::range R>
+    template <ranges::range R>
     constexpr auto operator()(R&& range) const {
-        return std::forward<R>(range) | std::views::transform([](auto x) { return x * 2; });
+        return forward<R>(range) | views::transform([](auto x) { return x * 2; });
     }
 };
 
-class DividedByTwoAdaptor : public std::ranges::range_adaptor_closure<DividedByTwoAdaptor> {
+class DividedByTwoAdaptor : public ranges::range_adaptor_closure<DividedByTwoAdaptor> {
 public:
-    template <std::ranges::range R>
+    template <ranges::range R>
     constexpr auto operator()(R&& range) const {
-        return std::forward<R>(range) | std::views::transform([](auto x) { return x / 2; });
+        return forward<R>(range) | views::transform([](auto x) { return x / 2; });
     }
 };
 
 constexpr bool test_user_defined_adaptors() {
-    const std::array<int, 3> numbers{1, 2, 3};
-    const std::array<int, 3> numbers_times_two{2, 4, 6};
-    assert(std::ranges::equal(numbers_times_two, numbers | TimesTwoAdaptor{}));
-    assert(std::ranges::equal(numbers, (numbers | TimesTwoAdaptor{}) | DividedByTwoAdaptor{}));
-    assert(std::ranges::equal(numbers, numbers | (TimesTwoAdaptor{} | DividedByTwoAdaptor{})));
-    assert(std::ranges::equal(numbers_times_two,
+    const array<int, 3> numbers{1, 2, 3};
+    const array<int, 3> numbers_times_two{2, 4, 6};
+    assert(ranges::equal(numbers_times_two, numbers | TimesTwoAdaptor{}));
+    assert(ranges::equal(numbers, (numbers | TimesTwoAdaptor{}) | DividedByTwoAdaptor{}));
+    assert(ranges::equal(numbers, numbers | (TimesTwoAdaptor{} | DividedByTwoAdaptor{})));
+    assert(ranges::equal(numbers_times_two,
         (numbers | TimesTwoAdaptor{})
             | (TimesTwoAdaptor{} | TimesTwoAdaptor{} | DividedByTwoAdaptor{} | DividedByTwoAdaptor{})));
 
@@ -214,16 +214,15 @@ constexpr bool test_user_defined_adaptors() {
 }
 
 constexpr bool test_mixing_of_range_adaptors() {
-    const std::array<int, 3> numbers{1, 2, 3};
-    assert(std::ranges::equal(
-        numbers, numbers | (TimesTwoAdaptor{} | std::views::transform([](int x) { return x / 2; }))));
+    const array<int, 3> numbers{1, 2, 3};
+    assert(ranges::equal(numbers, numbers | (TimesTwoAdaptor{} | views::transform([](int x) { return x / 2; }))));
 
-    const auto mixed_pipeline = TimesTwoAdaptor{} | std::views::reverse | DividedByTwoAdaptor{} | std::views::reverse;
-    assert(std::ranges::equal(mixed_pipeline(numbers), numbers));
-    assert(std::ranges::equal(numbers | mixed_pipeline, numbers));
+    const auto mixed_pipeline = TimesTwoAdaptor{} | views::reverse | DividedByTwoAdaptor{} | views::reverse;
+    assert(ranges::equal(mixed_pipeline(numbers), numbers));
+    assert(ranges::equal(numbers | mixed_pipeline, numbers));
 
-    auto factory_pipeline = std::views::iota(1) | TimesTwoAdaptor{} | std::views::take(3);
-    assert(std::ranges::equal(factory_pipeline, std::array{2, 4, 6}));
+    auto factory_pipeline = views::iota(1) | TimesTwoAdaptor{} | views::take(3);
+    assert(ranges::equal(factory_pipeline, array{2, 4, 6}));
 
     return true;
 }
