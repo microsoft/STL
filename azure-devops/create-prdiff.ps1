53e0527847831eea726488dd1e28bebac99d154b
nicole mazzuca <mazzucan@outlook.com>
2022-04-28 10:42:01 -0700

    do tests

--- azure-devops/create-prdiff.ps1
+++ azure-devops/create-prdiff.ps1
@@ -12,22 +12,27 @@ Start-Process -FilePath 'git' -ArgumentList 'diff' `
     -RedirectStandardOutput $DiffFile
 if (0 -ne (Get-Item -LiteralPath $DiffFile).Length) {
     $errorMessage = @(
-        'The formatting of the files in the repo was not what we expected.'
+        '##[error]The formatting of the files in the repo was not what we expected.'
         'Please access the diff from format.diff in the build artifacts,'
         'and apply it with `git apply`.'
         'Alternatively, you can run the `format` CMake target:'
         '    cmake --build <builddir> --target format'
     )
-    Write-Error ($errorMessage -join "`n") -ErrorAction Continue
+    [Console]::Error.Write($errorMessage -join "`n")
+    [Console]::Error.Flush()
+    [Console]::Out.Write($errorMessage -join "`n")
+    [Console]::Out.Flush()
 
     $restOfMessage = @(
         ''
         '##[group]Expected formatting - diff'
         Get-Content -LiteralPath $DiffFile -Raw
         '##[endgroup]'
+        '##[error]Does putting errors here work?'
         "##vso[artifact.upload artifactname=format.diff]$DiffFile"
         '##vso[task.complete result=Failed]DONE'
     )
 
-    Write-Host ($restOfMessage -join "`n")
+    [Console]::Out.Write($restOfMessage -join "`n")
+    [Console]::Out.Flush()
 }
