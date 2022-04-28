08354147db3d953c45c373fbf405fd2e78d6d0dd
Igor Zhukov <fsb4000@yandex.ru>
2022-04-28 13:48:46 +0700

    move a comment to the top.

--- stl/src/syserror_import_lib.cpp
+++ stl/src/syserror_import_lib.cpp
@@ -42,15 +42,15 @@ _NODISCARD size_t __CLRCALL_PURE_OR_STDCALL __std_get_string_size_without_traili
 
 _NODISCARD size_t __CLRCALL_PURE_OR_STDCALL __std_system_error_allocate_message(
     const unsigned long _Message_id, char** const _Ptr_str) noexcept {
+    // convert to name of Windows error, return 0 for failure, otherwise return number of chars in buffer
+    // __std_system_error_deallocate_message should be called even if 0 is returned
+    // pre: *_Ptr_str == nullptr
     DWORD _Lang_id;
     const int _Ret = GetLocaleInfoEx(LOCALE_NAME_SYSTEM_DEFAULT, LOCALE_ILANGUAGE | LOCALE_RETURN_NUMBER,
         reinterpret_cast<LPWSTR>(&_Lang_id), sizeof(_Lang_id) / sizeof(wchar_t));
     if (_Ret == 0) {
         _Lang_id = 0;
     }
-    // convert to name of Windows error, return 0 for failure, otherwise return number of chars in buffer
-    // __std_system_error_deallocate_message should be called even if 0 is returned
-    // pre: *_Ptr_str == nullptr
     const unsigned long _Chars =
         FormatMessageA(FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS,
             nullptr, _Message_id, _Lang_id, reinterpret_cast<char*>(_Ptr_str), 0, nullptr);
