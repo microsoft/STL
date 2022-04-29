fd8846405accb19039abc44af380727b923f5d9e
Stephan T. Lavavej <stl@nuwen.net>
2022-04-28 21:45:02 -0700

    Squashed changes from GH 2671 after feedback.

--- azure-devops/validate-files.cmd
+++
@@ -1,9 +0,0 @@
-:: Copyright (c) Microsoft Corporation.
-:: SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
-call "%ProgramFiles%\Microsoft Visual Studio\2022\Preview\Common7\Tools\VsDevCmd.bat" ^
-    -host_arch=amd64 -arch=amd64 -no_logo
-cmake -G Ninja -DCMAKE_CXX_COMPILER=cl -DCMAKE_BUILD_TYPE=Release ^
-    -S "%1\tools\validate" -B "%TEMP%\validate-build"
-cmake --build "%TEMP%\validate-build"
-"%TEMP%\validate-build\validate.exe"
-@echo If your build fails here, you need to fix the listed issues.
