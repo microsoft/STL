dbf26cc1e10650613178fea1023d3e574ba19852
Igor Zhukov <fsb4000@yandex.ru>
2022-05-12 01:29:39 +0700

    fix comment and test only `/Zc:alignedNew-`

--- tests/std/tests/GH_002711_Zc_alignedNew-/test.compile.pass.cpp
+++ tests/std/tests/GH_002711_Zc_alignedNew-/test.compile.pass.cpp
@@ -2,7 +2,6 @@
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
 // GH-2711 <functional>: incompatible with /std:c++latest and /Zc:alignedNew-
-// Just test every header can be parsed with /Zc:alignedNew and /Zc:alignedNew-
 #include <__msvc_all_public_headers.hpp>
 
 int main() {} // COMPILE-ONLY
