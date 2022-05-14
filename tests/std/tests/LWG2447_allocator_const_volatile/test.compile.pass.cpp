687fa16eccc36af726f34e31e4d9484b5ad4bf86
Igor Zhukov <fsb4000@yandex.ru>
2022-05-14 16:19:12 +0700

    more tests

--- tests/std/tests/LWG2447_allocator_const_volatile/test.compile.pass.cpp
+++ tests/std/tests/LWG2447_allocator_const_volatile/test.compile.pass.cpp
@@ -8,20 +8,30 @@
 #include <set>
 #include <string>
 #include <vector>
+
 using namespace std;
 
-// LWG2447: Allocators and volatile-qualified value types
-//  LWG-2447 doesn't say the default allocator doesn't support cv-qualified types, it says that a type need not support
-//  cv-qualified types to satisfy the allocator requirements.
+// LWG-2447 doesn't say the default allocator doesn't support cv-qualified types, it says that a type need not support
+// cv-qualified types to satisfy the allocator requirements.
 void test_lwg_2447() {
-    vector<const volatile int> v;
-    deque<const volatile int> d;
-    forward_list<const volatile int> f;
-    list<const volatile int> l;
-    set<const volatile int> s;
-    multiset<const volatile int> ms;
-    map<const volatile int, const volatile int> m;
-    multimap<const volatile int, const volatile int> mm;
+#if !_HAS_DEPRECATED_ALLOCATOR_MEMBERS
+    vector<const volatile int> v_cv;
+    deque<const volatile int> d_cv;
+    forward_list<const volatile int> f_cv;
+    list<const volatile int> l_cv;
+    set<const volatile int> s_cv;
+    multiset<const volatile int> ms_cv;
+    map<const volatile int, const volatile int> m_cv;
+    multimap<const volatile int, const volatile int> mm_cv;
+#endif // !_HAS_DEPRECATED_ALLOCATOR_MEMBERS
+    vector<volatile int> v_v;
+    deque<volatile int> d_v;
+    forward_list<volatile int> f_v;
+    list<volatile int> l_v;
+    set<volatile int> s_v;
+    multiset<volatile int> ms_v;
+    map<volatile int, volatile int> m_v;
+    multimap<volatile int, volatile int> mm_v;
 }
 
 int main() {} // COMPILE-ONLY
