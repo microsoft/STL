e82d1de24d5466bf827ac8148df9bbf0c9ceafa3
Charles Barto <chbarto@microsoft.com>
2022-05-20 16:23:53 -0700

    add comments to the #endif

--- tests/std/tests/GH_002655_alternate_name_broke_linker/test.cpp
+++ tests/std/tests/GH_002655_alternate_name_broke_linker/test.cpp
@@ -6,7 +6,7 @@
 #if defined(_M_IX86) || defined(_M_X64)
 extern "C" int __declspec(dllimport) __stdcall __std_init_once_begin_initialize(void**, unsigned long, int*, void**);
 extern "C" int __declspec(dllimport) __stdcall __std_init_once_complete(void**, unsigned long, void*);
-#endif
+#endif // defined(_M_IX86) || defined(_M_X64)
 
 int main() {
 #if defined(_M_IX86) || defined(_M_X64)
@@ -14,6 +14,6 @@ int main() {
     int pending = 0;
     __std_init_once_begin_initialize(&once, 0, &pending, 0);
     __std_init_once_complete(&once, 0, 0);
-#endif
+#endif // defined(_M_IX86) || defined(_M_X64)
     return 0;
 }
