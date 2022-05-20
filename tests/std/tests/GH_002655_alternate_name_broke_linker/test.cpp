d6e27c6a7a0a947029b68382cb2e8938175e0bb6
Charles Barto <chbarto@microsoft.com>
2022-05-19 20:50:11 -0700

    make the test actually call the function instead of using /INCLUDE
    This is so the mangling is right.

--- tests/std/tests/GH_002655_alternate_name_broke_linker/test.cpp
+++ tests/std/tests/GH_002655_alternate_name_broke_linker/test.cpp
@@ -5,9 +5,15 @@
 
 #if defined(_M_IX86) | defined(_M_X64)
 extern "C" int __declspec(dllimport) __stdcall __std_init_once_begin_initialize(void**, unsigned long, int*, void**);
-#pragma comment(linker, "/INCLUDE:__imp___std_init_once_begin_initialize")
+extern "C" int __declspec(dllimport) __stdcall __std_init_once_complete(void**, unsigned long, void*);
 #endif
 
 int main() {
+#if defined(_M_IX86) | defined(_M_X64)
+    void* once  = 0;
+    int pending = 0;
+    __std_init_once_begin_initialize(&once, 0, &pending, 0);
+    __std_init_once_complete(&once, 0, 0);
+#endif
     return 0;
 }
