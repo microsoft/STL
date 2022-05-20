bb1f5463301bdb27afd797533f69ee60d1bcce68
Charles Barto <chbarto@microsoft.com>
2022-05-20 16:24:24 -0700

    clean up comments+formatting in alias.asm

--- stl/src/alias.asm
+++ stl/src/alias.asm
@@ -10,9 +10,9 @@
 ; on x86_64 this does nothing, on x86 it adds a leading
 ; underscore and @sz, sz should be the size of the parameters
 mangle MACRO name, sz
-    ; This checks if we're in x86_64 mode (using ml64)
-    ; note that RAX isn't really an assembler symbol,
-    ; and this check only works in the IFDEF direction
+    ; This checks if we're in x64 mode (using ml64).
+    ; Note that RAX isn't really an assembler symbol,
+    ; and this check only works in the IFDEF direction.
     ; That is:
     ; IFDEF RAX
     ;   meow
@@ -29,25 +29,22 @@ mangle MACRO name, sz
     ENDIF
 ENDM
 
-; generate the name of the imp symbol for a function
+; Generate the name of the imp symbol for a function.
 imp_name MACRO name, sz
     EXITM @CatStr(__imp_, mangle(name, sz))
 ENDM
 
 create_alias MACRO oldname, newname, size
-    ; use EXTERN name : PROC instead of
-    ; EXTERN name : PROTO ...
-    ; to avoid masm adding underscores to the names
-    ; of (object) symbols.
+    ; use "EXTERN name : PROC" instead of "EXTERN name : PROTO ..."
+    ; to avoid masm adding underscores to the names of (object) symbols.
     ; Note that the MASM symbol always has the name that appears in
     ; the source file (after macro expansion), but when _using_ the symbol
-    ; masm will add underscores
+    ; masm will add underscores.
     EXTERN imp_name(newname, size) : PROC
-    ; the documentation for "ALIAS <alias> = <actual-name>" says angle brackets
-    ; are required around the alias, but this is not the case, it seems to just require
-    ; a single "text item", which angle brackets create, but which is also the result
-    ; of expanding these macros
-    ALIAS imp_name(oldname, size)=imp_name(newname,size)
+    ; The documentation for "ALIAS <alias> = <actual-name>" says angle brackets
+    ; are required around the parameters, but this is not the case; it seems to just require
+    ; a single "text item", which both angle brackets and this macro call create.
+    ALIAS imp_name(oldname, size) = imp_name(newname, size)
 ENDM
 
 create_alias __std_init_once_begin_initialize, InitOnceBeginInitialize, 16
