dcd5003ce828462e0248ca0177b5f8006db04f53
Charles Barto <chbarto@microsoft.com>
2022-05-19 20:36:45 -0700

    test all the libs

--- tests/std/tests/GH_002655_alternate_name_broke_linker/test.cpp
+++ tests/std/tests/GH_002655_alternate_name_broke_linker/test.cpp
@@ -1,7 +1,7 @@
 // Copyright (c) Microsoft Corporation.
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
-#include <use_ansi.h>
+#include <yvals.h>
 
 #if defined(_M_IX86) | defined(_M_X64)
 extern "C" int __declspec(dllimport) __stdcall __std_init_once_begin_initialize(void**, unsigned long, int*, void**);
