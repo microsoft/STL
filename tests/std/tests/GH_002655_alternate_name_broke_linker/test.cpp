ff56bea2c04a834a8c943d888c2f7d51514a2a07
Charles Barto <chbarto@microsoft.com>
2022-05-19 20:22:14 -0700

    Add a test to verify that you can call __std_init_once_begin_initialize without doing anything special.

---
+++ tests/std/tests/GH_002655_alternate_name_broke_linker/test.cpp
@@ -0,0 +1,13 @@
+// Copyright (c) Microsoft Corporation.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+#include <use_ansi.h>
+
+#if defined(_M_IX86) | defined(_M_X64)
+extern "C" int __declspec(dllimport) __stdcall __std_init_once_begin_initialize(void**, unsigned long, int*, void**);
+#pragma comment(linker, "/INCLUDE:__imp___std_init_once_begin_initialize")
+#endif
+
+int main() {
+    return 0;
+}
