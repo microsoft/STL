e83e7e175850a7e1db38291833d77faf29093b55
Charles Barto <chbarto@microsoft.com>
2022-05-19 20:21:27 -0700

    Add linker aliases for InitOnceBeginInitialize and InitOnceComplete
    This is enabled on x86 and x64 only, as it relies on a MASM assembly file,
    still, it's not actually assembly, there's no _code_ generated, just objects
    with the correct sections.
    
    whoops
    
    fix

---
+++ stl/src/alias.asm
@@ -0,0 +1,24 @@
+; weak symbol aliases
+
+; Copyright (c) Microsoft Corporation.
+; SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+; Note that while this is a masm file, it contains no code
+; masm does generate a .text section with zero size
+
+IFDEF RAX ; note: this only works with ifdef, not ifndef
+ELSE
+.MODEL FLAT, STDCALL
+ENDIF
+
+EXTERN __imp_InitOnceBeginInitialize :PROTO :PTR PTR VOID, :DWORD, :PTR DWORD, :PTR PTR VOID
+EXTERN __imp_InitOnceComplete :PROTO :PTR PTR VOID, :DWORD, :PTR VOID
+
+IFDEF RAX
+ALIAS <__imp___std_init_once_begin_initialize>=<__imp_InitOnceBeginInitialize>
+ALIAS <__imp___std_init_once_complete>=<__imp_InitOnceComplete>
+ELSE
+ALIAS <__imp____std_init_once_begin_initialize@16>=<__imp_InitOnceBeginInitialize>
+ALIAS <__imp____std_init_once_complete@16>=<__imp_InitOnceComplete>
+ENDIF
+END
