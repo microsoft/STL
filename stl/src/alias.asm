6b2bdf300f566eb080b9fdc307c7bed9cf46c2ec
Charles Barto <chbarto@microsoft.com>
2022-05-24 14:52:34 -0700

    remove the macro programming from alias.asm, so it doesn't trigger an assert in MASM.

--- stl/src/alias.asm
+++ stl/src/alias.asm
@@ -3,51 +3,28 @@
 
 ; weak symbol aliases
 
-; Note that while this is a masm file, it contains no code.
-; masm does generate a .text section with zero size.
-
-; Mangle identifiers for stdcall calling convention.
-; On x64 this does nothing, on x86 it adds a leading
-; underscore and trailing @sz, where sz should be the size of the parameters.
-mangle MACRO name, sz
-    ; This checks if we're in x64 mode (using ml64).
-    ; Note that RAX isn't really an assembler symbol,
-    ; and this check only works in the IFDEF direction.
-    ; That is:
-    ; IFDEF RAX
-    ;   meow
-    ; ENDIF
-    ; will expand to meow on only x64 systems, however:
-    ; IFNDEF RAX
-    ;   meow
-    ; ENDIF
-    ; will expand to meow on _BOTH_ x64 and x86 systems.
-    IFDEF RAX
-        EXITM <name>
-    ELSE
-        EXITM <_&name&@&sz&>
-    ENDIF
-ENDM
-
-; Generate the name of the imp symbol for a function.
-imp_name MACRO name, sz
-    EXITM @CatStr(__imp_, mangle(name, sz))
-ENDM
-
-create_alias MACRO oldname, newname, size
-    ; Use "EXTERN name : PROC" instead of "EXTERN name : PROTO ..."
-    ; to avoid masm adding underscores to the names of (object) symbols.
-    ; Note that the MASM symbol always has the name that appears in
-    ; the source file (after macro expansion), but when _using_ the symbol
-    ; masm will add underscores.
-    EXTERN imp_name(newname, size) : PROC
-    ; The documentation for "ALIAS <alias> = <actual-name>" says angle brackets
-    ; are required around the parameters, but this is not the case; it seems to just require
-    ; a single "text item", which both angle brackets and this macro call create.
-    ALIAS imp_name(oldname, size) = imp_name(newname, size)
-ENDM
-
-create_alias __std_init_once_begin_initialize, InitOnceBeginInitialize, 16
-create_alias __std_init_once_complete, InitOnceComplete, 12
+; This checks if we're in x64 mode (using ml64).
+; Note that RAX isn't really an assembler symbol,
+; and this check only works in the IFDEF direction.
+; That is:
+; IFDEF RAX
+;   meow
+; ENDIF
+; will expand to meow on only x64 system, however:
+; IFNDEF RAX
+;   meow
+; ENDIF
+; will expand to meow on _BOTH_ x64 and x86 systems.
+IFDEF RAX
+    EXTERN __imp_InitOnceBeginInitialize : PROC
+    EXTERN __imp_InitOnceComplete : PROC
+    ALIAS <__imp___std_init_once_begin_initialize> = <__imp_InitOnceBeginInitialize>
+    ALIAS <__imp___std_init_once_complete> = <__imp_InitOnceComplete>
+ELSE
+    EXTERN __imp__InitOnceBeginInitialize@16 : PROC
+    EXTERN __imp__InitOnceComplete@12 : PROC
+    ALIAS <__imp____std_init_once_begin_initialize@16> = <__imp__InitOnceBeginInitialize@16>
+    ALIAS <__imp____std_init_once_complete@12> = <__imp__InitOnceComplete@12>
+ENDIF
 
 END
