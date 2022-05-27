cf41f2be68ffd40076dc347a6389befe19860707
Stephan T. Lavavej <stl@nuwen.net>
2022-05-27 15:41:15 -0700

    Code review feedback.

--- tests/std/tests/GH_002334_branchless_clamp/test.cpp
+++ tests/std/tests/GH_002334_branchless_clamp/test.cpp
@@ -3,6 +3,7 @@
 
 #include <algorithm>
 #include <cassert>
+#include <functional>
 #include <string>
 
 using namespace std;
@@ -13,13 +14,13 @@ int main() {
     assert(clamp(6, 4, 7) == 6);
     assert(clamp(8, 4, 7) == 7);
 
-    assert(clamp(2, 4, 7, std::less{}) == 4);
-    assert(clamp(6, 4, 7, std::less{}) == 6);
-    assert(clamp(8, 4, 7, std::less{}) == 7);
+    assert(clamp(2, 4, 7, less{}) == 4);
+    assert(clamp(6, 4, 7, less{}) == 6);
+    assert(clamp(8, 4, 7, less{}) == 7);
 
-    assert(clamp(2, 4, 7, std::less<int>{}) == 4);
-    assert(clamp(6, 4, 7, std::less<int>{}) == 6);
-    assert(clamp(8, 4, 7, std::less<int>{}) == 7);
+    assert(clamp(2, 4, 7, less<int>{}) == 4);
+    assert(clamp(6, 4, 7, less<int>{}) == 6);
+    assert(clamp(8, 4, 7, less<int>{}) == 7);
 
     const int data[100] = {};
     assert(clamp(data + 22, data + 30, data + 70) == data + 30);
@@ -27,18 +28,18 @@ int main() {
     assert(clamp(data + 88, data + 30, data + 70) == data + 70);
 
     // Generic cases
-    assert(clamp(2, 4, 7, std::less<long long>{}) == 4);
-    assert(clamp(6, 4, 7, std::less<long long>{}) == 6);
-    assert(clamp(8, 4, 7, std::less<long long>{}) == 7);
+    assert(clamp(2, 4, 7, less<long long>{}) == 4);
+    assert(clamp(6, 4, 7, less<long long>{}) == 6);
+    assert(clamp(8, 4, 7, less<long long>{}) == 7);
 
-    assert(clamp<string>("apple", "banana", "coconut") == "banana");
-    assert(clamp<string>("blueberry", "banana", "coconut") == "blueberry");
-    assert(clamp<string>("cucumber", "banana", "coconut") == "coconut");
+    assert(clamp("apple"s, "banana"s, "coconut"s) == "banana"s);
+    assert(clamp("blueberry"s, "banana"s, "coconut"s) == "blueberry"s);
+    assert(clamp("cucumber"s, "banana"s, "coconut"s) == "coconut"s);
 
 #if _ITERATOR_DEBUG_LEVEL < 2
-    // make sure we keep early return for generic cases:
-    // either less than min or greater than max input should produce only one comparison
-    // for _ITERATOR_DEBUG_LEVEL == 2 we validate predicate, so make extra comparisons
+    // Make sure we keep the early return for generic cases:
+    // Either less than min or greater than max input should produce only one comparison.
+    // For _ITERATOR_DEBUG_LEVEL == 2 we validate the predicate, so there are extra comparisons.
 
     int comparisons        = 0;
     const auto counted_cmp = [&comparisons](auto a, auto b) {
@@ -52,9 +53,9 @@ int main() {
     assert(comparisons == 5);
 
     comparisons = 0;
-    assert(clamp<string>("apple", "banana", "coconut", counted_cmp) == "banana");
-    assert(clamp<string>("blueberry", "banana", "coconut", counted_cmp) == "blueberry");
-    assert(clamp<string>("cucumber", "banana", "coconut", counted_cmp) == "coconut");
+    assert(clamp("apple"s, "banana"s, "coconut"s, counted_cmp) == "banana"s);
+    assert(clamp("blueberry"s, "banana"s, "coconut"s, counted_cmp) == "blueberry"s);
+    assert(clamp("cucumber"s, "banana"s, "coconut"s, counted_cmp) == "coconut"s);
     assert(comparisons == 5);
 #endif // _ITERATOR_DEBUG_LEVEL < 2
 }
