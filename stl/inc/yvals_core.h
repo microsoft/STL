3faabf880d6cb498cd294fe00f35389a32ec608d
Igor Zhukov <fsb4000@yandex.ru>
2022-05-29 00:14:35 +0700

    add __EDG__

--- stl/inc/yvals_core.h
+++ stl/inc/yvals_core.h
@@ -603,13 +603,6 @@
 #endif // _STL_DISABLE_DEPRECATED_WARNING
 // clang-format on
 
-#if defined(__clang__) || defined(__CUDACC__) || defined(__INTEL_COMPILER)
-#define _STL_DECLSPEC_NOINITALL
-#else // ^^^ not MSVC/ MSVC vvv
-#define _STL_DECLSPEC_NOINITALL \
-    __pragma(warning(push)) __pragma(warning(disable : 4845)) __declspec(no_init_all) __pragma(warning(pop))
-#endif // ^^^ MSVC ^^^
-
 #ifndef _STL_RESTORE_DEPRECATED_WARNING
 #ifdef __clang__
 #define _STL_RESTORE_DEPRECATED_WARNING _Pragma("clang diagnostic pop")
@@ -619,6 +612,13 @@
 #define _STL_RESTORE_DEPRECATED_WARNING _Pragma("warning(pop)")
 #endif // ^^^ MSVC ^^^
 #endif // _STL_RESTORE_DEPRECATED_WARNING
+
+#if defined(__clang__) || defined(__CUDACC__) || defined(__INTEL_COMPILER) || defined(__EDG__)
+#define _STL_DECLSPEC_NOINITALL
+#else // ^^^ not MSVC/ MSVC vvv
+#define _STL_DECLSPEC_NOINITALL \
+    __pragma(warning(push)) __pragma(warning(disable : 4845)) __declspec(no_init_all) __pragma(warning(pop))
+#endif // ^^^ MSVC ^^^
 
 #define _CPPLIB_VER       650
 #define _MSVC_STL_VERSION 143
