a353a2d321112c178baf1bf279dbdd0df278faa3
Stephan T. Lavavej <stl@microsoft.com>
2022-06-14 17:40:45 -0700

    CUDA 11.6.0.

--- azure-devops/provision-image.ps1
+++ azure-devops/provision-image.ps1
@@ -143,13 +143,7 @@ $Sku = 'Enterprise'
 $VisualStudioBootstrapperUrl = 'https://aka.ms/vs/17/pre/vs_enterprise.exe'
 $PythonUrl = 'https://www.python.org/ftp/python/3.10.5/python-3.10.5-amd64.exe'
 
-$CudaUrl = `
-  'https://developer.download.nvidia.com/compute/cuda/10.1/Prod/local_installers/cuda_10.1.243_426.00_win10.exe'
-$CudaFeatures = 'nvcc_10.1 cuobjdump_10.1 nvprune_10.1 cupti_10.1 gpu_library_advisor_10.1 memcheck_10.1 ' + `
-  'nvdisasm_10.1 nvprof_10.1 visual_profiler_10.1 visual_studio_integration_10.1 cublas_10.1 cublas_dev_10.1 ' + `
-  'cudart_10.1 cufft_10.1 cufft_dev_10.1 curand_10.1 curand_dev_10.1 cusolver_10.1 cusolver_dev_10.1 cusparse_10.1 ' + `
-  'cusparse_dev_10.1 nvgraph_10.1 nvgraph_dev_10.1 npp_10.1 npp_dev_10.1 nvrtc_10.1 nvrtc_dev_10.1 nvml_dev_10.1 ' + `
-  'occupancy_calculator_10.1 fortran_examples_10.1'
+$CudaUrl = 'https://developer.download.nvidia.com/compute/cuda/11.6.0/local_installers/cuda_11.6.0_511.23_windows.exe'
 
 $ErrorActionPreference = 'Stop'
 $ProgressPreference = 'SilentlyContinue'
@@ -272,19 +266,14 @@ Function InstallPython {
 Installs NVIDIA's CUDA Toolkit.
 
 .DESCRIPTION
-InstallCuda installs the CUDA Toolkit with the features specified as a
-space-separated list of strings in $Features.
+InstallCuda installs the CUDA Toolkit.
 
 .PARAMETER Url
 The URL of the CUDA installer.
-
-.PARAMETER Features
-A space-separated list of features to install.
 #>
 Function InstallCuda {
   Param(
-    [String]$Url,
-    [String]$Features
+    [String]$Url
   )
 
   try {
@@ -292,7 +281,7 @@ Function InstallCuda {
     [string]$installerPath = Get-TempFilePath -Extension 'exe'
     curl.exe -L -o $installerPath -s -S $Url
     Write-Host 'Installing CUDA...'
-    $proc = Start-Process -FilePath $installerPath -ArgumentList @('-s ' + $Features) -Wait -PassThru
+    $proc = Start-Process -FilePath $installerPath -ArgumentList @('-s') -Wait -PassThru
     $exitCode = $proc.ExitCode
     if ($exitCode -eq 0) {
       Write-Host 'Installation successful!'
@@ -347,7 +336,7 @@ Add-MpPreference -ExclusionProcess python.exe
 
 InstallPython $PythonUrl
 InstallVisualStudio -Workloads $Workloads -BootstrapperUrl $VisualStudioBootstrapperUrl
-InstallCuda -Url $CudaUrl -Features $CudaFeatures
+InstallCuda -Url $CudaUrl
 
 Write-Host 'Updating PATH...'
 
