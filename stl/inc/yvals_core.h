4519b181a3b7b22ac6710125a9966762e9491417
AliKhalesi <alireza.khalesi@yahoo.com>
2022-07-02 11:32:42 +0800

    Resolves #2836

--- stl/inc/yvals_core.h
+++ stl/inc/yvals_core.h
@@ -612,7 +612,7 @@
 
 #define _CPPLIB_VER       650
 #define _MSVC_STL_VERSION 143
-#define _MSVC_STL_UPDATE  202206L
+#define _MSVC_STL_UPDATE  202207L
 
 #ifndef _ALLOW_COMPILER_AND_STL_VERSION_MISMATCH
 #if defined(__CUDACC__) && defined(__CUDACC_VER_MAJOR__)
