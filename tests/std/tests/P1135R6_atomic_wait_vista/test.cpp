8bd9faeee3bdbb52338740af5c21c64b5181862d
Alex Guteniev <gutenev@gmail.com>
2022-07-14 21:57:41 +0300

    Fix #2856

--- tests/std/tests/P1135R6_atomic_wait_vista/test.cpp
+++ tests/std/tests/P1135R6_atomic_wait_vista/test.cpp
@@ -4,6 +4,11 @@
 #include "test_atomic_wait.hpp"
 
 int main() {
-    assert(__std_atomic_set_api_level(__std_atomic_api_level::__has_srwlock) == __std_atomic_api_level::__has_srwlock);
+    auto level = __std_atomic_set_api_level(__std_atomic_api_level::__has_srwlock);
+#if _ATOMIC_WAIT_ON_ADDRESS_STATICALLY_AVAILABLE
+    assert(level == __std_atomic_api_level::__has_wait_on_address);
+#else // ^^^ wait on address statically available ^^^ / vvv wait on address NOT statically available vvv
+    assert(level == __std_atomic_api_level::__has_srwlock);
+#endif // ^^^ wait on address NOT statically available ^^^
     test_atomic_wait();
 }
