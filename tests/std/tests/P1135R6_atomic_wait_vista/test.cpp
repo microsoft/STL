0f0cc9335d42b08c562dcc3c02e8765fe358c806
Alex Guteniev <gutenev@gmail.com>
2022-07-14 22:48:01 +0300

    just skip it

--- tests/std/tests/P1135R6_atomic_wait_vista/test.cpp
+++ tests/std/tests/P1135R6_atomic_wait_vista/test.cpp
@@ -4,11 +4,8 @@
 #include "test_atomic_wait.hpp"
 
 int main() {
-    auto level = __std_atomic_set_api_level(__std_atomic_api_level::__has_srwlock);
-#if _ATOMIC_WAIT_ON_ADDRESS_STATICALLY_AVAILABLE
-    assert(level == __std_atomic_api_level::__has_wait_on_address);
-#else // ^^^ wait on address statically available ^^^ / vvv wait on address NOT statically available vvv
-    assert(level == __std_atomic_api_level::__has_srwlock);
-#endif // ^^^ wait on address NOT statically available ^^^
+#if defined(_M_IX86) || defined(_M_X64) && !defined(_M_ARM64EC)
+    assert(__std_atomic_set_api_level(__std_atomic_api_level::__has_srwlock) == __std_atomic_api_level::__has_srwlock);
     test_atomic_wait();
+#endif // defined(_M_IX86) || defined(_M_X64) && !defined(_M_ARM64EC)
 }
