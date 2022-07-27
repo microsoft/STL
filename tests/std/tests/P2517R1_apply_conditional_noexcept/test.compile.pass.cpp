91e329a2aa4c1e0745506d800dfd8b96ec4b8834
Nicole Mazzuca <mazzucan@outlook.com>
2022-07-27 14:12:59 -0700

    add tests

---
+++ tests/std/tests/P2517R1_apply_conditional_noexcept/test.compile.pass.cpp
@@ -0,0 +1,19 @@
+#include <string>
+#include <tuple>
+#include <type_traits>
+
+using namespace std;
+
+struct NoexceptIf {
+    template <bool b, class... Ts>
+    void operator()(std::bool_constant<b>, Ts...) const noexcept(b);
+};
+
+constexpr static NoexceptIf noexcept_if;
+
+static_assert(noexcept(apply(noexcept_if, forward_as_tuple(true_type{}))));
+static_assert(!noexcept(apply(noexcept_if, forward_as_tuple(false_type{}))));
+
+static_assert(noexcept(apply(noexcept_if, forward_as_tuple(true_type{}, 0))));
+static_assert(noexcept(apply(noexcept_if, forward_as_tuple(true_type{}, string{}))));
+static_assert(!noexcept(apply(noexcept_if, forward_as_tuple(true_type{}, static_cast<const string&>(string{})))));
