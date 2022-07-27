b5cdb12d5637873f4aabc51b4139a768cd4b7b40
A. Jiang <de34@live.cn>
2022-07-27 11:35:58 +0800

    Test coverage for LWG-3564

--- tests/std/tests/P0896R4_views_transform/test.cpp
+++ tests/std/tests/P0896R4_views_transform/test.cpp
@@ -321,6 +321,46 @@ constexpr bool test_one(Rng&& rng, Expected&& expected) {
     return true;
 }
 
+// Test a function object whose const and non-const versions behave differently
+struct difference_teller {
+    constexpr auto& operator()(auto&& x) noexcept {
+        auto& ref = x;
+        return ref;
+    }
+
+    constexpr auto operator()(auto&& x) const noexcept {
+        return type_identity<decltype(x)>{};
+    }
+};
+
+template <ranges::input_range Rng>
+constexpr void test_difference_on_const_functor(Rng&& rng) {
+    using ranges::transform_view, ranges::input_range, ranges::forward_range, ranges::bidirectional_range,
+        ranges::random_access_range, ranges::range_reference_t, ranges::range_value_t;
+
+    using V  = views::all_t<Rng>;
+    using TV = transform_view<V, difference_teller>;
+
+    auto r = forward<Rng>(rng) | views::transform(difference_teller{});
+    STATIC_ASSERT(is_same_v<decltype(r), TV>);
+
+    STATIC_ASSERT(input_range<TV>);
+    STATIC_ASSERT(forward_range<TV> == forward_range<Rng>);
+    STATIC_ASSERT(bidirectional_range<TV> == bidirectional_range<Rng>);
+    STATIC_ASSERT(random_access_range<TV> == random_access_range<Rng>);
+
+    STATIC_ASSERT(input_range<const TV> == input_range<const V>);
+    STATIC_ASSERT(!forward_range<TV>);
+    STATIC_ASSERT(!bidirectional_range<TV>);
+    STATIC_ASSERT(!random_access_range<TV>);
+
+    STATIC_ASSERT(is_lvalue_reference_v<range_reference_t<TV>>);
+    if constexpr (input_range<const TV>) {
+        STATIC_ASSERT(is_object_v<range_reference_t<const TV>>);
+        STATIC_ASSERT(!is_same_v<range_value_t<TV>, range_value_t<const TV>>);
+    }
+}
+
 static constexpr int some_ints[]        = {0, 1, 2, 3, 4, 5, 6, 7};
 static constexpr int transformed_ints[] = {8, 9, 10, 11, 12, 13, 14, 15};
 
@@ -329,6 +369,9 @@ struct instantiator {
     static constexpr void call() {
         R r{some_ints};
         test_one(r, transformed_ints);
+
+        R r2{some_ints};
+        test_difference_on_const_functor(r2);
     }
 };
 
