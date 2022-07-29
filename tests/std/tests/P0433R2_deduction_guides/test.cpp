f89209f84d0396eeb412cbec04eed3f6900451df
A. Jiang <de34@live.cn>
2022-07-29 17:28:34 +0800

    Cleanup wrong comments

--- tests/std/tests/P0433R2_deduction_guides/test.cpp
+++ tests/std/tests/P0433R2_deduction_guides/test.cpp
@@ -45,7 +45,7 @@
 
 #if _HAS_CXX23 && !defined(__clang__) // TRANSITION, DevCom-10107077, Clang has not implemented Deducing this
 #define HAS_EXPLICIT_THIS_PARAMETER
-#endif // _HAS_CXX23 && !defined(__EDG__) && !defined(__clang__)
+#endif // _HAS_CXX23 && !defined(__clang__)
 
 using namespace std;
 
