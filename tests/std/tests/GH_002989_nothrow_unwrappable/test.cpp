e53cbba6fade16d15ab929c810a47208a8a2b4ef
Nicole Mazzuca <mazzucan@outlook.com>
2022-08-05 13:40:54 -0700

    moar testing
    plus, fix reverse_iterator up with an `_Unwrapped() &&`

--- tests/std/tests/GH_002989_nothrow_unwrappable/test.cpp
+++ tests/std/tests/GH_002989_nothrow_unwrappable/test.cpp
@@ -7,31 +7,65 @@
 #include <vector>
 #include <xutility>
 
+#if _HAS_CXX20
+#include <ranges>
+#endif
+
 using namespace std;
+using filesystem::path;
 
 #define STATIC_ASSERT(...) static_assert(__VA_ARGS__, #__VA_ARGS__)
 
-template <class T, bool CopyUnwrapNothrow = true>
-void do_test() {
-    STATIC_ASSERT(_Is_nothrow_unwrappable_v<T>);
-    STATIC_ASSERT(_Is_nothrow_unwrappable_v<T&&>);
-    STATIC_ASSERT(noexcept(_Get_unwrapped(declval<T>())));
+struct Predicate {
+    template <class T>
+    bool operator()(const T&) const {
+        return true;
+    }
+};
 
-    STATIC_ASSERT(_Is_nothrow_unwrappable_v<const T&> == CopyUnwrapNothrow);
-    STATIC_ASSERT(noexcept(_Get_unwrapped(declval<const T&>())) == CopyUnwrapNothrow);
-    STATIC_ASSERT(_Is_nothrow_unwrappable_v<const T&&> == CopyUnwrapNothrow);
-    STATIC_ASSERT(noexcept(_Get_unwrapped(declval<const T&&>())) == CopyUnwrapNothrow);
+template <class It, bool CopyUnwrapNothrow = true>
+void do_single_test() {
+    STATIC_ASSERT(_Is_nothrow_unwrappable_v<It>);
+    STATIC_ASSERT(_Is_nothrow_unwrappable_v<It&&>);
+    STATIC_ASSERT(noexcept(_Get_unwrapped(declval<It>())));
+
+    STATIC_ASSERT(_Is_nothrow_unwrappable_v<const It&> == CopyUnwrapNothrow);
+    STATIC_ASSERT(noexcept(_Get_unwrapped(declval<const It&>())) == CopyUnwrapNothrow);
+    STATIC_ASSERT(_Is_nothrow_unwrappable_v<const It&&> == CopyUnwrapNothrow);
+    STATIC_ASSERT(noexcept(_Get_unwrapped(declval<const It&&>())) == CopyUnwrapNothrow);
+}
+
+template <class It, bool CopyUnwrapNothrow = true>
+void do_full_test() {
+    do_single_test<It, CopyUnwrapNothrow>();
+    do_single_test<reverse_iterator<It>, CopyUnwrapNothrow>();
+    do_single_test<move_iterator<It>, CopyUnwrapNothrow>();
+
+#ifdef __cpp_lib_concepts // TRANSITION, GH-395
+    using R = ranges::subrange<It, It>;
+
+    do_single_test<ranges::iterator_t<R>, CopyUnwrapNothrow>();
+
+    // iterator_t<filter_view> does not define _Unwrapped
+    do_single_test<ranges::iterator_t<ranges::filter_view<R, Predicate>>, true>();
+    // iterator_t<transform_view> does not define _Unwrapped
+    do_single_test<ranges::iterator_t<ranges::transform_view<R, Predicate>>, true>();
+    if constexpr (ranges::bidirectional_range<R>) {
+        // iterator_t<reverse_view> does not define _Unwrapped
+        do_single_test<ranges::iterator_t<ranges::reverse_view<R>>, true>();
+    }
+#endif // __cpp_lib_concepts
 }
 
 int main() {
-    do_test<int>();
-    do_test<int*>();
-    do_test<int[]>();
+    do_single_test<int>();
+    do_full_test<int*>();
+    do_single_test<int[]>();
 
-    do_test<vector<int>::iterator>();
-    do_test<vector<int>::const_iterator>();
-    do_test<list<int>::iterator>();
-    do_test<list<int>::const_iterator>();
+    do_full_test<vector<int>::iterator>();
+    do_full_test<vector<int>::const_iterator>();
+    do_full_test<list<int>::iterator>();
+    do_full_test<list<int>::const_iterator>();
 
-    do_test<filesystem::path::iterator, false>();
+    do_full_test<path::iterator, false>();
 }
