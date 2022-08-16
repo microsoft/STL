5775a22a41e68c1480771e1cd715c9c58496e43b
Stephan T. Lavavej <stl@nuwen.net>
2022-08-16 12:37:33 -0700

    Cleanup tests to be friendlier to modules (#3034)

--- tests/std/tests/P1206R7_set_insert_range/test.cpp
+++ tests/std/tests/P1206R7_set_insert_range/test.cpp
@@ -14,14 +14,14 @@
 
 using namespace std;
 
-static constexpr int expected[] = {0, 1, 2, 3, 4, 5, 6, 7};
+static constexpr int expected_vals[] = {0, 1, 2, 3, 4, 5, 6, 7};
 
 template <ranges::input_range Rng>
 void test_set(Rng&& rng) {
     if constexpr (is_reference_v<ranges::range_reference_t<Rng>>) {
         set<int> m;
         m.insert_range(forward<Rng>(rng));
-        assert(ranges::equal(m, expected));
+        assert(ranges::equal(m, expected_vals));
     }
 }
 
