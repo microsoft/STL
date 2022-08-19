3c8de09e6a1ae9538781e539d93aba801edd395a
Matt Stephanson <stephanson.matt@gmail.com>
2022-08-18 18:47:35 -0700

    review feedback

--- tests/std/tests/GH_000178_uniform_int/test.cpp
+++ tests/std/tests/GH_000178_uniform_int/test.cpp
@@ -10,28 +10,28 @@ using namespace std;
 
 template <class Generator>
 bool basic_test() {
-    constexpr auto max         = (1ull << 60);
+    constexpr auto maximum     = 1ull << 60;
     constexpr auto num_bins    = 20ull; // Don't change this without looking up a new threshold below.
-    constexpr auto bin_width   = max / num_bins; // except possibly the last bin
-    constexpr auto bin_freq    = static_cast<double>(bin_width) / max;
-    constexpr auto freq_rem    = static_cast<double>(max % bin_width) / max;
+    constexpr auto bin_width   = maximum / num_bins; // except possibly the last bin
+    constexpr auto bin_freq    = static_cast<double>(bin_width) / maximum;
+    constexpr auto freq_rem    = static_cast<double>(maximum % bin_width) / maximum;
     constexpr double threshold = 31.410; // chi-squared critical value for d.f. = 20 and p = 0.05
 
     Generator gen;
-    std::uniform_int_distribution<uint64_t> dist(0, max - 1u);
+    uniform_int_distribution<uint64_t> dist(0, maximum - 1u);
 
 
     const int N             = 20'000;
-    int frequency[num_bins] = {0};
+    int frequency[num_bins] = {};
     for (int i = 0; i < N; ++i) {
-        ++frequency[std::min(dist(gen) / bin_width, num_bins - 1u)];
+        ++frequency[min(dist(gen) / bin_width, num_bins - 1u)];
     }
 
     double chi_squared = 0.0;
-    for (unsigned i = 0; i < num_bins; ++i) {
+    for (unsigned int i = 0; i < num_bins; ++i) {
         const auto expected = (bin_freq + (i == num_bins - 1u ? freq_rem : 0.0)) * N;
         const auto delta    = static_cast<double>(frequency[i] - expected);
-        chi_squared += (delta * delta) / expected;
+        chi_squared += delta * delta / expected;
     }
 
     return chi_squared <= threshold;
@@ -42,20 +42,20 @@ bool test_modulus_bias() {
     // [0,R), then R mod s values must be rejected to ensure uniformity. By making (R mod s)/R large, we can introduce a
     // large bias if the rejection is incorrect.
 
-    constexpr int max          = 5; // Don't change this without looking up a new threshold below.
+    constexpr int maximum      = 5; // Don't change this without looking up a new threshold below.
     constexpr double threshold = 11.07; // chi-squared critical value for d.f. = 5 and p = 0.05
-    std::uniform_int_distribution<> rng(0, max - 1);
-    std::independent_bits_engine<std::mt19937, 3, uint32_t> gen;
+    uniform_int_distribution<> rng(0, maximum - 1);
+    independent_bits_engine<mt19937, 3, uint32_t> gen;
 
-    const int N        = 1'000;
-    int frequency[max] = {0};
+    const int N            = 1'000;
+    int frequency[maximum] = {};
     for (int i = 0; i < N; ++i) {
         ++frequency[rng(gen)];
     }
 
     double chi_squared = 0.0;
-    for (int i = 0; i < max; ++i) {
-        const double expected = static_cast<double>(N) / max;
+    for (int i = 0; i < maximum; ++i) {
+        const double expected = static_cast<double>(N) / maximum;
         const double delta    = frequency[i] - expected;
         chi_squared += delta * delta / expected;
     }
@@ -68,11 +68,11 @@ int main() {
     // (1) URBG provides enough bits to completely fill the underlying type
     // (2) URBG provides enough bits for our upper bound, but not enough to fill the type
     // (3) URBG is called multiple times, but doesn't fill the type
-    // (4) URBG is called multiple times and overflow the number of bits in the type
+    // (4) URBG is called multiple times and overflows the number of bits in the type
     assert((basic_test<mt19937_64>()));
-    assert((basic_test<independent_bits_engine<std::mt19937_64, 61, uint64_t>>()));
-    assert((basic_test<independent_bits_engine<std::mt19937, 31, uint32_t>>()));
-    assert((basic_test<independent_bits_engine<std::mt19937, 25, uint32_t>>()));
+    assert((basic_test<independent_bits_engine<mt19937_64, 61, uint64_t>>()));
+    assert((basic_test<independent_bits_engine<mt19937, 31, uint32_t>>()));
+    assert((basic_test<independent_bits_engine<mt19937, 25, uint32_t>>()));
 
     assert(test_modulus_bias());
 
