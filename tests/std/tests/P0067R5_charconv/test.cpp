53c80c52efc471f1de279c8c32dafd968c53af3b
Stephan T. Lavavej <stl@nuwen.net>
2022-08-24 13:34:42 -0700

    Add const.

--- tests/std/tests/P0067R5_charconv/test.cpp
+++ tests/std/tests/P0067R5_charconv/test.cpp
@@ -465,15 +465,15 @@ constexpr errc out_ran = errc::result_out_of_range;
 
 template <typename T>
 _CONSTEXPR23 bool test_integer_from_chars() {
-    string hundred_zeroes              = string(100, '0');
-    string hundred_zeroes_and_11       = hundred_zeroes + "11"s;
-    string minus_hundred_zeroes        = "-"s + hundred_zeroes;
-    string minus_hundred_zeroes_and_11 = "-"s + hundred_zeroes_and_11;
+    const string hundred_zeroes              = string(100, '0');
+    const string hundred_zeroes_and_11       = hundred_zeroes + "11"s;
+    const string minus_hundred_zeroes        = "-"s + hundred_zeroes;
+    const string minus_hundred_zeroes_and_11 = "-"s + hundred_zeroes_and_11;
 
-    string hundred_ones                  = string(100, '1');
-    string hundred_ones_and_atatat       = hundred_ones + "@@@"s;
-    string minus_hundred_ones            = "-"s + hundred_ones;
-    string minus_hundred_ones_and_atatat = "-"s + hundred_ones_and_atatat;
+    const string hundred_ones                  = string(100, '1');
+    const string hundred_ones_and_atatat       = hundred_ones + "@@@"s;
+    const string minus_hundred_ones            = "-"s + hundred_ones;
+    const string minus_hundred_ones_and_atatat = "-"s + hundred_ones_and_atatat;
 
     for (int base = 2; base <= 36; ++base) {
         test_from_chars<T>("", base, 0, inv_arg); // no characters
