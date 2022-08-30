91ea7222649e96a93bfa32a0ad58a6a935317213
Stephan T. Lavavej <stl@nuwen.net>
2022-08-29 22:36:23 -0700

    Move check within _STL_COMPILER_PREPROCESSOR.

--- stl/inc/yvals.h
+++ stl/inc/yvals.h
@@ -7,13 +7,12 @@
 #ifndef _YVALS
 #define _YVALS
 #include <yvals_core.h>
+#if _STL_COMPILER_PREPROCESSOR
 
 #ifdef _ENFORCE_ONLY_CORE_HEADERS
 _EMIT_STL_ERROR(
     STL1005, "Tried to include a non-core C++ Standard Library header file with _ENFORCE_ONLY_CORE_HEADERS defined.");
 #endif // _ENFORCE_ONLY_CORE_HEADERS
-
-#if _STL_COMPILER_PREPROCESSOR
 
 #include <crtdbg.h>
 #include <crtdefs.h>
