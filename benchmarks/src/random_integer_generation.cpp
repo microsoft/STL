7e025782d9f99ebfe0a629269dfb55e2e984eca7
Stephan T. Lavavej <stl@nuwen.net>
2022-09-12 16:06:18 -0700

    Include <cstdint>, qualify typedefs.

--- benchmarks/src/random_integer_generation.cpp
+++ benchmarks/src/random_integer_generation.cpp
@@ -2,6 +2,7 @@
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
 #include <benchmark/benchmark.h>
+#include <cstdint>
 #include <random>
 
 /// Test URBGs alone
@@ -30,19 +31,19 @@ static void BM_lcg(benchmark::State& state) {
 }
 BENCHMARK(BM_lcg);
 
-uint32_t GetMax() {
+std::uint32_t GetMax() {
     std::random_device gen;
-    std::uniform_int_distribution<uint32_t> dist(10'000'000, 20'000'000);
+    std::uniform_int_distribution<std::uint32_t> dist(10'000'000, 20'000'000);
     return dist(gen);
 }
 
-static const uint32_t max = GetMax(); // random divisor to prevent strength reduction
+static const std::uint32_t max = GetMax(); // random divisor to prevent strength reduction
 
 /// Test mt19937
 
 static void BM_raw_mt19937_old(benchmark::State& state) {
     std::mt19937 gen;
-    std::_Rng_from_urng<uint32_t, decltype(gen)> rng(gen);
+    std::_Rng_from_urng<std::uint32_t, decltype(gen)> rng(gen);
     for (auto _ : state) {
         benchmark::DoNotOptimize(rng(max));
     }
@@ -51,7 +52,7 @@ BENCHMARK(BM_raw_mt19937_old);
 
 static void BM_raw_mt19937_new(benchmark::State& state) {
     std::mt19937 gen;
-    std::_Rng_from_urng_v2<uint32_t, decltype(gen)> rng(gen);
+    std::_Rng_from_urng_v2<std::uint32_t, decltype(gen)> rng(gen);
     for (auto _ : state) {
         benchmark::DoNotOptimize(rng(max));
     }
@@ -62,7 +63,7 @@ BENCHMARK(BM_raw_mt19937_new);
 
 static void BM_raw_mt19937_64_old(benchmark::State& state) {
     std::mt19937_64 gen;
-    std::_Rng_from_urng<uint64_t, decltype(gen)> rng(gen);
+    std::_Rng_from_urng<std::uint64_t, decltype(gen)> rng(gen);
     for (auto _ : state) {
         benchmark::DoNotOptimize(rng(max));
     }
@@ -71,7 +72,7 @@ BENCHMARK(BM_raw_mt19937_64_old);
 
 static void BM_raw_mt19937_64_new(benchmark::State& state) {
     std::mt19937_64 gen;
-    std::_Rng_from_urng_v2<uint64_t, decltype(gen)> rng(gen);
+    std::_Rng_from_urng_v2<std::uint64_t, decltype(gen)> rng(gen);
     for (auto _ : state) {
         benchmark::DoNotOptimize(rng(max));
     }
@@ -82,7 +83,7 @@ BENCHMARK(BM_raw_mt19937_64_new);
 
 static void BM_raw_lcg_old(benchmark::State& state) {
     std::minstd_rand gen;
-    std::_Rng_from_urng<uint32_t, decltype(gen)> rng(gen);
+    std::_Rng_from_urng<std::uint32_t, decltype(gen)> rng(gen);
     for (auto _ : state) {
         benchmark::DoNotOptimize(rng(max));
     }
@@ -91,7 +92,7 @@ BENCHMARK(BM_raw_lcg_old);
 
 static void BM_raw_lcg_new(benchmark::State& state) {
     std::minstd_rand gen;
-    std::_Rng_from_urng_v2<uint32_t, decltype(gen)> rng(gen);
+    std::_Rng_from_urng_v2<std::uint32_t, decltype(gen)> rng(gen);
     for (auto _ : state) {
         benchmark::DoNotOptimize(rng(max));
     }
