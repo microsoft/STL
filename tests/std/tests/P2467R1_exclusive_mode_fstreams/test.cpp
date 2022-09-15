8db9f2c5df08b4a721d307fec7ffea438989d4b9
Casey Carter <Casey@Carter.net>
2022-09-14 17:07:48 -0700

    Casey's review comments

--- tests/std/tests/P2467R1_exclusive_mode_fstreams/test.cpp
+++ tests/std/tests/P2467R1_exclusive_mode_fstreams/test.cpp
@@ -7,12 +7,12 @@
 
 namespace fs = std::filesystem;
 
-#define FILENAME L"test.dat"
+const fs::path test_file{L"test.dat"};
 
 struct CreateTempFile {
     fs::path m_path;
     CreateTempFile(const fs::path& path) : m_path(path) {
-        std::ofstream c{path};
+        std::ofstream{path};
     }
 
     ~CreateTempFile() {
@@ -20,30 +20,27 @@ struct CreateTempFile {
     }
 };
 
-void test_file_create(std::ios_base::openmode mode) {
-    fs::path p{FILENAME};
+void test_file_create(const std::ios_base::openmode mode) {
     {
-        std::fstream file{p, mode};
+        std::fstream file{test_file, mode};
         assert(!file.fail());
     }
-    assert(fs::exists(p));
-    fs::remove(p);
+    assert(fs::exists(test_file));
+    fs::remove(test_file);
 }
 
-void test_file_already_exist(std::ios_base::openmode mode) {
-    fs::path p{FILENAME};
-    CreateTempFile tmp{p};
-    std::fstream file{p, mode};
+void test_file_already_exist(const std::ios_base::openmode mode) {
+    CreateTempFile tmp{test_file};
+    std::fstream file{test_file, mode};
     assert(file.fail());
 }
 
-void test_file_create_fail(std::ios_base::openmode bad_mode) {
-    fs::path p{FILENAME};
+void test_file_create_fail(const std::ios_base::openmode bad_mode) {
     {
-        std::fstream file{p, bad_mode};
+        std::fstream file{test_file, bad_mode};
         assert(file.fail());
     }
-    assert(!fs::exists(p));
+    assert(!fs::exists(test_file));
 }
 
 int main() {
