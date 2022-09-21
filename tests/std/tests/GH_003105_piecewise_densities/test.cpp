1710367a33beb4438f1b101e6e659f57a5cdc7f0
Stephan T. Lavavej <stl@nuwen.net>
2022-09-20 17:32:54 -0700

    Also fix piecewise_linear_distribution::densities().

--- tests/std/tests/GH_003105_piecewise_densities/test.cpp
+++
@@ -1,8 +0,0 @@
-// Copyright (c) Microsoft Corporation.
-// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
-
-#include <random>
-
-int main() {
-    (void) std::piecewise_constant_distribution<float>{}.densities();
-}
