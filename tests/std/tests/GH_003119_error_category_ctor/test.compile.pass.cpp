c4200702b18b63ca3c8f315bd411bab76f84287d
A. Jiang <de34@live.cn>
2022-10-02 03:44:33 +0800

    Test files for `constexpr` default ctor of `error_category`

---
+++ tests/std/tests/GH_003119_error_category_ctor/test.compile.pass.cpp
@@ -0,0 +1,27 @@
+// Copyright (c) Microsoft Corporation.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+#include <string>
+#include <system_error>
+
+using namespace std;
+
+#ifndef _M_CEE_PURE
+struct meow_category : error_category {
+    constexpr meow_category() = default;
+
+    const char* name() const noexcept override {
+        return "meow";
+    }
+
+    string message(int) const override {
+        return "meow";
+    }
+};
+
+#if _HAS_CXX20
+[[maybe_unused]] constexpr meow_category meowing_cat;
+#endif // _HAS_CXX20
+#endif // _M_CEE_PURE
+
+int main() {} // COMPILE-ONLY
