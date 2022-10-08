55c9bca7f2181240505f1e5b39d77303d1f94887
Igor Zhukov <fsb4000@yandex.ru>
2022-10-08 22:46:08 +0700

    clang-format

--- stl/src/syserror_import_lib.cpp
+++ stl/src/syserror_import_lib.cpp
@@ -6,8 +6,9 @@
 // Do not include or define anything else here.
 // In particular, basic_string must not be included here.
 
-#include <Windows.h>
 #include <__msvc_system_error_abi.hpp>
+
+#include <Windows.h>
 
 _EXTERN_C
 _NODISCARD size_t __CLRCALL_PURE_OR_STDCALL __std_system_error_allocate_message(
@@ -21,13 +22,13 @@ _NODISCARD size_t __CLRCALL_PURE_OR_STDCALL __std_system_error_allocate_message(
     if (_Ret == 0) {
         _Lang_id = 0;
     }
-    
-     // Flag FORMAT_MESSAGE_MAX_WIDTH_MASK Removes All Trailing \r \n Leaves Only Whitespace Character
-+    const unsigned long _Chars = FormatMessageA(FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM
-+                                                    | FORMAT_MESSAGE_IGNORE_INSERTS | FORMAT_MESSAGE_MAX_WIDTH_MASK,
-     nullptr, _Message_id, _Lang_id, reinterpret_cast<char*>(_Ptr_str), 0, nullptr);
 
-     return _Chars - 1;
+    // Flag FORMAT_MESSAGE_MAX_WIDTH_MASK Removes All Trailing \r \n Leaves Only Whitespace Character
+    +const unsigned long _Chars = FormatMessageA(FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM +
+                                                     | FORMAT_MESSAGE_IGNORE_INSERTS | FORMAT_MESSAGE_MAX_WIDTH_MASK,
+        nullptr, _Message_id, _Lang_id, reinterpret_cast<char*>(_Ptr_str), 0, nullptr);
+
+    return _Chars - 1;
 }
 
 void __CLRCALL_PURE_OR_STDCALL __std_system_error_deallocate_message(char* const _Str) noexcept {
