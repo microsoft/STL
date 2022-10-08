ef333867a5af3c63d673ab530674034127bf30a5
Martins Andzans <101216311+MartinsAndzans@users.noreply.github.com>
2022-10-08 18:27:42 +0300

    Update syserror_import_lib.cpp

--- stl/src/syserror_import_lib.cpp
+++ stl/src/syserror_import_lib.cpp
@@ -26,7 +26,7 @@ _NODISCARD size_t __CLRCALL_PURE_OR_STDCALL __std_system_error_allocate_message(
     const unsigned long _Chars = FormatMessageA(
         FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM |
         FORMAT_MESSAGE_IGNORE_INSERTS | FORMAT_MESSAGE_MAX_WIDTH_MASK,
-            nullptr, _Message_id, _Lang_id, reinterpret_cast<char*>(_Ptr_str), 0, nullptr);
+        nullptr, _Message_id, _Lang_id, reinterpret_cast<char*>(_Ptr_str), 0, nullptr);
 
     return _Chars - 1;
     
