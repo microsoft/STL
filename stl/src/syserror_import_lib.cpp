3affef71cc3bd7a3ed76ec9003e92b4c030cff30
Igor Zhukov <fsb4000@yandex.ru>
2022-10-08 23:05:23 +0700

    restore __std_get_string_size_without_trailing_whitespace

--- stl/src/syserror_import_lib.cpp
+++ stl/src/syserror_import_lib.cpp
@@ -10,6 +10,26 @@
 
 #include <Windows.h>
 
+namespace {
+    struct _Whitespace_bitmap_t {
+        bool _Is_whitespace[256];
+
+        constexpr _Whitespace_bitmap_t() noexcept : _Is_whitespace{} {
+            _Is_whitespace[' ']  = true;
+            _Is_whitespace['\n'] = true;
+            _Is_whitespace['\r'] = true;
+            _Is_whitespace['\t'] = true;
+            _Is_whitespace['\0'] = true;
+        }
+
+        _NODISCARD constexpr bool _Test(const char _Ch) const noexcept {
+            return _Is_whitespace[static_cast<unsigned char>(_Ch)];
+        }
+    };
+
+    constexpr _Whitespace_bitmap_t _Whitespace_bitmap;
+} // unnamed namespace
+
 _EXTERN_C
 _NODISCARD size_t __CLRCALL_PURE_OR_STDCALL __std_get_string_size_without_trailing_whitespace(
     const char* const _Str, size_t _Size) noexcept {
