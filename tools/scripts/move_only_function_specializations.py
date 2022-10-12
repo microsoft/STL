1d8fbece8abf2f30625703307076b4cf0f8d2f63
Brandon <pacewicbrandon@gmail.com>
2022-10-12 06:45:14 -0700

    Tools: python script cleanups (functionality unchanged) (#3123)

--- tools/scripts/move_only_function_specializations.py
+++ tools/scripts/move_only_function_specializations.py
@@ -3,7 +3,7 @@
 
 # This script generates the partial specializations of _Move_only_function_call in <functional>.
 
-def specialization(cv, ref, ref_inv, noex, noex_val, callable):
+def specialization(cv: str, ref: str, ref_inv: str, noex: str, noex_val: str, callable: str) -> str:
     return f"""template <class _Rx, class... _Types>
 class _Move_only_function_call<_Rx(_Types...) {cv} {ref} {noex}>
     : public _Move_only_function_base<_Rx, {noex_val}, _Types...> {{
@@ -22,18 +22,21 @@ public:
 }};
 """
 
-def ref_permutations(cv, noex, noex_val, trait):
+
+def ref_permutations(cv: str, noex: str, noex_val: str, trait: str) -> str:
     return specialization(cv, "", "&", noex, noex_val, \
         f"{trait}<_Rx, {cv} _Vt, _Types...> && {trait}<_Rx, {cv} _Vt&, _Types...>") + "\n" \
         + specialization(cv, "&", "&", noex, noex_val, f"{trait}<_Rx, {cv} _Vt&, _Types...>") + "\n" \
         + specialization(cv, "&&", "&&", noex, noex_val, f"{trait}<_Rx, {cv} _Vt, _Types...>")
 
-def cvref_permutations(noex, noex_val, trait):
+
+def cvref_permutations(noex: str, noex_val: str, trait: str) -> str:
     return ref_permutations("", noex, noex_val, trait) + "\n" \
         + ref_permutations("const", noex, noex_val, trait)
 
 
-print(cvref_permutations("", "false", "is_invocable_r_v") + "\n" \
-    + "#ifdef __cpp_noexcept_function_type" + "\n" \
-    + cvref_permutations("noexcept", "true", "is_nothrow_invocable_r_v") \
-    + "#endif // __cpp_noexcept_function_type")
+if __name__ == "__main__":
+    print(cvref_permutations("", "false", "is_invocable_r_v") + "\n" \
+        + "#ifdef __cpp_noexcept_function_type" + "\n" \
+        + cvref_permutations("noexcept", "true", "is_nothrow_invocable_r_v") \
+        + "#endif // __cpp_noexcept_function_type")
