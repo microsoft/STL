88f040f9c933e4e262c85ece3203eb22953acb27
Stephan T. Lavavej <stl@nuwen.net>
2022-10-12 17:05:19 -0700

    Manually fix formatting.

--- tests/std/tests/P0896R4_ranges_iterator_machinery/test.cpp
+++ tests/std/tests/P0896R4_ranges_iterator_machinery/test.cpp
@@ -1210,8 +1210,7 @@ namespace iterator_cust_swap_test {
 
     // N4820 [iterator.cust.swap]/4.4: "Otherwise, ranges::iter_swap(E1, E2) is ill-formed."
     template <class T, class U>
-    concept bullet4 = !
-    can_iter_swap<T, U>;
+    concept bullet4 = (!can_iter_swap<T, U>);
 
     STATIC_ASSERT(bullet4<void, void>);
     STATIC_ASSERT(bullet4<int, int>);
