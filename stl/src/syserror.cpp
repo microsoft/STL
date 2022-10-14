1467f0756ed72289286b1b2a80cebf36b2f0206d
Martins Andzans <101216311+MartinsAndzans@users.noreply.github.com>
2022-10-14 14:50:37 +0300

    Update syserror.cpp
    Formating

--- stl/src/syserror.cpp
+++ stl/src/syserror.cpp
@@ -229,11 +229,7 @@ _CRTIMP2_PURE unsigned long __CLRCALL_PURE_OR_CDECL _Winerror_message(
         FormatMessageA(FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS | FORMAT_MESSAGE_MAX_WIDTH_MASK,
             nullptr, _Message_id, 0, _Narrow, _Size, nullptr);
     
-    if (_Chars == 0) {
-        return 0;
-    } else {
-        return _Chars - 1;
-    }
+    return (_Chars == 0) ? 0 : _Chars - 1;
 }
 
 _CRTIMP2_PURE const char* __CLRCALL_PURE_OR_CDECL _Syserror_map(int _Errcode) { // convert to name of generic error
