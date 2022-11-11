0743f645849da4b0734881db9cf3353e1a9421da
Igor Zhukov <fsb4000@yandex.ru>
2022-11-12 05:52:48 +0700

    enable P0943R6_stdatomic_h test for clang (#3186)

--- tests/std/tests/P0943R6_stdatomic_h/test.compile.pass.cpp
+++ tests/std/tests/P0943R6_stdatomic_h/test.compile.pass.cpp
@@ -1,7 +1,6 @@
 // Copyright (c) Microsoft Corporation.
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
-#ifndef __clang__ // TRANSITION, GH-2862
 #include <__msvc_cxx_stdatomic.hpp>
 
 static_assert(ATOMIC_BOOL_LOCK_FREE == 2);
@@ -116,6 +115,5 @@ namespace test {
 
 static_assert(std::atomic_thread_fence == atomic_thread_fence);
 static_assert(std::atomic_signal_fence == atomic_signal_fence);
-#endif // TRANSITION, GH-2862
 
 int main() {} // COMPILE-ONLY
