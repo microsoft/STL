8f5de32f9d4877873109238bfb2561013d67f490
Daniel Marshall <xandan@gmail.com>
2022-12-15 21:52:51 +0000

    Update `source_location` to use `__builtin_FUNCSIG` (#3206)
    Co-authored-by: Igor Zhukov <fsb4000@yandex.ru>
    Co-authored-by: nicole mazzuca <83086508+strega-nil-ms@users.noreply.github.com>
    Co-authored-by: Stephan T. Lavavej <stl@nuwen.net>

--- tests/std/tests/P1208R6_source_location/header.h
+++ tests/std/tests/P1208R6_source_location/header.h
@@ -11,6 +11,10 @@ constexpr void header_test() {
     const auto x = source_location::current();
     assert(x.line() == __LINE__ - 1);
     assert(x.column() == 37);
+#if defined(__clang__) || defined(__EDG__) // TRANSITION, DevCom-10199227 and LLVM-58951
     assert(x.function_name() == "header_test"sv);
+#else // ^^^ workaround / no workaround vvv
+    assert(x.function_name() == "void __cdecl header_test(void)"sv);
+#endif // TRANSITION, DevCom-10199227 and LLVM-58951
     assert(string_view{x.file_name()}.ends_with("header.h"sv));
 }
