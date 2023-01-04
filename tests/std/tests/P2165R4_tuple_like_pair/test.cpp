f277096d2cef4897349d3c1f1291c7a249d4ea27
Jakub Mazurkiewicz <mazkuba3@gmail.com>
2023-01-04 12:27:27 +0100

    Test pair's new assignment operators

--- tests/std/tests/P2165R4_tuple_like_pair/test.cpp
+++ tests/std/tests/P2165R4_tuple_like_pair/test.cpp
@@ -6,10 +6,12 @@
 #include <tuple>
 #include <type_traits>
 #include <utility>
+#include <vector>
 
 using namespace std;
 
 struct TmpInt {
+    constexpr TmpInt() : TmpInt(0) {}
     constexpr TmpInt(int v) : val{v} {}
 
     constexpr TmpInt(const TmpInt&) = default;
@@ -26,12 +28,6 @@ struct TmpInt {
     int val;
     constexpr bool operator==(const TmpInt&) const = default;
 };
-
-template <class T>
-using BinaryTuple = tuple<T, T>;
-
-template <class T>
-using BinaryArray = array<T, 2>;
 
 template <template <class> class PairLike>
 constexpr bool test_pair_like_constructor() {
@@ -71,45 +67,123 @@ constexpr bool test_pair_like_constructor() {
     pair<TmpInt&, TmpInt&> p2(a);
     assert(&p2.first == &get<0>(a) && &p2.second == &get<1>(a));
 
-    pair<const TmpInt&, const TmpInt&> p3 = a;
+    pair<const TmpInt&, const TmpInt&> p3(a);
     assert(&p3.first == &get<0>(a) && &p3.second == &get<1>(a));
 
-    pair<TmpInt, TmpInt> p4 = as_const(a);
+    pair<TmpInt, TmpInt> p4(as_const(a));
     assert(p4.first == 1 && p4.second == 2 && get<0>(a) == 1 && get<1>(a) == 2);
 
-    pair<const TmpInt&, const TmpInt&> p5 = as_const(a);
+    pair<const TmpInt&, const TmpInt&> p5(as_const(a));
     assert(&p5.first == &get<0>(a) && &p5.second == &get<1>(a));
 
-    pair<TmpInt, TmpInt> p6 = std::move(a);
+    pair<TmpInt, TmpInt> p6(std::move(a));
     assert(p6.first == 1 && p6.second == 2 && get<0>(a) == -1 && get<1>(a) == -1);
     tie(get<0>(a), get<1>(a)) = std::move(p6);
 
-    pair<const TmpInt&, const TmpInt&> p7 = std::move(a);
+    pair<const TmpInt&, const TmpInt&> p7(std::move(a));
     assert(&p7.first == &get<0>(a) && &p7.second == &get<1>(a));
 
-    pair<TmpInt&&, TmpInt&&> p8 = std::move(a);
+    pair<TmpInt&&, TmpInt&&> p8(std::move(a));
     assert(&p8.first == &get<0>(a) && &p8.second == &get<1>(a));
 
-    pair<const TmpInt&&, const TmpInt&&> p9 = std::move(a);
+    pair<const TmpInt&&, const TmpInt&&> p9(std::move(a));
     assert(&p9.first == &get<0>(a) && &p9.second == &get<1>(a));
 
-    pair<TmpInt, TmpInt> p10 = std::move(as_const(a));
+    pair<TmpInt, TmpInt> p10(std::move(as_const(a)));
     assert(p10.first == 1 && p10.second == 2 && get<0>(a) == 1 && get<1>(a) == 2);
 
-    pair<const TmpInt&, const TmpInt&> p11 = std::move(as_const(a));
+    pair<const TmpInt&, const TmpInt&> p11(std::move(as_const(a)));
     assert(p11.first == 1 && p11.second == 2 && get<0>(a) == 1 && get<1>(a) == 2);
 
-    pair<const TmpInt&&, const TmpInt&&> p12 = std::move(as_const(a));
+    pair<const TmpInt&&, const TmpInt&&> p12(std::move(as_const(a)));
     assert(p12.first == 1 && p12.second == 2 && get<0>(a) == 1 && get<1>(a) == 2);
 
     return true;
 }
 
-// FIXME test assignment operators
+template <template <class> class PairLike>
+constexpr bool test_pair_like_assignment() {
+    static_assert(is_assignable_v<pair<int, int>&, PairLike<int>&>);
+    static_assert(is_assignable_v<pair<int, int>&, const PairLike<int>&>);
+    static_assert(is_assignable_v<pair<int, int>&, PairLike<int>>);
+    static_assert(is_assignable_v<pair<int, int>&, const PairLike<int>>);
+
+    PairLike<TmpInt> a = {1, 2};
+
+    pair<TmpInt, TmpInt> p1;
+    p1 = a;
+    assert(p1.first == 1 && p1.second == 2 && get<0>(a) == 1 && get<1>(a) == 2);
+
+    pair<TmpInt, TmpInt> p2;
+    p2 = as_const(a);
+    assert(p2.first == 1 && p2.second == 2 && get<0>(a) == 1 && get<1>(a) == 2);
+
+    pair<TmpInt, TmpInt> p3;
+    p3 = std::move(a);
+    assert(p3.first == 1 && p3.second == 2 && get<0>(a) == -1 && get<1>(a) == -1);
+    tie(get<0>(a), get<1>(a)) = std::move(p3);
+
+    pair<TmpInt, TmpInt> p4;
+    p4 = std::move(as_const(a));
+    assert(p4.first == 1 && p4.second == 2 && get<0>(a) == 1 && get<1>(a) == 2);
+
+    return true;
+}
+
+template <template <class> class PairLike>
+constexpr bool test_pair_like_const_assignment() {
+    using Ref  = std::vector<bool>::reference;
+    using Pair = pair<Ref, Ref>;
+
+    static_assert(is_assignable_v<const Pair&, PairLike<bool>&>);
+    static_assert(is_assignable_v<const Pair&, const PairLike<bool>&>);
+    static_assert(is_assignable_v<const Pair&, PairLike<bool>>);
+    static_assert(is_assignable_v<const Pair&, const PairLike<bool>>);
+
+    std::vector<bool> bools = {false, true};
+    PairLike<bool> a        = {true, false};
+
+    Pair p1{bools[0], bools[1]};
+    p1 = a;
+    assert(p1.first == true && p1.second == false);
+    bools = {false, true};
+
+    Pair p2{bools[0], bools[1]};
+    p2 = as_const(a);
+    assert(p2.first == true && p2.second == false);
+    bools = {false, true};
+
+    Pair p3{bools[0], bools[1]};
+    p3 = std::move(a);
+    assert(p3.first == true && p3.second == false);
+    bools = {false, true};
+
+    Pair p4{bools[0], bools[1]};
+    p4 = std::move(as_const(a));
+    assert(p4.first == true && p4.second == false);
+
+    return true;
+}
+
+template <class T>
+using BinaryTuple = tuple<T, T>;
+
+template <class T>
+using BinaryArray = array<T, 2>;
 
 int main() {
     static_assert(test_pair_like_constructor<BinaryArray>());
     assert((test_pair_like_constructor<BinaryArray>()));
     static_assert(test_pair_like_constructor<BinaryTuple>());
     assert((test_pair_like_constructor<BinaryTuple>()));
+
+    static_assert(test_pair_like_assignment<BinaryArray>());
+    assert((test_pair_like_assignment<BinaryArray>()));
+    static_assert(test_pair_like_assignment<BinaryTuple>());
+    assert((test_pair_like_assignment<BinaryTuple>()));
+
+    static_assert(test_pair_like_const_assignment<BinaryArray>());
+    assert((test_pair_like_const_assignment<BinaryArray>()));
+    static_assert(test_pair_like_const_assignment<BinaryTuple>());
+    assert((test_pair_like_const_assignment<BinaryTuple>()));
 }
