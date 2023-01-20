29cc2549578168189dd5645dd16f2828d147295f
Daniel Marshall <xandan@gmail.com>
2023-01-20 10:13:44 +0000

    before begin and update death test .

--- tests/std/tests/P2474R2_views_repeat_death/test.cpp
+++ tests/std/tests/P2474R2_views_repeat_death/test.cpp
@@ -77,7 +77,6 @@ void test_iter_add_neg_overflow() {
 int main(int argc, char* argv[]) {
     std_testing::death_test_executive exec;
 
-#if _ITERATOR_DEBUG_LEVEL != 0
     exec.add_death_tests({
         test_view_negative,
         test_iter_decrement,
@@ -90,10 +89,5 @@ int main(int argc, char* argv[]) {
         test_iter_add_pos_overflow,
         test_iter_add_neg_overflow,
     });
-#else // ^^^ test everything / test only _CONTAINER_DEBUG_LEVEL case vvv
-    exec.add_death_tests({
-        test_view_negative,
-    });
-#endif // _ITERATOR_DEBUG_LEVEL != 0
     return exec.run(argc, argv);
 }
