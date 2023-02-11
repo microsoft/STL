29638bab058069d8951fbcccc9bbd3c33e0d1971
MikiPaul <mikipaul46@gmail.com>
2023-02-11 05:45:11 +0530

    Updated _MSVC_STL_UPDATE to February 2023 (#3395)

--- stl/inc/yvals_core.h
+++ stl/inc/yvals_core.h
@@ -813,7 +813,7 @@
 
 #define _CPPLIB_VER       650
 #define _MSVC_STL_VERSION 143
-#define _MSVC_STL_UPDATE  202301L
+#define _MSVC_STL_UPDATE  202302L
 
 #ifndef _ALLOW_COMPILER_AND_STL_VERSION_MISMATCH
 #if defined(__CUDACC__) && defined(__CUDACC_VER_MAJOR__)
