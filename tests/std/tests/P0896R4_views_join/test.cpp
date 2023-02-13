4ccfc5eb7bc3d639d9c0dc8764e2951e22c772f3
cpplearner <cpplearner@outlook.com>
2023-02-13 19:04:14 +0800

    Revert "`<ranges>`: Temporarily disable `join_view` for non-`forward_range`s, pending resolution of LWG-3698 (#2727)"
    This reverts commit 296681379db18e0aeac36f2da3c416b811a176b3.

--- tests/std/tests/P0896R4_views_join/test.cpp
+++ tests/std/tests/P0896R4_views_join/test.cpp
@@ -1,8 +1,6 @@
 // Copyright (c) Microsoft Corporation.
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
-#define _USE_JOIN_VIEW_INPUT_RANGE
-
 #include <algorithm>
 #include <array>
 #include <cassert>
