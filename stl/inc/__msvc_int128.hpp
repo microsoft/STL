ec352e2f1ee68ccb779998746731f1d7b37ca8f0
steve02081504 <steve02081504@EDEN.fukwold>
2023-03-11 21:20:59 +0800

    fix of int128tofloat
    Co-Authored-By: A. Jiang <23228989+frederick-vs-ja@users.noreply.github.com>

--- stl/inc/__msvc_int128.hpp
+++ stl/inc/__msvc_int128.hpp
@@ -1385,10 +1385,9 @@ struct _Signed128 : _Base128 {
 
     template <class _Ty, enable_if_t<is_floating_point_v<_Ty>, int> = 0>
     _NODISCARD constexpr explicit operator _Ty() const noexcept {
-        bool _Negative = int64_t(_Word[1]) < 0;
-        const _Unsigned128 _Abs{_Word[0], (_Negative ? uint64_t(-int64_t(_Word[1])) : uint64_t(_Word[1]))};
-        auto _Result = static_cast<_Ty>(_Abs);
-        return _Negative ? -_Result : _Result;
+        const auto _Unsigned_self = static_cast<_Unsigned128>(*this);
+        return static_cast<int64_t>(_Word[1]) < 0 ? -static_cast<_Ty>(-_Unsigned_self)
+                                                  : static_cast<_Ty>(_Unsigned_self);
     }
     template <class _Ty, enable_if_t<is_floating_point_v<_Ty>, int> = 0>
     constexpr explicit _Signed128(const _Ty _Val) noexcept {
