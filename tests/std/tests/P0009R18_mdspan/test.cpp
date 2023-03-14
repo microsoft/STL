187a036681484dc81501e76bfcbb4bf9b0ed0c46
Jakub Mazurkiewicz <mazkuba3@gmail.com>
2023-03-14 01:36:41 +0100

    `<mdspan>`: Use concepts everywhere and add `mdspan` deduction guides (#3564)

--- tests/std/tests/P0009R18_mdspan/test.cpp
+++ tests/std/tests/P0009R18_mdspan/test.cpp
@@ -1,6 +1,7 @@
 // Copyright (c) Microsoft Corporation.
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
+#include <algorithm>
 #include <cassert>
 #include <concepts>
 #include <mdspan>
