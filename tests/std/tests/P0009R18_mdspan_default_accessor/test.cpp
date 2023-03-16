7d51c1172daf53f8bb3e39420b2026bb5a5f8045
Jakub Mazurkiewicz <mazkuba3@gmail.com>
2023-03-17 00:32:23 +0100

    Test `default_accessor`

---
+++ tests/std/tests/P0009R18_mdspan_default_accessor/test.cpp
@@ -0,0 +1,62 @@
+// Copyright (c) Microsoft Corporation.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+#include <array>
+#include <cassert>
+#include <concepts>
+#include <mdspan>
+#include <string>
+#include <type_traits>
+
+using namespace std;
+
+template <class ElementType>
+constexpr void test_one(array<ElementType, 3> elems) {
+    using DefaultAccessor = default_accessor<ElementType>;
+
+    // Check modeled concepts
+    static_assert(is_trivially_copyable_v<DefaultAccessor>);
+    static_assert(semiregular<DefaultAccessor>);
+
+    // Check nested types
+    static_assert(same_as<typename DefaultAccessor::offset_policy, DefaultAccessor>);
+    static_assert(same_as<typename DefaultAccessor::element_type, ElementType>);
+    static_assert(same_as<typename DefaultAccessor::reference, ElementType&>);
+    static_assert(same_as<typename DefaultAccessor::data_handle_type, ElementType*>);
+
+    // Check default constructor
+    DefaultAccessor accessor;
+    static_assert(is_nothrow_default_constructible_v<DefaultAccessor>);
+
+    { // Check converting constructor from other accessor
+        [[maybe_unused]] default_accessor<const ElementType> const_accessor = accessor;
+        static_assert(is_nothrow_constructible_v<default_accessor<const ElementType>, DefaultAccessor>);
+        static_assert(!is_constructible_v<DefaultAccessor, default_accessor<const ElementType>>);
+    }
+
+    { // Check 'access' member function
+        same_as<ElementType&> decltype(auto) ref = accessor.access(elems.data(), 1);
+        assert(ref == elems[1]);
+        static_assert(noexcept(accessor.access(elems.data(), 0)));
+    }
+
+    { // Check 'offset' member function
+        same_as<ElementType*> auto ptr = accessor.offset(elems.data(), 1);
+        assert(ptr == elems.data() + 1);
+        static_assert(noexcept(accessor.offset(elems.data(), 0)));
+    }
+}
+
+constexpr bool test() {
+    test_one<char>({'a', 'b', 'c'});
+    test_one<long>({1, 2, 3});
+    test_one<float>({1.1, 2.2, 3.3});
+    test_one<wstring>({L"1", L"2", L"3"});
+    test_one<intmax_t>({3, 2, 1});
+    return true;
+}
+
+int main() {
+    static_assert(test());
+    test();
+}
