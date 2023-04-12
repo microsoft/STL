aeef02980ef96e5efeeed0c4e0b7475e2615db12
A. Jiang <de34@live.cn>
2023-04-12 12:05:47 +0800

    Restrict the test, leaving UB alone

--- tests/std/tests/P0355R7_calendars_and_time_zones_formatting/test.cpp
+++ tests/std/tests/P0355R7_calendars_and_time_zones_formatting/test.cpp
@@ -264,7 +264,8 @@ void test_duration_formatter() {
     empty_braces_helper(duration<int, ratio<22, 7>>{40}, STR("40[22/7]s"));
     empty_braces_helper(duration<int, ratio<53, 101>>{40}, STR("40[53/101]s"));
     empty_braces_helper(duration<int, ratio<201, 2147483647>>{40}, STR("40[201/2147483647]s"));
-    empty_braces_helper(duration<int, LongRatio>{40}, STR("40[9223372036854775806/9223372036854775807]s"));
+    // TRANSITION, duration_cast used in formatting may raise UB
+    empty_braces_helper(duration<int, LongRatio>{1}, STR("1[9223372036854775806/9223372036854775807]s"));
 
     // formatting small types needs to work as iostreams << VSO-1521926
     empty_braces_helper(duration<long long, atto>{123}, STR("123as"));
