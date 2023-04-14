d939f8c9ef995d2b7295bcdd80cab80d348d3a77
YairBorn <77966440+YairBorn@users.noreply.github.com>
2023-04-14 06:35:15 +0300

    Update `yvals_core.h` to April 2023 (#3643)

--- stl/inc/yvals_core.h
+++ stl/inc/yvals_core.h
@@ -829,7 +829,7 @@
 
 #define _CPPLIB_VER       650
 #define _MSVC_STL_VERSION 143
-#define _MSVC_STL_UPDATE  202303L
+#define _MSVC_STL_UPDATE  202304L
 
 #ifndef _ALLOW_COMPILER_AND_STL_VERSION_MISMATCH
 #if defined(__CUDACC__) && defined(__CUDACC_VER_MAJOR__)
