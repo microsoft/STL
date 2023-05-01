b400671e6a9952e41980240d895aaebff16b43ed
Stephan T. Lavavej <stl@nuwen.net>
2023-05-01 15:06:49 -0700

    Add test cases.

--- tests/std/tests/P2374R4_checked_arithmetic_operations/test.cpp
+++ tests/std/tests/P2374R4_checked_arithmetic_operations/test.cpp
@@ -18,7 +18,9 @@ constexpr void check_add_overflow() {
     const T minval = numeric_limits<T>::min();
     const T maxval = numeric_limits<T>::max();
 
+    assert(!_Add_overflow(T{0}, T{0}, out) && out == T{0});
     assert(!_Add_overflow(T{1}, T{1}, out) && out == T{2});
+    assert(!_Add_overflow(minval, T{0}, out) && out == minval);
     assert(!_Add_overflow(maxval, T{0}, out) && out == maxval);
     assert(!_Add_overflow(minval, maxval, out) && out == static_cast<T>(-1));
     assert(_Add_overflow(maxval, T{1}, out));
@@ -30,6 +32,7 @@ constexpr void check_add_overflow() {
     if constexpr (is_T_signed) {
         assert(!_Add_overflow(T{1}, T{-1}, out) && out == T{0});
         assert(_Add_overflow(minval, T{-1}, out));
+        assert(_Add_overflow(minval, minval, out));
         assert(_Add_overflow(T{100}, T{28}, out) == (sizeof(T) == 1));
     } else {
         assert(_Add_overflow(T{200}, T{56}, out) == (sizeof(T) == 1));
@@ -43,11 +46,15 @@ constexpr void check_mul_overflow() {
     const T minval = numeric_limits<T>::min();
     const T maxval = numeric_limits<T>::max();
 
+    assert(!_Mul_overflow(T{0}, T{0}, out) && out == T{0});
     assert(!_Mul_overflow(T{1}, T{1}, out) && out == T{1});
+    assert(!_Mul_overflow(minval, T{0}, out) && out == T{0});
+    assert(!_Mul_overflow(minval, T{1}, out) && out == minval);
     assert(!_Mul_overflow(maxval, T{0}, out) && out == T{0});
     assert(!_Mul_overflow(maxval, T{1}, out) && out == maxval);
     assert(_Mul_overflow(maxval, maxval, out));
     assert(_Mul_overflow(minval, maxval, out) == is_T_signed);
+    assert(_Mul_overflow(minval, minval, out) == is_T_signed);
     assert(!_Mul_overflow(static_cast<T>(maxval / 2), T{2}, out) && out == static_cast<T>(maxval - 1));
     assert(_Mul_overflow(T{0x77}, T{0x78}, out) == (sizeof(T) == 1));
 
