5e256cecf2594b730eecc0295f649e31beba12e4
Jakub Mazurkiewicz <mazkuba3@gmail.com>
2023-05-10 17:14:44 +0200

    Add DevCom issue comment

--- tests/std/include/test_header_units_and_modules.hpp
+++ tests/std/include/test_header_units_and_modules.hpp
@@ -393,12 +393,11 @@ void test_mdspan() {
     using namespace std;
     puts("Testing <mdspan>.");
     int arr[] = {1, 0, 0, 0, 1, 0, 0, 0, 1};
-
-    // TRANSITION, check std::mdspan class too (currently constructors and deduction guides does not work with modules)
     layout_right::mapping<extents<int, 3, 3>> mp;
     assert(arr[mp(0, 0)] == 1);
     assert(arr[mp(1, 1)] == 1);
     assert(arr[mp(2, 2)] == 1);
+    // TRANSITION, test std::mdspan too (DevCom-10359857)
 }
 
 void test_memory() {
