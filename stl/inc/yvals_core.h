91d499486f1ed156a3468483c2700ff8e8a8c57c
Stephan T. Lavavej <stl@nuwen.net>
2023-05-16 12:57:51 -0700

    Require Clang 16.

--- stl/inc/yvals_core.h
+++ stl/inc/yvals_core.h
@@ -840,8 +840,8 @@ _EMIT_STL_ERROR(STL1002, "Unexpected compiler version, expected CUDA 11.6 or new
 #elif defined(__EDG__)
 // not attempting to detect __EDG_VERSION__ being less than expected
 #elif defined(__clang__)
-#if __clang_major__ < 15
-_EMIT_STL_ERROR(STL1000, "Unexpected compiler version, expected Clang 15.0.0 or newer.");
+#if __clang_major__ < 16
+_EMIT_STL_ERROR(STL1000, "Unexpected compiler version, expected Clang 16.0.0 or newer.");
 #endif // ^^^ old Clang ^^^
 #elif defined(_MSC_VER)
 #if _MSC_VER < 1936 // Coarse-grained, not inspecting _MSC_FULL_VER
