47679bbaa8d1960174a0732e0184e7ff9fc49626
A. Jiang <de34@live.cn>
2023-06-15 16:21:12 +0800

    Concentrated header for internal bit utilities (#3721)
    Co-authored-by: Stephan T. Lavavej <stl@microsoft.com>

--- tests/std/tests/GH_002206_unreserved_names/test.compile.pass.cpp
+++ tests/std/tests/GH_002206_unreserved_names/test.compile.pass.cpp
@@ -1,9 +1,10 @@
 // Copyright (c) Microsoft Corporation.
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
-#define nsec      delete
-#define sec       delete
-#define xtime     delete
-#define xtime_get delete
+#define ISA_AVAILABILITY delete
+#define nsec             delete
+#define sec              delete
+#define xtime            delete
+#define xtime_get        delete
 
 #include <__msvc_all_public_headers.hpp>
