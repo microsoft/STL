5226b57d00f06a813e6cc10959f7e9acce2f2182
A. Jiang <de34@live.cn>
2023-06-15 16:57:51 +0800

    Workaround for non-_Ugly attribute tokens (#3760)
    Co-authored-by: Stephan T. Lavavej <stl@nuwen.net>

--- tests/std/tests/GH_002206_unreserved_names/test.compile.pass.cpp
+++ tests/std/tests/GH_002206_unreserved_names/test.compile.pass.cpp
@@ -7,4 +7,31 @@
 #define xtime            delete
 #define xtime_get        delete
 
+// Also test GH-2645: <yvals_core.h>: Conformance issue on [[msvc::known_semantics]]
+#define msvc            1
+#define known_semantics 2
+#define intrinsic       3
+#define lifetimebound   4
+#define noop_dtor       5
+
 #include <__msvc_all_public_headers.hpp>
+
+#if msvc != 1
+#error bad macro expansion
+#endif // msvc != 1
+
+#if known_semantics != 2
+#error bad macro expansion
+#endif // known_semantics != 2
+
+#if intrinsic != 3
+#error bad macro expansion
+#endif // intrinsic != 3
+
+#if lifetimebound != 4
+#error bad macro expansion
+#endif // lifetimebound != 4
+
+#if noop_dtor != 5
+#error bad macro expansion
+#endif // noop_dtor != 5
