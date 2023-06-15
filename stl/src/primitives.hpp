c3114d1876eba6eb6b066f335f3067c9cae41a7d
cpplearner <cpplearner@outlook.com>
2023-06-15 11:03:16 +0800

    Alternative approach

--- stl/src/primitives.hpp
+++ stl/src/primitives.hpp
@@ -10,25 +10,7 @@
 
 namespace Concurrency {
     namespace details {
-        class __declspec(novtable) stl_critical_section_interface {
-        public:
-            virtual void lock()                     = 0;
-            virtual bool try_lock()                 = 0;
-            virtual bool try_lock_for(unsigned int) = 0;
-            virtual void unlock()                   = 0;
-            virtual void destroy()                  = 0;
-        };
-
-        class __declspec(novtable) stl_condition_variable_interface {
-        public:
-            virtual void wait(stl_critical_section_interface*)                   = 0;
-            virtual bool wait_for(stl_critical_section_interface*, unsigned int) = 0;
-            virtual void notify_one()                                            = 0;
-            virtual void notify_all()                                            = 0;
-            virtual void destroy()                                               = 0;
-        };
-
-        class stl_critical_section_win7 final : public stl_critical_section_interface {
+        class stl_critical_section_win7 {
         public:
             stl_critical_section_win7() = default;
 
@@ -36,22 +18,22 @@ namespace Concurrency {
             stl_critical_section_win7(const stl_critical_section_win7&)            = delete;
             stl_critical_section_win7& operator=(const stl_critical_section_win7&) = delete;
 
-            void destroy() override {}
+            void destroy() {}
 
-            void lock() override {
+            void lock() {
                 AcquireSRWLockExclusive(&m_srw_lock);
             }
 
-            bool try_lock() override {
+            bool try_lock() {
                 return TryAcquireSRWLockExclusive(&m_srw_lock) != 0;
             }
 
-            bool try_lock_for(unsigned int) override {
+            bool try_lock_for(unsigned int) {
                 // STL will call try_lock_for once again if this call will not succeed
                 return stl_critical_section_win7::try_lock();
             }
 
-            void unlock() override {
+            void unlock() {
                 _Analysis_assume_lock_held_(m_srw_lock);
                 ReleaseSRWLockExclusive(&m_srw_lock);
             }
@@ -61,61 +43,49 @@ namespace Concurrency {
             }
 
         private:
+            void* unused       = nullptr; // TRANSITON, ABI: was the vptr
             SRWLOCK m_srw_lock = SRWLOCK_INIT;
         };
 
-        class stl_condition_variable_win7 final : public stl_condition_variable_interface {
+        class stl_condition_variable_win7 {
         public:
-            stl_condition_variable_win7() {
-                InitializeConditionVariable(&m_condition_variable);
-            }
+            stl_condition_variable_win7() = default;
 
             ~stl_condition_variable_win7()                                             = delete;
             stl_condition_variable_win7(const stl_condition_variable_win7&)            = delete;
             stl_condition_variable_win7& operator=(const stl_condition_variable_win7&) = delete;
 
-            void destroy() override {}
+            void destroy() {}
 
-            void wait(stl_critical_section_interface* lock) override {
+            void wait(stl_critical_section_win7* lock) {
                 if (!stl_condition_variable_win7::wait_for(lock, INFINITE)) {
                     std::terminate();
                 }
             }
 
-            bool wait_for(stl_critical_section_interface* lock, unsigned int timeout) override {
-                return SleepConditionVariableSRW(&m_condition_variable,
-                           static_cast<stl_critical_section_win7*>(lock)->native_handle(), timeout, 0)
-                    != 0;
+            bool wait_for(stl_critical_section_win7* lock, unsigned int timeout) {
+                return SleepConditionVariableSRW(&m_condition_variable, lock->native_handle(), timeout, 0) != 0;
             }
 
-            void notify_one() override {
+            void notify_one() {
                 WakeConditionVariable(&m_condition_variable);
             }
 
-            void notify_all() override {
+            void notify_all() {
                 WakeAllConditionVariable(&m_condition_variable);
             }
 
         private:
-            CONDITION_VARIABLE m_condition_variable;
-        };
-
-        struct stl_critical_section {
-            void* unused       = nullptr;
-            SRWLOCK m_srw_lock = SRWLOCK_INIT;
-        };
-
-        inline void create_stl_critical_section(stl_critical_section_interface* p) {
-            new (p) stl_critical_section;
-        }
-
-        struct stl_condition_variable {
-            void* unused                            = nullptr;
+            void* unused                            = nullptr; // TRANSITON, ABI: was the vptr
             CONDITION_VARIABLE m_condition_variable = CONDITION_VARIABLE_INIT;
         };
 
-        inline void create_stl_condition_variable(stl_condition_variable_interface* p) {
-            new (p) stl_condition_variable;
+        inline void create_stl_critical_section(stl_critical_section_win7* p) {
+            new (p) stl_critical_section_win7;
+        }
+
+        inline void create_stl_condition_variable(stl_condition_variable_win7* p) {
+            new (p) stl_condition_variable_win7;
         }
 
 #ifdef _WIN64
