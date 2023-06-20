265bcb02751a8eb992f3e0dc7d81b158eb850a62
achabense <60953653+achabense@users.noreply.github.com>
2023-06-20 20:39:20 +0800

    Add test.cpp

---
+++ tests/std/tests/GH_003003_format_fix_decimal_point_and_trailing_zero/test.cpp
@@ -0,0 +1,26 @@
+// Copyright (c) Microsoft Corporation.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+#include <cassert>
+#include <format>
+
+int main() {
+    assert(std::format("{:#.0}", 1200.0) == "1.e+03");
+    assert(std::format("{:#.1}", 1200.0) == "1.e+03");
+    assert(std::format("{:#.2}", 1200.0) == "1.2e+03");
+    assert(std::format("{:#.3}", 1200.0) == "1.2e+03");
+    assert(std::format("{:#.4}", 1200.0) == "1200.");
+    assert(std::format("{:#.5}", 1200.0) == "1200.");
+    assert(std::format("{:#.6}", 1200.0) == "1200.");
+
+    assert(std::format("{:#.0}", 0.123) == "0.1");
+    assert(std::format("{:#.1}", 0.123) == "0.1");
+    assert(std::format("{:#.2}", 0.123) == "0.12");
+    assert(std::format("{:#.3}", 0.123) == "0.123");
+    assert(std::format("{:#.4}", 0.123) == "0.123");
+    assert(std::format("{:#.5}", 0.123) == "0.123");
+
+    assert(std::format("{:#.0}", 0.0) == "0.");
+    assert(std::format("{:#.1}", 0.0) == "0.");
+    assert(std::format("{:#.2}", 0.0) == "0.");
+}
