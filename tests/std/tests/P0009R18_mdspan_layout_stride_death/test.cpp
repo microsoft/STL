6cd745ba9b159bc2c2a4d5d6920447dd2ff74602
Jakub Mazurkiewicz <mazkuba3@gmail.com>
2023-06-21 12:44:09 +0200

    `layout_stride`: Guard checks with `#if _CONTAINER_DEBUG_LEVEL > 0`

--- tests/std/tests/P0009R18_mdspan_layout_stride_death/test.cpp
+++ tests/std/tests/P0009R18_mdspan_layout_stride_death/test.cpp
@@ -1,6 +1,8 @@
 // Copyright (c) Microsoft Corporation.
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
+#define _CONTAINER_DEBUG_LEVEL 1
+
 #include <array>
 #include <cstddef>
 #include <mdspan>
