63e652b31aec19f0742b8a2aae854f31a584d659
Jakub Mazurkiewicz <mazkuba3@gmail.com>
2023-06-21 15:10:32 +0200

    `layout_stride`: implement default constructor's preconditions

--- tests/std/tests/P0009R18_mdspan_layout_stride_death/test.cpp
+++ tests/std/tests/P0009R18_mdspan_layout_stride_death/test.cpp
@@ -12,6 +12,13 @@
 
 using namespace std;
 
+void test_default_construction() {
+    using Ext = extents<signed char, dynamic_extent, 4, 5, 7>;
+    // Value of layout_right::mapping<extents_type>().required_span_size() must be
+    // representable as a value of type index_type
+    [[maybe_unused]] layout_stride::mapping<Ext> m{}; // NB: strides are [140, 35, 7, 1]
+}
+
 void test_construction_from_extents_and_array() {
     // Value of s[i] must be greater than 0 for all i in the range [0, rank_)
     [[maybe_unused]] layout_stride::mapping<dextents<int, 1>> m1{extents<int, 1>{}, array<int, 1>{-1}};
@@ -38,7 +45,7 @@ void test_call_operator() {
 int main(int argc, char* argv[]) {
     std_testing::death_test_executive exec;
     exec.add_death_tests({
-        // TRANSITION more tests
+        test_default_construction,
         test_construction_from_extents_and_array,
         test_construction_from_extents_and_span,
         test_construction_from_strided_layout_mapping,
