360d1f7fd09c494b9e2da4845097625aa4c18786
Jakub Mazurkiewicz <mazkuba3@gmail.com>
2023-06-22 17:35:38 +0200

    `mdspan`: Implement precondition check in `operator[]`

--- tests/std/tests/P0009R18_mdspan_mdspan_death/test.cpp
+++ tests/std/tests/P0009R18_mdspan_mdspan_death/test.cpp
@@ -19,10 +19,28 @@ void test_construction_from_other_mdspan() {
     [[maybe_unused]] mdspan<const int, extents<int, 8, 8, 2>> mds2{mds1};
 }
 
+#ifdef __cpp_multidimensional_subscript // TRANSITION, P2128R6
+void test_access_with_invalid_multidimensional_index_1() {
+    auto mds = mdspan{some_ints.data(), 4, 4};
+    // I must be a multidimensional index in extents()
+    (void) mds[3, 4];
+}
+#endif // __cpp_multidimensional_subscript
+
+void test_access_with_invalid_multidimensional_index_2() {
+    auto mds = mdspan{some_ints.data(), 5, 5};
+    // I must be a multidimensional index in extents()
+    (void) mds[array{4, 5}];
+}
+
 int main(int argc, char* argv[]) {
     std_testing::death_test_executive exec;
     exec.add_death_tests({
         test_construction_from_other_mdspan,
+#ifdef __cpp_multidimensional_subscript // TRANSITION, P2128R6
+        test_access_with_invalid_multidimensional_index_1,
+#endif // __cpp_multidimensional_subscript
+        test_access_with_invalid_multidimensional_index_2,
     });
     return exec.run(argc, argv);
 }
