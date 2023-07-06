364735244e8d23bf6567c708b935482a32b41804
achabense <60953653+achabense@users.noreply.github.com>
2023-07-06 21:17:24 +0800

    refactor benchmark

--- benchmarks/src/bitset_to_string.cpp
+++ benchmarks/src/bitset_to_string.cpp
@@ -3,80 +3,46 @@
 
 #include <array>
 #include <benchmark/benchmark.h>
+#include <bit>
 #include <bitset>
 #include <random>
 
 using namespace std;
 
 namespace {
-    auto random_bits = [] {
-        mt19937_64 rnd(12345);
-        array<uint64_t, 100> data;
+    const auto random_bits = [] {
+        mt19937_64 rnd{};
+        array<uint64_t, 32> data;
         for (auto& d : data) {
             d = rnd();
         }
         return data;
     }();
 
-    auto large_bitset = [] {
-        constexpr size_t len = 2000;
-        bitset<len> bs;
-        mt19937_64 rnd(12345);
-        for (int i = 0; i < len; i++) {
-            bs[i] = rnd() & 1;
+    template <size_t N, class charT>
+    void BM_bitset_to_string(benchmark::State& state) {
+        for (auto _ : state) {
+            for (auto bits : random_bits) {
+                bitset<N> bs{bits};
+                benchmark::DoNotOptimize(bs.to_string<charT>());
+            }
         }
-        return bs;
-    }();
+    }
 
-    void BM_to_string_small(benchmark::State& state) {
+    template <class charT>
+    void BM_bitset_to_string_large_single(benchmark::State& state) {
+        const auto large_bitset = bit_cast<bitset<CHAR_BIT * sizeof random_bits>>(random_bits);
         for (auto _ : state) {
-            for (auto bits : random_bits) {
-                std::bitset<15> bs{bits};
-                benchmark::DoNotOptimize(bs.to_string());
-            }
-        }
-    }
-    void BM_to_string_medium(benchmark::State& state) {
-        for (auto _ : state) {
-            for (auto bits : random_bits) {
-                std::bitset<64> bs{bits};
-                benchmark::DoNotOptimize(bs.to_string());
-            }
-        }
-    }
-    void BM_to_string_large(benchmark::State& state) {
-        for (auto _ : state) {
-            benchmark::DoNotOptimize(large_bitset.to_string());
-        }
-    }
-    void BM_to_wstring_small(benchmark::State& state) {
-        for (auto _ : state) {
-            for (auto bits : random_bits) {
-                std::bitset<7> bs{bits};
-                benchmark::DoNotOptimize(bs.to_string<wchar_t>());
-            }
-        }
-    }
-    void BM_to_wstring_medium(benchmark::State& state) {
-        for (auto _ : state) {
-            for (auto bits : random_bits) {
-                std::bitset<64> bs{bits};
-                benchmark::DoNotOptimize(bs.to_string<wchar_t>());
-            }
-        }
-    }
-    void BM_to_wstring_large(benchmark::State& state) {
-        for (auto _ : state) {
-            benchmark::DoNotOptimize(large_bitset.to_string<wchar_t>());
+            benchmark::DoNotOptimize(large_bitset.to_string<charT>());
         }
     }
 } // namespace
 
-BENCHMARK(BM_to_string_small);
-BENCHMARK(BM_to_string_medium);
-BENCHMARK(BM_to_string_large);
-BENCHMARK(BM_to_wstring_small);
-BENCHMARK(BM_to_wstring_medium);
-BENCHMARK(BM_to_wstring_large);
+BENCHMARK(BM_bitset_to_string<15, char>);
+BENCHMARK(BM_bitset_to_string<64, char>);
+BENCHMARK(BM_bitset_to_string_large_single<char>);
+BENCHMARK(BM_bitset_to_string<7, wchar_t>);
+BENCHMARK(BM_bitset_to_string<64, wchar_t>);
+BENCHMARK(BM_bitset_to_string_large_single<wchar_t>);
 
 BENCHMARK_MAIN();
