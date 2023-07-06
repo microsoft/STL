37a03937112ce373ce2fa4cb08ea03082f6dd2ee
achabense <60953653+achabense@users.noreply.github.com>
2023-07-06 15:53:28 +0800

    refactor benchmark

--- benchmarks/src/path_lexically_normal.cpp
+++ benchmarks/src/path_lexically_normal.cpp
@@ -3,49 +3,24 @@
 
 #include <benchmark/benchmark.h>
 #include <filesystem>
-
-using namespace std;
-namespace fs = std::filesystem;
+#include <xstring>
 
 namespace {
-    void BM_lexically_normal_0(benchmark::State& state) {
-        const fs::path& p(LR"(X:DriveRelative)"sv);
-        for (auto _ : state) {
-            benchmark::DoNotOptimize(p.lexically_normal());
-        }
-    }
-    void BM_lexically_normal_1(benchmark::State& state) {
-        const fs::path& p(LR"(\\server\\\share)"sv);
-        for (auto _ : state) {
-            benchmark::DoNotOptimize(p.lexically_normal());
-        }
-    }
-    void BM_lexically_normal_2(benchmark::State& state) {
-        const fs::path& p(LR"(STL/.github/workflows/../..)"sv);
-        for (auto _ : state) {
-            benchmark::DoNotOptimize(p.lexically_normal());
-        }
-    }
-    void BM_lexically_normal_3(benchmark::State& state) {
-        const fs::path& p(
-            LR"(C:\Program Files\Azure Data Studio\resources\app\extensions\bat\snippets\batchfile.code-snippets)"sv);
-        for (auto _ : state) {
-            benchmark::DoNotOptimize(p.lexically_normal());
-        }
-    }
-    void BM_lexically_normal_4(benchmark::State& state) {
-        const fs::path& p(
-            LR"(/\server/\share/\a/\b/\c/\./\./\d/\../\../\../\../\../\../\../\other/x/y/z/.././..\meow.txt)"sv);
+    void BM_lexically_normal(benchmark::State& state) {
+        using namespace std::literals;
+        static constexpr std::wstring_view args[5]{LR"(X:DriveRelative)"sv, LR"(\\server\\\share)"sv,
+            LR"(STL/.github/workflows/../..)"sv,
+            LR"(C:\Program Files\Azure Data Studio\resources\app\extensions\bat\snippets\batchfile.code-snippets)"sv,
+            LR"(/\server/\share/\a/\b/\c/\./\./\d/\../\../\../\../\../\../\../\other/x/y/z/.././..\meow.txt)"sv};
+
+        const auto index = state.range(0);
+        const std::filesystem::path p(args[index]);
         for (auto _ : state) {
             benchmark::DoNotOptimize(p.lexically_normal());
         }
     }
 } // namespace
 
-BENCHMARK(BM_lexically_normal_0);
-BENCHMARK(BM_lexically_normal_1);
-BENCHMARK(BM_lexically_normal_2);
-BENCHMARK(BM_lexically_normal_3);
-BENCHMARK(BM_lexically_normal_4);
+BENCHMARK(BM_lexically_normal)->Arg(0)->Arg(1)->Arg(2)->Arg(3)->Arg(4);
 
 BENCHMARK_MAIN();
