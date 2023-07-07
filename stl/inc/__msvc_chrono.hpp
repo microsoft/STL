d9d5de131a3644efee6e806206417d6af27cb5ea
Stephan T. Lavavej <stl@nuwen.net>
2023-07-07 01:57:35 -0700

    Restore emulation comment.

--- stl/inc/__msvc_chrono.hpp
+++ stl/inc/__msvc_chrono.hpp
@@ -693,7 +693,7 @@ namespace chrono {
                 constexpr long long _Multiplier = period::den / _TenMHz;
                 return time_point(duration(_Ctr * _Multiplier));
             } else if (_Freq == _TwentyFourMHz) _LIKELY_ARM_ARM64 {
-                // 24 MHz is a common frequency on ARM/ARM64.
+                // 24 MHz is a common frequency on ARM/ARM64, including cases where it emulates x86/x64.
                 const long long _Whole = (_Ctr / _TwentyFourMHz) * period::den;
                 const long long _Part  = (_Ctr % _TwentyFourMHz) * period::den / _TwentyFourMHz;
                 return time_point(duration(_Whole + _Part));
