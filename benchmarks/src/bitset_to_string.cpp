b75f4c307472524197aa4847f90b20c0a4059499
Stephan T. Lavavej <stl@nuwen.net>
2023-07-10 20:49:35 -0700

    Code review feedback.

--- benchmarks/src/bitset_to_string.cpp
+++ benchmarks/src/bitset_to_string.cpp
@@ -5,6 +5,9 @@
 #include <benchmark/benchmark.h>
 #include <bit>
 #include <bitset>
+#include <climits>
+#include <cstddef>
+#include <cstdint>
 #include <random>
 
 using namespace std;
@@ -12,17 +15,17 @@ using namespace std;
 namespace {
     const auto random_bits = [] {
         mt19937_64 rnd{};
-        array<uint64_t, 32> data;
-        for (auto& d : data) {
+        array<uint64_t, 32> arr;
+        for (auto& d : arr) {
             d = rnd();
         }
-        return data;
+        return arr;
     }();
 
     template <size_t N, class charT>
     void BM_bitset_to_string(benchmark::State& state) {
         for (auto _ : state) {
-            for (auto bits : random_bits) {
+            for (const auto& bits : random_bits) {
                 bitset<N> bs{bits};
                 benchmark::DoNotOptimize(bs.to_string<charT>());
             }
@@ -31,7 +34,7 @@ namespace {
 
     template <class charT>
     void BM_bitset_to_string_large_single(benchmark::State& state) {
-        const auto large_bitset = bit_cast<bitset<CHAR_BIT * sizeof random_bits>>(random_bits);
+        const auto large_bitset = bit_cast<bitset<CHAR_BIT * sizeof(random_bits)>>(random_bits);
         for (auto _ : state) {
             benchmark::DoNotOptimize(large_bitset.to_string<charT>());
         }
