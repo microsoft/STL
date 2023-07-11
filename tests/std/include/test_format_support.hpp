4e1e84ca769ee7123427f8127b507a048ec2a6f4
Jakub Mazurkiewicz <mazkuba3@gmail.com>
2023-07-11 13:02:20 +0200

    Testing: implement `choose_literal` for character types

--- tests/std/include/test_format_support.hpp
+++ tests/std/include/test_format_support.hpp
@@ -18,12 +18,20 @@ struct choose_literal<char> {
     static constexpr const char* choose(const char* s, const wchar_t*) {
         return s;
     }
+
+    static constexpr char choose(char c, wchar_t) {
+        return c;
+    }
 };
 
 template <>
 struct choose_literal<wchar_t> {
     static constexpr const wchar_t* choose(const char*, const wchar_t* s) {
         return s;
+    }
+
+    static constexpr wchar_t choose(char, wchar_t c) {
+        return c;
     }
 };
 
