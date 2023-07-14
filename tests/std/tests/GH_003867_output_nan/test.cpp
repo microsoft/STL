e8ba27936279eb492c48541f7138fa46eeb3bf0b
Stephan T. Lavavej <stl@nuwen.net>
2023-07-14 12:45:13 -0700

    Add `TEST_CUSTOM_FACET` guard.

--- tests/std/tests/GH_003867_output_nan/test.cpp
+++ tests/std/tests/GH_003867_output_nan/test.cpp
@@ -9,12 +9,30 @@
 
 using namespace std;
 
+// Because std::string crosses the DLL boundary via overridden virtual functions,
+// we can test custom facets only when:
+// * linking statically, or
+// * linking dynamically with IDL set to its default value (so the user code and the DLL match).
+#ifdef _DEBUG
+#define DEFAULT_IDL 2
+#else
+#define DEFAULT_IDL 0
+#endif
+
+#if !defined(_DLL) || _ITERATOR_DEBUG_LEVEL == DEFAULT_IDL
+#define TEST_CUSTOM_FACET 1
+#else
+#define TEST_CUSTOM_FACET 0
+#endif
+
+#if TEST_CUSTOM_FACET
 struct groups_of_1 : numpunct<char> {
     // The standard char specialization of std::numpunct::do_thousands_sep returns ','
     string do_grouping() const override {
         return "\1";
     } // groups of 1 digit
 };
+#endif // TEST_CUSTOM_FACET
 
 template <typename T>
 void test_gh_3867() {
@@ -25,12 +43,15 @@ void test_gh_3867() {
         s << -numeric_limits<T>::quiet_NaN();
         assert(s.str() == "-nan(ind)");
     }
+
+#if TEST_CUSTOM_FACET
     {
         ostringstream s;
         s.imbue(locale(s.getloc(), new groups_of_1));
         s << -numeric_limits<T>::infinity();
         assert(s.str() == "-inf");
     }
+#endif // TEST_CUSTOM_FACET
 }
 
 int main() {
