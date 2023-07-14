7124e06e4bc93821c1df3f938b7a606e5a4581b0
Igor Zhukov <fsb4000@yandex.ru>
2023-07-14 21:45:52 +0700

    Fix iostreams with imbued locales to print INFs correctly

--- tests/std/tests/GH_003867_output_nan/test.cpp
+++ tests/std/tests/GH_003867_output_nan/test.cpp
@@ -5,16 +5,32 @@
 #include <limits>
 #include <locale>
 #include <sstream>
+#include <string>
 
 using namespace std;
+
+struct groups_of_1 : numpunct<char> {
+    // The standard char specialization of std::numpunct::do_thousands_sep returns ','
+    string do_grouping() const {
+        return "\1";
+    } // groups of 1 digit
+};
 
 template <typename T>
 void test_gh_3867() {
     // GH-3867 Writing NaN to the output stream with a set locale results in a weird output
-    ostringstream s;
-    s.imbue(locale("en-US"));
-    s << -numeric_limits<T>::quiet_NaN();
-    assert(s.str() == "-nan(ind)");
+    {
+        ostringstream s;
+        s.imbue(locale("en-US"));
+        s << -numeric_limits<T>::quiet_NaN();
+        assert(s.str() == "-nan(ind)");
+    }
+    {
+        ostringstream s;
+        s.imbue(locale(s.getloc(), new groups_of_1));
+        s << -numeric_limits<T>::infinity();
+        assert(s.str() == "-inf");
+    }
 }
 
 int main() {
