08f1279916071df56ae6fa70734a958af33c4227
Stephan T. Lavavej <stl@nuwen.net>
2023-07-17 18:27:49 -0700

    Fuse into GH_002030_asan_annotate_string, assert result.

--- tests/std/tests/GH_003883_replace_asan_failure/test.cpp
+++
@@ -1,9 +0,0 @@
-// Copyright (c) Microsoft Corporation.
-// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
-
-#include <string>
-
-int main() {
-    std::string t = "0123456789ABCDEF"; // large string
-    t.replace(0, 30, 7, 'A');
-}
