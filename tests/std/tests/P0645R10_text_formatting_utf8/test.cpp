5ac83464bd54656c2eca9c3b5148a52ba5a55b88
achabense <60953653+achabense@users.noreply.github.com>
2023-07-27 11:39:17 +0800

    review feedback; add comments for `_Unicode_width_estimate`; add test cases: all 2->1 cases; and some 1->2 cases

--- tests/std/tests/P0645R10_text_formatting_utf8/test.cpp
+++ tests/std/tests/P0645R10_text_formatting_utf8/test.cpp
@@ -69,7 +69,7 @@ void test_width_estimation() {
         {TYPED_LITERAL(CharT, "\x58"), 1},
         {TYPED_LITERAL(CharT, "x\x58"), 2},
 
-        // test the boundaries of the intervals defined in n4885 [format.string.std]/11
+        // Test the boundaries of the intervals defined in n4885 [format.string.std]/11
         {TYPED_LITERAL(CharT, "\u10ff\x58"), 2},
         {TYPED_LITERAL(CharT, "\u1100\x58"), 3},
         {TYPED_LITERAL(CharT, "\u115f\x58"), 3},
@@ -82,8 +82,6 @@ void test_width_estimation() {
         {TYPED_LITERAL(CharT, "\u2e80\x58"), 3},
         {TYPED_LITERAL(CharT, "\u303e\x58"), 3},
         {TYPED_LITERAL(CharT, "\u303f\x58"), 2},
-        {TYPED_LITERAL(CharT, "\u3040\x58"), 2},
-        {TYPED_LITERAL(CharT, "\ua4cf\x58"), 2},
         {TYPED_LITERAL(CharT, "\ua4d0\x58"), 2},
         {TYPED_LITERAL(CharT, "\uabff\x58"), 2},
         {TYPED_LITERAL(CharT, "\uac00\x58"), 3},
@@ -91,7 +89,7 @@ void test_width_estimation() {
         {TYPED_LITERAL(CharT, "\ud7a4\x58"), 2},
         {TYPED_LITERAL(CharT, "\ud7ff\x58"), 2},
 
-        // skip over the surrogate pair range (\ud800-\udfff)
+        // Skip over the surrogate pair range (\ud800-\udfff)
 
         {TYPED_LITERAL(CharT, "\ue000\x58"), 2},
         {TYPED_LITERAL(CharT, "\uf8ff\x58"), 2},
@@ -104,10 +102,8 @@ void test_width_estimation() {
         {TYPED_LITERAL(CharT, "\ufe1a\x58"), 2},
         {TYPED_LITERAL(CharT, "\ufe2f\x58"), 2},
         {TYPED_LITERAL(CharT, "\ufe30\x58"), 3},
-        {TYPED_LITERAL(CharT, "\ufe6f\x58"), 2},
         {TYPED_LITERAL(CharT, "\ufe70\x58"), 2},
         {TYPED_LITERAL(CharT, "\ufeff\x58"), 2},
-        {TYPED_LITERAL(CharT, "\uff00\x58"), 2},
         {TYPED_LITERAL(CharT, "\uff60\x58"), 3},
         {TYPED_LITERAL(CharT, "\uff61\x58"), 2},
         {TYPED_LITERAL(CharT, "\uffdf\x58"), 2},
@@ -143,8 +139,58 @@ void test_width_estimation() {
         {TYPED_LITERAL(CharT, "\ufe40\u2000\ufe40\x58"), 6},
         {TYPED_LITERAL(CharT, "\ufe40\ufe40\u2000\x58"), 6},
         {TYPED_LITERAL(CharT, "\ufe40\ufe40\ufe40\x58"), 7},
+
         {TYPED_LITERAL(CharT, "\u0061\u200D\x58"), 2},
         {TYPED_LITERAL(CharT, "\u0061\U0001F1E6\U0001F1E7\u200D\U0001F1E8\u0062\x58"), 5},
+
+        // Test some codepoints affected by P2675R1
+        // Used to be 2, now 1:
+        {TYPED_LITERAL(CharT, "\u2e9a\x58"), 2},
+        {TYPED_LITERAL(CharT, "\u2ef4\u2eff\x58"), 3},
+        {TYPED_LITERAL(CharT, "\u2fd6\u2fef\x58"), 3},
+        {TYPED_LITERAL(CharT, "\u2ffc\u2fff\x58"), 3},
+        {TYPED_LITERAL(CharT, "\u3040\x58"), 2},
+        {TYPED_LITERAL(CharT, "\u3097\u3098\x58"), 3},
+        {TYPED_LITERAL(CharT, "\u3100\u3104\x58"), 3},
+        {TYPED_LITERAL(CharT, "\u3130\x58"), 2},
+        {TYPED_LITERAL(CharT, "\u318f\x58"), 2},
+        {TYPED_LITERAL(CharT, "\u31e4\u31ef\x58"), 3},
+        {TYPED_LITERAL(CharT, "\u321f\x58"), 2},
+        {TYPED_LITERAL(CharT, "\u3248\u324f\x58"), 3},
+        {TYPED_LITERAL(CharT, "\ua48d\ua48f\x58"), 3},
+        {TYPED_LITERAL(CharT, "\ua4c7\ua4cf\x58"), 3},
+        {TYPED_LITERAL(CharT, "\ufe53\x58"), 2},
+        {TYPED_LITERAL(CharT, "\ufe67\x58"), 2},
+        {TYPED_LITERAL(CharT, "\ufe6c\ufe6f\x58"), 3},
+        {TYPED_LITERAL(CharT, "\uff00\x58"), 2},
+        // Used to be 1, now 2:
+        {TYPED_LITERAL(CharT, "\u23f0\x58"), 3},
+        {TYPED_LITERAL(CharT, "\u23f3\x58"), 3},
+        {TYPED_LITERAL(CharT, "\u267f\x58"), 3},
+        {TYPED_LITERAL(CharT, "\u2693\x58"), 3},
+        {TYPED_LITERAL(CharT, "\u26a1\x58"), 3},
+        {TYPED_LITERAL(CharT, "\u26ce\x58"), 3},
+        {TYPED_LITERAL(CharT, "\u26d4\x58"), 3},
+        {TYPED_LITERAL(CharT, "\u26ea\x58"), 3},
+        {TYPED_LITERAL(CharT, "\u26f5\x58"), 3},
+        {TYPED_LITERAL(CharT, "\u26fa\x58"), 3},
+        {TYPED_LITERAL(CharT, "\u26fd\x58"), 3},
+        {TYPED_LITERAL(CharT, "\u2705\x58"), 3},
+        {TYPED_LITERAL(CharT, "\u2728\x58"), 3},
+        {TYPED_LITERAL(CharT, "\u274c\x58"), 3},
+        {TYPED_LITERAL(CharT, "\u274e\x58"), 3},
+        {TYPED_LITERAL(CharT, "\u2757\x58"), 3},
+        {TYPED_LITERAL(CharT, "\u27b0\x58"), 3},
+        {TYPED_LITERAL(CharT, "\u27bf\x58"), 3},
+        {TYPED_LITERAL(CharT, "\u2b50\x58"), 3},
+        {TYPED_LITERAL(CharT, "\u2b55\x58"), 3},
+        {TYPED_LITERAL(CharT, "\U0001b132\x58"), 3},
+        {TYPED_LITERAL(CharT, "\U0001b155\x58"), 3},
+        {TYPED_LITERAL(CharT, "\U0001f004\x58"), 3},
+        {TYPED_LITERAL(CharT, "\U0001f0cf\x58"), 3},
+        {TYPED_LITERAL(CharT, "\U0001f18e\x58"), 3},
+        {TYPED_LITERAL(CharT, "\U0001f6cc\x58"), 3},
+        {TYPED_LITERAL(CharT, "\U0001f7f0\x58"), 3},
     };
 
     for (const auto& test : test_cases) {
