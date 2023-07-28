b3fd0b096b34b621500ac309fa697bb36637cc87
Stephan T. Lavavej <stl@nuwen.net>
2023-07-28 12:27:47 -0700

    Invert, always invert.

--- stl/src/ppltasks.cpp
+++ stl/src/ppltasks.cpp
@@ -37,14 +37,12 @@ namespace Concurrency {
     namespace details {
         [[noreturn]] _CRTIMP2 void __cdecl _ReportUnobservedException() {
 #if (defined(_M_IX86) || defined(_M_X64)) && !defined(_CRT_APP) && _STL_WIN32_WINNT < _WIN32_WINNT_WIN8
-            if (IsProcessorFeaturePresent(PF_FASTFAIL_AVAILABLE)) {
-                __fastfail(FAST_FAIL_INVALID_ARG);
+            if (!IsProcessorFeaturePresent(PF_FASTFAIL_AVAILABLE)) {
+                std::abort();
             }
+#endif // ^^^ __fastfail conditionally available ^^^
 
-            std::abort();
-#else // ^^^ __fastfail conditionally available / __fastfail always available vvv
             __fastfail(FAST_FAIL_INVALID_ARG);
-#endif // ^^^ __fastfail always available ^^^
         }
 
         namespace platform {
