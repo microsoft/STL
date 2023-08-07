58fc6b91faa1eb4eff80b6b954bbf537ac26342b
Alex Guteniev <gutenev@gmail.com>
2023-08-07 17:03:32 +0300

    simplify benchmark

--- benchmarks/src/minmax_element.cpp
+++ benchmarks/src/minmax_element.cpp
@@ -7,6 +7,7 @@
 #include <cstdint>
 #include <random>
 #include <ranges>
+#include <type_traits>
 
 enum class Op {
     Min,
@@ -17,31 +18,20 @@ enum class Op {
 using namespace std;
 
 template <class T, size_t Size, Op Operation>
-void bm(benchmark::State& state) {
-    mt19937 gen(84710);
-    uniform_int_distribution<conditional_t<sizeof(T) != 1, T, int>> dis(1, 20);
-
-    T a[Size];
-    ranges::generate(a, [&] { return dis(gen); });
-
-    for (auto _ : state) {
-        if constexpr (Operation == Op::Min) {
-            benchmark::DoNotOptimize(ranges::min_element(a));
-        } else if constexpr (Operation == Op::Max) {
-            benchmark::DoNotOptimize(ranges::max_element(a));
-        } else if constexpr (Operation == Op::Both) {
-            benchmark::DoNotOptimize(ranges::minmax_element(a));
-        }
-    }
-}
 
 template <class T, size_t Size, Op Operation>
-void bmf(benchmark::State& state) {
-    mt19937 gen(84710);
-    normal_distribution<T> dis(0, 10000.0);
-
+void bm(benchmark::State& state) {
     T a[Size];
-    generate(begin(a), end(a), [&] { return dis(gen); });
+
+    mt19937 gen(84710);
+
+    if constexpr (is_floating_point_v<T>) {
+        normal_distribution<T> dis(0, 10000.0);
+        ranges::generate(a, [&] { return dis(gen); });
+    } else {
+        uniform_int_distribution<conditional_t<sizeof(T) != 1, T, int>> dis(1, 20);
+        ranges::generate(a, [&] { return dis(gen); });
+    }
 
     for (auto _ : state) {
         if constexpr (Operation == Op::Min) {
@@ -86,13 +76,13 @@ BENCHMARK(bm<int64_t, 8021, Op::Min>);
 BENCHMARK(bm<int64_t, 8021, Op::Max>);
 BENCHMARK(bm<int64_t, 8021, Op::Both>);
 
-BENCHMARK(bmf<float, 8021, Op::Min>);
-BENCHMARK(bmf<float, 8021, Op::Max>);
-BENCHMARK(bmf<float, 8021, Op::Both>);
+BENCHMARK(bm<float, 8021, Op::Min>);
+BENCHMARK(bm<float, 8021, Op::Max>);
+BENCHMARK(bm<float, 8021, Op::Both>);
 
-BENCHMARK(bmf<double, 8021, Op::Min>);
-BENCHMARK(bmf<double, 8021, Op::Max>);
-BENCHMARK(bmf<double, 8021, Op::Both>);
+BENCHMARK(bm<double, 8021, Op::Min>);
+BENCHMARK(bm<double, 8021, Op::Max>);
+BENCHMARK(bm<double, 8021, Op::Both>);
 
 
 BENCHMARK_MAIN();
