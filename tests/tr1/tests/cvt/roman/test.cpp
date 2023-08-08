e49ae2c49d22f2189773b6759a89186120f1f669
Stephan T. Lavavej <stl@nuwen.net>
2023-08-07 18:36:46 -0700

    Centralize suppression in `tests/tr1/tests/cvt/env.lst`.

--- tests/tr1/tests/cvt/roman/test.cpp
+++ tests/tr1/tests/cvt/roman/test.cpp
@@ -1,8 +1,6 @@
 // Copyright (c) Microsoft Corporation.
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
-#define _SILENCE_STDEXT_CVT_DEPRECATION_WARNING
-
 #include "tdefs.h"
 #include <cvt/roman>
 
