76b7d3813f81f2ba97404f1044dff62692669f7c
Stephan T. Lavavej <stl@nuwen.net>
2023-09-08 18:56:46 -0700

    `rd` is unused.

--- benchmarks/src/std/containers/sequences/vector.bool/move/test.cpp
+++ benchmarks/src/std/containers/sequences/vector.bool/move/test.cpp
@@ -10,7 +10,6 @@
 using namespace std;
 
 static vector<bool> createRandomVector(const size_t size) {
-    static random_device rd;
     static mt19937 gen{random_device{}()};
     vector<bool> result(size);
     generate_n(result.begin(), size, [] { return bernoulli_distribution{0.5}(gen); });
