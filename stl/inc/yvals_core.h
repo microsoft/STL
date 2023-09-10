45aee0c1ec6241217e7032a4f2f25231032fa77e
Igor Zhukov <fsb4000@yandex.ru>
2023-09-10 15:02:25 +0700

    use only `static_assert` for `_EMIT_STL_ERROR`

--- stl/inc/yvals_core.h
+++ stl/inc/yvals_core.h
@@ -507,11 +507,9 @@
 #define _EMIT_STL_MESSAGE(MESSAGE)   _STL_PRAGMA_MESSAGE(__FILE__ "(" _CRT_STRINGIZE(__LINE__) "): " MESSAGE)
 
 #define _EMIT_STL_WARNING(NUMBER, MESSAGE)             \
-    _EMIT_STL_MESSAGE("warning " #NUMBER ": " MESSAGE) \
-    static_assert(true, "")
+    _EMIT_STL_MESSAGE("warning " #NUMBER ": " MESSAGE)
 #define _EMIT_STL_ERROR(NUMBER, MESSAGE)             \
-    _EMIT_STL_MESSAGE("error " #NUMBER ": " MESSAGE) \
-    static_assert(false, "Error in C++ Standard Library usage.")
+    static_assert(false, "error " #NUMBER ": " MESSAGE)
 
 #ifndef _STL_WARNING_LEVEL
 #if defined(_MSVC_WARNING_LEVEL) && _MSVC_WARNING_LEVEL >= 4
