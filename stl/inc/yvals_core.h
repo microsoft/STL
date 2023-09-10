62cd5675ae7333d5e73d1388a46bf7f20f5fbcf1
Igor Zhukov <fsb4000@yandex.ru>
2023-09-10 15:12:54 +0700

    clang-format

--- stl/inc/yvals_core.h
+++ stl/inc/yvals_core.h
@@ -506,10 +506,8 @@
 #define _STL_PRAGMA_MESSAGE(MESSAGE) _STL_PRAGMA(message(MESSAGE))
 #define _EMIT_STL_MESSAGE(MESSAGE)   _STL_PRAGMA_MESSAGE(__FILE__ "(" _CRT_STRINGIZE(__LINE__) "): " MESSAGE)
 
-#define _EMIT_STL_WARNING(NUMBER, MESSAGE)             \
-    _EMIT_STL_MESSAGE("warning " #NUMBER ": " MESSAGE)
-#define _EMIT_STL_ERROR(NUMBER, MESSAGE)             \
-    static_assert(false, "error " #NUMBER ": " MESSAGE)
+#define _EMIT_STL_WARNING(NUMBER, MESSAGE) _EMIT_STL_MESSAGE("warning " #NUMBER ": " MESSAGE)
+#define _EMIT_STL_ERROR(NUMBER, MESSAGE)   static_assert(false, "error " #NUMBER ": " MESSAGE)
 
 #ifndef _STL_WARNING_LEVEL
 #if defined(_MSVC_WARNING_LEVEL) && _MSVC_WARNING_LEVEL >= 4
