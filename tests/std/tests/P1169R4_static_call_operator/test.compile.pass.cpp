be86c8693b28f23e0c34e2910bde069004e74817
Stephan T. Lavavej <stl@nuwen.net>
2023-09-25 22:25:50 -0700

    Clang backported this feature unconditionally.

--- tests/std/tests/P1169R4_static_call_operator/test.compile.pass.cpp
+++ tests/std/tests/P1169R4_static_call_operator/test.compile.pass.cpp
@@ -1,7 +1,7 @@
 // Copyright (c) Microsoft Corporation.
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
-#ifdef __cpp_static_call_operator // TRANSITION, P1169R4
+#ifdef __cpp_static_call_operator
 
 #include <functional>
 #include <future>
