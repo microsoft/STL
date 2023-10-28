e9ff55626ba962d2571358b8c03b0f2242f41416
A. Jiang <de34@live.cn>
2023-10-29 01:00:06 +0800

    Implement `formatter<vector<bool>::reference>`

--- tests/std/tests/P2286R8_text_formatting_formattable/test.compile.pass.cpp
+++ tests/std/tests/P2286R8_text_formatting_formattable/test.compile.pass.cpp
@@ -16,6 +16,7 @@
 #include <array>
 #include <chrono>
 #include <concepts>
+#include <cstddef>
 #include <deque>
 #include <format>
 #include <forward_list>
@@ -51,6 +52,23 @@ concept encoded_character_type = same_as<CharT, char>
                               || same_as<CharT, char8_t>
 #endif // defined(__cpp_char8_t)
                               || same_as<CharT, char16_t> || same_as<CharT, char32_t> || same_as<CharT, wchar_t>;
+
+template <class T>
+struct alternative_allocator {
+    using value_type = T;
+
+    alternative_allocator() = default;
+    template <class U>
+    constexpr alternative_allocator(const alternative_allocator<U>&) noexcept {}
+
+    T* allocate(size_t n) {
+        return allocator<T>{}.allocate(n);
+    }
+
+    void deallocate(T* p, size_t n) {
+        allocator<T>{}.deallocate(p, n);
+    }
+};
 
 template <class T, class CharT>
 void assert_is_not_formattable() {
@@ -181,6 +199,22 @@ void test_P2693() {
         assert_is_not_formattable<stacktrace_entry, CharT>();
         assert_is_not_formattable<stacktrace, CharT>();
     }
+}
+
+template <class CharT, class Vector>
+void test_P2286_vector_bool() {
+    assert_is_formattable<typename Vector::reference, CharT>();
+
+    // The const_reference shall be bool.
+    assert_is_formattable<typename Vector::const_reference, CharT>();
+}
+
+// Tests for P2286 Formatting ranges
+template <class CharT>
+void test_P2286() {
+    test_P2286_vector_bool<CharT, vector<bool>>();
+    test_P2286_vector_bool<CharT, pmr::vector<bool>>();
+    test_P2286_vector_bool<CharT, vector<bool, alternative_allocator<bool>>>();
 }
 
 // Tests volatile qualified objects are no longer formattable.
---
+++ tests/std/tests/P2286R8_text_formatting_vector_bool_reference/test.cpp
@@ -0,0 +1,81 @@
+// Copyright (c) Microsoft Corporation.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+#include <cassert>
+#include <cstddef>
+#include <format>
+#include <locale>
+#include <memory>
+#include <string>
+#include <string_view>
+#include <type_traits>
+#include <vector>
+
+using namespace std;
+
+template <typename CharT>
+[[nodiscard]] constexpr const CharT* choose_literal(const char* const str, const wchar_t* const wstr) noexcept {
+    if constexpr (is_same_v<CharT, char>) {
+        return str;
+    } else {
+        return wstr;
+    }
+}
+
+#define STR(Literal) (choose_literal<CharT>(Literal, L##Literal))
+
+template <class T>
+struct alternative_allocator {
+    using value_type = T;
+
+    alternative_allocator() = default;
+    template <class U>
+    constexpr alternative_allocator(const alternative_allocator<U>&) noexcept {}
+
+    T* allocate(size_t n) {
+        return allocator<T>{}.allocate(n);
+    }
+
+    void deallocate(T* p, size_t n) {
+        allocator<T>{}.deallocate(p, n);
+    }
+};
+
+#if !defined(_DLL) || _ITERATOR_DEBUG_LEVEL == DEFAULT_IDL_SETTING
+template <class CharT>
+struct yes_no_punct : numpunct<CharT> {
+    basic_string<CharT> do_truename() const override {
+        return STR("yes");
+    }
+
+    basic_string<CharT> do_falsename() const override {
+        return STR("no");
+    }
+};
+#endif // !defined(_DLL) || _ITERATOR_DEBUG_LEVEL == DEFAULT_IDL_SETTING
+
+template <class CharT, class Alloc>
+void test_formatting_vector_bool_reference() {
+    std::vector<bool, Alloc> vb{false, true};
+
+    assert(format(STR("{}, {}"), vb[0], vb[1]) == format(STR("{}, {}"), false, true));
+    assert(format(STR("{:}, {:}"), vb[0], vb[1]) == format(STR("{:}, {:}"), false, true));
+    assert(format(STR("{:d}, {:x}"), vb[0], vb[1]) == format(STR("{:d}, {:x}"), false, true));
+
+#if !defined(_DLL) || _ITERATOR_DEBUG_LEVEL == DEFAULT_IDL_SETTING
+    locale loc{locale::classic(), new yes_no_punct<CharT>};
+
+    assert(format(loc, STR("{:L}"), vb[1]) == STR("yes"));
+    assert(format(loc, STR("{:L}"), vb[0]) == STR("no"));
+#endif // !defined(_DLL) || _ITERATOR_DEBUG_LEVEL == DEFAULT_IDL_SETTING
+}
+
+int main() {
+    test_formatting_vector_bool_reference<char, allocator<bool>>();
+    test_formatting_vector_bool_reference<char, pmr::polymorphic_allocator<bool>>();
+    test_formatting_vector_bool_reference<char, alternative_allocator<bool>>();
+
+    test_formatting_vector_bool_reference<wchar_t, allocator<bool>>();
+    test_formatting_vector_bool_reference<wchar_t, pmr::polymorphic_allocator<bool>>();
+    test_formatting_vector_bool_reference<wchar_t, alternative_allocator<bool>>();
+}
