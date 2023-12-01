35989797c5459814de07b5f9496aee40d1735d5c
Stephan T. Lavavej <stl@nuwen.net>
2023-12-01 15:22:07 -0800

    Drop low-value file comments and outdated "for ConcRT" comment.
    This doesn't drop all file comments, just the ones that I thought were especially unnecessary.

--- stl/src/mutex.cpp
+++ stl/src/mutex.cpp
@@ -1,8 +1,6 @@
 // Copyright (c) Microsoft Corporation.
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
-// mutex functions
-
 #include <cstdio>
 #include <cstdlib>
 #include <internal_shared.h>
