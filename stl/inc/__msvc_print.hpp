221218d789c0675bbad0a058a70b03bff2b4eba6
cpplearner <cpplearner@outlook.com>
2024-01-03 22:32:19 +0800

    Remove another use of __cpp_lib_concepts

--- stl/inc/__msvc_print.hpp
+++ stl/inc/__msvc_print.hpp
@@ -8,7 +8,7 @@
 #include <yvals_core.h>
 #if _STL_COMPILER_PREPROCESSOR
 
-#ifndef __cpp_lib_concepts // note: <format> includes this header in C++20 mode
+#if !_HAS_CXX20 // note: <format> includes this header in C++20 mode
 #error The contents of <print> are available only with C++23. (Also, you should not include this internal header.)
 #endif // ^^^ !_HAS_CXX20 ^^^
 
