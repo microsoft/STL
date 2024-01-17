d67f701c827ad530a2ce67d30fb675b9d38f50da
Stephan T. Lavavej <stl@microsoft.com>
2024-01-16 19:39:40 -0800

    /clr hates thread_local.

--- tests/std/include/vso1913897.hpp
+++ tests/std/include/vso1913897.hpp
@@ -3,9 +3,13 @@
 
 #pragma once
 
+#ifndef _M_CEE
+
 // TRANSITION, dynamically initialize a thread_local to workaround VSO-1913897
 inline int __stl_asan_init_function() {
     static volatile int __stl_asan_init_volatile = 42;
     return __stl_asan_init_volatile;
 }
 static thread_local int __stl_asan_init_variable = __stl_asan_init_function();
+
+#endif // _M_CEE
