194725575c4e4ec6e1e369ba984794f676d35ce8
Igor Zhukov <fsb4000@yandex.ru>
2024-02-18 15:54:48 +0700

    Equality for unordered_set and unordered_map should be based on equality of elements

---
+++ tests/std/tests/GH_0004388_unordered_meow_operator_equal/test.cpp
@@ -0,0 +1,110 @@
+// Copyright (c) Microsoft Corporation.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+#include <algorithm>
+#include <cassert>
+#include <map>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+
+using namespace std;
+
+namespace modulo {
+    // Functors for comparison and hashing
+    // based on modular arithmetic equivalence classes
+    template <int N>
+    struct compare {
+        bool operator()(int lhs, int rhs) const {
+            return (lhs % N) < (rhs % N);
+        }
+    };
+
+    template <int N>
+    struct equals {
+        bool operator()(int lhs, int rhs) const {
+            return (lhs % N) == (rhs % N);
+        }
+    };
+
+    template <int N>
+    struct hash {
+        size_t operator()(int value) const {
+            return value % N;
+        }
+    };
+} // namespace modulo
+
+// Overloaded function to get a key from a set / map's value_type
+int const& get_key(int const& value) {
+    return value;
+}
+int const& get_key(pair<int const, int> const& pair) {
+    return pair.first;
+}
+
+// Equality comparison for unordered sets and maps as per the C++ standard
+// It also gives the correct result for associative containers (though is sub-optimal)
+template <typename Container>
+bool std_equal(Container const& lhs, Container const& rhs) {
+    if (lhs.size() != rhs.size()) {
+        return false;
+    }
+
+    for (auto it = lhs.cbegin(); it != lhs.cend();) {
+        auto const& key = get_key(*it);
+        auto l_range    = lhs.equal_range(key);
+        auto r_range    = rhs.equal_range(key);
+
+        if (!is_permutation(l_range.first, l_range.second, r_range.first, r_range.second)) {
+            return false;
+        }
+        it = l_range.second;
+    }
+    return true;
+}
+
+template <typename Map>
+bool test_maps() {
+    // In all cases, these sets should compare not equal, even though their elements are equivalent
+    Map map1 = {{1, 1}, {2, 2}};
+    Map map2 = {{21, 1}, {12, 2}};
+
+    return (map1 == map2) == std_equal(map1, map2);
+}
+
+template <typename Set>
+bool test_sets() {
+    // In all cases, these sets should compare not equal, even though their elements are equivalent
+    Set set1 = {1, 2};
+    Set set2 = {21, 12};
+
+    return (set1 == set2) == std_equal(set1, set2);
+}
+
+// GH-4388: <unordered_set>, <unordered_map>: operator== is incorrect with custom equivalence functor
+void test_gh_4388() {
+    using Set               = set<int, modulo::compare<10>>;
+    using MultiSet          = multiset<int, modulo::compare<10>>;
+    using UnorderedSet      = unordered_set<int, modulo::hash<10>, modulo::equals<10>>;
+    using UnorderedMultiSet = unordered_multiset<int, modulo::hash<10>, modulo::equals<10>>;
+
+    assert(test_sets<Set>());
+    assert(test_sets<MultiSet>());
+    assert(test_sets<UnorderedSet>());
+    assert(test_sets<UnorderedMultiSet>());
+
+    using Map               = map<int, int, modulo::compare<10>>;
+    using MultiMap          = multimap<int, int, modulo::compare<10>>;
+    using UnorderedMap      = unordered_map<int, int, modulo::hash<10>, modulo::equals<10>>;
+    using UnorderedMultiMap = unordered_multimap<int, int, modulo::hash<10>, modulo::equals<10>>;
+
+    assert(test_maps<Map>());
+    assert(test_maps<MultiMap>());
+    assert(test_maps<UnorderedMap>());
+    assert(test_maps<UnorderedMultiMap>());
+}
+
+int main() {
+    test_gh_4388();
+}
