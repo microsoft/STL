95a2e2232cda3c61b8cab7ec5d2f40e2fbfc5804
Alex Guteniev <gutenev@gmail.com>
2024-02-18 12:05:44 +0200

    `<new>`: `SRWLOCK` to protect the `new` handler...
    ...instead of an indexed `CRITICAL_SECTION`

--- stl/src/stdhndlr.cpp
+++ stl/src/stdhndlr.cpp
@@ -5,9 +5,11 @@
 
 #include <new.h>
 #include <new>
+#include <mutex>
 
 namespace {
     _STD new_handler _New_handler;
+    constinit std::mutex _New_handler_mutex;
 
     int __cdecl _New_handler_interface(size_t) { // interface to existing Microsoft _callnewh mechanism
         _New_handler();
@@ -18,18 +20,16 @@ namespace {
 _STD_BEGIN
 
 _CRTIMP2 new_handler __cdecl set_new_handler(_In_opt_ new_handler pnew) noexcept { // remove current handler
-    _BEGIN_LOCK(_LOCK_MALLOC) // lock thread to ensure atomicity
+    unique_lock _Lock{_New_handler_mutex};
     new_handler pold = _New_handler;
     _New_handler     = pnew;
     _set_new_handler(pnew ? _New_handler_interface : nullptr);
     return pold;
-    _END_LOCK()
 }
 
 _CRTIMP2 new_handler __cdecl get_new_handler() noexcept { // get current new handler
-    _BEGIN_LOCK(_LOCK_MALLOC) // lock thread to ensure atomicity
+    unique_lock _Lock{_New_handler_mutex};
     return _New_handler;
-    _END_LOCK()
 }
 
 _STD_END
