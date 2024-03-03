5bcbcaecdeee30d9c7a60d3bff9c0a50223d9575
Igor Zhukov <fsb4000@yandex.ru>
2024-03-04 01:09:37 +0700

    Enforce `std::time_base::mdy` for "C" locale

--- stl/src/xdateord.cpp
+++ stl/src/xdateord.cpp
@@ -10,8 +10,14 @@
 _EXTERN_C_UNLESS_PURE
 
 _CRTIMP2_PURE int __CLRCALL_PURE_OR_CDECL _Getdateorder() noexcept { // return date order for current locale
-    wchar_t buf[2] = {0};
-    GetLocaleInfoEx(___lc_locale_name_func()[LC_TIME], LOCALE_ILDATE, buf, static_cast<int>(std::size(buf)));
+    wchar_t buf[2]             = {0};
+    const wchar_t* locale_name = ___lc_locale_name_func()[LC_TIME];
+    if (locale_name == nullptr) {
+        // Enforce std::time_base::mdy for "C" locale
+        buf[0] = L'0';
+    } else {
+        GetLocaleInfoEx(locale_name, LOCALE_ILDATE, buf, static_cast<int>(std::size(buf)));
+    }
 
     switch (buf[0]) {
     case L'0':
