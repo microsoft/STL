4e20186b04521607a6d0db6877a7c6f846341e14
Stephan T. Lavavej <stl@nuwen.net>
2024-03-26 09:14:13 -0700

    Drop ALL constraints on `_Make_from_tuple_impl`, then drop test coverage.

--- tests/std/tests/LWG3528_make_from_tuple_impl/test.compile.pass.cpp
+++ tests/std/tests/LWG3528_make_from_tuple_impl/test.compile.pass.cpp
@@ -33,14 +33,6 @@ template <class T, class Tuple>
 inline constexpr bool
     has_make_from_tuple<T, Tuple, std::void_t<decltype(std::make_from_tuple<T>(std::declval<Tuple>()))>> = true;
 
-template <class T, class Tuple, class = void>
-inline constexpr bool has_make_from_tuple_impl = false;
-
-template <class T, class Tuple>
-inline constexpr bool has_make_from_tuple_impl<T, Tuple,
-    std::void_t<decltype(std::_Make_from_tuple_impl<T>(
-        std::declval<Tuple>(), std::make_index_sequence<std::tuple_size_v<std::remove_reference_t<Tuple>>>{}))>> = true;
-
 // Test std::make_from_tuple.
 
 // reinterpret_cast, std::tuple<T>
@@ -80,43 +72,3 @@ static_assert(!has_make_from_tuple<D, std::array<int, 1>>);
 static_assert(has_make_from_tuple<long, std::array<int, 1>>);
 static_assert(has_make_from_tuple<double, std::array<float, 1>>);
 static_assert(has_make_from_tuple<float, std::array<double, 1>>);
-
-// Test std::_Make_from_tuple_impl.
-
-// reinterpret_cast, std::tuple<T>
-static_assert(!has_make_from_tuple_impl<int*, std::tuple<A*>>);
-static_assert(has_make_from_tuple_impl<A*, std::tuple<A*>>);
-
-// reinterpret_cast, std::array<T, 1>
-static_assert(!has_make_from_tuple_impl<int*, std::array<A*, 1>>);
-static_assert(has_make_from_tuple_impl<A*, std::array<A*, 1>>);
-
-// const_cast, std::tuple<T>
-static_assert(!has_make_from_tuple_impl<char*, std::tuple<const char*>>);
-static_assert(!has_make_from_tuple_impl<volatile char*, std::tuple<const volatile char*>>);
-static_assert(has_make_from_tuple_impl<volatile char*, std::tuple<volatile char*>>);
-static_assert(has_make_from_tuple_impl<char*, std::tuple<char*>>);
-static_assert(has_make_from_tuple_impl<const char*, std::tuple<char*>>);
-static_assert(has_make_from_tuple_impl<const volatile char*, std::tuple<volatile char*>>);
-
-// const_cast, std::array<T, 1>
-static_assert(!has_make_from_tuple_impl<char*, std::array<const char*, 1>>);
-static_assert(!has_make_from_tuple_impl<volatile char*, std::array<const volatile char*, 1>>);
-static_assert(has_make_from_tuple_impl<volatile char*, std::array<volatile char*, 1>>);
-static_assert(has_make_from_tuple_impl<char*, std::array<char*, 1>>);
-static_assert(has_make_from_tuple_impl<const char*, std::array<char*, 1>>);
-static_assert(has_make_from_tuple_impl<const volatile char*, std::array<volatile char*, 1>>);
-
-// static_cast, std::tuple<T>
-static_assert(!has_make_from_tuple_impl<int, std::tuple<D>>);
-static_assert(!has_make_from_tuple_impl<D, std::tuple<int>>);
-static_assert(has_make_from_tuple_impl<long, std::tuple<int>>);
-static_assert(has_make_from_tuple_impl<double, std::tuple<float>>);
-static_assert(has_make_from_tuple_impl<float, std::tuple<double>>);
-
-// static_cast, std::array<T, 1>
-static_assert(!has_make_from_tuple_impl<int, std::array<D, 1>>);
-static_assert(!has_make_from_tuple_impl<D, std::array<int, 1>>);
-static_assert(has_make_from_tuple_impl<long, std::array<int, 1>>);
-static_assert(has_make_from_tuple_impl<double, std::array<float, 1>>);
-static_assert(has_make_from_tuple_impl<float, std::array<double, 1>>);
