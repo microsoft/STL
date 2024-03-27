b0fbdca3a1233553a9fa0ae78e88a6a9af0b306b
Stephan T. Lavavej <stl@nuwen.net>
2024-03-27 12:02:06 -0700

    Add an empty line, then clang-format suppression can be removed.

--- stl/src/vector_algorithms.cpp
+++ stl/src/vector_algorithms.cpp
@@ -92,7 +92,7 @@ namespace {
     __m256i _Avx2_tail_mask_32(const size_t _Count_in_dwords) {
         constexpr unsigned int _Dx = 0; // All zeros 32 bit mask
         constexpr unsigned int _Ex = ~_Dx; // All ones 32 bit mask
-        // clang-format off
+
         alignas(32) static constexpr unsigned int _Tail_masks[8][8] = {
             {_Dx, _Dx, _Dx, _Dx, _Dx, _Dx, _Dx, _Dx},
             {_Ex, _Dx, _Dx, _Dx, _Dx, _Dx, _Dx, _Dx},
@@ -103,7 +103,6 @@ namespace {
             {_Ex, _Ex, _Ex, _Ex, _Ex, _Ex, _Dx, _Dx},
             {_Ex, _Ex, _Ex, _Ex, _Ex, _Ex, _Ex, _Dx},
         };
-        // clang-format on
         return _mm256_load_si256(reinterpret_cast<const __m256i*>(_Tail_masks[_Count_in_dwords]));
     }
 } // unnamed namespace
