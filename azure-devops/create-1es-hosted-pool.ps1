607d7121a7e40c0c5939986d415fe66d0854fe35
Stephan T. Lavavej <stl@microsoft.com>
2024-04-17 12:44:23 -0700

    [toolset update] Spot D32ds_v5 => Regular D32ads_v5

--- azure-devops/create-1es-hosted-pool.ps1
+++ azure-devops/create-1es-hosted-pool.ps1
@@ -14,7 +14,7 @@ $ErrorActionPreference = 'Stop'
 $CurrentDate = Get-Date
 
 $Location = 'eastus'
-$VMSize = 'Standard_D32ds_v5'
+$VMSize = 'Standard_D32ads_v5'
 $ProtoVMName = 'PROTOTYPE'
 $ImagePublisher = 'MicrosoftWindowsServer'
 $ImageOffer = 'WindowsServer'
@@ -167,10 +167,11 @@ $Nic = New-AzNetworkInterface `
 ####################################################################################################
 Display-ProgressBar -Status 'Creating prototype VM'
 
+# Previously: -Priority 'Spot'
 $VM = New-AzVMConfig `
   -Name $ProtoVMName `
   -VMSize $VMSize `
-  -Priority 'Spot' `
+  -Priority 'Regular' `
   -MaxPrice -1
 
 $VM = Set-AzVMOperatingSystem `
@@ -342,7 +343,7 @@ $PoolName = $ResourceGroupName + '-Pool'
 $PoolProperties = @{
   'organization' = 'https://dev.azure.com/vclibs'
   'projects' = @('STL')
-  'sku' = @{ 'name' = $VMSize; 'tier' = 'StandardSSD'; 'enableSpot' = $true; }
+  'sku' = @{ 'name' = $VMSize; 'tier' = 'StandardSSD'; 'enableSpot' = $false; }
   'images' = @(@{ 'imageName' = $ImageName; 'poolBufferPercentage' = '100'; })
   'maxPoolSize' = 64
   'agentProfile' = @{ 'type' = 'Stateless'; }
