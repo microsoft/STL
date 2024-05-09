143366a85e0335d4c08ff7f0b40fb154aa9d4515
Stephan T. Lavavej <stl@nuwen.net>
2024-05-09 14:30:22 -0700

    Improve temp_file_name(): "msvc_stl_" prefix, 128 bits.
    The ".tmp" extension is already clear, so let's change the prefix to make the source of these files obvious.
    
    In GH 2210, I chose 256 bits of entropy without careful consideration, and it was way too much.
    128 bits is plenty; see Wikipedia's birthday problem article for a probability table.

--- tests/std/include/temp_file_name.hpp
+++ tests/std/include/temp_file_name.hpp
@@ -7,11 +7,11 @@
 #include <string>
 
 [[nodiscard]] inline std::string temp_file_name() {
-    std::string ret{"temp_file_"};
+    std::string ret{"msvc_stl_"};
     std::uniform_int_distribution<int> dist{0, 15};
     std::random_device rd;
 
-    for (int i = 0; i < 64; ++i) { // 64 hexits = 256 bits of entropy
+    for (int i = 0; i < 32; ++i) { // 32 hexits = 128 bits of entropy
         ret.push_back("0123456789ABCDEF"[dist(rd)]);
     }
 
