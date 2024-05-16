3e45a7db5cc3aa5b6e206bafbafd4dad9896d5b4
Stephan T. Lavavej <stl@nuwen.net>
2024-05-15 23:52:02 -0700

    Add 10 to the int values to make them distinctive.

--- tests/std/tests/GH_004597_self_swap/test.cpp
+++ tests/std/tests/GH_004597_self_swap/test.cpp
@@ -26,45 +26,45 @@ struct swap_counter {
 _CONSTEXPR20 bool test_gh_4597() {
     {
         unsigned int cnt{};
-        std::pair<swap_counter, int> pr{swap_counter{&cnt}, 0};
+        std::pair<swap_counter, int> pr{swap_counter{&cnt}, 10};
         pr.swap(pr);
         assert(cnt == 2u);
         assert(pr.first == 2u);
-        assert(pr.second == 0);
+        assert(pr.second == 10);
     }
 
     {
         unsigned int cnt{};
-        std::pair<swap_counter, int> p1{swap_counter{&cnt}, 0};
-        std::pair<swap_counter, int> p2{swap_counter{&cnt}, 1};
+        std::pair<swap_counter, int> p1{swap_counter{&cnt}, 10};
+        std::pair<swap_counter, int> p2{swap_counter{&cnt}, 11};
         p1.swap(p2);
         assert(cnt == 2u);
         assert(p1.first == 2u);
-        assert(p1.second == 1);
+        assert(p1.second == 11);
         assert(p2.first == 2u);
-        assert(p2.second == 0);
+        assert(p2.second == 10);
     }
 
     {
         unsigned int c1{};
         unsigned int c2{2};
-        std::pair<swap_counter, int> p1{swap_counter{&c1}, 1};
-        std::pair<swap_counter, int> p2{swap_counter{&c2}, 3};
+        std::pair<swap_counter, int> p1{swap_counter{&c1}, 11};
+        std::pair<swap_counter, int> p2{swap_counter{&c2}, 13};
         p1.swap(p2);
         assert(c1 == 1u);
         assert(c2 == 3u);
         assert(p1.first == 3u);
-        assert(p1.second == 3);
+        assert(p1.second == 13);
         assert(p2.first == 1u);
-        assert(p2.second == 1);
+        assert(p2.second == 11);
     }
 
 #if _HAS_CXX23
     {
         unsigned int c1{};
         unsigned int c2{2};
-        int i1 = 1;
-        int i2 = 3;
+        int i1 = 11;
+        int i2 = 13;
         swap_counter s1{&c1};
         swap_counter s2{&c2};
         const std::pair<swap_counter&, int&> p1{s1, i1};
@@ -73,9 +73,9 @@ _CONSTEXPR20 bool test_gh_4597() {
         assert(c1 == 1u);
         assert(c2 == 3u);
         assert(p1.first == 3u);
-        assert(p1.second == 3);
+        assert(p1.second == 13);
         assert(p2.first == 1u);
-        assert(p2.second == 1);
+        assert(p2.second == 11);
     }
 #endif // _HAS_CXX23
 
