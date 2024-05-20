ccf9e1f020a3a83ef7417bd166419a2427cbd670
Alex Guteniev <gutenev@gmail.com>
2024-05-21 02:02:05 +0300

    Help the compiler vectorize `ranges::iota` (#4647)
    Co-authored-by: Stephan T. Lavavej <stl@nuwen.net>

--- benchmarks/src/iota.cpp
+++ benchmarks/src/iota.cpp
@@ -7,14 +7,24 @@
 #include <numeric>
 #include <vector>
 
-template <class T>
+enum class Alg {
+    Std,
+    Rng,
+};
+
+template <class T, Alg Algorithm>
 void bm(benchmark::State& state) {
     const auto size = static_cast<std::size_t>(state.range(0));
 
     std::vector<T> a(size);
 
     for (auto _ : state) {
-        std::iota(a.begin(), a.end(), T{22});
+        if constexpr (Algorithm == Alg::Std) {
+            std::iota(a.begin(), a.end(), T{22});
+        } else if constexpr (Algorithm == Alg::Rng) {
+            std::ranges::iota(a, T{22});
+        }
+
         benchmark::DoNotOptimize(a);
     }
 }
@@ -23,7 +33,10 @@ void common_args(auto bm) {
     bm->Arg(7)->Arg(18)->Arg(43)->Arg(131)->Arg(315)->Arg(1212);
 }
 
-BENCHMARK(bm<std::uint32_t>)->Apply(common_args);
-BENCHMARK(bm<std::uint64_t>)->Apply(common_args);
+BENCHMARK(bm<std::uint32_t, Alg::Std>)->Apply(common_args);
+BENCHMARK(bm<std::uint64_t, Alg::Std>)->Apply(common_args);
+
+BENCHMARK(bm<std::uint32_t, Alg::Rng>)->Apply(common_args);
+BENCHMARK(bm<std::uint64_t, Alg::Rng>)->Apply(common_args);
 
 BENCHMARK_MAIN();
