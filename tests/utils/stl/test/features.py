37e0168c82ad3b61d389a8a68c3afb760453b13e
Stephan T. Lavavej <stl@nuwen.net>
2024-05-22 23:34:17 -0700

    We 'has-no-zdump' (a unix program).

--- tests/utils/stl/test/features.py
+++ tests/utils/stl/test/features.py
@@ -25,6 +25,7 @@ def getDefaultFeatures(config, litConfig):
     DEFAULT_FEATURES = [
         Feature(name='has-64-bit-atomics'),
         Feature(name='has-1024-bit-atomics'),
+        Feature(name='has-no-zdump'),
         Feature(name='msvc'),
         Feature(name='windows'),
     ]
