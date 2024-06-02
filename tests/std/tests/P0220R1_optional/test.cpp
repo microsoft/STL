e1e4bdb4264143860b91d29db717b191983bee00
Stephan T. Lavavej <stl@nuwen.net>
2024-06-02 13:24:26 -0700

    P0220R1_optional: Restore EDG workarounds.
    With updated "no workaround" arrow comments.

--- tests/std/tests/P0220R1_optional/test.cpp
+++ tests/std/tests/P0220R1_optional/test.cpp
@@ -856,16 +856,20 @@ int run_test()
         assert(std::hash<optional<T>>{}(opt) == std::hash<T>{}(*opt));
     }
     {
+#ifndef __EDG__ // TRANSITION, DevCom-10107834
       test_hash_enabled_for_type<std::optional<int> >();
       test_hash_enabled_for_type<std::optional<int*> >();
       test_hash_enabled_for_type<std::optional<const int> >();
       test_hash_enabled_for_type<std::optional<int* const> >();
+#endif // ^^^ no workaround ^^^
 
       test_hash_disabled_for_type<std::optional<A>>();
       test_hash_disabled_for_type<std::optional<const A>>();
 
+#ifndef __EDG__ // TRANSITION, DevCom-10107834
       test_hash_enabled_for_type<std::optional<B>>();
       test_hash_enabled_for_type<std::optional<const B>>();
+#endif // ^^^ no workaround ^^^
     }
 
   return 0;
