bdf44e779ab672c0e68aa6fa8eee4ad2628239a7
Stephan T. Lavavej <stl@nuwen.net>
2024-06-08 05:46:31 -0700

    Fix `range_formatter` warnings, add test coverage.
    These are both covered by the new test.
    
    The empty `default: break;` is used, i.e. making it `abort()` causes tests to fail.
    
    Add a comment to explain why `_Parse_range_specs()` guarantees that the other can't happen.

--- tests/std/tests/GH_002094_cpp_core_guidelines/test.compile.pass.cpp
+++ tests/std/tests/GH_002094_cpp_core_guidelines/test.compile.pass.cpp
@@ -2,3 +2,36 @@
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
 #include <__msvc_all_public_headers.hpp>
+
+using namespace std;
+
+#if _HAS_CXX23
+template <class T>
+struct WrappedVector : vector<T> {
+    using vector<T>::vector;
+};
+
+template <class T>
+struct std::formatter<WrappedVector<T>, char> {
+public:
+    template <class ParseContext>
+    constexpr auto parse(ParseContext& ctx) {
+        return underlying.parse(ctx);
+    }
+
+    template <class FormatContext>
+    auto format(const WrappedVector<T>& rng, FormatContext& ctx) const {
+        return underlying.format(rng, ctx);
+    }
+
+private:
+    range_formatter<T, char> underlying;
+};
+
+void instantiate_range_formatter_machinery() {
+    const WrappedVector<int> v{11, 22, 33, 44};
+    assert(format("{}", v) == "[11, 22, 33, 44]");
+    assert(format("{:}", v) == "[11, 22, 33, 44]");
+    assert(format("{:n}", v) == "11, 22, 33, 44");
+}
+#endif // ^^^ _HAS_CXX23 ^^^
