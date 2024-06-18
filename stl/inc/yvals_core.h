065a036b765bd56301f6e57075a2fb14b945340f
chauneyWang <42889779+chauney@users.noreply.github.com>
2024-06-18 13:15:21 +0800

    Update month to June 2024 (#4706)
    Co-authored-by: Arup Chauhan <arupchauhan26@gmail.com>

--- stl/inc/yvals_core.h
+++ stl/inc/yvals_core.h
@@ -880,7 +880,7 @@
 
 #define _CPPLIB_VER       650
 #define _MSVC_STL_VERSION 143
-#define _MSVC_STL_UPDATE  202405L
+#define _MSVC_STL_UPDATE  202406L
 
 #ifndef _ALLOW_COMPILER_AND_STL_VERSION_MISMATCH
 #if defined(__CUDACC__) && defined(__CUDACC_VER_MAJOR__)
