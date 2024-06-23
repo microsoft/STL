2608a58d52c69d039b264280ede3c12aa77bba14
Alex Guteniev <gutenev@gmail.com>
2024-06-23 13:14:48 +0300

    Vectorize large char type only for small needle

--- stl/inc/__msvc_string_view.hpp
+++ stl/inc/__msvc_string_view.hpp
@@ -696,7 +696,11 @@ constexpr size_t _Traits_find_first_of(_In_reads_(_Hay_size) const _Traits_ptr_t
     if (_Needle_size != 0 && _Start_at < _Hay_size) { // room for match, look for it
         if constexpr (_Special) {
 #if _USE_STD_VECTOR_ALGORITHMS
-            if (!_STD _Is_constant_evaluated() && _Hay_size - _Start_at > _Threshold_find_first_of) {
+            bool _Try_vectorize = !_STD _Is_constant_evaluated() && _Hay_size - _Start_at > _Threshold_find_first_of;
+            constexpr size_t _Elem_size = sizeof(*_Haystack);
+
+            // Additional condition for the case where the table lookup outperforms the vectorization
+            if (_Try_vectorize && (_Elem_size == 1 || _Elem_size * _Needle_size <= 16)) {
                 const _Traits_ptr_t<_Traits> _Found = _STD _Find_first_of_vectorized(
                     _Haystack + _Start_at, _Haystack + _Hay_size, _Needle, _Needle + _Needle_size);
 
@@ -710,6 +714,19 @@ constexpr size_t _Traits_find_first_of(_In_reads_(_Hay_size) const _Traits_ptr_t
             _String_bitmap<typename _Traits::char_type> _Matches;
             if (!_Matches._Mark(_Needle, _Needle + _Needle_size)) { // couldn't put one of the characters into the
                                                                     // bitmap, fall back to the serial algorithm
+#if _USE_STD_VECTOR_ALGORITHMS
+                if (_Try_vectorize) {
+                    const _Traits_ptr_t<_Traits> _Found = _STD _Find_first_of_vectorized(
+                        _Haystack + _Start_at, _Haystack + _Hay_size, _Needle, _Needle + _Needle_size);
+
+                    if (_Found != _Haystack + _Hay_size) {
+                        return static_cast<size_t>(_Found - _Haystack);
+                    } else {
+                        return static_cast<size_t>(-1); // no match
+                    }
+                }
+#endif // _USE_STD_VECTOR_ALGORITHMS
+
                 return _Traits_find_first_of<_Traits, false>(_Haystack, _Hay_size, _Start_at, _Needle, _Needle_size);
             }
 
