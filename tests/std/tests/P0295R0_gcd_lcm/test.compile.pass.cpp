1093d23e5a812454ee62673884aafed3da769073
Stephan T. Lavavej <stl@microsoft.com>
2024-07-10 22:00:14 -0700

    Fix gcd/lcm to work with all nonbool integrals.
    numeric: Add a `static_cast` when dividing `_Common_unsigned` by `_Common_unsigned`,
    due to the usual arithmetic conversions which will emit sign conversion warnings for tiny types.
    
    utility:
    * Move `_Is_standard_integer` down to the `_HAS_CXX20` region. (It's also used by `<mdspan>`.)
    * Relax the internal `_Cmp_equal`, `_Cmp_less`, and `_In_range` to accept nonbool integrals.
      + Because they're internal, we can `_STL_INTERNAL_STATIC_ASSERT`.
      + Comment that this "allows character types".
    * Enforce `_Is_standard_integer` at the user-visible layer.
    
    P0295R0_gcd_lcm: Add test coverage, because gcd/lcm are required to accept nonbool integrals.
    (An MSVC-internal test found this by using `char`.)

--- tests/std/tests/P0295R0_gcd_lcm/test.compile.pass.cpp
+++ tests/std/tests/P0295R0_gcd_lcm/test.compile.pass.cpp
@@ -49,3 +49,28 @@ static_assert(lcm(1, 0) == 0);
 static_assert(lcm(1073741824, 536870912) == 1073741824);
 static_assert(lcm(1073741824, -536870912) == 1073741824);
 static_assert(lcm(-1073741824, 536870912) == 1073741824);
+
+template <class T>
+constexpr bool test_nonbool_integral_type() {
+    static_assert(gcd(T{60}, T{24}) == T{12});
+    static_assert(lcm(T{60}, T{24}) == T{120});
+    return true;
+}
+
+static_assert(test_nonbool_integral_type<char>());
+static_assert(test_nonbool_integral_type<wchar_t>());
+#ifdef __cpp_char8_t
+static_assert(test_nonbool_integral_type<char8_t>());
+#endif // __cpp_char8_t
+static_assert(test_nonbool_integral_type<char16_t>());
+static_assert(test_nonbool_integral_type<char32_t>());
+static_assert(test_nonbool_integral_type<signed char>());
+static_assert(test_nonbool_integral_type<short>());
+static_assert(test_nonbool_integral_type<int>());
+static_assert(test_nonbool_integral_type<long>());
+static_assert(test_nonbool_integral_type<long long>());
+static_assert(test_nonbool_integral_type<unsigned char>());
+static_assert(test_nonbool_integral_type<unsigned short>());
+static_assert(test_nonbool_integral_type<unsigned int>());
+static_assert(test_nonbool_integral_type<unsigned long>());
+static_assert(test_nonbool_integral_type<unsigned long long>());
