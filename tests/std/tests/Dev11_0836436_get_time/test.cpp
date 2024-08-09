aa0e8b5b2121c53a6791436e92e2adbf2a4a57fa
Nikolay Baklicharov <nikolay.baklicharov@gmail.com>
2024-08-10 00:50:57 +0300

    Fix clang-format issue

--- tests/std/tests/Dev11_0836436_get_time/test.cpp
+++ tests/std/tests/Dev11_0836436_get_time/test.cpp
@@ -1,11 +1,11 @@
 // Copyright (c) Microsoft Corporation.
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
+#include <algorithm>
 #include <cassert>
 #include <cstdio>
 #include <cstdlib>
 #include <ctime>
-#include <algorithm>
 #include <iomanip>
 #include <ios>
 #include <locale>
