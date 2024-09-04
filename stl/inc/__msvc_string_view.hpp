46ecdc8064cee5368ae5735b8518d11b16ca18e3
Alex Guteniev <gutenev@gmail.com>
2024-09-04 14:19:16 +0300

    last

--- stl/inc/__msvc_string_view.hpp
+++ stl/inc/__msvc_string_view.hpp
@@ -823,9 +823,9 @@ constexpr size_t _Traits_find_last_of(_In_reads_(_Hay_size) const _Traits_ptr_t<
                 if constexpr (sizeof(_Elem) <= 2) {
                     _Try_vectorize = _Hay_size - _Start_at > _Threshold_find_first_of;
                     // Additional condition for when the vectorization outperforms the table lookup
-                    constexpr size_t _Find_first_of_bitmap_threshold = sizeof(_Elem) == 1 ? 48 : 8;
+                    constexpr size_t _Find_last_of_bitmap_threshold = sizeof(_Elem) == 1 ? 48 : 8;
 
-                    _Use_bitmap = !_Try_vectorize || _Needle_size > _Find_first_of_bitmap_threshold;
+                    _Use_bitmap = !_Try_vectorize || _Needle_size > _Find_last_of_bitmap_threshold;
                 }
 #endif // _USE_STD_VECTOR_ALGORITHMS
 
