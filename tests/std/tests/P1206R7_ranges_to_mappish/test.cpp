ab20dbd32ed7df565160c7f6a30361f5aeb3999c
Casey Carter <cacarter@microsoft.com>
2024-09-09 20:55:05 -0700

    Begin removing EDG workaround from `ranges::to` tests (#4944)

--- tests/std/tests/P1206R7_ranges_to_mappish/test.cpp
+++ tests/std/tests/P1206R7_ranges_to_mappish/test.cpp
@@ -156,13 +156,13 @@ struct mappish_instantiator {
             assert(c5.get_allocator().state == 13);
             assert(ranges::is_permutation(c5, expected, any_pair_eq));
         }
-#ifndef __EDG__ // TRANSITION, VSO-2208356
+#if defined(_MSVC_INTERNAL_TESTING) || !defined(__EDG__) // TRANSITION, VS 17.12p3
         {
             std::same_as<T> auto c6 = R{some_pairs} | ranges::to<T>(Alloc{13});
             assert(c6.get_allocator().state == 13);
             assert(ranges::is_permutation(c6, expected, any_pair_eq));
         }
-#endif // ^^^ no workaround ^^^
+#endif
         {
             std::same_as<T> auto c7 = R{some_pairs} | ranges::to<C>(Alloc{13});
             assert(c7.get_allocator().state == 13);
