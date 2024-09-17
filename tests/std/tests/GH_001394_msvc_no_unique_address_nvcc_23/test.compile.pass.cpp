52cc346d69b9f2dac0ad4aeaa5116d2c484ec319
A. Jiang <de34@live.cn>
2024-09-17 23:54:24 +0800

    Initially use `[[msvc::no_unique_address]]` for some C++23 components
    - `in_value_result`
    - `out_value_result`
    - `mdspan`
    - `join_with_view`'s sentinel
    - `enumerate_view`'s iterator and sentinel
    - `chunk_view`'s iterator
    - `slide_view`'s sentinel

---
+++ tests/std/tests/GH_001394_msvc_no_unique_address_nvcc_23/test.compile.pass.cpp
@@ -0,0 +1,4 @@
+// Copyright (c) Microsoft Corporation.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+#include "../GH_001394_msvc_no_unique_address_23/test.compile.pass.cpp"
---
+++ tests/std/tests/P0009R18_mdspan_msabi_nvcc/test.compile.pass.cpp
@@ -0,0 +1,4 @@
+// Copyright (c) Microsoft Corporation.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+#include "../P0009R18_mdspan_msabi/test.compile.pass.cpp"
