fcadb1d5d2fcc6e5b5e1aaa105360d9c87d47aa3
Stephan T. Lavavej <stl@microsoft.com>
2024-10-11 06:44:15 -0700

    Perma-workaround VSO-2279389.
    VSO-2279389 "/clr C++20 can't handle struct MoveOnlyFunctor defined in a function template, emitting fatal error C1193: an error expected in yyaction.cpp(2899) not reached"

--- tests/std/tests/GH_002488_promise_not_default_constructible_types/test.cpp
+++ tests/std/tests/GH_002488_promise_not_default_constructible_types/test.cpp
@@ -79,6 +79,17 @@ void assert_throws_future_error(F f, std::error_code expected_code) {
 }
 
 template <class T>
+struct MoveOnlyFunctor {
+    MoveOnlyFunctor()                             = default;
+    MoveOnlyFunctor(MoveOnlyFunctor&&)            = default;
+    MoveOnlyFunctor& operator=(MoveOnlyFunctor&&) = default;
+
+    T operator()() const {
+        return T{172};
+    }
+};
+
+template <class T>
 void run_tests() {
     using Promise = std::promise<T>;
     using Future  = std::future<T>;
@@ -179,16 +190,7 @@ void run_tests() {
 
     // Also test GH-321: "<future>: packaged_task can't be constructed from a move-only lambda"
     {
-        struct MoveOnlyFunctor {
-            MoveOnlyFunctor()                             = default;
-            MoveOnlyFunctor(MoveOnlyFunctor&&)            = default;
-            MoveOnlyFunctor& operator=(MoveOnlyFunctor&&) = default;
-
-            T operator()() const {
-                return T{172};
-            }
-        };
-        std::packaged_task<T()> pt(MoveOnlyFunctor{});
+        std::packaged_task<T()> pt(MoveOnlyFunctor<T>{});
         Future f = pt.get_future();
         pt();
 
