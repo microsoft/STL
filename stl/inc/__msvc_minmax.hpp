d1f993dded3e29abad2ab8ebbf0305eb2221ee5f
Alex Guteniev <gutenev@gmail.com>
2024-10-12 19:33:46 +0300

    Auto-vectorize  minmax

--- stl/inc/__msvc_minmax.hpp
+++ stl/inc/__msvc_minmax.hpp
@@ -21,61 +21,6 @@ struct _Min_max_element_t {
     const void* _Min;
     const void* _Max;
 };
-
-struct _Min_max_1i {
-    int8_t _Min;
-    int8_t _Max;
-};
-
-struct _Min_max_1u {
-    uint8_t _Min;
-    uint8_t _Max;
-};
-
-struct _Min_max_2i {
-    int16_t _Min;
-    int16_t _Max;
-};
-
-struct _Min_max_2u {
-    uint16_t _Min;
-    uint16_t _Max;
-};
-
-struct _Min_max_4i {
-    int32_t _Min;
-    int32_t _Max;
-};
-
-struct _Min_max_4u {
-    uint32_t _Min;
-    uint32_t _Max;
-};
-
-struct _Min_max_8i {
-    int64_t _Min;
-    int64_t _Max;
-};
-
-struct _Min_max_8u {
-    uint64_t _Min;
-    uint64_t _Max;
-};
-
-struct _Min_max_f {
-    float _Min;
-    float _Max;
-};
-
-struct _Min_max_d {
-    double _Min;
-    double _Max;
-};
-
-struct _Min_max_p {
-    void* _Min;
-    void* _Max;
-};
 } // extern "C"
 
 #pragma pop_macro("new")
--- stl/src/vector_algorithms.cpp
+++ stl/src/vector_algorithms.cpp
@@ -488,6 +488,61 @@ __declspec(noalias) void __cdecl __std_reverse_copy_trivially_copyable_8(
         static_cast<unsigned long long*>(_Dest));
 }
 
+struct _Min_max_1i {
+    int8_t _Min;
+    int8_t _Max;
+};
+
+struct _Min_max_1u {
+    uint8_t _Min;
+    uint8_t _Max;
+};
+
+struct _Min_max_2i {
+    int16_t _Min;
+    int16_t _Max;
+};
+
+struct _Min_max_2u {
+    uint16_t _Min;
+    uint16_t _Max;
+};
+
+struct _Min_max_4i {
+    int32_t _Min;
+    int32_t _Max;
+};
+
+struct _Min_max_4u {
+    uint32_t _Min;
+    uint32_t _Max;
+};
+
+struct _Min_max_8i {
+    int64_t _Min;
+    int64_t _Max;
+};
+
+struct _Min_max_8u {
+    uint64_t _Min;
+    uint64_t _Max;
+};
+
+struct _Min_max_f {
+    float _Min;
+    float _Max;
+};
+
+struct _Min_max_d {
+    double _Min;
+    double _Max;
+};
+
+struct _Min_max_p {
+    void* _Min;
+    void* _Max;
+};
+
 } // extern "C"
 
 namespace {
