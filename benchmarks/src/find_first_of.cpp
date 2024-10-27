6f6f97b5da29f3556dc72b2c9c3c92303c94dcec
Alex Guteniev <gutenev@gmail.com>
2024-10-27 15:51:45 +0200

    ensure the same (mis)alignment

--- benchmarks/src/find_first_of.cpp
+++ benchmarks/src/find_first_of.cpp
@@ -9,11 +9,49 @@
 #include <cstdlib>
 #include <limits>
 #include <numeric>
+#include <stdexcept>
 #include <string>
 #include <type_traits>
 #include <vector>
 
 using namespace std;
+
+#if 1 // TRANSITION, GH-5043
+
+template <class T, size_t Alignment, size_t Skew>
+struct skewed_allocator {
+    using value_type = T;
+
+    T* allocate(size_t n) {
+        const auto p = static_cast<unsigned char*>(_aligned_malloc(n * sizeof(T) + Skew, Alignment));
+        if (!p) {
+            throw std::bad_alloc{};
+        }
+        return reinterpret_cast<T*>(p + Skew);
+    }
+
+    void deallocate(T* p, size_t) {
+        if (p) {
+            _aligned_free(reinterpret_cast<unsigned char*>(p) - Skew);
+        }
+    }
+};
+
+// The purpose is to provide consistent behavior for benchmarks.
+// 64 seems to be reasonable alignment for practical perf uses,
+// as it is both cache line size and maximum vector instruction size (on x64).
+// However to provide even more consistency, aligning to page,
+// to make sure the same number of page boundaries is crossed each time.
+constexpr size_t page_size = 4096;
+
+// A realistic skew relative to allocation granularity, when a variable is placed
+// next to a pointer in a structure or on stack. Also corresponds to the default packing.
+constexpr size_t skew = 8;
+
+template <class T>
+struct unaligned_allocator : skewed_allocator<T, page_size, skew> {};
+
+#endif // ^^^ TRANSITION, GH-5043 ^^^
 
 enum class AlgType { std_func, str_member_first, str_member_last };
 
@@ -24,7 +62,8 @@ void bm(benchmark::State& state) {
     const size_t HSize = Pos * 2;
     const size_t Which = 0;
 
-    using container = conditional_t<Alg == AlgType::std_func, vector<T>, basic_string<T>>;
+    using container = conditional_t<Alg == AlgType::std_func, vector<T, unaligned_allocator<T>>,
+        basic_string<T, char_traits<T>, unaligned_allocator<T>>>;
 
     constexpr T HaystackFiller{' '};
     static_assert(HaystackFiller < Start, "The following iota() should not produce the haystack filler.");
