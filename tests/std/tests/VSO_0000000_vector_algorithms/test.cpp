0a9802a98f7d3a8c8ad690add715dc1d159dc4f1
Stephan T. Lavavej <stl@microsoft.com>
2024-10-29 12:50:12 -0700

    GH 4958

--- tests/std/tests/VSO_0000000_vector_algorithms/test.cpp
+++ tests/std/tests/VSO_0000000_vector_algorithms/test.cpp
@@ -12,6 +12,7 @@
 #include <functional>
 #include <limits>
 #include <list>
+#include <numeric>
 #include <random>
 #include <stdexcept>
 #include <string>
@@ -33,6 +34,63 @@ using namespace std;
 #ifdef __clang__
 #pragma clang diagnostic ignored "-Wc++17-extensions" // constexpr if is a C++17 extension
 #endif // __clang__
+
+template <class InIt, class OutIt, class BinOp>
+OutIt last_known_good_adj_diff(InIt first, InIt last, OutIt dest, BinOp binop) {
+    if (first == last) {
+        return dest;
+    }
+
+    auto val = *first;
+    *dest    = val;
+
+    for (++first, ++dest; first != last; ++first, ++dest) {
+        auto tmp = *first;
+        *dest    = binop(tmp, val);
+        val      = tmp;
+    }
+
+    return dest;
+}
+
+template <class T>
+void test_case_adj_diff(const vector<T>& input, vector<T>& output_expected, vector<T>& output_actual) {
+    // Avoid truncation warnings:
+    const auto subtract = [](const T& left, const T& right) { return static_cast<T>(left - right); };
+    const auto expected = last_known_good_adj_diff(input.begin(), input.end(), output_expected.begin(), subtract);
+    const auto actual   = adjacent_difference(input.begin(), input.end(), output_actual.begin(), subtract);
+
+    assert(actual - output_actual.begin() == expected - output_expected.begin());
+    assert(output_actual == output_expected);
+}
+
+template <class T>
+void test_adjacent_difference(mt19937_64& gen) {
+    using Limits = numeric_limits<T>;
+
+    uniform_int_distribution<conditional_t<sizeof(T) == 1, int, T>> dis(
+        is_signed_v<T> ? static_cast<T>(Limits::min() / 2) : Limits::min(),
+        is_signed_v<T> ? static_cast<T>(Limits::max() / 2) : Limits::max());
+
+    vector<T> input;
+    vector<T> output_expected;
+    vector<T> output_actual;
+
+    for (const auto& v : {&input, &output_expected, &output_actual}) {
+        v->reserve(dataCount);
+    }
+
+    test_case_adj_diff(input, output_expected, output_actual);
+    for (size_t attempts = 0; attempts < dataCount; ++attempts) {
+        input.push_back(static_cast<T>(dis(gen)));
+
+        for (const auto& v : {&output_expected, &output_actual}) {
+            v->assign(input.size(), 0);
+        }
+
+        test_case_adj_diff(input, output_expected, output_actual);
+    }
+}
 
 template <class FwdIt, class T>
 ptrdiff_t last_known_good_count(FwdIt first, FwdIt last, T v) {
@@ -895,6 +953,16 @@ void test_swap_arrays(mt19937_64& gen) {
 }
 
 void test_vector_algorithms(mt19937_64& gen) {
+    test_adjacent_difference<char>(gen);
+    test_adjacent_difference<signed char>(gen);
+    test_adjacent_difference<unsigned char>(gen);
+    test_adjacent_difference<short>(gen);
+    test_adjacent_difference<unsigned short>(gen);
+    test_adjacent_difference<int>(gen);
+    test_adjacent_difference<unsigned int>(gen);
+    test_adjacent_difference<long long>(gen);
+    test_adjacent_difference<unsigned long long>(gen);
+
     test_count<char>(gen);
     test_count<signed char>(gen);
     test_count<unsigned char>(gen);
