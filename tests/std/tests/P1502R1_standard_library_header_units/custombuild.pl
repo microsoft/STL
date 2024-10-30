56709a2a109b9e4bbdbc13f18984d6c387b3ca17
Stephan T. Lavavej <stl@microsoft.com>
2024-10-30 12:24:41 -0700

    Avoid `return` within `try`.

--- tests/std/tests/P1502R1_standard_library_header_units/custombuild.pl
+++ tests/std/tests/P1502R1_standard_library_header_units/custombuild.pl
@@ -20,22 +20,26 @@ sub loadJson
 {
     my $filename = $_[0];
     my $jsonStr = readFile($filename);
+    my $decoded = "";
     try {
-        return JSON::PP->new->utf8->decode($jsonStr);
+        $decoded = JSON::PP->new->utf8->decode($jsonStr);
     } catch {
         Run::PrintError(__FILE__, __LINE__, "Caught exception in loadJson.");
     };
+    return $decoded;
 }
 
 sub loadJsonWithComments
 {
     my $filename = $_[0];
     my $jsonStr = readFile($filename);
+    my $decoded = "";
     try {
-        return JSON::PP->new->relaxed->utf8->decode($jsonStr);
+        $decoded = JSON::PP->new->relaxed->utf8->decode($jsonStr);
     } catch {
         Run::PrintError(__FILE__, __LINE__, "Caught exception in loadJsonWithComments.");
     };
+    return $decoded;
 }
 
 sub getAllHeaders
