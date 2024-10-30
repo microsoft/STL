b34af28da353abd9bb19f51555265ccc26fff2f3
Alex Guteniev <gutenev@gmail.com>
2024-10-30 20:31:08 +0200

    GH-5043 usage

--- benchmarks/src/find_first_of.cpp
+++ benchmarks/src/find_first_of.cpp
@@ -8,66 +8,14 @@
 #include <cstdio>
 #include <cstdlib>
 #include <limits>
-#include <new>
 #include <numeric>
 #include <string>
 #include <type_traits>
 #include <vector>
 
+#include "skewed_allocator.hpp"
+
 using namespace std;
-
-#if 1 // TRANSITION, GH-5043
-
-template <class T, size_t Alignment, size_t Skew>
-struct skewed_allocator {
-    using value_type = T;
-    static_assert(Alignment % alignof(T) == 0, "Chosen Alignment will produce unaligned T objects");
-    static_assert(Skew % alignof(T) == 0, "Chosen Skew will produce unaligned T objects");
-
-    template <class U>
-    struct rebind {
-        using other = skewed_allocator<U, Alignment, Skew>;
-    };
-
-    skewed_allocator() = default;
-    template <class U>
-    skewed_allocator(const skewed_allocator<U, Alignment, Skew>&) {}
-
-    template <class U>
-    bool operator==(const skewed_allocator<U, Alignment, Skew>&) const {
-        return true;
-    }
-
-    T* allocate(const size_t n) {
-        const auto p = static_cast<unsigned char*>(_aligned_malloc(n * sizeof(T) + Skew, Alignment));
-        if (!p) {
-            throw std::bad_alloc{};
-        }
-        return reinterpret_cast<T*>(p + Skew);
-    }
-
-    void deallocate(T* const p, size_t) {
-        if (p) {
-            _aligned_free(reinterpret_cast<unsigned char*>(p) - Skew);
-        }
-    }
-};
-
-// The purpose is to provide consistent behavior for benchmarks.
-// 64 would be a reasonable alignment for practical perf uses,
-// as it is both the cache line size and the maximum vector instruction size (on x64).
-// However, aligning to the page size will provide even more consistency
-// by ensuring that the same number of page boundaries is crossed each time.
-inline constexpr size_t page_size = 4096;
-
-// A realistic skew relative to allocation granularity, when a variable is placed
-// next to a pointer in a structure or on the stack. Also corresponds to the default packing.
-inline constexpr size_t realistic_skew = 8;
-
-template <class T>
-using not_highly_aligned_allocator = skewed_allocator<T, page_size, realistic_skew>;
-
-#endif // ^^^ TRANSITION, GH-5043 ^^^
 
 enum class AlgType { std_func, str_member_first, str_member_last };
 
