1ba7719f54351f7df5bdd7e612927933a4d1e18b
Alex Guteniev <gutenev@gmail.com>
2024-10-31 21:21:41 +0200

    benchmark

--- benchmarks/src/remove.cpp
+++ benchmarks/src/remove.cpp
@@ -7,13 +7,14 @@
 #include <vector>
 
 #include "lorem.hpp"
+#include "skewed_allocator.hpp"
 
 enum class alg_type { std_fn, rng };
 
 template <alg_type Type, class T>
 void r(benchmark::State& state) {
-    const std::vector<T> src(lorem_ipsum.begin(), lorem_ipsum.end());
-    std::vector<T> v;
+    const std::vector<T, not_highly_aligned_allocator<T>> src(lorem_ipsum.begin(), lorem_ipsum.end());
+    std::vector<T, not_highly_aligned_allocator<T>> v;
     v.reserve(lorem_ipsum.size());
     for (auto _ : state) {
         v = src;
@@ -22,6 +23,22 @@ void r(benchmark::State& state) {
             benchmark::DoNotOptimize(std::remove(v.begin(), v.end(), T{'l'}));
         } else {
             benchmark::DoNotOptimize(std::ranges::remove(v, T{'l'}));
+        }
+    }
+}
+
+
+template <alg_type Type, class T>
+void rc(benchmark::State& state) {
+    std::vector<T, not_highly_aligned_allocator<T>> src(lorem_ipsum.begin(), lorem_ipsum.end());
+    std::vector<T, not_highly_aligned_allocator<T>> dst;
+    dst.resize(src.size());
+    for (auto _ : state) {
+        benchmark::DoNotOptimize(src);
+        if constexpr (Type == alg_type::std_fn) {
+            benchmark::DoNotOptimize(std::remove_copy(src.begin(), src.end(), dst.begin(), T{'l'}));
+        } else {
+            benchmark::DoNotOptimize(std::ranges::remove_copy(src, dst.begin(), T{'l'}));
         }
     }
 }
@@ -35,5 +52,15 @@ BENCHMARK(r<alg_type::rng, std::uint8_t>);
 BENCHMARK(r<alg_type::rng, std::uint16_t>);
 BENCHMARK(r<alg_type::rng, std::uint32_t>);
 BENCHMARK(r<alg_type::rng, std::uint64_t>);
+
+BENCHMARK(rc<alg_type::std_fn, std::uint8_t>);
+BENCHMARK(rc<alg_type::std_fn, std::uint16_t>);
+BENCHMARK(rc<alg_type::std_fn, std::uint32_t>);
+BENCHMARK(rc<alg_type::std_fn, std::uint64_t>);
+
+BENCHMARK(rc<alg_type::rng, std::uint8_t>);
+BENCHMARK(rc<alg_type::rng, std::uint16_t>);
+BENCHMARK(rc<alg_type::rng, std::uint32_t>);
+BENCHMARK(rc<alg_type::rng, std::uint64_t>);
 
 BENCHMARK_MAIN();
