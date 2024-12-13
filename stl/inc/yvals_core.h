2378c816cf8d0613d47888509135fcc56820587f
chandankumar <33762417+kumar80@users.noreply.github.com>
2024-12-13 11:40:31 +0530

    Update _MSVC_STL_UPDATE to December 2024 (#5162)

--- stl/inc/yvals_core.h
+++ stl/inc/yvals_core.h
@@ -914,7 +914,7 @@
 
 #define _CPPLIB_VER       650
 #define _MSVC_STL_VERSION 143
-#define _MSVC_STL_UPDATE  202411L
+#define _MSVC_STL_UPDATE  202412L
 
 #ifndef _ALLOW_COMPILER_AND_STL_VERSION_MISMATCH
 #if defined(__CUDACC__) && defined(__CUDACC_VER_MAJOR__)
