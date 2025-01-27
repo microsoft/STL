311d1742c86e0bb52e83688f25272efbf083714b
Stephan T. Lavavej <stl@microsoft.com>
2025-01-27 15:51:32 -0800

    Exclude slide_view, update test coverage.

--- tests/std/tests/P2442R1_views_slide_death/test.cpp
+++ tests/std/tests/P2442R1_views_slide_death/test.cpp
@@ -1,8 +1,6 @@
 // Copyright (c) Microsoft Corporation.
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
-#define _CONTAINER_DEBUG_LEVEL 1
-
 #include <ranges>
 
 #include <test_death.hpp>
@@ -23,10 +21,12 @@ void test_view_zero_window_size() {
 int main(int argc, char* argv[]) {
     std_testing::death_test_executive exec;
 
+#if _ITERATOR_DEBUG_LEVEL != 0
     exec.add_death_tests({
         test_view_negative_window_size,
         test_view_zero_window_size,
     });
+#endif // _ITERATOR_DEBUG_LEVEL != 0
 
     return exec.run(argc, argv);
 }
