a96d50ba56e96351de9258d101630336ba423395
Stephan T. Lavavej <stl@microsoft.com>
2025-01-27 19:48:05 -0800

    Exclude most of `<mdspan>`, update test coverage.
    Need to silence Clang -Wunused-variable warnings in the tests.

--- tests/std/tests/P0009R18_mdspan_mdspan_death/test.cpp
+++ tests/std/tests/P0009R18_mdspan_mdspan_death/test.cpp
@@ -57,16 +57,25 @@ void test_size_when_index_type_is_unsigned() {
 
 int main(int argc, char* argv[]) {
     std_testing::death_test_executive exec;
+
+#if _ITERATOR_DEBUG_LEVEL != 0
     exec.add_death_tests({
-        test_construction_from_other_mdspan,
 #ifdef __cpp_multidimensional_subscript // TRANSITION, P2128R6
-        test_access_with_invalid_multidimensional_index_1,
         test_access_with_nonrepresentable_index_1,
 #endif // __cpp_multidimensional_subscript
-        test_access_with_invalid_multidimensional_index_2,
         test_access_with_nonrepresentable_index_2,
         test_size_when_index_type_is_signed,
         test_size_when_index_type_is_unsigned,
     });
+#endif // _ITERATOR_DEBUG_LEVEL != 0
+
+    exec.add_death_tests({
+        test_construction_from_other_mdspan,
+#ifdef __cpp_multidimensional_subscript // TRANSITION, P2128R6
+        test_access_with_invalid_multidimensional_index_1,
+#endif // __cpp_multidimensional_subscript
+        test_access_with_invalid_multidimensional_index_2,
+    });
+
     return exec.run(argc, argv);
 }
