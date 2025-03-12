eeccc10a5fd8541ee9a34b5cbeb62f8da4986851
Alex Guteniev <gutenev@gmail.com>
2025-03-12 19:42:22 +0200

    LWG-4172

---
+++ tests/std/tests/LWG4172_unique_lock_self_move_assignment/test.cpp
@@ -0,0 +1,101 @@
+// Copyright (c) Microsoft Corporation.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+#include <cassert>
+#include <mutex>
+#include <shared_mutex>
+
+using namespace std;
+
+struct lockable_with_counters {
+    void lock() {
+        ++lock_count;
+    }
+
+    void unlock() {
+        ++unlock_count;
+    }
+
+    void lock_shared() {
+        ++shared_lock_count;
+    }
+
+    void unlock_shared() {
+        ++shared_unlock_count;
+    }
+
+    int lock_count          = 0;
+    int unlock_count        = 0;
+    int shared_lock_count   = 0;
+    int shared_unlock_count = 0;
+};
+
+
+int main() {
+    lockable_with_counters lockable1;
+    lockable_with_counters lockable2;
+
+    {
+        unique_lock<lockable_with_counters> lock_a(lockable1);
+        assert(lockable1.lock_count == 1);
+        assert(lockable1.unlock_count == 0);
+
+        lock_a = move(lock_a);
+        assert(lockable1.lock_count == 1);
+        assert(lockable1.unlock_count == 0);
+        {
+            unique_lock<lockable_with_counters> lock_b(lockable2);
+            assert(lockable2.lock_count == 1);
+            assert(lockable2.unlock_count == 0);
+
+            lock_a = move(lock_b);
+
+            assert(lockable1.lock_count == 1);
+            assert(lockable1.unlock_count == 1);
+            assert(lockable2.lock_count == 1);
+            assert(lockable2.unlock_count == 0);
+        }
+
+        assert(lockable1.lock_count == 1);
+        assert(lockable1.unlock_count == 1);
+        assert(lockable2.lock_count == 1);
+        assert(lockable2.unlock_count == 0);
+    }
+
+    assert(lockable1.lock_count == 1);
+    assert(lockable1.unlock_count == 1);
+    assert(lockable2.lock_count == 1);
+    assert(lockable2.unlock_count == 1);
+
+    {
+        shared_lock<lockable_with_counters> lock_a(lockable1);
+        assert(lockable1.shared_lock_count == 1);
+        assert(lockable1.shared_unlock_count == 0);
+
+        lock_a = move(lock_a);
+        assert(lockable1.shared_lock_count == 1);
+        assert(lockable1.shared_unlock_count == 0);
+        {
+            shared_lock<lockable_with_counters> lock_b(lockable2);
+            assert(lockable2.shared_lock_count == 1);
+            assert(lockable2.shared_unlock_count == 0);
+
+            lock_a = move(lock_b);
+
+            assert(lockable1.shared_lock_count == 1);
+            assert(lockable1.shared_unlock_count == 1);
+            assert(lockable2.shared_lock_count == 1);
+            assert(lockable2.shared_unlock_count == 0);
+        }
+
+        assert(lockable1.shared_lock_count == 1);
+        assert(lockable1.shared_unlock_count == 1);
+        assert(lockable2.shared_lock_count == 1);
+        assert(lockable2.shared_unlock_count == 0);
+    }
+
+    assert(lockable1.shared_lock_count == 1);
+    assert(lockable1.shared_unlock_count == 1);
+    assert(lockable2.shared_lock_count == 1);
+    assert(lockable2.shared_unlock_count == 1);
+}
