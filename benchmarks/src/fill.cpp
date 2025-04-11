ce5616ca509974398df9451d146258dd4b6ca47d
Charlie Barto <chbarto@microsoft.com>
2025-04-10 18:03:09 -0700

    clang-format

--- benchmarks/src/fill.cpp
+++ benchmarks/src/fill.cpp
@@ -1,100 +1,100 @@
 #include <benchmark/benchmark.h>
-#include <vector>
-#include <memory>
 #include <cstring>
+#include <memory>
 #include <ranges>
+#include <vector>
 namespace {
-    template<typename Contained, Contained Value>
+    template <typename Contained, Contained Value>
     void handwritten_loop(benchmark::State& state) {
         const size_t r0 = static_cast<size_t>(state.range(0));
         std::unique_ptr<Contained[]> buffer(new Contained[r0]);
-        for([[maybe_unused]] auto _ : state) {
+        for ([[maybe_unused]] auto _ : state) {
             benchmark::DoNotOptimize(buffer.get());
-            Contained* ptr = buffer.get();
+            Contained* ptr                 = buffer.get();
             const Contained* const ptr_end = ptr + r0;
-            while(ptr != ptr_end) {
+            while (ptr != ptr_end) {
                 *ptr++ = Value;
             }
             benchmark::DoNotOptimize(buffer.get());
         }
     }
 
-    template<typename Contained, Contained Value, size_t Size>
+    template <typename Contained, Contained Value, size_t Size>
     void handwritten_loop_const_size(benchmark::State& state) {
         std::unique_ptr<Contained[]> buffer(new Contained[Size]);
-        for([[maybe_unused]] auto _ : state) {
+        for ([[maybe_unused]] auto _ : state) {
             benchmark::DoNotOptimize(buffer.get());
-            Contained* ptr = buffer.get();
+            Contained* ptr                 = buffer.get();
             const Contained* const ptr_end = ptr + Size;
-            while(ptr != ptr_end) {
+            while (ptr != ptr_end) {
                 *ptr++ = Value;
             }
             benchmark::DoNotOptimize(buffer.get());
         }
     }
 
-    template<typename Contained, Contained Value>
+    template <typename Contained, Contained Value>
     void handwritten_loop_n(benchmark::State& state) {
         const size_t r0 = static_cast<size_t>(state.range(0));
         std::unique_ptr<Contained[]> buffer(new Contained[r0]);
-        for([[maybe_unused]] auto _ : state) {
+        for ([[maybe_unused]] auto _ : state) {
             benchmark::DoNotOptimize(buffer.get());
             Contained* ptr = buffer.get();
-            for(size_t idx = 0; idx < r0; ++idx) {
+            for (size_t idx = 0; idx < r0; ++idx) {
                 ptr[idx] = Value;
             }
             benchmark::DoNotOptimize(buffer.get());
         }
     }
-    template<typename Contained, Contained Value, size_t Size>
+    template <typename Contained, Contained Value, size_t Size>
     void handwritten_loop_n_const_size(benchmark::State& state) {
         std::unique_ptr<Contained[]> buffer(new Contained[Size]);
-        for([[maybe_unused]] auto _ : state) {
+        for ([[maybe_unused]] auto _ : state) {
             benchmark::DoNotOptimize(buffer.get());
             Contained* ptr = buffer.get();
-            for(size_t idx = 0; idx < Size; ++idx) {
+            for (size_t idx = 0; idx < Size; ++idx) {
                 ptr[idx] = Value;
             }
             benchmark::DoNotOptimize(buffer.get());
         }
     }
 
-    template<typename Contained, Contained Value>
+    template <typename Contained, Contained Value>
     void memset_call(benchmark::State& state) {
         const size_t r0 = static_cast<size_t>(state.range(0));
         std::unique_ptr<Contained[]> buffer(new Contained[r0]);
-        for([[maybe_unused]] auto _ : state) {
+        for ([[maybe_unused]] auto _ : state) {
             benchmark::DoNotOptimize(buffer.get());
             Contained* ptr = buffer.get();
             std::memset(ptr, Value, r0);
             benchmark::DoNotOptimize(buffer.get());
         }
     }
-    template<typename Contained, Contained Value>
+    template <typename Contained, Contained Value>
     void std_fill_call(benchmark::State& state) {
         const size_t r0 = static_cast<size_t>(state.range(0));
         std::unique_ptr<Contained[]> buffer(new Contained[r0]);
-        for([[maybe_unused]] auto _ : state) {
+        for ([[maybe_unused]] auto _ : state) {
             benchmark::DoNotOptimize(buffer.get());
             auto begin_it = buffer.get();
-            auto end_it = buffer.get() + r0;
+            auto end_it   = buffer.get() + r0;
             std::fill(begin_it, end_it, Value);
             benchmark::DoNotOptimize(buffer.get());
         }
     }
 
-    template<typename Contained, Contained Value>
+    template <typename Contained, Contained Value>
     void std_fill_n_call(benchmark::State& state) {
         const size_t r0 = static_cast<size_t>(state.range(0));
         std::unique_ptr<Contained[]> buffer(new Contained[r0]);
-        for([[maybe_unused]] auto _ : state) {
+        for ([[maybe_unused]] auto _ : state) {
             benchmark::DoNotOptimize(buffer.get());
             auto begin_it = buffer.get();
             std::fill_n(begin_it, r0, Value);
             benchmark::DoNotOptimize(buffer.get());
         }
     }
-}
+} // namespace
 
 BENCHMARK(handwritten_loop<char, 0>)->Range(0, 1 << 18);
 BENCHMARK(handwritten_loop<char, 1>)->Range(0, 1 << 18);
