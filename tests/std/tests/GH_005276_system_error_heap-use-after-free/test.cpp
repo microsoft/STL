2c9bee265596bb29092e833a1d326c3dc71b4e41
wolframw <5099115+wolframw@users.noreply.github.com>
2025-04-13 19:24:17 +0200

    fix heap-use-after-free for _HAS_EXCEPTIONS=0

---
+++ tests/std/tests/GH_005276_system_error_heap-use-after-free/test.cpp
@@ -0,0 +1,21 @@
+// Copyright (c) Microsoft Corporation.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+#include <chrono>
+#include <cstdio>
+#include <string>
+#include <system_error>
+using namespace std;
+using namespace chrono;
+
+int main() {
+    string str{"abc"};
+    error_code ec{2, system_category()};
+    system_error syserr{ec};
+
+    ambiguous_local_time alt{local_time<seconds>{}, local_info{}};
+
+    nonexistent_local_time nlt{local_time<seconds>{}, local_info{}};
+
+    printf("%s\n%s\n%s\n", syserr.what(), alt.what(), nlt.what());
+}
