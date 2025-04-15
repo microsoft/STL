c7fd832ef7ae3a93bb4c7c15115fbb1dff89f7e4
Stephan T. Lavavej <stl@microsoft.com>
2025-04-15 16:08:07 -0700

    Remove squirrels, test wchar_t as an ordinary user (real or fake).

--- tests/std/tests/GH_005236_collate_facet/test.cpp
+++ tests/std/tests/GH_005236_collate_facet/test.cpp
@@ -1,13 +1,6 @@
 // Copyright (c) Microsoft Corporation.
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
-// ensure collate<unsigned short> is valid even if wchar_t is a native type
-#ifdef _ENFORCE_FACET_SPECIALIZATIONS
-#undef _ENFORCE_FACET_SPECIALIZATIONS
-#endif
-
-#define _ENFORCE_FACET_SPECIALIZATIONS 0
-
 #include <cassert>
 #include <locale>
 #include <string>
@@ -26,20 +19,15 @@
 
 using namespace std;
 
-// circumvent error C2491 on collate<unsigned short>::id in native wchar_t build
-#if defined(_DLL_CPPLIB) && defined(_NATIVE_WCHAR_T_DEFINED)
-template __PURE_APPDOMAIN_GLOBAL locale::id collate<unsigned short>::id;
-#endif // defined(_DLL_CPPLIB) && defined(_NATIVE_WCHAR_T_DEFINED)
-
 // GH-5236 "std::collate<wchar_t> does not respect collation order when compiled with /MD(d) /Zc:wchar_t-"
 void test_gh_5236() {
-    const unsigned short Ue = L'\u00DC'; // U+00DC LATIN CAPITAL LETTER U WITH DIARESIS
-    const unsigned short U  = L'U';
-    const unsigned short V  = L'V';
+    const wchar_t Ue = L'\u00DC'; // U+00DC LATIN CAPITAL LETTER U WITH DIARESIS
+    const wchar_t U  = L'U';
+    const wchar_t V  = L'V';
 
     // German phonebook order: "U+00DC" is sorted between U and V in collation order
     locale loc("de-DE_phoneb");
-    auto& coll = use_facet<collate<unsigned short>>(loc);
+    auto& coll = use_facet<collate<wchar_t>>(loc);
 
     assert(coll.compare(&U, &U + 1, &Ue, &Ue + 1) < 0);
     assert(coll.compare(&V, &V + 1, &Ue, &Ue + 1) > 0);
