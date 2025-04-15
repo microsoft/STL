0caea0385ca3a73aa39450580f7f8612b55eb67b
Stephan T. Lavavej <stl@microsoft.com>
2025-04-14 22:59:46 -0700

    Drop unnamed namespace.

--- benchmarks/src/fill.cpp
+++ benchmarks/src/fill.cpp
@@ -8,98 +8,96 @@
 
 #include <skewed_allocator.hpp>
 
-namespace {
-    template <typename Contained, Contained Value>
-    void handwritten_loop(benchmark::State& state) {
-        const size_t r0 = static_cast<size_t>(state.range(0));
-        std::vector<Contained, highly_aligned_allocator<Contained>> buffer(r0);
-        for ([[maybe_unused]] auto _ : state) {
-            benchmark::DoNotOptimize(buffer.data());
-            Contained* ptr                 = buffer.data();
-            const Contained* const ptr_end = ptr + r0;
-            while (ptr != ptr_end) {
-                *ptr++ = Value;
-            }
-            benchmark::DoNotOptimize(buffer.data());
+template <typename Contained, Contained Value>
+void handwritten_loop(benchmark::State& state) {
+    const size_t r0 = static_cast<size_t>(state.range(0));
+    std::vector<Contained, highly_aligned_allocator<Contained>> buffer(r0);
+    for ([[maybe_unused]] auto _ : state) {
+        benchmark::DoNotOptimize(buffer.data());
+        Contained* ptr                 = buffer.data();
+        const Contained* const ptr_end = ptr + r0;
+        while (ptr != ptr_end) {
+            *ptr++ = Value;
         }
+        benchmark::DoNotOptimize(buffer.data());
     }
+}
 
-    template <typename Contained, Contained Value, size_t Size>
-    void handwritten_loop_const_size(benchmark::State& state) {
-        std::vector<Contained, not_highly_aligned_allocator<Contained>> buffer(Size);
-        for ([[maybe_unused]] auto _ : state) {
-            benchmark::DoNotOptimize(buffer.data());
-            Contained* ptr                 = buffer.data();
-            const Contained* const ptr_end = ptr + Size;
-            while (ptr != ptr_end) {
-                *ptr++ = Value;
-            }
-            benchmark::DoNotOptimize(buffer.data());
+template <typename Contained, Contained Value, size_t Size>
+void handwritten_loop_const_size(benchmark::State& state) {
+    std::vector<Contained, not_highly_aligned_allocator<Contained>> buffer(Size);
+    for ([[maybe_unused]] auto _ : state) {
+        benchmark::DoNotOptimize(buffer.data());
+        Contained* ptr                 = buffer.data();
+        const Contained* const ptr_end = ptr + Size;
+        while (ptr != ptr_end) {
+            *ptr++ = Value;
         }
+        benchmark::DoNotOptimize(buffer.data());
     }
+}
 
-    template <typename Contained, Contained Value>
-    void handwritten_loop_n(benchmark::State& state) {
-        const size_t r0 = static_cast<size_t>(state.range(0));
-        std::vector<Contained, highly_aligned_allocator<Contained>> buffer(r0);
-        for ([[maybe_unused]] auto _ : state) {
-            benchmark::DoNotOptimize(buffer.data());
-            Contained* ptr = buffer.data();
-            for (size_t idx = 0; idx < r0; ++idx) {
-                ptr[idx] = Value;
-            }
-            benchmark::DoNotOptimize(buffer.data());
+template <typename Contained, Contained Value>
+void handwritten_loop_n(benchmark::State& state) {
+    const size_t r0 = static_cast<size_t>(state.range(0));
+    std::vector<Contained, highly_aligned_allocator<Contained>> buffer(r0);
+    for ([[maybe_unused]] auto _ : state) {
+        benchmark::DoNotOptimize(buffer.data());
+        Contained* ptr = buffer.data();
+        for (size_t idx = 0; idx < r0; ++idx) {
+            ptr[idx] = Value;
         }
+        benchmark::DoNotOptimize(buffer.data());
     }
-    template <typename Contained, Contained Value, size_t Size>
-    void handwritten_loop_n_const_size(benchmark::State& state) {
-        std::vector<Contained, not_highly_aligned_allocator<Contained>> buffer(Size);
-        for ([[maybe_unused]] auto _ : state) {
-            benchmark::DoNotOptimize(buffer.data());
-            Contained* ptr = buffer.data();
-            for (size_t idx = 0; idx < Size; ++idx) {
-                ptr[idx] = Value;
-            }
-            benchmark::DoNotOptimize(buffer.data());
+}
+template <typename Contained, Contained Value, size_t Size>
+void handwritten_loop_n_const_size(benchmark::State& state) {
+    std::vector<Contained, not_highly_aligned_allocator<Contained>> buffer(Size);
+    for ([[maybe_unused]] auto _ : state) {
+        benchmark::DoNotOptimize(buffer.data());
+        Contained* ptr = buffer.data();
+        for (size_t idx = 0; idx < Size; ++idx) {
+            ptr[idx] = Value;
         }
+        benchmark::DoNotOptimize(buffer.data());
     }
+}
 
-    template <typename Contained, Contained Value>
-    void memset_call(benchmark::State& state) {
-        const size_t r0 = static_cast<size_t>(state.range(0));
-        std::vector<Contained, highly_aligned_allocator<Contained>> buffer(r0);
-        for ([[maybe_unused]] auto _ : state) {
-            benchmark::DoNotOptimize(buffer.data());
-            Contained* ptr = buffer.data();
-            std::memset(ptr, Value, r0);
-            benchmark::DoNotOptimize(buffer.data());
-        }
+template <typename Contained, Contained Value>
+void memset_call(benchmark::State& state) {
+    const size_t r0 = static_cast<size_t>(state.range(0));
+    std::vector<Contained, highly_aligned_allocator<Contained>> buffer(r0);
+    for ([[maybe_unused]] auto _ : state) {
+        benchmark::DoNotOptimize(buffer.data());
+        Contained* ptr = buffer.data();
+        std::memset(ptr, Value, r0);
+        benchmark::DoNotOptimize(buffer.data());
     }
-    template <typename Contained, Contained Value>
-    void std_fill_call(benchmark::State& state) {
-        const size_t r0 = static_cast<size_t>(state.range(0));
-        std::vector<Contained, highly_aligned_allocator<Contained>> buffer(r0);
-        for ([[maybe_unused]] auto _ : state) {
-            benchmark::DoNotOptimize(buffer.data());
-            auto begin_it = buffer.data();
-            auto end_it   = buffer.data() + r0;
-            std::fill(begin_it, end_it, Value);
-            benchmark::DoNotOptimize(buffer.data());
-        }
+}
+template <typename Contained, Contained Value>
+void std_fill_call(benchmark::State& state) {
+    const size_t r0 = static_cast<size_t>(state.range(0));
+    std::vector<Contained, highly_aligned_allocator<Contained>> buffer(r0);
+    for ([[maybe_unused]] auto _ : state) {
+        benchmark::DoNotOptimize(buffer.data());
+        auto begin_it = buffer.data();
+        auto end_it   = buffer.data() + r0;
+        std::fill(begin_it, end_it, Value);
+        benchmark::DoNotOptimize(buffer.data());
     }
+}
 
-    template <typename Contained, Contained Value>
-    void std_fill_n_call(benchmark::State& state) {
-        const size_t r0 = static_cast<size_t>(state.range(0));
-        std::vector<Contained, not_highly_aligned_allocator<Contained>> buffer(r0);
-        for ([[maybe_unused]] auto _ : state) {
-            benchmark::DoNotOptimize(buffer.data());
-            auto begin_it = buffer.data();
-            std::fill_n(begin_it, r0, Value);
-            benchmark::DoNotOptimize(buffer.data());
-        }
+template <typename Contained, Contained Value>
+void std_fill_n_call(benchmark::State& state) {
+    const size_t r0 = static_cast<size_t>(state.range(0));
+    std::vector<Contained, not_highly_aligned_allocator<Contained>> buffer(r0);
+    for ([[maybe_unused]] auto _ : state) {
+        benchmark::DoNotOptimize(buffer.data());
+        auto begin_it = buffer.data();
+        std::fill_n(begin_it, r0, Value);
+        benchmark::DoNotOptimize(buffer.data());
     }
-} // namespace
+}
 
 BENCHMARK(handwritten_loop<char, 0>)->Range(0, 1 << 18);
 BENCHMARK(handwritten_loop<char, 1>)->Range(0, 1 << 18);
