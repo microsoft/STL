b984ff72559a2ea08a084d4e025d58d4fe48dc61
Stephan T. Lavavej <stl@microsoft.com>
2025-04-15 23:42:46 -0700

    printf => assert.

--- tests/std/tests/GH_005276_system_error_heap_use_after_free/test.cpp
+++ tests/std/tests/GH_005276_system_error_heap_use_after_free/test.cpp
@@ -1,9 +1,10 @@
 // Copyright (c) Microsoft Corporation.
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
+#include <cassert>
 #include <chrono>
-#include <cstdio>
 #include <string>
+#include <string_view>
 #include <system_error>
 using namespace std;
 using namespace chrono;
@@ -12,10 +13,11 @@ int main() {
     string str{"abc"};
     error_code ec{2, system_category()};
     system_error syserr{ec};
+    assert(syserr.what() == "system error"sv);
 
     ambiguous_local_time alt{local_seconds{}, local_info{}};
+    assert(alt.what() == "ambiguous local time"sv);
 
     nonexistent_local_time nlt{local_seconds{}, local_info{}};
-
-    printf("%s\n%s\n%s\n", syserr.what(), alt.what(), nlt.what());
+    assert(nlt.what() == "nonexistent local time"sv);
 }
