0f60ba75ab9df568a484afb98e109d21245983b9
Stephan T. Lavavej <stl@microsoft.com>
2025-04-20 14:43:48 -0700

    Add newlines.

--- stl/src/vector_algorithms.cpp
+++ stl/src/vector_algorithms.cpp
@@ -3103,18 +3103,23 @@ namespace {
                     _MskX = (_MskX >> 1) & _MskX;
                     _MskX = __ull_rshift(_MskX, _Sh1) & _MskX;
                 }
+
                 if constexpr (sizeof(_Ty) == 2) {
                     _MskX = (_MskX >> 2) & _MskX;
                 }
+
                 if constexpr (sizeof(_Ty) < 4) {
                     _MskX = __ull_rshift(_MskX, _Sh2) & _MskX;
                 }
+
                 if constexpr (sizeof(_Ty) == 4) {
                     _MskX = (_MskX >> 4) & _MskX;
                 }
+
                 if constexpr (sizeof(_Ty) < 8) {
                     _MskX = __ull_rshift(_MskX, _Sh3) & _MskX;
                 }
+
                 if constexpr (sizeof(_Ty) == 8) {
                     _MskX = (_MskX >> 8) & _MskX;
                 }
