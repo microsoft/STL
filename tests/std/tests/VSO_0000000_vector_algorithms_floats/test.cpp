896ced293a28393896f8c0ff99a30ed0bc9069e7
Alex Guteniev <gutenev@gmail.com>
2025-04-22 16:24:00 +0300

    includes

--- tests/std/tests/VSO_0000000_vector_algorithms_floats/test.cpp
+++ tests/std/tests/VSO_0000000_vector_algorithms_floats/test.cpp
@@ -1,7 +1,9 @@
 // Copyright (c) Microsoft Corporation.
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
+#include <algorithm>
 #include <cstddef>
+#include <functional>
 #include <limits>
 #include <random>
 #include <vector>
