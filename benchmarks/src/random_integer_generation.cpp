720b30541eedaac25f46c43b4483406eb107335b
mirounga <rosnytsa@microsoft.com>
2025-04-22 15:59:50 -0700

    `<random>`: Fast `discard()` for `minstd_rand` and `minstd_rand0` via fast exponentiation (#5412)
    Co-authored-by: Stephan T. Lavavej <stl@microsoft.com>

--- benchmarks/src/random_integer_generation.cpp
+++ benchmarks/src/random_integer_generation.cpp
@@ -31,6 +31,23 @@ void BM_lcg(benchmark::State& state) {
 }
 BENCHMARK(BM_lcg);
 
+/// Test discard()
+
+template <class Engine>
+void BM_discard(benchmark::State& state) {
+    Engine gen;
+    const auto n = static_cast<unsigned long long>(state.range(0));
+    for (auto _ : state) {
+        gen.discard(n);
+        benchmark::DoNotOptimize(gen());
+    }
+}
+BENCHMARK(BM_discard<std::mt19937>)->Range(0, 1 << 18);
+BENCHMARK(BM_discard<std::mt19937_64>)->Range(0, 1 << 18);
+BENCHMARK(BM_discard<std::minstd_rand>)->Range(0, 1 << 18);
+
+/// Support machinery for testing _Rng_from_urng and _Rng_from_urng_v2
+
 std::uint32_t GetMax() {
     std::mt19937 gen;
     std::uniform_int_distribution<std::uint32_t> dist(10'000'000, 20'000'000);
