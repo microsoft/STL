ddf0b193be4aa6f05905ebe478dfa0976373a1e6
Stephan T. Lavavej <stl@nuwen.net>
2025-04-23 15:35:16 -0700

    Revert "Temporarily restore support for Win7 / Server 2008 R2 (#4857)"
    This reverts commit d2fa3a3cf02f09e2396a65c076be8be4c26352e9.

--- stl/src/winapisupp.cpp
+++ stl/src/winapisupp.cpp
@@ -14,10 +14,6 @@ namespace {
 // Use this macro for defining the following function pointers
 #define DEFINEFUNCTIONPOINTER(fn_name) decltype(&fn_name) __KERNEL32Function_##fn_name = nullptr
 
-#if _STL_WIN32_WINNT < _WIN32_WINNT_WIN8
-    DEFINEFUNCTIONPOINTER(GetSystemTimePreciseAsFileTime);
-#endif // _STL_WIN32_WINNT < _WIN32_WINNT_WIN8
-
     DEFINEFUNCTIONPOINTER(GetTempPath2W);
 
 // Use this macro for caching a function pointer from a DLL
@@ -158,15 +154,9 @@ extern "C" _CRTIMP2 BOOL __cdecl __crtSetFileInformationByHandle(_In_ HANDLE con
 
 #if _STL_WIN32_WINNT < _WIN32_WINNT_WIN8
 
+// TRANSITION, ABI: preserved for binary compatibility
 extern "C" _CRTIMP2 void __cdecl __crtGetSystemTimePreciseAsFileTime(_Out_ LPFILETIME lpSystemTimeAsFileTime) noexcept {
-    // use GetSystemTimePreciseAsFileTime if it is available (only on Windows 8+)...
-    IFDYNAMICGETCACHEDFUNCTION(GetSystemTimePreciseAsFileTime) {
-        pfGetSystemTimePreciseAsFileTime(lpSystemTimeAsFileTime);
-        return;
-    }
-
-    // ...otherwise use GetSystemTimeAsFileTime.
-    GetSystemTimeAsFileTime(lpSystemTimeAsFileTime);
+    GetSystemTimePreciseAsFileTime(lpSystemTimeAsFileTime);
 }
 
 #endif // _STL_WIN32_WINNT < _WIN32_WINNT_WIN8
@@ -195,10 +185,6 @@ extern "C" _Success_(return > 0 && return < BufferLength) DWORD __stdcall __crtG
 static int __cdecl initialize_pointers() noexcept {
     HINSTANCE hKernel32 = GetModuleHandleW(L"kernel32.dll");
     _Analysis_assume_(hKernel32);
-
-#if _STL_WIN32_WINNT < _WIN32_WINNT_WIN8
-    STOREFUNCTIONPOINTER(hKernel32, GetSystemTimePreciseAsFileTime);
-#endif // _STL_WIN32_WINNT < _WIN32_WINNT_WIN8
 
     // Note that GetTempPath2W is defined as of Windows 10 Build 20348 (a server release) or Windows 11,
     // but there is no "_WIN32_WINNT_WIN11" constant, so we will always dynamically load it
