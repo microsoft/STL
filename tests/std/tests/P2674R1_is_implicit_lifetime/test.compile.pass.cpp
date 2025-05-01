f2fd5370a2f16e90ca339f506e87e1baa9a3c328
TymianekPL <tymi@tymi.org>
2025-05-01 16:41:51 +0200

    Remove explicit std resolution path
    Signed-off-by: TymianekPL <tymi@tymi.org>

--- tests/std/tests/P2674R1_is_implicit_lifetime/test.compile.pass.cpp
+++ tests/std/tests/P2674R1_is_implicit_lifetime/test.compile.pass.cpp
@@ -18,31 +18,31 @@ int main()
 {
 #ifdef __cpp_lib_is_implicit_lifetime 
 	// this is a test to test whether
-	// std::is_implicit_lifetime_v produces desired results
+	// is_implicit_lifetime_v produces desired results
 	// compiles under std namespace
 
 	// Basics
-	static_assert(std::is_implcit_lifetime_v<int>);
-	static_assert(std::is_implcit_lifetime_v<int*>);
-	static_assert(std::is_implcit_lifetime_v<std::nullptr_t>);
-	static_assert(std::is_implcit_lifetime_v<UnscopedEnum>);
-	static_assert(std::is_implcit_lifetime_v<ScopedEnum>);
-	static_assert(std::is_implcit_lifetime_v<void(*)()>);
-	static_assert(std::is_implcit_lifetime_v<Class>);
-	static_assert(!std::is_implcit_lifetime_v<UserProvidedDestructorClass>);
-	static_assert(!std::is_implcit_lifetime_v<IncompleteClass[]>);
-	static_assert(!std::is_implcit_lifetime_v<IncompleteClass[20]>);
+	static_assert(is_implcit_lifetime_v<int>);
+	static_assert(is_implcit_lifetime_v<int*>);
+	static_assert(is_implcit_lifetime_v<nullptr_t>);
+	static_assert(is_implcit_lifetime_v<UnscopedEnum>);
+	static_assert(is_implcit_lifetime_v<ScopedEnum>);
+	static_assert(is_implcit_lifetime_v<void(*)()>);
+	static_assert(is_implcit_lifetime_v<Class>);
+	static_assert(!is_implcit_lifetime_v<UserProvidedDestructorClass>);
+	static_assert(!is_implcit_lifetime_v<IncompleteClass[]>);
+	static_assert(!is_implcit_lifetime_v<IncompleteClass[20]>);
 
 	// Arrays
-	static_assert(std::is_implcit_lifetime_v<int[]>);
-	static_assert(std::is_implcit_lifetime_v<int[2]>);
-	static_assert(std::is_implcit_lifetime_v<Class[10]>);
-	static_assert(!std::is_implcit_lifetime_v<UserProvidedDestructorClass[12]>);
+	static_assert(is_implcit_lifetime_v<int[]>);
+	static_assert(is_implcit_lifetime_v<int[2]>);
+	static_assert(is_implcit_lifetime_v<Class[10]>);
+	static_assert(!is_implcit_lifetime_v<UserProvidedDestructorClass[12]>);
 
-	static_assert(!std::is_implcit_lifetime_v<void>);
-	static_assert(!std::is_implcit_lifetime_v<const volatile void>);
-	static_assert(!std::is_implcit_lifetime_v<long&>);
-	static_assert(!std::is_implcit_lifetime_v<long&&>);
+	static_assert(!is_implcit_lifetime_v<void>);
+	static_assert(!is_implcit_lifetime_v<const volatile void>);
+	static_assert(!is_implcit_lifetime_v<long&>);
+	static_assert(!is_implcit_lifetime_v<long&&>);
 #endif
 }
 
