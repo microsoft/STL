dad053ab05773a81fbf0b1f69cf0acc7d1343a3a
Stephan T. Lavavej <stl@microsoft.com>
2025-05-08 02:15:55 -0700

    Drop comment about the size of `Mask`; `popcount` handles this.

--- stl/src/cthread.cpp
+++ stl/src/cthread.cpp
@@ -123,7 +123,6 @@ _CRTIMP2_PURE unsigned int __cdecl _Thrd_hardware_concurrency() noexcept { // re
 
         if (GetLogicalProcessorInformationEx(RelationProcessorPackage, buffer_ptr, buffer_size)) {
             for (WORD i = 0; i != buffer_ptr->Processor.GroupCount; ++i) {
-                // Mask is 8 bytes on ARM64 and X64, and 4 bytes on X86 and ARM32
                 count += _STD popcount(buffer_ptr->Processor.GroupMask[i].Mask);
             }
         }
