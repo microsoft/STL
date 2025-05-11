573785e6e2e2947000dc1779110f42500ff77565
Alex Guteniev <gutenev@gmail.com>
2025-05-11 22:28:44 +0300

    remove the check from copycats

--- tests/std/tests/GH_005472_do_not_overlap/test.cpp
+++ tests/std/tests/GH_005472_do_not_overlap/test.cpp
@@ -15,23 +15,15 @@ int main(int argc, char* argv[]) {
 
     exec.add_death_tests({
         +[] { swap_ranges(arr, arr + 2, arr + 1); },
-        +[] { copy_if(arr, arr + 2, arr + 1, [](auto) { return false; }); },
         +[] { reverse_copy(arr, arr + 2, arr + 1); },
         +[] { replace_copy(arr, arr + 2, arr + 1, 1, 2); },
         +[] { replace_copy_if(arr, arr + 2, arr + 1, [](auto) { return false; }, 2); },
         +[] { rotate_copy(arr, arr + 1, arr + 2, arr + 1); },
-        +[] { remove_copy(arr, arr + 2, arr + 1, 1); },
-        +[] { remove_copy_if(arr, arr + 2, arr + 1, [](auto) { return false; }); },
-        +[] { unique_copy(arr, arr + 2, arr + 1); },
 #if _HAS_CXX20
-        +[] { ranges::copy_if(arr, arr + 2, arr + 1, [](auto) { return false; }); },
         +[] { ranges::reverse_copy(arr, arr + 2, arr + 1); },
         +[] { ranges::replace_copy(arr, arr + 2, arr + 1, 1, 2); },
         +[] { ranges::replace_copy_if(arr, arr + 2, arr + 1, [](auto) { return false; }, 2); },
         +[] { ranges::rotate_copy(arr, arr + 1, arr + 2, arr + 1); },
-        +[] { ranges::remove_copy(arr, arr + 2, arr + 1, 1); },
-        +[] { ranges::remove_copy_if(arr, arr + 2, arr + 1, [](auto) { return false; }); },
-        +[] { ranges::unique_copy(arr, arr + 2, arr + 1); },
 #endif // _HAS_CXX20
     });
 
