082d80ae53925cad65e5f0b6a3ca00fd786fb96d
Stephan T. Lavavej <stl@microsoft.com>
2025-07-29 15:42:56 -0700

    Update WinSDK comments (#5659)

--- tests/utils/stl/test/tests.py
+++ tests/utils/stl/test/tests.py
@@ -258,7 +258,7 @@ class STLTest(Test):
                 self.compileFlags.append('/arm64EC')
                 self.linkFlags.append('/machine:arm64ec')
 
-                # TRANSITION, Windows SDK 10.0.26100.3916 emits
+                # TRANSITION, Windows SDK 10.0.26100.4188 emits
                 # "warning C28301: No annotations for first declaration of 'meow'"
                 # for various intrinsics when building for ARM64EC.
                 self.compileFlags.append('/wd28301')
