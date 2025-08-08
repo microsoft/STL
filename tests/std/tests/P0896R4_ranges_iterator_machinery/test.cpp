9ed9371aa15a6a8c48b0400fe11a915a803a5c28
A. Jiang <de34@live.cn>
2025-08-09 01:11:28 +0800

    Implement LWG-4242 `ranges::distance` does not work with volatile iterators (#5603)

--- tests/std/tests/P0896R4_ranges_iterator_machinery/test.cpp
+++ tests/std/tests/P0896R4_ranges_iterator_machinery/test.cpp
@@ -2095,6 +2095,10 @@ struct std::common_type<std::default_sentinel_t, ::iter_ops::trace_iterator<Cate
 namespace iter_ops {
     using ranges::advance, ranges::distance, ranges::next, ranges::prev;
 
+    template <class I, class S>
+    concept can_call_ranges_difference =
+        requires(I&& it, S&& se) { distance(std::forward<I>(it), std::forward<S>(se)); };
+
     constexpr bool test_iter_forms() {
         {
             // Call next(i), validating that ++i is called once
@@ -3086,7 +3090,6 @@ namespace iter_ops {
         }
 
         {
-#ifndef __EDG__ // TRANSITION, VSO-1898890
             // Call distance(i, s) with arrays which must be decayed to pointers.
             // (This behavior was regressed by LWG-3392.)
             int some_ints[] = {1, 2, 3};
@@ -3104,7 +3107,23 @@ namespace iter_ops {
             static_assert(noexcept(distance(const_ints + 1, const_ints)));
             assert(distance(const_ints, const_ints) == 0);
             static_assert(noexcept(distance(const_ints, const_ints)));
-#endif // ^^^ no workaround ^^^
+        }
+
+        { // Test LWG-4242 "ranges::distance does not work with volatile iterators"
+            static_assert(can_call_ranges_difference<int* volatile, int[3]>);
+            static_assert(can_call_ranges_difference<int* volatile&, int[3]>);
+
+            // Per LWG-4303, ranges::distance should be well-constrained for non-pointer volatile iterators.
+            static_assert(
+                !can_call_ranges_difference<volatile std::reverse_iterator<int*>, std::reverse_iterator<int*>>);
+            static_assert(
+                !can_call_ranges_difference<volatile std::reverse_iterator<int*>&, std::reverse_iterator<int*>>);
+
+            if (!std::is_constant_evaluated()) {
+                int arr[]{1, 2, 3};
+                int* volatile ptr = arr;
+                assert(distance(ptr, arr + 3) == 3);
+            }
         }
 
         return true;
