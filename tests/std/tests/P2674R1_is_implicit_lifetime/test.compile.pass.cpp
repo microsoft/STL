51f5433c7175a2974e7c7886431b4dec24c8d9fa
Tymianek <tymi@tymi.org>
2025-09-16 07:18:16 +0200

    [is_implicit_lifetime/test] Fix typos, remove main, rename classes, fix checks

--- tests/std/tests/P2674R1_is_implicit_lifetime/test.compile.pass.cpp
+++ tests/std/tests/P2674R1_is_implicit_lifetime/test.compile.pass.cpp
@@ -5,45 +5,43 @@
 
 enum UnscopedEnum { Enumerator };
 enum struct ScopedEnum { Enumerator };
-class Class {};
 class IncompleteClass;
-class UserProvidedDestructorClass
+struct TrivialClass {};
+struct AggregateClassWithUserProvidedDestructor
 {
-	~UserProvidedDestructorClass() {}
+	~AggregateClassWithUserProvidedDestructor() {}
 };
 
 using namespace std;
 
-int main()
-{
 #ifdef __cpp_lib_is_implicit_lifetime 
-	// this is a test to test whether
-	// is_implicit_lifetime_v produces desired results
-	// compiles under std namespace
+// this is a test to test whether
+// is_implicit_lifetime_v produces desired results
+// compiles under std namespace
 
-	// Basics
-	static_assert(is_implcit_lifetime_v<int>);
-	static_assert(is_implcit_lifetime_v<int*>);
-	static_assert(is_implcit_lifetime_v<nullptr_t>);
-	static_assert(is_implcit_lifetime_v<UnscopedEnum>);
-	static_assert(is_implcit_lifetime_v<ScopedEnum>);
-	static_assert(is_implcit_lifetime_v<void(*)()>);
-	static_assert(is_implcit_lifetime_v<Class>);
-	static_assert(!is_implcit_lifetime_v<UserProvidedDestructorClass>);
-	static_assert(!is_implcit_lifetime_v<IncompleteClass[]>);
-	static_assert(!is_implcit_lifetime_v<IncompleteClass[20]>);
+template <typename T>
+using test_implicit_lifetime = is_implicit_lifetime_v<T> && is_implicit_lifetime<T>::value;
 
-	// Arrays
-	static_assert(is_implcit_lifetime_v<int[]>);
-	static_assert(is_implcit_lifetime_v<int[2]>);
-	static_assert(is_implcit_lifetime_v<Class[10]>);
-	static_assert(!is_implcit_lifetime_v<UserProvidedDestructorClass[12]>);
+// Basics (arrays thereof included in Arrays section)
+static_assert(test_implicit_lifetime<int>);
+static_assert(test_implicit_lifetime<int*>);
+static_assert(test_implicit_lifetime<nullptr_t>);
+static_assert(test_implicit_lifetime<UnscopedEnum>);
+static_assert(test_implicit_lifetime<ScopedEnum>);
+static_assert(test_implicit_lifetime<void(*)()>);
+static_assert(test_implicit_lifetime<TrivialClass>);
+static_assert(!test_implicit_lifetime<UserProvidedDestructorClass>);
 
-	static_assert(!is_implcit_lifetime_v<void>);
-	static_assert(!is_implcit_lifetime_v<const volatile void>);
-	static_assert(!is_implcit_lifetime_v<long&>);
-	static_assert(!is_implcit_lifetime_v<long&&>);
+// Arrays
+static_assert(test_implicit_lifetime<int[]>);
+static_assert(test_implicit_lifetime<int[2]>);
+static_assert(test_implicit_lifetime<TrivialClass[10]>);
+static_assert(test_implicit_lifetime<UserProvidedDestructorClass[12]>);
+static_assert(test_implicit_lifetime<IncompleteClass[]>);
+static_assert(test_implicit_lifetime<IncompleteClass[20]>);
+
+static_assert(!test_implicit_lifetime<void>);
+static_assert(!test_implicit_lifetime<const volatile void>);
+static_assert(!test_implicit_lifetime<long&>);
+static_assert(!test_implicit_lifetime<long&&>);
 #endif
-}
-
-class IncompleteClass {};
