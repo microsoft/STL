e82c3548a3f338d1374c52ed0b301936e5ca79c3
Stephan T. Lavavej <stl@microsoft.com>
2025-09-24 12:04:25 -0700

    Test review, part 1: Minor nitpicks.
    Include `<cstddef>` for `nullptr_t`.
    Move up the using-directive.
    Use `enum class`.
    Rename scoped enumerator to `Enumerator2`.
    Drop unnecessary comment.
    Strictly verify both variable template and struct.
    Add preprocessor comment.

--- tests/std/tests/P2674R1_is_implicit_lifetime/test.compile.pass.cpp
+++ tests/std/tests/P2674R1_is_implicit_lifetime/test.compile.pass.cpp
@@ -1,46 +1,42 @@
 // Copyright (c) Microsoft Corporation.
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
+#include <cstddef>
 #include <type_traits>
+using namespace std;
 
 enum UnscopedEnum { Enumerator };
-enum struct ScopedEnum { Enumerator };
+enum class ScopedEnum { Enumerator2 };
 class IncompleteClass;
 struct TrivialClass {};
 struct UserProvidedDestructorClass {
     ~UserProvidedDestructorClass() {}
 };
 
-using namespace std;
-
 #ifdef __cpp_lib_is_implicit_lifetime
-// this is a test to test whether
-// is_implicit_lifetime_v produces desired results
-// compiles under std namespace
-
-template <typename T>
-constexpr bool test_implicit_lifetime = is_implicit_lifetime_v<T> && is_implicit_lifetime<T>::value;
+template <bool Val, typename T>
+constexpr bool test_implicit_lifetime = is_implicit_lifetime_v<T> == Val && is_implicit_lifetime<T>::value == Val;
 
 // Basics (arrays thereof included in Arrays section)
-static_assert(test_implicit_lifetime<int>);
-static_assert(test_implicit_lifetime<int*>);
-static_assert(test_implicit_lifetime<nullptr_t>);
-static_assert(test_implicit_lifetime<UnscopedEnum>);
-static_assert(test_implicit_lifetime<ScopedEnum>);
-static_assert(test_implicit_lifetime<void (*)()>);
-static_assert(test_implicit_lifetime<TrivialClass>);
-static_assert(!test_implicit_lifetime<UserProvidedDestructorClass>);
+static_assert(test_implicit_lifetime<true, int>);
+static_assert(test_implicit_lifetime<true, int*>);
+static_assert(test_implicit_lifetime<true, nullptr_t>);
+static_assert(test_implicit_lifetime<true, UnscopedEnum>);
+static_assert(test_implicit_lifetime<true, ScopedEnum>);
+static_assert(test_implicit_lifetime<true, void (*)()>);
+static_assert(test_implicit_lifetime<true, TrivialClass>);
+static_assert(test_implicit_lifetime<false, UserProvidedDestructorClass>);
 
 // Arrays
-static_assert(test_implicit_lifetime<int[]>);
-static_assert(test_implicit_lifetime<int[2]>);
-static_assert(test_implicit_lifetime<TrivialClass[10]>);
-static_assert(test_implicit_lifetime<UserProvidedDestructorClass[12]>);
-static_assert(test_implicit_lifetime<IncompleteClass[]>);
-static_assert(test_implicit_lifetime<IncompleteClass[20]>);
+static_assert(test_implicit_lifetime<true, int[]>);
+static_assert(test_implicit_lifetime<true, int[2]>);
+static_assert(test_implicit_lifetime<true, TrivialClass[10]>);
+static_assert(test_implicit_lifetime<true, UserProvidedDestructorClass[12]>);
+static_assert(test_implicit_lifetime<true, IncompleteClass[]>);
+static_assert(test_implicit_lifetime<true, IncompleteClass[20]>);
 
-static_assert(!test_implicit_lifetime<void>);
-static_assert(!test_implicit_lifetime<const volatile void>);
-static_assert(!test_implicit_lifetime<long&>);
-static_assert(!test_implicit_lifetime<long&&>);
-#endif
+static_assert(test_implicit_lifetime<false, void>);
+static_assert(test_implicit_lifetime<false, const volatile void>);
+static_assert(test_implicit_lifetime<false, long&>);
+static_assert(test_implicit_lifetime<false, long&&>);
+#endif // ^^^ defined(__cpp_lib_is_implicit_lifetime) ^^^
