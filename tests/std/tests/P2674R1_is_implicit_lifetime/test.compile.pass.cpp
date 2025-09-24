8212b8a17c5016b91fece611521ceeb2e5222ae0
Stephan T. Lavavej <stl@microsoft.com>
2025-09-24 15:19:08 -0700

    Test review, part 4: Expand test coverage of class types, finding a Clang bug!

--- tests/std/tests/P2674R1_is_implicit_lifetime/test.compile.pass.cpp
+++ tests/std/tests/P2674R1_is_implicit_lifetime/test.compile.pass.cpp
@@ -2,6 +2,7 @@
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
 #include <cstddef>
+#include <string>
 #include <type_traits>
 using namespace std;
 
@@ -9,8 +10,91 @@ enum UnscopedEnum { Enumerator };
 enum class ScopedEnum { Enumerator2 };
 class IncompleteClass;
 struct TrivialClass {};
+struct DefaultedDestructorClass {
+    ~DefaultedDestructorClass() = default;
+};
+struct DeletedDestructorClass {
+    ~DeletedDestructorClass() = delete;
+};
 struct UserProvidedDestructorClass {
     ~UserProvidedDestructorClass() {}
+};
+struct StringAggregateWithImplicitlyDeclaredDestructor {
+    string juliette_andromeda_meow;
+    string clarissa_melpomene_meow;
+};
+struct StringAggregateWithUserProvidedDestructor {
+    string chaos_engine_one;
+    string chaos_engine_two;
+    ~StringAggregateWithUserProvidedDestructor() {}
+};
+
+class NonAggregateWithTrivialCtorAndTrivialDtor {
+public:
+    void set_num(int x) {
+        num = x;
+    }
+    int get_num() const {
+        return num;
+    }
+
+private:
+    int num;
+};
+
+class NonAggregateWithNonTrivialCtor {
+public:
+    void set_num(int x) {
+        num = x;
+    }
+    int get_num() const {
+        return num;
+    }
+
+private:
+    int num{0}; // default member initializer => non-trivial default constructor, N5014 [class.default.ctor]/3.2
+};
+
+class NonAggregateWithUserProvidedCtor {
+public:
+    NonAggregateWithUserProvidedCtor() {}
+    void set_num(int x) {
+        num = x;
+    }
+    int get_num() const {
+        return num;
+    }
+
+private:
+    int num;
+};
+
+class NonAggregateWithDeletedDtor {
+public:
+    ~NonAggregateWithDeletedDtor() = delete;
+    void set_num(int x) {
+        num = x;
+    }
+    int get_num() const {
+        return num;
+    }
+
+private:
+    int num;
+};
+
+class NonAggregateWithUserProvidedDtor {
+public:
+    ~NonAggregateWithUserProvidedDtor() {}
+    void set_num(int x) {
+        num = x;
+    }
+    int get_num() const {
+        return num;
+    }
+
+private:
+    int num;
 };
 
 #ifdef __cpp_lib_is_implicit_lifetime
@@ -33,9 +117,23 @@ static_assert(test_implicit_lifetime_cv<true, int TrivialClass::*>);
 static_assert(test_implicit_lifetime_cv<true, int (TrivialClass::*)(int)>);
 static_assert(test_implicit_lifetime_cv<true, nullptr_t>);
 
-// Implicit-lifetime class types, N5014 [class.prop]/16
+// Implicit-lifetime class types, N5014 [class.prop]/16:
+// "A class S is an implicit-lifetime class if
+// - it is an aggregate whose destructor is not user-provided or
+// - it has at least one trivial eligible constructor and a trivial, non-deleted destructor."
 static_assert(test_implicit_lifetime_cv<true, TrivialClass>);
+static_assert(test_implicit_lifetime_cv<true, DefaultedDestructorClass>);
+static_assert(test_implicit_lifetime_cv<true, DeletedDestructorClass>);
 static_assert(test_implicit_lifetime_cv<false, UserProvidedDestructorClass>);
+static_assert(test_implicit_lifetime_cv<true, StringAggregateWithImplicitlyDeclaredDestructor>);
+static_assert(test_implicit_lifetime_cv<false, StringAggregateWithUserProvidedDestructor>);
+static_assert(test_implicit_lifetime_cv<true, NonAggregateWithTrivialCtorAndTrivialDtor>);
+#ifndef __clang__ // TRANSITION, LLVM-160610
+static_assert(test_implicit_lifetime_cv<false, NonAggregateWithNonTrivialCtor>);
+static_assert(test_implicit_lifetime_cv<false, NonAggregateWithUserProvidedCtor>);
+#endif // ^^^ no workaround ^^^
+static_assert(test_implicit_lifetime_cv<false, NonAggregateWithDeletedDtor>);
+static_assert(test_implicit_lifetime_cv<false, NonAggregateWithUserProvidedDtor>);
 
 // Arrays
 static_assert(test_implicit_lifetime_cv<true, int[]>);
