093a628967e762d93b093b180e7ff4e45c0279e4
Stephan T. Lavavej <stl@microsoft.com>
2025-10-02 05:35:35 -0700

    Add missing TRANSITION comment.

--- tests/std/tests/P2255R2_tuple_pair_construction/test.compile.pass.cpp
+++ tests/std/tests/P2255R2_tuple_pair_construction/test.compile.pass.cpp
@@ -1,6 +1,7 @@
 // Copyright (c) Microsoft Corporation.
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
+// TRANSITION, MSVC and EDG haven't implemented intrinsics needed for P2255R2.
 #if defined(__clang__) && !defined(__EDG__)
 #include <tuple>
 #include <type_traits>
