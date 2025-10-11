c66de14d340c4a3fa893beb1e279423fff44b692
Stephan T. Lavavej <stl@microsoft.com>
2025-10-11 10:00:57 -0700

    Overhaul VSO_0744055_atomic_load_8_bytes_readonly.
    Don't printf(), instead assert() to check correctness.
    
    Cite documentation explaining why the bytes are 0.
    
    Use `std::uint64_t` instead of `unsigned long long` for clarity.
    
    There's no need to `static_cast<unsigned char*>` the result of MapViewOfFileEx.
    
    Use std::align() as I don't see any documented alignment guarantees.

--- tests/std/tests/VSO_0744055_atomic_load_8_bytes_readonly/test.cpp
+++ tests/std/tests/VSO_0744055_atomic_load_8_bytes_readonly/test.cpp
@@ -3,7 +3,9 @@
 
 #include <atomic>
 #include <cassert>
-#include <cstdio>
+#include <cstddef>
+#include <cstdint>
+#include <memory>
 
 #pragma warning(push) // TRANSITION, OS-23694920
 #pragma warning(disable : 4668) // 'MEOW' is not defined as a preprocessor macro, replacing with '0' for '#if/#elif'
@@ -13,16 +15,30 @@
 int main() {
     // See DevCom-274938, where a customer wanted to read a std::atomic from a read-only memory-mapped file.
     // This was originally implemented with cmpxchg8b on x86 which raised a write access violation.
+
+    // https://learn.microsoft.com/en-us/windows/win32/api/memoryapi/nf-memoryapi-createfilemappingw
+    // "If hFile is INVALID_HANDLE_VALUE, [...] CreateFileMapping creates a file mapping object of a
+    // specified size that is backed by the system paging file instead of by a file in the file system."
+    // "The initial contents of the pages in a file mapping object
+    // backed by the operating system paging file are 0 (zero)."
+
     constexpr DWORD bufferSize = 200;
     HANDLE hMapFile = CreateFileMappingW(INVALID_HANDLE_VALUE, nullptr, PAGE_READONLY, 0, bufferSize, nullptr);
     assert(hMapFile);
-    const auto view = static_cast<unsigned char*>(MapViewOfFileEx(hMapFile, FILE_MAP_READ, 0, 0, bufferSize, nullptr));
+    void* view = MapViewOfFileEx(hMapFile, FILE_MAP_READ, 0, 0, bufferSize, nullptr);
     assert(view);
 
+    using Atom               = std::atomic<std::uint64_t>;
+    std::size_t space        = bufferSize;
+    const auto adjusted_view = std::align(alignof(Atom), sizeof(Atom), view, space);
+    assert(adjusted_view == view);
+
     // Formally UB, but we want std::atomic loads to work from read-only memory regions:
-    const auto a = reinterpret_cast<std::atomic<unsigned long long>*>(view);
+    const auto a = reinterpret_cast<Atom*>(view);
+
     // In the original repro, a->load() failed with an access violation.
-    printf("atomic load: %llu, should be 0\n", a->load());
+    assert(a->load() == 0);
+
     UnmapViewOfFile(view);
     CloseHandle(hMapFile);
 }
