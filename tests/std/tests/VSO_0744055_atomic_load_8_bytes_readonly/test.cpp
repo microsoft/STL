5f715d81220e4bb65d6bbc7a669b88241c6f834e
Stephan T. Lavavej <stl@microsoft.com>
2025-10-11 08:33:37 -0700

    VSO_0744055_atomic_load_8_bytes_readonly: Update comment to no longer mention "MSVC 19.24".

--- tests/std/tests/VSO_0744055_atomic_load_8_bytes_readonly/test.cpp
+++ tests/std/tests/VSO_0744055_atomic_load_8_bytes_readonly/test.cpp
@@ -11,9 +11,8 @@
 #pragma warning(pop)
 
 int main() {
-    // A customer wanted to read a std::atomic from a read-only memory-mapped file.
-    // Before MSVC 19.24 this was implemented with cmpxchg8b on x86 which raised a write access violation. See
-    // DevCom-274938
+    // See DevCom-274938, where a customer wanted to read a std::atomic from a read-only memory-mapped file.
+    // This was originally implemented with cmpxchg8b on x86 which raised a write access violation.
     constexpr DWORD bufferSize = 200;
     HANDLE hMapFile = CreateFileMappingW(INVALID_HANDLE_VALUE, nullptr, PAGE_READONLY, 0, bufferSize, nullptr);
     assert(hMapFile);
@@ -22,7 +21,7 @@ int main() {
 
     // Formally UB, but we want std::atomic loads to work from read-only memory regions:
     const auto a = reinterpret_cast<std::atomic<unsigned long long>*>(view);
-    // In the original repro, a->load() fails with an access violation
+    // In the original repro, a->load() failed with an access violation.
     printf("atomic load: %llu, should be 0\n", a->load());
     UnmapViewOfFile(view);
     CloseHandle(hMapFile);
