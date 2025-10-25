c99855721c5221dcd90606de98cd01b851dfdedc
Alex Guteniev <gutenev@gmail.com>
2025-10-25 11:27:29 +0300

    status quo coverage

---
+++ tests/std/tests/GH_005504_avoid_function_call_wrapping/test.cpp
@@ -0,0 +1,84 @@
+// Copyright (c) Microsoft Corporation.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+#include <cassert>
+#include <functional>
+
+#pragma warning(disable : 4324) // 'large_callable': structure was padded due to alignment specifier
+
+using namespace std;
+
+struct copy_counter {
+    copy_counter() = default;
+    copy_counter(const copy_counter& other) : count(other.count + 1) {}
+
+    int count = 0;
+};
+
+using function_type = int(copy_counter);
+
+struct small_callable {
+    int context = 42;
+
+    int operator()(const copy_counter& counter) {
+        assert(context == 42);
+        return counter.count;
+    }
+};
+
+struct alignas(128) large_callable {
+    int context = 1729;
+
+    int operator()(const copy_counter& counter) {
+        assert(context == 1729);
+        return counter.count;
+    }
+};
+
+int main() {
+    // Plain calls
+    {
+        function<function_type> fn{small_callable{}};
+        assert(fn(copy_counter{}) == 0);
+    }
+    {
+        function<function_type> fn{large_callable{}};
+        assert(fn(copy_counter{}) == 0);
+    }
+    {
+        move_only_function<function_type> fn{small_callable{}};
+        assert(fn(copy_counter{}) == 0);
+    }
+    {
+        move_only_function<function_type> fn{large_callable{}};
+        assert(fn(copy_counter{}) == 0);
+    }
+
+    // Moves to the same
+    {
+        function<function_type> fn{function<function_type>{small_callable{}}};
+        assert(fn(copy_counter{}) == 0);
+    }
+    {
+        function<function_type> fn{function<function_type>{large_callable{}}};
+        assert(fn(copy_counter{}) == 0);
+    }
+    {
+        move_only_function<function_type> fn{move_only_function<function_type>{small_callable{}}};
+        assert(fn(copy_counter{}) == 0);
+    }
+    {
+        move_only_function<function_type> fn{move_only_function<function_type>{large_callable{}}};
+        assert(fn(copy_counter{}) == 0);
+    }
+
+    // Moves from function to move_only_function
+    {
+        move_only_function<function_type> fn{function<function_type>{small_callable{}}};
+        assert(fn(copy_counter{}) == 1);
+    }
+    {
+        move_only_function<function_type> fn{function<function_type>{large_callable{}}};
+        assert(fn(copy_counter{}) == 1);
+    }
+}
