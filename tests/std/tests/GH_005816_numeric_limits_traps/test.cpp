2b4fb315518cc62cc096ac06369146e0f5757a76
Alex Guteniev <gutenev@gmail.com>
2025-10-28 18:07:29 +0200

    Integer coverage

---
+++ tests/std/tests/GH_005816_numeric_limits_traps/test.cpp
@@ -0,0 +1,30 @@
+// Copyright (c) Microsoft Corporation.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+#include <limits>
+
+#include <test_death.hpp>
+
+#ifdef __clang__
+#pragma clang diagnostic ignored "-Wc++17-extensions" // constexpr if is a C++17 extension
+#else
+#pragma warning(disable : 4984) //  warning C4984: 'if constexpr' is a C++17 language extension
+#endif // __clang__
+
+void trap_operation() {
+    volatile int op1{1};
+    volatile int op2{0};
+    volatile int res = op1 / op2;
+    (void) res;
+}
+
+int main(int argc, char* argv[]) {
+    if constexpr (std::numeric_limits<int>::traps) {
+        std_testing::death_test_executive exec;
+        const std_testing::death_function_t one_trap[] = {trap_operation};
+        exec.add_death_tests(one_trap);
+        return exec.run(argc, argv);
+    } else {
+        trap_operation();
+    }
+}
