a49bce3e303e127a66f57d42c80c2e7be357fe5c
Alex Guteniev <gutenev@gmail.com>
2025-10-30 20:45:13 +0200

    comprehensive compile-time coverage

--- tests/std/tests/GH_005816_numeric_limits_traps/test.cpp
+++ tests/std/tests/GH_005816_numeric_limits_traps/test.cpp
@@ -11,6 +11,21 @@
 #pragma warning(disable : 4984) //  warning C4984: 'if constexpr' is a C++17 language extension
 #endif // __clang__
 
+template <class T>
+constexpr bool traps_ = std::numeric_limits<T>::traps;
+
+static_assert(!traps_<bool>, "bool does not trap for a moot reason");
+
+static_assert(!traps_<float> && !traps_<double> && !traps_<long double>,
+    "floats don't trap because even if '/fp:except' is passed, it should be enabled at runtime");
+
+static_assert(traps_<char> == traps_<int> && traps_<signed char> == traps_<int> && traps_<unsigned char> == traps_<int>
+                  && traps_<short> == traps_<int> && traps_<unsigned short> == traps_<int>
+                  && traps_<unsigned int> == traps_<int> && traps_<long> == traps_<int>
+                  && traps_<unsigned long> == traps_<int> && traps_<long long> == traps_<int>
+                  && traps_<unsigned long long> == traps_<int>,
+    "all integers should trap or not trap equally");
+
 void trap_operation() {
     const volatile int op1 = 1;
     const volatile int op2 = 0;
@@ -19,7 +34,7 @@ void trap_operation() {
 }
 
 int main(int argc, char* argv[]) {
-    if constexpr (std::numeric_limits<int>::traps) {
+    if constexpr (traps_<int>) {
         std_testing::death_test_executive exec;
         const std_testing::death_function_t one_trap[] = {trap_operation};
         exec.add_death_tests(one_trap);
