dcf8058dcfdb86a10af7f7ae2fe71f9cb1b8d714
Vojtěch Michal <vojta.michall@email.cz>
2025-11-13 13:29:04 +0100

    Add constexpr tests for std::initializer_list and iteration over braced-init-list

--- tests/std/tests/P3016R6_inconsistent_begin_end/test.cpp
+++ tests/std/tests/P3016R6_inconsistent_begin_end/test.cpp
@@ -6,6 +6,8 @@
 #include <type_traits>
 #include <valarray>
 
+#define STATIC_ASSERT(...) static_assert(__VA_ARGS__, #__VA_ARGS__)
+
 namespace my {
 
     template <typename T>
@@ -21,45 +23,65 @@ namespace my {
     void data(std::initializer_list<T>);
 } // namespace my
 
+_CONSTEXPR17 bool test_initializer_list() {
+    // Check that free functions in std still can be invoked on std::initializer_list
+    std::initializer_list<int> il = {1, 2, 3};
+
+    using namespace std;
+    (void) begin(il);
+    (void) cbegin(il);
+    (void) end(il);
+    (void) cend(il);
+    (void) size(il);
+    (void) empty(il);
+    (void) data(il);
+
+    return true;
+}
+
+bool test_valarray() {
+    // Check that free functions in std can be invoked on std::valarray
+    std::valarray<int> v{1};
+
+    using namespace std;
+    (void) begin(v);
+    (void) cbegin(v); // Did not compile before P3016R6
+    (void) end(v);
+    (void) cend(v); // Did not compile before P3016R6
+    (void) size(v);
+    // There are no members 'empty' and 'data' of valarray
+    // (void) empty(il);
+    // (void) data(il);
+
+    return true;
+}
+
+constexpr bool test_braced_init_list_iteration() {
+    int sum = 0;
+    for (int n : {1, 2, 3, 4}) {
+        sum += n;
+    }
+    return sum == 10;
+}
+
 
 int main() {
-    {
-        // Check that free functions in std can't be invoked with braced-initializer-list.
-        // If they could be invoked, the following expressions would be ambiguous between std:: and my::
-        using namespace std;
-        using namespace my;
-        static_assert(std::is_same_v<decltype(begin({1, 2, 3})), void>);
-        static_assert(std::is_same_v<decltype(end({1, 2, 3})), void>);
-        static_assert(std::is_same_v<decltype(empty({1, 2, 3})), void>);
-        static_assert(std::is_same_v<decltype(data({1, 2, 3})), void>);
-    }
+    // Check that free functions in std can't be invoked with braced-initializer-list.
+    // If they could be invoked, the following expressions would be ambiguous between std:: and my::
+    using namespace std;
+    using namespace my;
+    STATIC_ASSERT(std::is_same_v<decltype(begin({1, 2, 3})), void>);
+    STATIC_ASSERT(std::is_same_v<decltype(end({1, 2, 3})), void>);
+    STATIC_ASSERT(std::is_same_v<decltype(empty({1, 2, 3})), void>);
+    STATIC_ASSERT(std::is_same_v<decltype(data({1, 2, 3})), void>);
 
-    {
-        // Check that free functions in std still can be invoked on std::initializer_list
-        std::initializer_list<int> il = {1, 2, 3};
+    test_initializer_list();
+    test_valarray();
+    test_braced_init_list_iteration();
 
-        using namespace std;
-        (void) begin(il);
-        (void) cbegin(il);
-        (void) end(il);
-        (void) cend(il);
-        (void) size(il);
-        (void) empty(il);
-        (void) data(il);
-    }
-
-    {
-        // Check that free functions in std can be invoked on std::valarray
-        std::valarray<int> v{1};
-
-        using namespace std;
-        (void) begin(v);
-        (void) cbegin(v); // Did not compile before P3016R6
-        (void) end(v);
-        (void) cend(v); // Did not compile before P3016R6
-        (void) size(v);
-        // There are no members 'empty' and 'data' of valarray
-        // (void) empty(il);
-        // (void) data(il);
-    }
+#if _HAS_CXX17
+    STATIC_ASSERT(test_initializer_list());
+#endif // _HAS_CXX17
+    // STATIC_ASSERT(test_valarray()); // valarray is not literal
+    STATIC_ASSERT(test_braced_init_list_iteration());
 }
