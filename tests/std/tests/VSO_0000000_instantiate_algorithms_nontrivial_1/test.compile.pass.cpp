73cd0452c9ca3a4c87c9aca9f00e0dee8c441d1b
Stephan T. Lavavej <stl@nuwen.net>
2025-11-14 12:41:07 -0800

    DELETE DELETE DELETE

--- tests/std/tests/VSO_0000000_instantiate_algorithms_nontrivial_1/test.compile.pass.cpp
+++
@@ -1,9 +0,0 @@
-// Copyright (c) Microsoft Corporation.
-// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
-
-// The instantiate_algorithm* tests take too long individually, so must be split into two parts.
-// instantiate_algorithms_nontrivial.hpp contains the common test code.
-
-#define _USE_NAMED_IDL_NAMESPACE          1
-#define INSTANTIATE_ALGORITHMS_SPLIT_MODE 1
-#include <instantiate_algorithms_nontrivial.hpp>
--- tests/std/tests/VSO_0527559_pragma_managed/test.compile.pass.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright (c) Microsoft Corporation.
-// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
-
-#pragma managed(push, off)
-#include <__msvc_all_public_headers.hpp>
-#pragma managed(pop)
