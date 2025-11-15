db1c4c7c9d78e6a05557edb4df4d7bec65ac1107
Alex Guteniev <gutenev@gmail.com>
2025-11-15 12:55:35 +0200

    promoted integers don't trap

--- tests/std/tests/GH_005816_numeric_limits_traps/test.compile.pass.cpp
+++ tests/std/tests/GH_005816_numeric_limits_traps/test.compile.pass.cpp
@@ -27,14 +27,20 @@ constexpr bool traps_ = std::numeric_limits<T>::traps;
 static_assert(traps_<int> == _Integer_zero_division_traps,
     "integer trap behavior should match the expectation from the current platform and complier");
 
-static_assert(traps_<char> == traps_<int> && traps_<signed char> == traps_<int> && traps_<unsigned char> == traps_<int>
-                  && traps_<short> == traps_<int> && traps_<unsigned short> == traps_<int>
-                  && traps_<unsigned int> == traps_<int> //
+static_assert(traps_<unsigned int> == traps_<int> && traps_<unsigned int> == traps_<char32_t>
                   && traps_<long> == traps_<int> && traps_<unsigned long> == traps_<int>
                   && traps_<long long> == traps_<int> && traps_<unsigned long long> == traps_<int>,
-    "all integers should trap or not trap equally");
+    "all non-promoted integers should trap or not trap equally");
 
 static_assert(!traps_<bool>, "bool does not trap for a moot reason; see LWG-554 resolution");
+
+static_assert(
+    !traps_<char> && !traps_<signed char> && !traps_<unsigned char> && !traps_<unsigned short> && !traps_<char16_t>,
+    "promoted integers do not trap for a moot reason; see LWG-554 resolution");
+
+#ifdef __cpp_char8_t
+static_assert(!traps_<char8_t>, "promoted integers do not trap for a moot reason; see LWG-554 resolution");
+#endif
 
 static_assert(!traps_<float> && !traps_<double> && !traps_<long double>,
     "floats don't trap because even if '/fp:except' is passed, it should be enabled at runtime");
