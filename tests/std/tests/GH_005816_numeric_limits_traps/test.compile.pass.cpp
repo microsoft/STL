a6936c2d367aa1b0b4cccfb00b949ac0daa2ef57
Alex Guteniev <gutenev@gmail.com>
2025-11-16 11:44:18 +0200

    backwards

--- tests/std/tests/GH_005816_numeric_limits_traps/test.compile.pass.cpp
+++ tests/std/tests/GH_005816_numeric_limits_traps/test.compile.pass.cpp
@@ -22,27 +22,29 @@ static_assert(traps_<int>, "The hardware does not trap. MSVC inserts check for z
 #endif
 
 template <class T>
-constexpr bool promoted_and_traps_if_int_does = std::is_same_v<decltype(~T{}), T> && traps_<T> == traps_<int>;
+constexpr bool non_promoted_and_traps_if_int_does = std::is_same_v<decltype(~T{}), T> && traps_<T> == traps_<int>;
 
-static_assert(promoted_and_traps_if_int_does<unsigned int> //
-                  && promoted_and_traps_if_int_does<long> && promoted_and_traps_if_int_does<unsigned long>
-                  && promoted_and_traps_if_int_does<long long> && promoted_and_traps_if_int_does<unsigned long long>,
+static_assert(non_promoted_and_traps_if_int_does<unsigned int> //
+                  && non_promoted_and_traps_if_int_does<long> && non_promoted_and_traps_if_int_does<unsigned long>
+                  && non_promoted_and_traps_if_int_does<long long>
+                  && non_promoted_and_traps_if_int_does<unsigned long long>,
     "all non-promoted integers should trap or not trap equally");
 
 template <class T>
-constexpr bool not_promoted_and_does_not_trap = !std::is_same_v<decltype(~T{}), T> && !traps_<T>;
+constexpr bool promoted_and_does_not_trap = !std::is_same_v<decltype(~T{}), T> && !traps_<T>;
 
-static_assert(!traps_<bool>, "bool does not trap for a moot reason; see LWG-554 resolution");
+static_assert(promoted_and_does_not_trap<bool>, "bool does not trap for a moot reason; see LWG-554 resolution");
 
-static_assert(not_promoted_and_does_not_trap<char> && not_promoted_and_does_not_trap<signed char>
-                  && not_promoted_and_does_not_trap<unsigned char> //
-                  && not_promoted_and_does_not_trap<short> && not_promoted_and_does_not_trap<unsigned short>
-                  && not_promoted_and_does_not_trap<wchar_t> && not_promoted_and_does_not_trap<char16_t>
-                  && not_promoted_and_does_not_trap<char32_t>,
+static_assert(promoted_and_does_not_trap<char> && promoted_and_does_not_trap<signed char>
+                  && promoted_and_does_not_trap<unsigned char> //
+                  && promoted_and_does_not_trap<short> && promoted_and_does_not_trap<unsigned short>
+                  && promoted_and_does_not_trap<wchar_t> && promoted_and_does_not_trap<char16_t>
+                  && promoted_and_does_not_trap<char32_t>,
     "promoted integers do not trap for a moot reason; see LWG-554 resolution");
 
 #ifdef __cpp_char8_t
-static_assert(!traps_<char8_t>, "promoted integers do not trap for a moot reason; see LWG-554 resolution");
+static_assert(
+    promoted_and_does_not_trap<char8_t>, "promoted integers do not trap for a moot reason; see LWG-554 resolution");
 #endif
 
 static_assert(!traps_<float> && !traps_<double> && !traps_<long double>,
