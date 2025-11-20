790759e1fa53cdf14093c910430affd2df7adb8e
Vojtěch Michal <vojta.michall@email.cz>
2025-11-20 01:49:16 +0100

    Provide tests of immutability of timeout/duration passed to conditional_variable{_any}::wait_{for, until}

--- tests/std/tests/GH_000685_condition_variable_any/test.cpp
+++ tests/std/tests/GH_000685_condition_variable_any/test.cpp
@@ -2,11 +2,13 @@
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
 // Test GH-685 "wait_for in condition_variable_any should unlock and lock"
+// Test LWG-4301 "condition_variable{_any}::wait_{for, until} should take timeout by value"
 
 #include <cassert>
 #include <chrono>
 #include <condition_variable>
 #include <mutex>
+#include <thread>
 #include <type_traits>
 
 using namespace std;
@@ -79,9 +81,96 @@ namespace {
         assert(m.num_locks() == 4);
 #endif // _HAS_CXX20
     }
+
+    // Minimal example inspired by LWG-4301, modified due to missing latch in C++11 to 20
+    // and generalized to test all overloads of condition_variable{_any}::wait_{for, until}
+    template <typename CV>
+    void test_timeout_immutable(int test_index) {
+
+        mutex m;
+        CV cv;
+        unique_lock<mutex> lock(m); // Ensure main thread waitins on cv before other_thread starts
+
+        // Start with very short timeout and let other_thread change it to very large while main thread is waiting
+        constexpr auto short_timeout = 100ms;
+        constexpr auto long_timeout  = 2s;
+
+        auto timeout_duration = short_timeout;
+        auto timeout          = steady_clock::now() + timeout_duration;
+
+        thread other_thread([&] {
+            // Immediatelly blocks since the main thread owns lock.
+            unique_lock<mutex> lock(m);
+
+            // If the timeout provided to condition_variable{_any}::wait_{for, until} was mutable,
+            // we will get timeout after much longer time
+            timeout_duration = long_timeout;
+            timeout          = steady_clock::now() + timeout_duration;
+        });
+
+        const auto wait_start = steady_clock::now();
+#define CHECK_TIMEOUT_NOT_CHANGED() assert(steady_clock::now() - wait_start < long_timeout / 2);
+
+        switch (test_index) {
+        case 0:
+            assert(cv.wait_until(lock, timeout) == cv_status::timeout);
+            CHECK_TIMEOUT_NOT_CHANGED();
+            break;
+
+        case 1:
+            assert(cv.wait_until(lock, timeout, [] { return false; }) == false);
+            CHECK_TIMEOUT_NOT_CHANGED();
+            break;
+
+        case 2:
+            assert(cv.wait_for(lock, timeout_duration) == cv_status::timeout);
+            CHECK_TIMEOUT_NOT_CHANGED();
+            break;
+
+        case 3:
+            assert(cv.wait_for(lock, timeout_duration, [] { return false; }) == false);
+            CHECK_TIMEOUT_NOT_CHANGED();
+            break;
+
+#if _HAS_CXX20 // because of stop_token
+        case 4:
+            if constexpr (std::is_same_v<CV, std::condition_variable_any>) {
+                stop_source source;
+                assert(cv.wait_until(lock, source.get_token(), timeout, [] { return false; }) == false);
+                CHECK_TIMEOUT_NOT_CHANGED();
+            } else {
+                assert(false);
+            }
+            break;
+
+        case 5:
+            if constexpr (std::is_same_v<CV, std::condition_variable_any>) {
+                stop_source source;
+                assert(cv.wait_for(lock, source.get_token(), timeout_duration, [] { return false; }) == false);
+                CHECK_TIMEOUT_NOT_CHANGED();
+            } else {
+                assert(false);
+            }
+            break;
+#endif // _HAS_CXX20
+
+        default:
+            assert(false);
+        }
+
+        other_thread.join();
+    }
 } // unnamed namespace
 
 int main() {
     test_condition_variable_any();
     test_condition_variable_any_already_timed_out();
+
+    for (int i = 0; i < 4; i++) {
+        test_timeout_immutable<std::condition_variable>(i);
+    }
+
+    for (int i = 0; i < (_HAS_CXX20 ? 6 : 4); i++) {
+        test_timeout_immutable<std::condition_variable_any>(i);
+    }
 }
