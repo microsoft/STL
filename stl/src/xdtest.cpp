50dd871db74201746cc8a2118fb86cb0d613fd72
Stephan T. Lavavej <stl@nuwen.net>
2025-12-11 01:43:38 -0800

    Fuse xfdtest.cpp into xdtest.cpp.

--- stl/src/xdtest.cpp
+++ stl/src/xdtest.cpp
@@ -1,8 +1,6 @@
 // Copyright (c) Microsoft Corporation.
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
-// _Dtest function -- IEEE 754 version
-
 #include "xmath.hpp"
 
 _EXTERN_C_UNLESS_PURE
@@ -22,6 +20,18 @@ _CRTIMP2_PURE short __CLRCALL_PURE_OR_CDECL _Dtest(double* px) noexcept { // cat
 
 _CRTIMP2_PURE short __CLRCALL_PURE_OR_CDECL _LDtest(long double* px) noexcept { // categorize *px -- 64-bit
     return _Dtest(reinterpret_cast<double*>(px));
+}
+
+_CRTIMP2_PURE short __CLRCALL_PURE_OR_CDECL _FDtest(float* px) noexcept { // categorize *px
+    const auto ps = reinterpret_cast<_Fval*>(px);
+
+    if ((ps->_Sh[_F0] & _FMASK) == _FMAX << _FOFF) {
+        return (ps->_Sh[_F0] & _FFRAC) != 0 || ps->_Sh[_F1] != 0 ? _NANCODE : _INFCODE;
+    } else if ((ps->_Sh[_F0] & ~_FSIGN) != 0 || ps->_Sh[_F1] != 0) {
+        return (ps->_Sh[_F0] & _FMASK) == 0 ? _DENORM : _FINITE;
+    } else {
+        return 0;
+    }
 }
 
 _END_EXTERN_C_UNLESS_PURE
