aba61fe8b7a91641ac7874d621996171327a2533
Stephan T. Lavavej <stl@nuwen.net>
2025-12-11 01:11:36 -0800

    Fuse xfcosh.cpp into xcosh.cpp.

--- stl/src/xcosh.cpp
+++ stl/src/xcosh.cpp
@@ -40,4 +40,35 @@ _CRTIMP2_PURE long double __CLRCALL_PURE_OR_CDECL _LCosh(long double x, long dou
     return _Cosh(static_cast<double>(x), static_cast<double>(y));
 }
 
+_CRTIMP2_PURE float __CLRCALL_PURE_OR_CDECL _FCosh(float x, float y) noexcept { // compute y * cosh(x), |y| <= 1
+    switch (_FDtest(&x)) { // test for special codes
+    case _NANCODE:
+    case _INFCODE:
+        return x;
+    case 0:
+        return y;
+    default: // finite
+        if (y == 0.0) {
+            return y;
+        }
+
+        if (x < 0.0) {
+            x = -x;
+        }
+
+        if (x < _FXbig) { // worth adding in exp(-x)
+            _FExp(&x, 1.0F, -1);
+            return y * (x + 0.25F / x);
+        }
+        switch (_FExp(&x, y, -1)) { // report over/underflow
+        case 0:
+            _Feraise(_FE_UNDERFLOW);
+            break;
+        case _INFCODE:
+            _Feraise(_FE_OVERFLOW);
+        }
+        return x;
+    }
+}
+
 _END_EXTERN_C_UNLESS_PURE
