273a6e3ff5f7588863c3354df42b73cd105e98f6
Stephan T. Lavavej <stl@nuwen.net>
2025-12-11 01:23:37 -0800

    `_CSTD _Cosh(_Left, _Right)` => `_STD cosh(_Left) * _Right`

--- stl/src/xcosh.cpp
+++ stl/src/xcosh.cpp
@@ -5,70 +5,19 @@
 
 _EXTERN_C_UNLESS_PURE
 
-_CRTIMP2_PURE double __CLRCALL_PURE_OR_CDECL _Cosh(double x, double y) noexcept { // compute y * cosh(x), |y| <= 1
-    switch (_Dtest(&x)) { // test for special codes
-    case _NANCODE:
-    case _INFCODE:
-        return x;
-    case 0:
-        return y;
-    default: // finite
-        if (y == 0.0) {
-            return y;
-        }
-
-        if (x < 0.0) {
-            x = -x;
-        }
-
-        if (x < _Xbig) { // worth adding in exp(-x)
-            _Exp(&x, 1.0, -1);
-            return y * (x + 0.25 / x);
-        }
-        switch (_Exp(&x, y, -1)) { // report over/underflow
-        case 0:
-            _Feraise(_FE_UNDERFLOW);
-            break;
-        case _INFCODE:
-            _Feraise(_FE_OVERFLOW);
-        }
-        return x;
-    }
+// TRANSITION, ABI: preserved for binary compatibility
+_CRTIMP2_PURE double __CLRCALL_PURE_OR_CDECL _Cosh(double x, double y) noexcept {
+    return _STD cosh(x) * y;
 }
 
+// TRANSITION, ABI: preserved for binary compatibility
 _CRTIMP2_PURE long double __CLRCALL_PURE_OR_CDECL _LCosh(long double x, long double y) noexcept {
-    return _Cosh(static_cast<double>(x), static_cast<double>(y));
+    return _STD cosh(x) * y;
 }
 
-_CRTIMP2_PURE float __CLRCALL_PURE_OR_CDECL _FCosh(float x, float y) noexcept { // compute y * cosh(x), |y| <= 1
-    switch (_FDtest(&x)) { // test for special codes
-    case _NANCODE:
-    case _INFCODE:
-        return x;
-    case 0:
-        return y;
-    default: // finite
-        if (y == 0.0) {
-            return y;
-        }
-
-        if (x < 0.0) {
-            x = -x;
-        }
-
-        if (x < _FXbig) { // worth adding in exp(-x)
-            _FExp(&x, 1.0F, -1);
-            return y * (x + 0.25F / x);
-        }
-        switch (_FExp(&x, y, -1)) { // report over/underflow
-        case 0:
-            _Feraise(_FE_UNDERFLOW);
-            break;
-        case _INFCODE:
-            _Feraise(_FE_OVERFLOW);
-        }
-        return x;
-    }
+// TRANSITION, ABI: preserved for binary compatibility
+_CRTIMP2_PURE float __CLRCALL_PURE_OR_CDECL _FCosh(float x, float y) noexcept {
+    return _STD cosh(x) * y;
 }
 
 _END_EXTERN_C_UNLESS_PURE
