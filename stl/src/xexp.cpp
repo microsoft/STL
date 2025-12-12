a6d8c16d8f3f8d8b13d661b467dc8f14a6bff273
Stephan T. Lavavej <stl@nuwen.net>
2025-12-12 04:36:11 -0800

    Move `_Xfe_overflow()`, `_Xfe_underflow()` into xexp.cpp's unnamed namespace.
    They obviously aren't dllexported.

--- stl/src/xexp.cpp
+++ stl/src/xexp.cpp
@@ -196,6 +196,30 @@ namespace {
             }
         }
     }
+
+// raise IEEE 754 exceptions
+#ifndef _M_CEE_PURE
+#pragma float_control(except, on, push)
+#endif
+
+    template <class T>
+    [[nodiscard]] T _Xfe_overflow(const T sign) noexcept {
+        static_assert(_STD is_floating_point_v<T>, "Expected is_floating_point_v<T>.");
+        constexpr T huge = _STD numeric_limits<T>::max();
+        return _STD copysign(huge, sign) * huge;
+    }
+
+    template <class T>
+    [[nodiscard]] T _Xfe_underflow(const T sign) noexcept {
+        static_assert(_STD is_floating_point_v<T>, "Expected is_floating_point_v<T>.");
+        constexpr T tiny = _STD numeric_limits<T>::min();
+        return _STD copysign(tiny, sign) * tiny;
+    }
+
+#ifndef _M_CEE_PURE
+#pragma float_control(pop)
+#endif
+
 } // unnamed namespace
 
 _EXTERN_C_UNLESS_PURE
--- stl/src/xmath.hpp
+++ stl/src/xmath.hpp
@@ -12,26 +12,3 @@ _EXTERN_C_UNLESS_PURE
 extern const double _Xbig;
 extern const float _FXbig;
 _END_EXTERN_C_UNLESS_PURE
-
-// raise IEEE 754 exceptions
-#ifndef _M_CEE_PURE
-#pragma float_control(except, on, push)
-#endif
-
-template <class T>
-[[nodiscard]] T _Xfe_overflow(const T sign) noexcept {
-    static_assert(_STD is_floating_point_v<T>, "Expected is_floating_point_v<T>.");
-    constexpr T huge = _STD numeric_limits<T>::max();
-    return _STD copysign(huge, sign) * huge;
-}
-
-template <class T>
-[[nodiscard]] T _Xfe_underflow(const T sign) noexcept {
-    static_assert(_STD is_floating_point_v<T>, "Expected is_floating_point_v<T>.");
-    constexpr T tiny = _STD numeric_limits<T>::min();
-    return _STD copysign(tiny, sign) * tiny;
-}
-
-#ifndef _M_CEE_PURE
-#pragma float_control(pop)
-#endif
