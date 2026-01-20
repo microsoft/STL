705ba1391386bc77e9c091f60a7bb39e61fb53d1
Stephan T. Lavavej <stl@microsoft.com>
2026-01-20 06:51:39 -0800

    Extract and comment a combined compare-key trait.

--- stl/inc/__msvc_string_view.hpp
+++ stl/inc/__msvc_string_view.hpp
@@ -2151,7 +2151,7 @@ inline namespace literals {
 
 #if _HAS_CXX23
 template <class _Elem, class _Traits>
-constexpr bool _Equivalence_is_equality<basic_string_view<_Elem, _Traits>> =
+constexpr bool _Equivalence_is_equality_impl<basic_string_view<_Elem, _Traits>> =
     _Is_implementation_handled_char_traits<_Traits>;
 #endif // _HAS_CXX23
 _STD_END
