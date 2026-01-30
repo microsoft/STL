352f0f9f9138ee51aaa5511eff39c52acc9c4a0f
Stephan T. Lavavej <stl@nuwen.net>
2026-01-30 02:30:40 -0800

    benchmarks/src/find_and_count.cpp: Fix Clang -Wmissing-field-initializers warning "missing field 'y' initializer".

--- benchmarks/src/find_and_count.cpp
+++ benchmarks/src/find_and_count.cpp
@@ -73,8 +73,8 @@ void common_args(benchmark::Benchmark* bm) {
 }
 
 struct point {
-    int16_t x;
-    int16_t y;
+    int16_t x{};
+    int16_t y{};
 
     bool operator==(const point&) const = default;
 };
