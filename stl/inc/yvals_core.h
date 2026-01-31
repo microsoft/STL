9138a1dc5b663538b59c44e9da4d7eedd875bdba
Stephan T. Lavavej <stl@nuwen.net>
2026-01-31 04:27:12 -0800

    Improve message wrapping.

--- stl/inc/yvals_core.h
+++ stl/inc/yvals_core.h
@@ -561,22 +561,20 @@
     [[nodiscard("The 'unique' algorithm returns the iterator past the last element that should be kept. " \
                 "You need to call container.erase(result, container.end()) afterwards.")]]
 
-#define _NODISCARD_EMPTY_MEMBER                                                                                     \
-    [[nodiscard(                                                                                                    \
-        "This member function returns a bool indicating whether the collection is empty and has no other effects. " \
-        "It is not useful to call this member function and discard the return value. "                              \
-        "Use the 'clear()' member function if you want to erase all elements.")]]
+#define _NODISCARD_EMPTY_MEMBER                                                                        \
+    [[nodiscard("This member function returns a bool indicating whether the collection is empty and "  \
+                "has no other effects. It is not useful to call this member function and discard the " \
+                "return value. Use the 'clear()' member function if you want to erase all elements.")]]
 
-#define _NODISCARD_EMPTY_ARRAY_MEMBER                                                                                  \
-    [[nodiscard("This member function returns a bool indicating whether the array is empty and has no other effects. " \
-                "It is not useful to call this member function and discard the return value. "                         \
-                "There's no way to clear an array as its size is fixed.")]]
+#define _NODISCARD_EMPTY_ARRAY_MEMBER                                                                  \
+    [[nodiscard("This member function returns a bool indicating whether the array is empty and "       \
+                "has no other effects. It is not useful to call this member function and discard the " \
+                "return value. There's no way to clear an array as its size is fixed.")]]
 
-#define _NODISCARD_EMPTY_MEMBER_NO_CLEAR                                                                            \
-    [[nodiscard(                                                                                                    \
-        "This member function returns a bool indicating whether the collection is empty and has no other effects. " \
-        "It is not useful to call this member function and discard the return value. "                              \
-        "This collection can be cleared by assigning an empty value to it.")]]
+#define _NODISCARD_EMPTY_MEMBER_NO_CLEAR                                                               \
+    [[nodiscard("This member function returns a bool indicating whether the collection is empty and "  \
+                "has no other effects. It is not useful to call this member function and discard the " \
+                "return value. This collection can be cleared by assigning an empty value to it.")]]
 
 #define _NODISCARD_EMPTY_NON_MEMBER                                                            \
     [[nodiscard("This function returns a bool indicating whether the collection is empty and " \
