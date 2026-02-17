b77a97c2a2545b10943747f54fa84acfd8441975
Vojtěch Michal <vojta.michall@email.cz>
2026-02-17 15:50:05 +0100

    Improve test diagnostics when `expected_results.txt` contains syntax errors (#6002)
    Co-authored-by: Stephan T. Lavavej <stl@microsoft.com>

--- tools/scripts/check_libcxx_paths.py
+++ tools/scripts/check_libcxx_paths.py
@@ -21,16 +21,49 @@ if __name__ == "__main__":
         stripped_lines = [line.strip() for line in file]
 
     filtered_lines = [s for s in stripped_lines if s and not s.startswith("#")] # Ignore empty lines and comments.
-    config_rgx = re.compile(r"(:\d+)? (FAIL|SKIPPED)$")
-    unique_tests = {config_rgx.sub("", line) for line in filtered_lines}
+
+    damaged_lines = set()
+    unknown_results = set()
+    unique_tests = set()
+
+    syntax_rgx = re.compile(r"(?P<test>[^: ]+)(?P<config>:\d+)? (?P<result>\w+)")
+    for line in filtered_lines:
+        m = syntax_rgx.fullmatch(line)
+        if m is None:
+            damaged_lines.add(line)
+        else:
+            result = m.group("result")
+            if result == "FAIL" or result == "SKIPPED":
+                unique_tests.add(m.group("test"))
+            else:
+                unknown_results.add(line)
 
     # Build up a list of nonexistent tests so they can be printed in sorted order.
     absolute_libcxx_test = absolute_repo_path / "llvm-project/libcxx/test"
     nonexistent_tests = [s for s in unique_tests if not (absolute_libcxx_test / s).is_file()]
 
+    failed = False
+
+    if damaged_lines:
+        failed = True
+        print(f"Failure: {expected_results_txt} contains {len(damaged_lines)} damaged lines:", file=sys.stderr)
+        print(f"(Double-check their syntax. Did you forget the result code at the end?)", file=sys.stderr)
+        for s in sorted(damaged_lines):
+            print(f"{s}", file=sys.stderr)
+
+    if unknown_results:
+        failed = True
+        print(f"Failure: {expected_results_txt} contains {len(unknown_results)} unknown results:", file=sys.stderr)
+        print(f"(Only FAIL and SKIPPED are known.)", file=sys.stderr)
+        for s in sorted(unknown_results):
+            print(f"{s}", file=sys.stderr)
+
     if nonexistent_tests:
+        failed = True
         print(f"Failure: {expected_results_txt} contains {len(nonexistent_tests)} nonexistent tests:", file=sys.stderr)
         for s in sorted(nonexistent_tests):
             print(f"{s}", file=sys.stderr)
+
+    if failed:
         print(f"##vso[task.logissue type=error]{expected_results_txt} failed validation, see log.", file=sys.stderr)
         sys.exit(1)
