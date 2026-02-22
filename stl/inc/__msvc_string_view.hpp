63614951093ecad831c0c6d792717b1657256107
Alex Guteniev <gutenev@gmail.com>
2026-02-22 11:51:29 +0200

    Simplistic macro

--- stl/inc/__msvc_string_view.hpp
+++ stl/inc/__msvc_string_view.hpp
@@ -474,17 +474,13 @@ public:
     }
 };
 
-#ifndef _ALLOW_ANY_TYPE_STD_CHAR_TRAIS
-#define _ALLOW_ANY_TYPE_STD_CHAR_TRAIS 0
-#endif // _ALLOW_ANY_TYPE_STD_CHAR_TRAIS
-
 _EXPORT_STD template <class _Elem>
 struct char_traits : _Char_traits<_Elem, long> { // properties of a string or stream unknown element
-#if !_ALLOW_ANY_TYPE_STD_CHAR_TRAIS
+#ifndef _ALLOW_ANY_TYPE_STD_CHAR_TRAIS
     static_assert(is_integral_v<_Elem>,
         "Standard char_traits is only provided for char, wchar_t, char8_t, char16_t, and char32_t. "
         "See N5032 [char.traits]. Visual C++ accepts other unsigned integral types as an extension.");
-#endif // !_ALLOW_ANY_TYPE_STD_CHAR_TRAIS
+#endif // _ALLOW_ANY_TYPE_STD_CHAR_TRAIS
 };
 
 template <>
