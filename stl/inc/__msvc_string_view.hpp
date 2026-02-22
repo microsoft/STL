dcc2aa31508a71b8baba947f2aa63c9b9eeeb6eb
Alex Guteniev <gutenev@gmail.com>
2026-02-22 11:49:08 +0200

    It's getting compilcated, let's go even more conservatively

--- stl/inc/__msvc_string_view.hpp
+++ stl/inc/__msvc_string_view.hpp
@@ -478,28 +478,10 @@ public:
 #define _ALLOW_ANY_TYPE_STD_CHAR_TRAIS 0
 #endif // _ALLOW_ANY_TYPE_STD_CHAR_TRAIS
 
-#ifndef _ALLOW_SIGNED_INTEGER_STD_CHAR_TRAIS
-#if defined(_ENABLE_STL_INTERNAL_CHECK)
-#define _ALLOW_SIGNED_INTEGER_STD_CHAR_TRAIS 0
-#else // ^^^ defined(_ENABLE_STL_INTERNAL_CHECK) / !defined(_ENABLE_STL_INTERNAL_CHECK) vvv
-#define _ALLOW_SIGNED_INTEGER_STD_CHAR_TRAIS 1
-#endif // ^^^ !defined(_ENABLE_STL_INTERNAL_CHECK) ^^^
-#endif // _ALLOW_SIGNED_INTEGER_STD_CHAR_TRAIS
-
-#ifndef _ALLOW_UNSIGNED_INTEGER_STD_CHAR_TRAIS
-#if defined(_ENABLE_STL_INTERNAL_CHECK)
-#define _ALLOW_UNSIGNED_INTEGER_STD_CHAR_TRAIS 0
-#else // ^^^ defined(_ENABLE_STL_INTERNAL_CHECK) / !defined(_ENABLE_STL_INTERNAL_CHECK) vvv
-#define _ALLOW_UNSIGNED_INTEGER_STD_CHAR_TRAIS 1
-#endif // ^^^ !defined(_ENABLE_STL_INTERNAL_CHECK) ^^^
-#endif // _ALLOW_UNSIGNED_INTEGER_STD_CHAR_TRAIS
-
 _EXPORT_STD template <class _Elem>
 struct char_traits : _Char_traits<_Elem, long> { // properties of a string or stream unknown element
 #if !_ALLOW_ANY_TYPE_STD_CHAR_TRAIS
-    static_assert(
-        is_integral_v<_Elem>
-            && (is_signed_v<_Elem> ? _ALLOW_SIGNED_INTEGER_STD_CHAR_TRAIS : _ALLOW_UNSIGNED_INTEGER_STD_CHAR_TRAIS),
+    static_assert(is_integral_v<_Elem>,
         "Standard char_traits is only provided for char, wchar_t, char8_t, char16_t, and char32_t. "
         "See N5032 [char.traits]. Visual C++ accepts other unsigned integral types as an extension.");
 #endif // !_ALLOW_ANY_TYPE_STD_CHAR_TRAIS
