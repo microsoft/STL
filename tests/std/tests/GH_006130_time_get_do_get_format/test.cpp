60e5e9e16629ee1985bb371753e0884ef7cd62ad
Mirion <51433180+mirion-dev@users.noreply.github.com>
2026-03-04 15:55:44 +0800

    Add eofbit check for GH_006130

--- tests/std/tests/GH_006130_time_get_do_get_format/test.cpp
+++ tests/std/tests/GH_006130_time_get_do_get_format/test.cpp
@@ -7,8 +7,16 @@
 #include <sstream>
 
 int main() {
-    std::tm t{};
-    std::stringstream ss{"% "};
-    ss >> std::get_time(&t, "%%");
-    assert(!ss.fail());
+    {
+        std::tm t{};
+        std::stringstream ss{"%"};
+        ss >> std::get_time(&t, "%%");
+        assert(!ss.fail() && ss.eof());
+    }
+    {
+        std::tm t{};
+        std::stringstream ss{"% "};
+        ss >> std::get_time(&t, "%%");
+        assert(!ss.fail() && ss.tellg() == 1);
+    }
 }
