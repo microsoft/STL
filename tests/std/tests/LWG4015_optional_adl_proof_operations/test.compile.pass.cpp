ad850344827627d98c4bfa9f9e9507268eac872a
Stephan T. Lavavej <stl@microsoft.com>
2026-03-04 13:05:17 -0800

    Expand test coverage.

--- tests/std/tests/LWG4015_optional_adl_proof_operations/test.compile.pass.cpp
+++ tests/std/tests/LWG4015_optional_adl_proof_operations/test.compile.pass.cpp
@@ -2,9 +2,11 @@
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
 // LWG-4015: "LWG-3973 broke const overloads of std::optional monadic operations"
-// This test verifies that optional's converting constructors and assignments
+// This test verifies that optional's operations
 // don't use unqualified operator* which would be vulnerable to ADL hijacking.
 
+#include <cstddef>
+#include <functional>
 #include <optional>
 #include <utility>
 
@@ -19,6 +21,15 @@ namespace bad {
         }
     };
 } // namespace bad
+
+namespace std {
+    template <>
+    struct hash<bad::S> {
+        [[nodiscard]] std::size_t operator()(const bad::S& obj) const noexcept {
+            return std::hash<int>{}(obj);
+        }
+    };
+} // namespace std
 
 void test_converting_construction() {
     const std::optional<bad::S> src_copy{bad::S{42}};
@@ -38,4 +49,47 @@ void test_converting_assignment() {
 
     std::optional<bad::S> src_move{bad::S{42}};
     d = std::move(src_move);
+}
+
+void test_swap() {
+    std::optional<bad::S> left{bad::S{17}};
+    std::optional<bad::S> right{bad::S{29}};
+    left.swap(right);
+}
+
+void test_compare_optional_with_optional() {
+    const std::optional<bad::S> left{bad::S{17}};
+    const std::optional<bad::S> right{bad::S{29}};
+
+    (void) (left == right);
+    (void) (left != right);
+    (void) (left < right);
+    (void) (left > right);
+    (void) (left <= right);
+    (void) (left >= right);
+
+#if _HAS_CXX20
+    (void) (left <=> right);
+#endif
+}
+
+void test_compare_optional_with_value() {
+    const std::optional<bad::S> left{bad::S{17}};
+
+    (void) (left == 29);
+    (void) (left != 29);
+    (void) (left < 29);
+    (void) (left > 29);
+    (void) (left <= 29);
+    (void) (left >= 29);
+
+#if _HAS_CXX20
+    (void) (left <=> 29);
+#endif
+}
+
+void test_hash() {
+    const std::optional<bad::S> opt{bad::S{1729}};
+    std::hash<std::optional<bad::S>> hasher;
+    (void) hasher(opt);
 }
