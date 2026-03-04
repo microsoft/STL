9aea92555ffb54b9451ef28a2327be6703d30bd7
Mirion <51433180+mirion-dev@users.noreply.github.com>
2026-03-04 13:37:40 +0800

    Fix `time_get::do_get` to parse a literal `%`

---
+++ tests/std/tests/GH_006130_time_get_do_get_format/test.cpp
@@ -0,0 +1,14 @@
+// Copyright (c) Microsoft Corporation.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+#include <cassert>
+#include <ctime>
+#include <iomanip>
+#include <sstream>
+
+int main() {
+    std::tm t{};
+    std::stringstream ss{"% "};
+    ss >> std::get_time(&t, "%%");
+    assert(!ss.fail());
+}
