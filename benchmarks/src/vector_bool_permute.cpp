5a0ba816377c67a2b06d88d16f18dd64c2cc7719
Alex Guteniev <gutenev@gmail.com>
2026-03-08 09:42:40 +0200

    benchmark plain dynamic array

--- benchmarks/src/vector_bool_permute.cpp
+++ benchmarks/src/vector_bool_permute.cpp
@@ -12,33 +12,74 @@
 
 using namespace std;
 
-void perm_check_3(benchmark::State& state) {
+enum class equality { eq, neq };
+enum class args { three, four };
+
+
+template <equality Eq, args Args>
+void perm_arr_check(benchmark::State& state) {
     const auto size = static_cast<size_t>(state.range(0));
     vector<bool> v1 = random_vector<bool>(size);
-    vector<bool> v2 = random_vector<bool>(size, 1u);
+    vector<bool> v2;
+    if constexpr (Eq == equality::eq) {
+        v2 = v1;
+    } else {
+        v2 = random_vector<bool>(size, 1u);
+    }
+
+    auto a1 = make_unique<bool[]>(size);
+    auto a2 = make_unique<bool[]>(size);
+    copy(v1.begin(), v1.end(), a1.get());
+    copy(v2.begin(), v2.end(), a2.get());
 
     for (auto _ : state) {
         benchmark::DoNotOptimize(v1);
         benchmark::DoNotOptimize(v2);
-        bool r = is_permutation(v1.begin(), v1.end(), v2.begin());
+        bool r;
+        if constexpr (Args == args::three) {
+            r = is_permutation(a1.get(), a1.get() + size, a2.get());
+        } else {
+            r = is_permutation(a1.get(), a1.get() + size, a2.get(), a2.get() + size);
+        }
         benchmark::DoNotOptimize(r);
     }
 }
 
-void perm_check_4(benchmark::State& state) {
+
+template <equality Eq, args Args>
+void perm_vbool_check(benchmark::State& state) {
     const auto size = static_cast<size_t>(state.range(0));
     vector<bool> v1 = random_vector<bool>(size);
-    vector<bool> v2 = random_vector<bool>(size, 1u);
+    vector<bool> v2;
+    if constexpr (Eq == equality::eq) {
+        v2 = v1;
+    }
+    else
+    {
+        v2 = random_vector<bool>(size, 1u);
+    }
 
     for (auto _ : state) {
         benchmark::DoNotOptimize(v1);
         benchmark::DoNotOptimize(v2);
-        bool r = is_permutation(v1.begin(), v1.end(), v2.begin(), v2.end());
+        bool r;
+        if constexpr (Args == args::three) {
+            r = is_permutation(v1.begin(), v1.end(), v2.begin());
+        } else {
+            r = is_permutation(v1.begin(), v1.end(), v2.begin(), v2.end());
+        }
         benchmark::DoNotOptimize(r);
     }
 }
 
-BENCHMARK(perm_check_3)->RangeMultiplier(64)->Range(64, 64 << 10);
-BENCHMARK(perm_check_4)->RangeMultiplier(64)->Range(64, 64 << 10);
+BENCHMARK(perm_arr_check<equality::eq, args::three>)->RangeMultiplier(64)->Range(64, 64 << 10);
+BENCHMARK(perm_arr_check<equality::eq, args::four>)->RangeMultiplier(64)->Range(64, 64 << 10);
+BENCHMARK(perm_arr_check<equality::neq, args::three>)->RangeMultiplier(64)->Range(64, 64 << 10);
+BENCHMARK(perm_arr_check<equality::neq, args::four>)->RangeMultiplier(64)->Range(64, 64 << 10);
+
+BENCHMARK(perm_vbool_check<equality::eq, args::three>)->RangeMultiplier(64)->Range(64, 64 << 10);
+BENCHMARK(perm_vbool_check<equality::eq, args::four>)->RangeMultiplier(64)->Range(64, 64 << 10);
+BENCHMARK(perm_vbool_check<equality::neq, args::three>)->RangeMultiplier(64)->Range(64, 64 << 10);
+BENCHMARK(perm_vbool_check<equality::neq, args::four>)->RangeMultiplier(64)->Range(64, 64 << 10);
 
 BENCHMARK_MAIN();
