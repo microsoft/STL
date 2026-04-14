65bdbf8fd906fd9b7db20db419eefa321abad588
Stephan T. Lavavej <stl@microsoft.com>
2026-04-14 05:57:45 -0700

    Style: Move `__declspec(selectany)` to the beginning.

--- stl/src/asan.cpp
+++ stl/src/asan.cpp
@@ -9,8 +9,8 @@ namespace std {
     // __declspec(selectany) in __msvc_sanitizer_annotate_container.hpp.
     // The new method is preferred because previously enabling just string
     // would also enable vector and optional. See GH-6186 for details.
-    extern __declspec(selectany) const bool _Asan_string_should_annotate   = true;
-    extern __declspec(selectany) const bool _Asan_vector_should_annotate   = true;
-    extern __declspec(selectany) const bool _Asan_optional_should_annotate = true;
+    __declspec(selectany) extern const bool _Asan_string_should_annotate   = true;
+    __declspec(selectany) extern const bool _Asan_vector_should_annotate   = true;
+    __declspec(selectany) extern const bool _Asan_optional_should_annotate = true;
     } // extern "C"
 } // namespace std
