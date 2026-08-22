38d94b8f209565bbdf82939e2712e8dde4b33209
Stephan T. Lavavej <stl@microsoft.com>
2026-08-22 14:18:46 -0700

    __msvc_stdlib.hpp: This must be a core header because cstdlib is core.

--- stl/inc/__msvc_stdlib.hpp
+++ stl/inc/__msvc_stdlib.hpp
@@ -1,4 +1,4 @@
-// __msvc_stdlib.hpp internal header
+// __msvc_stdlib.hpp internal header (core)
 
 // Copyright (c) Microsoft Corporation.
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
