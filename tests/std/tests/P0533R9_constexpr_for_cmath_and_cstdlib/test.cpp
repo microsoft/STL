de31d845e0e65c83326c6d89a36a41264d1b06da
Stephan T. Lavavej <stl@nuwen.net>
2026-09-09 10:17:36 -0700

    Cite MSVC-PR-777572 which was merged 2026-09-03.

--- tests/std/tests/P0533R9_constexpr_for_cmath_and_cstdlib/test.cpp
+++ tests/std/tests/P0533R9_constexpr_for_cmath_and_cstdlib/test.cpp
@@ -1,7 +1,7 @@
 // Copyright (c) Microsoft Corporation.
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
-// TRANSITION, MSVC frontend needs to intercept inclusions of <inttypes.h>:
+// TRANSITION, MSVC-PR-777572 (merged 2026-09-03) changed the MSVC frontend to intercept inclusions of <inttypes.h>:
 #ifndef _M_CEE_PURE
 #include <__msvc_inttypes.hpp>
 #endif
