5be4d74fd246741859a81ee8b84c97efbb7797cc
unknown <lol>
2026-09-13 09:19:24 -0400

    Revert "printf() everywhere..."
    This reverts commit 23ebfd7a5b817cd7033c3672e3bf8345d6517780.

--- tests/std/tests/P0645R10_text_formatting_grapheme_clusterization/test.cpp
+++ tests/std/tests/P0645R10_text_formatting_grapheme_clusterization/test.cpp
@@ -4,7 +4,6 @@
 #include <algorithm>
 #include <cassert>
 #include <cstddef>
-#include <cstdio>
 #include <format>
 #include <iterator>
 #include <span>
@@ -1354,24 +1353,18 @@ void test_unicode_decoding() {
 
 int main() {
     run_unicode_test_data_utf32();
-    std::printf("Passed run_unicode_test_data_utf32()");
     run_unicode_test_data_utf32_utf8_comparison();
-    std::printf("Passed run_unicode_test_data_utf32_utf8_comparison()");
 
     test_unicode_properties();
-    std::printf("Passed test_unicode_properties()");
     static_assert(test_unicode_properties());
 
     test_utf8_decode();
-    std::printf("Passed test_utf8_decode()");
     static_assert(test_utf8_decode());
 
     test_utf16_decode();
-    std::printf("Passed test_utf16_decode()");
     static_assert(test_utf16_decode());
 
     test_unicode_decoding();
-    std::printf("Passed test_unicode_decoding()");
 
     static_assert(forward_iterator<_Unicode_codepoint_iterator<char>>);
     static_assert(sentinel_for<default_sentinel_t, _Unicode_codepoint_iterator<char>>);
