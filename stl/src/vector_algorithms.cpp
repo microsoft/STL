01b29a02dd197cb04befe2e1afe9e55e1376d65a
Stephan T. Lavavej <stl@microsoft.com>
2026-09-14 15:20:01 -0700

    Drop unused `_Size_bits` parameters.

--- stl/src/vector_algorithms.cpp
+++ stl/src/vector_algorithms.cpp
@@ -11939,8 +11939,8 @@ namespace {
         }
 
         template <class _Traits, class _Elem>
-        bool _Impl(void* _Dest, const _Elem* const _Src, const size_t _Size_bytes, const size_t _Size_bits,
-            const size_t _Size_chars, const size_t _Size_convert, const _Elem _Elem0, const _Elem _Elem1) noexcept {
+        bool _Impl(void* _Dest, const _Elem* const _Src, const size_t _Size_bytes, const size_t _Size_chars,
+            const size_t _Size_convert, const _Elem _Elem0, const _Elem _Elem1) noexcept {
             [[maybe_unused]] typename _Traits::_Guard _Guard; // TRANSITION, DevCom-10331414
             const auto _Dx0 = _Traits::_Set(_Elem0);
             const auto _Dx1 = _Traits::_Set(_Elem1);
@@ -12008,8 +12008,7 @@ namespace {
         // IMPORTANT: __declspec(noinline) is necessary because any use of SVE intrinsics
         // will generate an SVE prologue outside of branches like `if (_Use_FEAT_SVE())`.
         __declspec(noinline) bool _Impl_sve_1(void* const _Dest, const char* const _Src, const size_t _Size_bytes,
-            const size_t _Size_bits, const size_t _Size_chars, const size_t _Size_convert, const char _Elem0,
-            const char _Elem1) noexcept {
+            const size_t _Size_chars, const size_t _Size_convert, const char _Elem0, const char _Elem1) noexcept {
             const size_t _Step_in    = svcntb();
             const size_t _Step_out   = _Step_in / 8;
             const char* _Src_end     = _Src + _Size_convert;
@@ -12043,8 +12042,7 @@ namespace {
         // IMPORTANT: __declspec(noinline) is necessary because any use of SVE intrinsics
         // will generate an SVE prologue outside of branches like `if (_Use_FEAT_SVE())`.
         __declspec(noinline) bool _Impl_sve_2(void* const _Dest, const wchar_t* const _Src, const size_t _Size_bytes,
-            const size_t _Size_bits, const size_t _Size_chars, const size_t _Size_convert, const wchar_t _Elem0,
-            const wchar_t _Elem1) noexcept {
+            const size_t _Size_chars, const size_t _Size_convert, const wchar_t _Elem0, const wchar_t _Elem1) noexcept {
             const size_t _Step_in    = svcntb();
             const size_t _Step_out   = _Step_in / 8;
             const size_t _Half_step  = _Step_in / 2;
@@ -12083,8 +12081,8 @@ namespace {
 #endif // ^^^ defined(_M_ARM64) ^^^
 
         template <class _Elem>
-        bool _Fallback(void* const _Dest, const _Elem* const _Src, const size_t _Size_bytes, const size_t _Size_bits,
-            const size_t _Size_chars, const size_t _Size_convert, const _Elem _Elem0, const _Elem _Elem1) noexcept {
+        bool _Fallback(void* const _Dest, const _Elem* const _Src, const size_t _Size_bytes, const size_t _Size_chars,
+            const size_t _Size_convert, const _Elem _Elem0, const _Elem _Elem1) noexcept {
             const auto _Dest_bytes = static_cast<uint8_t*>(_Dest);
 
             for (size_t _Ix = _Size_convert; _Ix < _Size_chars; ++_Ix) {
@@ -12113,11 +12111,11 @@ namespace {
         bool _Dispatch(void* _Dest, const _Elem* _Src, size_t _Size_bytes, size_t _Size_bits, size_t _Size_chars,
             size_t _Size_convert, _Elem _Elem0, _Elem _Elem1) noexcept {
             if (_Use_avx2() && _Size_bits >= 256) {
-                return _Impl<_Avx>(_Dest, _Src, _Size_bytes, _Size_bits, _Size_chars, _Size_convert, _Elem0, _Elem1);
+                return _Impl<_Avx>(_Dest, _Src, _Size_bytes, _Size_chars, _Size_convert, _Elem0, _Elem1);
             } else if (_Use_sse42()) {
-                return _Impl<_Sse>(_Dest, _Src, _Size_bytes, _Size_bits, _Size_chars, _Size_convert, _Elem0, _Elem1);
+                return _Impl<_Sse>(_Dest, _Src, _Size_bytes, _Size_chars, _Size_convert, _Elem0, _Elem1);
             } else {
-                return _Fallback(_Dest, _Src, _Size_bytes, _Size_bits, _Size_chars, _Size_convert, _Elem0, _Elem1);
+                return _Fallback(_Dest, _Src, _Size_bytes, _Size_chars, _Size_convert, _Elem0, _Elem1);
             }
         }
 #endif // ^^^ !defined(_M_ARM64) && !defined(_M_ARM64EC) ^^^
@@ -12135,12 +12133,12 @@ __declspec(noalias) bool __stdcall __std_bitset_from_string_1(void* const _Dest,
 
 #if defined(_M_ARM64) // not ARM64EC, which lacks SVE
     if (_Use_FEAT_SVE() && _Size_convert >= _Sve_vl()) {
-        return _Impl_sve_1(_Dest, _Src, _Size_bytes, _Size_bits, _Size_chars, _Size_convert, _Elem0, _Elem1);
+        return _Impl_sve_1(_Dest, _Src, _Size_bytes, _Size_chars, _Size_convert, _Elem0, _Elem1);
     }
 #endif // ^^^ defined(_M_ARM64) ^^^
 
 #if defined(_M_ARM64) || defined(_M_ARM64EC)
-    return _Impl<_Traits_1_neon>(_Dest, _Src, _Size_bytes, _Size_bits, _Size_chars, _Size_convert, _Elem0, _Elem1);
+    return _Impl<_Traits_1_neon>(_Dest, _Src, _Size_bytes, _Size_chars, _Size_convert, _Elem0, _Elem1);
 #else // ^^^ defined(_M_ARM64) || defined(_M_ARM64EC) / !defined(_M_ARM64) && !defined(_M_ARM64EC) vvv
     return _Dispatch<_Traits_1_avx, _Traits_1_sse>(
         _Dest, _Src, _Size_bytes, _Size_bits, _Size_chars, _Size_convert, _Elem0, _Elem1);
@@ -12156,12 +12154,12 @@ __declspec(noalias) bool __stdcall __std_bitset_from_string_2(void* const _Dest,
 
 #if defined(_M_ARM64) // not ARM64EC, which lacks SVE
     if (_Use_FEAT_SVE() && _Size_convert >= _Sve_vl()) {
-        return _Impl_sve_2(_Dest, _Src, _Size_bytes, _Size_bits, _Size_chars, _Size_convert, _Elem0, _Elem1);
+        return _Impl_sve_2(_Dest, _Src, _Size_bytes, _Size_chars, _Size_convert, _Elem0, _Elem1);
     }
 #endif // ^^^ defined(_M_ARM64) ^^^
 
 #if defined(_M_ARM64) || defined(_M_ARM64EC)
-    return _Impl<_Traits_2_neon>(_Dest, _Src, _Size_bytes, _Size_bits, _Size_chars, _Size_convert, _Elem0, _Elem1);
+    return _Impl<_Traits_2_neon>(_Dest, _Src, _Size_bytes, _Size_chars, _Size_convert, _Elem0, _Elem1);
 #else // ^^^ defined(_M_ARM64) || defined(_M_ARM64EC) / !defined(_M_ARM64) && !defined(_M_ARM64EC) vvv
     return _Dispatch<_Traits_2_avx, _Traits_2_sse>(
         _Dest, _Src, _Size_bytes, _Size_bits, _Size_chars, _Size_convert, _Elem0, _Elem1);
