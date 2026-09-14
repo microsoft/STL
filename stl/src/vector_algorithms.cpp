29b1332053d3116693c9f5dfe97e209b7c2792ac
Stephan T. Lavavej <stl@microsoft.com>
2026-09-14 14:58:58 -0700

    Remove now-unnecessary branch in `_Fallback`.
    This isn't needed to guard against a do-nothing for-loop.

--- stl/src/vector_algorithms.cpp
+++ stl/src/vector_algorithms.cpp
@@ -12087,11 +12087,9 @@ namespace {
             const size_t _Size_chars, const size_t _Size_convert, const _Elem _Elem0, const _Elem _Elem1) noexcept {
             const auto _Dest_bytes = static_cast<uint8_t*>(_Dest);
 
-            if (_Size_chars > _Size_bits) {
-                for (size_t _Ix = _Size_bits; _Ix < _Size_chars; ++_Ix) {
-                    if (const _Elem _Cur = _Src[_Ix]; _Cur != _Elem0 && _Cur != _Elem1) {
-                        return false;
-                    }
+            for (size_t _Ix = _Size_bits; _Ix < _Size_chars; ++_Ix) {
+                if (const _Elem _Cur = _Src[_Ix]; _Cur != _Elem0 && _Cur != _Elem1) {
+                    return false;
                 }
             }
 
