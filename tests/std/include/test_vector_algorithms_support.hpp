b1dac207078f41b9cf0a3bc662d14c69ea21fe1b
Stephan T. Lavavej <stl@nuwen.net>
2026-09-17 14:57:23 -0700

    test_vector_algorithms_support.hpp: Test different ISA levels only when vectorization is active.
    `#if _USE_STD_VECTOR_ALGORITHMS` supersedes `#if !defined(_M_CEE_PURE)` because `/clr:pure` always sets `_USE_STD_VECTOR_ALGORITHMS` to `0`.

--- tests/std/include/test_vector_algorithms_support.hpp
+++ tests/std/include/test_vector_algorithms_support.hpp
@@ -72,7 +72,7 @@ template <class TestFunc>
 void run_tests_with_different_isa_levels(TestFunc tests) {
     tests();
 
-#if !defined(_M_CEE_PURE)
+#if _USE_STD_VECTOR_ALGORITHMS
 #if defined(_M_ARM64) // not ARM64EC, which lacks SVE
     // No STL_TEST_DOWNLEVEL_MACHINE logic here because MSVC-internal testing currently doesn't support SVE
 
@@ -100,7 +100,7 @@ void run_tests_with_different_isa_levels(TestFunc tests) {
 
     __isa_enabled = original_isa;
 #endif // ^^^ defined(_M_IX86) || defined(_M_X64) ^^^
-#endif // ^^^ !defined(_M_CEE_PURE) ^^^
+#endif // ^^^ _USE_STD_VECTOR_ALGORITHMS ^^^
 }
 
 template <class TestFunc>
