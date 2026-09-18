c27d422722166ddbfc366c74d80051a4857d11e2
Stephan T. Lavavej <stl@nuwen.net>
2026-09-18 11:05:45 -0700

    VSO_0000000_vector_algorithms: Fuse VSO_0000000_vector_algorithms_search_n.
    No changes, except that I need to restore the exclusion of forward_only_iterators from modified ISAs.

--- tests/std/tests/VSO_0000000_vector_algorithms/test.cpp
+++ tests/std/tests/VSO_0000000_vector_algorithms/test.cpp
@@ -13,6 +13,7 @@
 #include <deque>
 #include <functional>
 #include <iostream>
+#include <iterator>
 #include <limits>
 #include <list>
 #include <map>
@@ -1258,6 +1259,145 @@ void test_swap_arrays(mt19937_64& gen) {
 #endif // _HAS_CXX20
 }
 
+template <class UnderlyingIt>
+struct forward_iter_adaptor {
+    using iterator_category = forward_iterator_tag;
+    using reference         = typename UnderlyingIt::reference;
+    using value_type        = typename UnderlyingIt::value_type;
+    using pointer           = typename UnderlyingIt::pointer;
+    using difference_type   = ptrdiff_t;
+
+    constexpr forward_iter_adaptor() : ptr{} {}
+    constexpr explicit forward_iter_adaptor(const UnderlyingIt ptr_) : ptr(ptr_) {}
+
+    constexpr reference operator*() const {
+        return *ptr;
+    }
+    constexpr pointer operator->() const {
+        return ptr;
+    }
+    constexpr forward_iter_adaptor& operator++() {
+        ++ptr;
+        return *this;
+    }
+    constexpr forward_iter_adaptor operator++(int) {
+        forward_iter_adaptor old;
+        ++ptr;
+        return old;
+    }
+    constexpr bool operator==(const forward_iter_adaptor& o) const {
+        return ptr == o.ptr;
+    }
+    constexpr bool operator!=(const forward_iter_adaptor& o) const {
+        return ptr != o.ptr;
+    }
+
+    UnderlyingIt ptr;
+};
+
+template <class FwdIt, class T>
+auto last_known_good_search_n(FwdIt first, const FwdIt last, const size_t count, const T val) {
+    // Deliberately using simple approach, not smart bidi/random iterators "check from the other end" stuff
+    if (count == 0) {
+        return first;
+    }
+
+    size_t found = 0;
+    FwdIt match{};
+    for (; first != last; ++first) {
+        if (*first == val) {
+            ++found;
+            if (found == 1) {
+                match = first;
+            }
+
+            if (found == count) {
+                return match;
+            }
+        } else {
+            found = 0;
+        }
+    }
+    return last;
+}
+
+template <bool forward_only_iterators, class It, class T>
+void test_case_search_n(const It first, const It last, const size_t count, const T val) {
+    if constexpr (forward_only_iterators) {
+        using iter_type = forward_iter_adaptor<It>;
+        test_case_search_n<false>(iter_type(first), iter_type(last), count, val);
+    } else {
+        const auto expected = last_known_good_search_n(first, last, count, val);
+        const auto actual   = search_n(first, last, count, val);
+        assert(expected == actual);
+
+#if _HAS_CXX20
+        const auto ranges_actual = ranges::search_n(first, last, static_cast<ptrdiff_t>(count), val);
+        assert(expected == begin(ranges_actual));
+        if (expected == last) {
+            assert(end(ranges_actual) == last);
+        } else {
+            assert(distance(expected, end(ranges_actual)) == static_cast<ptrdiff_t>(count));
+        }
+#endif // _HAS_CXX20
+    }
+}
+
+template <class T, bool forward_only_iterators = false>
+void test_search_n(mt19937_64& gen) {
+    constexpr size_t countBound    = 70;
+    constexpr size_t countTrials   = 10;
+    constexpr size_t patternTrials = 2;
+    using TD                       = conditional_t<sizeof(T) == 1, int, T>;
+    uniform_int_distribution<TD> dis((numeric_limits<T>::min)(), (numeric_limits<T>::max)());
+    vector<T> input_src;
+    vector<T> input;
+    input_src.reserve(dataCount);
+    input.reserve(dataCount);
+
+    vector<size_t> count_vec(countBound);
+    iota(count_vec.begin(), count_vec.end(), size_t{0});
+
+    for (;;) {
+        shuffle(count_vec.begin(), count_vec.end(), gen); // shuffle before each round of trials
+
+        for (size_t trial = 0; trial != countTrials; ++trial) {
+            assert(trial < count_vec.size());
+            const size_t count = count_vec[trial]; // sample a value in [0, countBound) without reuse
+
+            input = input_src;
+
+            const T val = static_cast<T>(dis(gen));
+
+            test_case_search_n<forward_only_iterators>(input.begin(), input.end(), count, val);
+
+            if (input.empty()) {
+                continue;
+            }
+
+            binomial_distribution<size_t> pattern_length_dis(count * 2, 0.5);
+            uniform_int_distribution<size_t> pos_dis(0, input.size() - 1);
+
+            for (size_t pattern = 0; pattern != patternTrials; ++pattern) {
+                const size_t pattern_length = pattern_length_dis(gen);
+                const size_t pattern_pos    = pos_dis(gen);
+
+                if (pattern_length + pattern_pos <= input.size()) {
+                    fill_n(input.begin() + static_cast<ptrdiff_t>(pattern_pos), pattern_length, val);
+
+                    test_case_search_n<forward_only_iterators>(input.begin(), input.end(), count, val);
+                }
+            }
+        }
+
+        if (input.size() == dataCount) {
+            break;
+        }
+
+        input_src.push_back(static_cast<T>(dis(gen)));
+    }
+}
+
 void test_vector_algorithms(mt19937_64& gen) {
     (void) gen;
 
@@ -1589,6 +1729,25 @@ void test_vector_algorithms(mt19937_64& gen) {
         elapsed_time[make_pair(170, "test_swap_arrays")] += finish_time - start_time;
     }
 #endif // ^^^ !defined(TEST_PART) || TEST_PART == 3 ^^^
+
+    {
+        const auto start_time = steady_clock::now();
+        test_search_n<int8_t>(gen);
+        test_search_n<uint8_t>(gen);
+        test_search_n<int16_t>(gen);
+        test_search_n<uint16_t>(gen);
+        test_search_n<int32_t>(gen);
+        test_search_n<uint32_t>(gen);
+        test_search_n<int64_t>(gen);
+        test_search_n<uint64_t>(gen);
+
+        // Test only one case with forward iterators. It is a different and complex code path,
+        // hence it's worth testing, but it is not vectorized, so there's no point in trying different types.
+        test_search_n<short, true>(gen);
+
+        const auto finish_time = steady_clock::now();
+        elapsed_time[make_pair(180, "search_n")] += finish_time - start_time;
+    }
 }
 
 template <typename Container1, typename Container2>
