e22f84cbac9ce6c0138c36accba2856a8b760669
Stephan T. Lavavej <stl@microsoft.com>
2026-09-20 10:10:30 -0700

    Cycle locations. (Both are even length, so we can reset to 0.)

--- azure-devops/create-1es-hosted-pool.ps1
+++ azure-devops/create-1es-hosted-pool.ps1
@@ -53,7 +53,7 @@ if ($VMSku -ieq 'Fadsv7') {
   $AvailableLocations = @('australiaeast', 'southcentralus')
 }
 
-$AvailableLocationIdx = 15 # Increment for each new set of pools, to cycle through the available locations.
+$AvailableLocationIdx = 0 # Increment for each new set of pools, to cycle through the available locations.
 $Location = $AvailableLocations[$AvailableLocationIdx % $AvailableLocations.Length]
 
 if ($Arch -ieq 'x64') {
