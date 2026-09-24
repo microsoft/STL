6f1974b4954abf9cc2d3c5f2ed73f37fe5f40352
Stephan T. Lavavej <stl@microsoft.com>
2026-09-24 15:40:11 -0700

    Benchmark only the current code.

--- benchmarks/src/signed128_mul.cpp
+++ benchmarks/src/signed128_mul.cpp
@@ -12,17 +12,6 @@
 
 using namespace std;
 
-_NODISCARD constexpr _Signed128 old_mul128(_Signed128 _Left, _Signed128 _Right) {
-    bool _negative = false;
-    _Left._Strip_negative(_negative);
-    _Right._Strip_negative(_negative);
-    _Signed128 _Result{_Base128::_Multiply(_Left, _Right)};
-    if (_negative) {
-        _Result = -_Result;
-    }
-    return _Result;
-}
-
 vector<pair<_Signed128, _Signed128>> init_test_set() {
     vector<pair<_Signed128, _Signed128>> vec(1'000'000);
     mt19937_64 mt64{};
@@ -52,24 +41,6 @@ void bm_signed128_mul(benchmark::State& state) {
     }
 }
 
-void bm_signed128_oldmul(benchmark::State& state) {
-    const auto vec = init_test_set();
-
-    auto it        = vec.begin();
-    _Signed128 res = 0;
-
-    for (auto _ : state) {
-        res = old_mul128(it->first, it->second);
-
-        benchmark::DoNotOptimize(res);
-        ++it;
-        if (it == vec.end()) {
-            it = vec.begin();
-        }
-    }
-}
-
 BENCHMARK(bm_signed128_mul);
-BENCHMARK(bm_signed128_oldmul);
 
 BENCHMARK_MAIN();
